0f4a9af44efcc833bf36172e4ddab957b5779f3f
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-16 07:53:31 +0900

    refactor Implement ViewModel.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/calendar/CalendarViewModelImplementation.kt
@@ -0,0 +1,31 @@
+package jp.toastkid.yobidashi4.infrastructure.viewmodel.calendar
+
+import androidx.compose.runtime.mutableStateOf
+import java.time.LocalDate
+import jp.toastkid.yobidashi4.presentation.viewmodel.calendar.CalendarViewModel
+import org.koin.core.annotation.Single
+
+@Single
+class CalendarViewModelImplementation : CalendarViewModel {
+
+    private val localDateState =  mutableStateOf(LocalDate.now())
+
+    override fun localDate(): LocalDate = localDateState.value
+
+    override fun plusMonths(i: Long) {
+        localDateState.value = localDateState.value.plusMonths(i)
+    }
+
+    override fun setYear(year: Int) {
+        localDateState.value = localDateState.value.withYear(year)
+    }
+
+    override fun moveMonth(month: Int) {
+        localDateState.value = localDateState.value.withMonth(month)
+    }
+
+    override fun getFirstDay(): LocalDate {
+        return localDateState.value.withDayOfMonth(1)
+    }
+
+}
