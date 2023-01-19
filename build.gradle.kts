cfd524f02f4c69da1b618eb8ee1c3023235b9fa3
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-19 09:24:43 +0900

    feature Upgrade Koin to 3.3.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("org.slf4j:slf4j-reload4j:2.0.6")
 
-    implementation("io.insert-koin:koin-core:3.3.0")
+    implementation("io.insert-koin:koin-core:3.3.2")
     implementation("io.insert-koin:koin-annotations:1.0.3")
     ksp("io.insert-koin:koin-ksp-compiler:1.0.3")
     testImplementation(kotlin("test"))
