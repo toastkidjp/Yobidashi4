c5ddfc96d8a6f1f973f2f33de57676e37358f631
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-21 10:13:02 +0900

    feature Add function and enum for displaying icon.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/FileTab.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/FileTab.kt
@@ -5,11 +5,21 @@ import java.nio.file.Path
 data class FileTab(
     private val title: String,
     val items: List<Path>,
-    private val closeable: Boolean = true
+    private val closeable: Boolean = true,
+    private val type: Type = Type.ARTICLE
 ): Tab {
 
     override fun title(): String = title
 
     override fun closeable(): Boolean = closeable
 
+    override fun iconPath(): String? {
+        return type.iconPath()
+    }
+
+    enum class Type(private val iconPath: String) {
+        ARTICLE("images/icon/ic_edit.xml"), MUSIC("images/icon/ic_music.xml"), FIND("images/icon/ic_search.xml");
+
+        fun iconPath() = iconPath
+    }
 }
