de07a189b7d5266fe001b0d58660edb6bf5937a5
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-22 19:24:59 +0900

    feature Enable to display site's icon on tab.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TabsView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TabsView.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.presentation.main.content
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.forEachGesture
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
@@ -25,9 +27,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import java.nio.file.Paths
 import jp.toastkid.yobidashi4.domain.model.tab.CalendarTab
 import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
 import jp.toastkid.yobidashi4.domain.model.tab.FileTab
@@ -46,6 +50,7 @@ import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import jp.toastkid.yobidashi4.presentation.viewmodel.web.WebTabViewModel
 import jp.toastkid.yobidashi4.presentation.web.WebTabView
 import jp.toastkid.yobidashi4.presentation.web.bookmark.WebBookmarkTabView
+import kotlin.io.path.inputStream
 import kotlin.io.path.name
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -84,12 +89,22 @@ fun TabsView(modifier: Modifier) {
                 ) {
                     Box {
                         Row(verticalAlignment = Alignment.CenterVertically) {
-                            tab.iconPath()?.let {
-                                Icon(
-                                    painterResource(it),
-                                    contentDescription = "Tab's icon",
-                                    tint = if (tab.useIconTint()) MaterialTheme.colors.onPrimary else Color.Transparent
-                                )
+                            tab.iconPath()?.let { iconPath ->
+                                if (iconPath.contains("data")) {
+                                    Paths.get(iconPath).inputStream().use { inputStream ->
+                                        Image(
+                                            loadImageBitmap(inputStream),
+                                            contentDescription = "Tab's icon",
+                                            modifier = Modifier.size(24.dp)
+                                        )
+                                    }
+                                } else {
+                                    Icon(
+                                        painterResource(iconPath),
+                                        contentDescription = "Tab's icon",
+                                        tint = if (tab.useIconTint()) MaterialTheme.colors.onPrimary else Color.Transparent
+                                    )
+                                }
                             }
 
                             Text(tab.title(),
