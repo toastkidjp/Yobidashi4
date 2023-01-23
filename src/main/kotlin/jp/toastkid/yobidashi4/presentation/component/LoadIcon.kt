e16d34f852d6ddf4a22fe00f86643986496575fc
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-24 08:58:39 +0900

    refactor Divide to other composable function.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/LoadIcon.kt
@@ -0,0 +1,33 @@
+package jp.toastkid.yobidashi4.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.painterResource
+import java.nio.file.Paths
+import kotlin.io.path.inputStream
+
+@Composable
+internal fun LoadIcon(iconPath: String?, modifier: Modifier = Modifier) {
+    iconPath ?: return
+    if (iconPath.contains("data")) {
+        Paths.get(iconPath).inputStream().use { inputStream ->
+            Image(
+                loadImageBitmap(inputStream),
+                contentDescription = "Tab's icon",
+                modifier = modifier
+            )
+        }
+        return
+    }
+
+    Icon(
+        painterResource(iconPath),
+        contentDescription = "Tab's icon",
+        tint = MaterialTheme.colors.onPrimary,
+        modifier = modifier
+    )
+}
