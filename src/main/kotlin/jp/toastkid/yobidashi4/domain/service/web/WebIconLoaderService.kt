7381b417085a29e73aa2bdf9b08c97e6b25f62d8
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-23 09:54:52 +0900

    feature Enable to get favicon.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/WebIconLoaderService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/WebIconLoaderService.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.domain.service.web
 
+import java.net.HttpURLConnection
 import java.net.URI
 import java.net.URL
 import java.nio.file.Files
@@ -12,17 +13,28 @@ class WebIconLoaderService {
         val iconUrls = Jsoup.parse(htmlSource).select("link")
             .filter { elem -> elem.attr("rel").contains("icon") }
             .map { it.attr("href") }
+            .toMutableList()
         val faviconFolder = Paths.get("data/web/icon")
         if (Files.exists(faviconFolder).not()) {
             Files.createDirectories(faviconFolder)
         }
+
+        val targetUrl = URL(browserUrl)
+
+        if (iconUrls.isEmpty()) {
+            iconUrls.add("${targetUrl.protocol}://${targetUrl.host}/favicon.ico")
+        }
+
         iconUrls.forEach {
             val fileExtension = URL(it).path.split(".").lastOrNull() ?: "png"
-            val iconPath = faviconFolder.resolve("${URL(browserUrl).host}.$fileExtension")
+            val iconPath = faviconFolder.resolve("${targetUrl.host}.$fileExtension")
             if (Files.exists(iconPath)) {
                 return@forEach
             }
-            val urlConnection = URI(it).toURL().openConnection()
+            val urlConnection = URI(it).toURL().openConnection() as? HttpURLConnection ?: return
+            if (urlConnection.responseCode != 200) {
+                return@forEach
+            }
             urlConnection.getInputStream().use {
                 Files.write(iconPath, it.readAllBytes())
             }
