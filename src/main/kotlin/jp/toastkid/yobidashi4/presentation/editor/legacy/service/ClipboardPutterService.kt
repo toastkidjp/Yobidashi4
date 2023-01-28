9b327c6b0aa6b21cf6fdf82a8933068afcc05f30
Toast Kid <super_q1m@yahoo.co.jp>
2023-01-29 08:16:46 +0900

    feature Add function.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/ClipboardPutterService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/ClipboardPutterService.kt
@@ -1,13 +1,19 @@
 package jp.toastkid.yobidashi4.presentation.editor.legacy.service
 
+import java.awt.Image
 import java.awt.Toolkit
 import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.StringSelection
+import jp.toastkid.yobidashi4.domain.model.clipboard.TransferableImage
 
 class ClipboardPutterService(private val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard) {
 
     operator fun invoke(string: String?) {
         clipboard.setContents(StringSelection(string)) { _, _ -> }
+    }
+
+    operator fun invoke(string: Image) {
+        clipboard.setContents(TransferableImage(string)) { _, _ -> }
     }
 
 }
