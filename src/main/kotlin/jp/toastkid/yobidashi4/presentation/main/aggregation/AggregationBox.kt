bff2aa3ddde25cf01ca5d4ff2c42ff1eb2b4c02d
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-01 09:05:24 +0900

    bugfix Modify using function.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/aggregation/AggregationBox.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/aggregation/AggregationBox.kt
@@ -196,7 +196,7 @@ internal fun AggregationBox(viewModel: MainViewModel) {
                     selectedSite.value.value(query.value.text).let {
                         viewModel.openAggregationResultTab(it.resultTitleSuffix(), it)
                     }
-                    viewModel.setShowWebSearch(false)
+                    viewModel.switchAggregationBox(false)
                 }
             ) {
                 Text("Start")
