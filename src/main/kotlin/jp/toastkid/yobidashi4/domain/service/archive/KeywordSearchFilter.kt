13278a97db28b4805667a6b63f9b0dfa0e9fb1a0
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-02 10:02:09 +0900

    feature Implement keyword search filter.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/archive/KeywordSearchFilter.kt
@@ -0,0 +1,40 @@
+package jp.toastkid.yobidashi4.domain.service.archive
+
+class KeywordSearchFilter(private val input: String?) {
+
+    private val keywords: MutableSet<String> = mutableSetOf()
+
+    private val useAndSearch: Boolean
+
+    init {
+        if (input == null) {
+            useAndSearch = false
+        } else if (input.contains("*")) {
+            input.split("*").forEach { keywords.add(it) }
+            useAndSearch = true
+        } else if (input.contains(" ")) {
+            input.split(" ").forEach { keywords.add(it) }
+            useAndSearch = false
+        } else {
+            keywords.add(input)
+            useAndSearch = false
+        }
+    }
+
+    operator fun invoke(text: String?): Boolean {
+        if (text == null) {
+            return false
+        }
+
+        if (keywords.isEmpty()) {
+            return true
+        }
+
+        return if (useAndSearch) {
+            keywords.all { actualKeyword -> text.contains(actualKeyword) }
+        } else {
+            keywords.any { actualKeyword -> text.contains(actualKeyword) }
+        }
+    }
+
+}
