be8438236a0f0807a517cd71c5752c48c1f9f819
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-02 09:59:08 +0900

    refactor Delete koin-test dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,6 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("io.mockk:mockk:1.10.6")
     testRuntimeOnly("net.bytebuddy:byte-buddy:1.12.22")
-    testImplementation("io.insert-koin:koin-test-jvm:3.3.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.21")
 }
 
 compose.desktop {
