c97dd068082c540424434dc681eecb7f5c734c28
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-07 08:53:15 +0900

    refactor Add unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/preview/InternalLinkSchemeTest.kt
@@ -0,0 +1,58 @@
+package jp.toastkid.yobidashi4.presentation.editor.preview
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class InternalLinkSchemeTest {
+
+    private lateinit var internalLinkScheme: InternalLinkScheme
+
+    @BeforeEach
+    fun setUp() {
+        internalLinkScheme = InternalLinkScheme()
+    }
+
+    @Test
+    fun makeLink() {
+        assertEquals(
+            "internal-article://tomato",
+            internalLinkScheme.makeLink("tomato")
+        )
+    }
+
+    @Test
+    fun isInternalLink() {
+        assertFalse(internalLinkScheme.isInternalLink(""))
+        assertFalse(internalLinkScheme.isInternalLink(" "))
+        assertFalse(internalLinkScheme.isInternalLink("https://www.yahoo.co.jp"))
+        assertFalse(internalLinkScheme.isInternalLink("tomato"))
+        assertTrue(internalLinkScheme.isInternalLink(internalLinkScheme.makeLink("tomato")))
+    }
+
+    @Test
+    fun extract() {
+        assertEquals(
+            "",
+            internalLinkScheme.extract("")
+        )
+        assertEquals(
+            " ",
+            internalLinkScheme.extract(" ")
+        )
+        assertEquals(
+            "tomato",
+            internalLinkScheme.extract("tomato")
+        )
+        assertEquals(
+            "https://www.yahoo.co.jp",
+            internalLinkScheme.extract("https://www.yahoo.co.jp")
+        )
+        assertEquals(
+            "tomato",
+            internalLinkScheme.extract(internalLinkScheme.makeLink("tomato"))
+        )
+    }
+}
