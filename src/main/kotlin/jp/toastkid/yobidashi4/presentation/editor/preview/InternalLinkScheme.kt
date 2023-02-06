04aa5db8f9c7c5483c808421fe3917f12aea225d
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-07 08:52:56 +0900

    feature Implement internal link scheme.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/preview/InternalLinkScheme.kt
@@ -0,0 +1,24 @@
+package jp.toastkid.yobidashi4.presentation.editor.preview
+
+class InternalLinkScheme {
+
+    fun makeLink(title: String?) = "[$title]($INTERNAL_LINK_SCHEME$title)"
+
+    fun isInternalLink(url: String): Boolean {
+        return url.startsWith(INTERNAL_LINK_SCHEME)
+    }
+
+    fun extract(url: String): String {
+        if (url.length <= INTERNAL_LINK_SCHEME.length || !url.startsWith(INTERNAL_LINK_SCHEME)) {
+            return url
+        }
+        return url.substring(INTERNAL_LINK_SCHEME.length)
+    }
+
+    companion object {
+
+        private const val INTERNAL_LINK_SCHEME = "internal-article://"
+
+    }
+
+}
