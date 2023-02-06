5831378b0459470e6a86fac3f35805f7dcb2dc8a
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-07 08:53:46 +0900

    feature Implement link generator.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/preview/LinkGenerator.kt
@@ -0,0 +1,33 @@
+package jp.toastkid.yobidashi4.presentation.editor.preview
+
+import java.util.regex.Pattern
+
+/**
+ * @author toastkidjp
+ */
+class LinkGenerator {
+
+    operator fun invoke(text: String): String {
+        return embedLinks(text, internalLinkPattern, 1)
+    }
+
+    private fun embedLinks(text: String, pattern: Pattern, group: Int): String {
+        var converted = text
+        var matcher = pattern.matcher(converted)
+        while (matcher.find()) {
+            converted = matcher.replaceFirst(InternalLinkScheme().makeLink(matcher.group(group)))
+            matcher = pattern.matcher(converted)
+        }
+        return converted
+    }
+
+    companion object {
+
+        private val internalLinkPattern =
+            Pattern.compile("\\[\\[(.+?)\\]\\]", Pattern.DOTALL)
+
+        private val httpPattern =
+            Pattern.compile("https?://[a-zA-Z0-9/:%#&~=_!'\\\\\$\\\\?\\\\.\\\\+\\\\*\\\\-]+")
+
+    }
+}
