e7ab51d1e3a94109b2e662a6efcc137a61422550
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-12 09:37:54 +0900

    feature Implement new tab view.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
@@ -0,0 +1,22 @@
+package jp.toastkid.yobidashi4.presentation.converter
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import jp.toastkid.yobidashi4.domain.service.converter.UnixTimeConverterService
+import jp.toastkid.yobidashi4.domain.service.converter.UrlEncodeConverterService
+
+@Composable
+fun ConverterToolTabView() {
+    Surface(
+        color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
+        elevation = 4.dp
+    ) {
+        Column {
+            UnixTimeConverterView(UnixTimeConverterService())
+            UnixTimeConverterView(UrlEncodeConverterService())
+        }
+    }
+}
