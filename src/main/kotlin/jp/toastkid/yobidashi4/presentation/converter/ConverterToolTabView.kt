1aac5d4abfa06e4480c3d65a121ce5fa6c93418a
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-14 09:03:38 +0900

    feature Add padding.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
@@ -1,9 +1,11 @@
 package jp.toastkid.yobidashi4.presentation.converter
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import jp.toastkid.yobidashi4.domain.service.converter.UnixTimeConverterService
 import jp.toastkid.yobidashi4.domain.service.converter.UrlEncodeConverterService
@@ -14,7 +16,7 @@ fun ConverterToolTabView() {
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
         elevation = 4.dp
     ) {
-        Column {
+        Column(modifier = Modifier.padding(8.dp)) {
             TwoValueConverterBox(UnixTimeConverterService())
             TwoValueConverterBox(UrlEncodeConverterService())
         }
