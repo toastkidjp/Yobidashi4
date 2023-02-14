f4716b2edc8376906b8db89782e434b934cb33cd
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-14 09:21:16 +0900

    refactor Implement article's factory.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleFactoryImplementation.kt
@@ -0,0 +1,19 @@
+package jp.toastkid.yobidashi4.domain.model.article
+
+import java.nio.file.Paths
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import org.koin.core.annotation.Single
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+@Single
+class ArticleFactoryImplementation : KoinComponent, ArticleFactory {
+
+    private val setting: Setting by inject()
+
+    override fun withTitle(title: String): Article {
+        val file = Paths.get(setting.articleFolder(), "$title.md")
+        return Article(file)
+    }
+
+}
