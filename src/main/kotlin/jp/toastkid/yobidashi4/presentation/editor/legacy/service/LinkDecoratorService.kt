3a64a9bc37017edd7c75306b45f344b92ff65b98
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-18 10:13:47 +0900

    refactor Use JSoup.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/LinkDecoratorService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/LinkDecoratorService.kt
@@ -1,11 +1,9 @@
 package jp.toastkid.yobidashi4.presentation.editor.legacy.service
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.net.MalformedURLException
 import java.net.URL
 import java.util.regex.Pattern
-import kotlin.streams.asSequence
+import org.jsoup.Jsoup
 
 class LinkDecoratorService {
 
@@ -16,14 +14,8 @@ class LinkDecoratorService {
             return link
         }
 
-        BufferedReader(InputStreamReader(url.openConnection().inputStream)).use { reader ->
-            val content = reader.lines()
-                    .asSequence()
-                    .firstOrNull { it.contains("<title") } ?: return link
-
-            val matcher = PATTERN.matcher(content)
-            return if (matcher.find()) "[${matcher.group(1)}]($link)" else link
-        }
+        val title = Jsoup.parse(url, 3000).title()
+        return "[$title]($link)"
     }
 
     companion object {
