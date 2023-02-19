7c8b1003b18fb3cbd89479e94b23c6a00f6e9009
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-19 11:32:23 +0900

    refactor Safe delete 'desktop'.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/UrlOpenerService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/UrlOpenerService.kt
@@ -1,10 +1,9 @@
 package jp.toastkid.yobidashi4.domain.service.web
 
-import java.awt.Desktop
 import java.net.URI
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 
-class UrlOpenerService(private val desktop: Desktop = Desktop.getDesktop()) {
+class UrlOpenerService {
 
     operator fun invoke(url: String?) {
         if (url.isNullOrBlank()) {
