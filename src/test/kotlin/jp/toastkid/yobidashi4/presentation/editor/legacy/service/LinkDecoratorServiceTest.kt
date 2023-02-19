9a87e53776f973c8e8d097742e7af05e4ddcda64
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-20 07:52:13 +0900

    refactor Add test case.

--- src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/LinkDecoratorServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/LinkDecoratorServiceTest.kt
@@ -1,5 +1,10 @@
 package jp.toastkid.yobidashi4.presentation.editor.legacy.service
 
+import io.mockk.every
+import io.mockk.mockkStatic
+import java.net.MalformedURLException
+import java.net.URL
+import org.jsoup.Jsoup
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -9,6 +14,14 @@ internal class LinkDecoratorServiceTest {
     fun test() {
         assertEquals("www.yahoo.co.jp", LinkDecoratorService().invoke("www.yahoo.co.jp"))
         assertEquals("[Yahoo! JAPAN](https://www.yahoo.co.jp)", LinkDecoratorService().invoke("https://www.yahoo.co.jp"))
+    }
+
+    @Test
+    fun testException() {
+        mockkStatic(Jsoup::class)
+        every { Jsoup.parse(any<URL>(), any()) }.throws(MalformedURLException())
+
+        assertEquals("https://www.yahoo.co.jp", LinkDecoratorService().invoke("https://www.yahoo.co.jp"))
     }
 
 }
