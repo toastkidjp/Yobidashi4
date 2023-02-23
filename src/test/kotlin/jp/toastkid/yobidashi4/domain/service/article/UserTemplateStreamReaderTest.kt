37a5fef05828dfbcea20bb5c81a36d4e107ff19f
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-23 09:39:38 +0900

    refactor Add test case.

---
+++ src/test/kotlin/jp/toastkid/yobidashi4/domain/service/article/UserTemplateStreamReaderTest.kt
@@ -0,0 +1,42 @@
+package jp.toastkid.yobidashi4.domain.service.article
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import java.nio.file.Files
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class UserTemplateStreamReaderTest {
+
+    @InjectMockKs
+    private lateinit var userTemplateStreamReader: UserTemplateStreamReader
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        mockkStatic(Files::class)
+        every { Files.exists(any()) }.returns(true)
+        every { Files.newInputStream(any()) }.returns(mockk())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        userTemplateStreamReader.invoke()
+
+        verify { Files.exists(any()) }
+        verify { Files.newInputStream(any()) }
+    }
+
+}
