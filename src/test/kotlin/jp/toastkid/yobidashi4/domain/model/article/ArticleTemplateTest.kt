a9a431b5b4e5bfd83220fe6c09b3d5e52d2e0aa5
Toast Kid <super_q1m@yahoo.co.jp>
2023-02-23 09:30:47 +0900

    refactor Add test case.

--- src/test/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleTemplateTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleTemplateTest.kt
@@ -1,9 +1,20 @@
 package jp.toastkid.yobidashi4.domain.model.article
 
+import io.mockk.every
+import io.mockk.mockkConstructor
+import java.time.LocalDate
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import jp.toastkid.yobidashi4.domain.model.setting.TestSettingImplementation
 import jp.toastkid.yobidashi4.domain.service.article.ArticleTemplate
+import jp.toastkid.yobidashi4.domain.service.article.UserTemplateStreamReader
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 internal class ArticleTemplateTest {
 
@@ -11,13 +22,37 @@ internal class ArticleTemplateTest {
 
     @BeforeEach
     fun setUp() {
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { TestSettingImplementation() } bind(Setting::class)
+                }
+            )
+        }
         articleTemplate = ArticleTemplate()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
     }
 
     @Test
     fun test() {
         val content = articleTemplate.invoke("test")
         assertTrue(content.startsWith("# test"))
+    }
+
+    @Test
+    fun testContainsStockDay() {
+        mockkConstructor(UserTemplateStreamReader::class)
+        every { anyConstructed<UserTemplateStreamReader>().invoke() }.returns("""
+            {{stock}}
+test
+            {{/stock}}
+        """.trimIndent().byteInputStream())
+        val content = ArticleTemplate(LocalDate.of(2023, 2, 23)).invoke("test")
+        assertTrue(content.contains("test"))
     }
 
 }
