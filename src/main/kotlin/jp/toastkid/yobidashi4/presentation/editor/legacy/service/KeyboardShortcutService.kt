1914b113ad0788d59aac4bc24e48264ab2c36c28
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-02 09:13:45 +0900

    feature Use flow.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/KeyboardShortcutService.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/KeyboardShortcutService.kt
@@ -2,10 +2,13 @@ package jp.toastkid.yobidashi4.presentation.editor.legacy.service
 
 import java.awt.event.KeyEvent
 import jp.toastkid.yobidashi4.presentation.editor.legacy.MenuCommand
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class KeyboardShortcutService(private val channel: Channel<MenuCommand>) {
 
@@ -14,49 +17,50 @@ class KeyboardShortcutService(private val channel: Channel<MenuCommand>) {
             return
         }
 
+        val viewModel = object : KoinComponent { val viewModel: MainViewModel by inject() }.viewModel
         if (e.isShiftDown && e.keyCode == KeyEvent.VK_UP) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.EDITOR_TO_TOP)
+                viewModel.emitEditorCommand(MenuCommand.EDITOR_TO_TOP)
             }
         }
 
         if (e.isShiftDown && e.keyCode == KeyEvent.VK_DOWN) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.EDITOR_TO_BOTTOM)
+                viewModel.emitEditorCommand(MenuCommand.EDITOR_TO_BOTTOM)
             }
         }
 
         if (e.isShiftDown && e.keyCode == KeyEvent.VK_U) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.REVERSE_CASE)
+                viewModel.emitEditorCommand(MenuCommand.REVERSE_CASE)
             }
             return
         }
 
         if (e.isShiftDown && e.keyCode == KeyEvent.VK_O) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.WEB_SEARCH)
+                viewModel.emitEditorCommand(MenuCommand.WEB_SEARCH)
             }
             return
         }
 
         if (e.isShiftDown && e.keyCode == KeyEvent.VK_W) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.SWITCH_WRAP_LINE)
+                viewModel.emitEditorCommand(MenuCommand.SWITCH_WRAP_LINE)
             }
             return
         }
 
         if (e.isShiftDown && e.keyCode == KeyEvent.VK_N) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.SWITCH_EDITABLE)
+                viewModel.emitEditorCommand(MenuCommand.SWITCH_EDITABLE)
             }
             return
         }
 
         if (e.isAltDown && e.keyCode == KeyEvent.VK_O) {
             CoroutineScope(Dispatchers.Default).launch {
-                channel.send(MenuCommand.OPEN_WITH_BROWSER)
+                viewModel.emitEditorCommand(MenuCommand.OPEN_WITH_BROWSER)
             }
             return
         }
@@ -79,7 +83,7 @@ class KeyboardShortcutService(private val channel: Channel<MenuCommand>) {
                 KeyEvent.VK_CLOSE_BRACKET -> MenuCommand.SURROUND_JA_BRACKET
                 else -> null
             } ?: return@launch
-            channel.send(command)
+            viewModel.emitEditorCommand(command)
         }
     }
 
