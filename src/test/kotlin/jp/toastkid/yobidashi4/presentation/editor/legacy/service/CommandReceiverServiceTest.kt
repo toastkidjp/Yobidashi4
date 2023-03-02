4848d390b955a7080d2dc2b18ea324d1e30043e2
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-03 08:45:08 +0900

    refactor Safe delete 'channel'.

--- src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/CommandReceiverServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/CommandReceiverServiceTest.kt
@@ -5,12 +5,9 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.unmockkAll
 import java.nio.file.Path
-import jp.toastkid.yobidashi4.presentation.editor.legacy.MenuCommand
-import jp.toastkid.yobidashi4.presentation.editor.legacy.model.Editing
 import jp.toastkid.yobidashi4.presentation.editor.legacy.view.EditorAreaView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -22,22 +19,10 @@ internal class CommandReceiverServiceTest {
     private lateinit var commandReceiverService: CommandReceiverService
 
     @MockK
-    private lateinit var channel: Channel<MenuCommand>
-
-    @MockK
     private lateinit var editorAreaView: EditorAreaView
 
     @MockK
     private lateinit var currentArticle: () -> Path
-
-    @MockK
-    private lateinit var editing: Editing
-
-    @MockK
-    private lateinit var resetFrameTitle: () -> Unit
-
-    @MockK
-    private lateinit var switchFinder: () -> Unit
 
     @BeforeEach
     fun setUp() {
