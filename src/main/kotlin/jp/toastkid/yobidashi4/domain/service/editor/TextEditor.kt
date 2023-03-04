d35323c07afa90571ef49ef40b831f3522f901ab
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-04 09:46:52 +0900

    refactor Extract to interface.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/editor/TextEditor.kt
@@ -0,0 +1,22 @@
+package jp.toastkid.yobidashi4.domain.service.editor
+
+import java.nio.file.Path
+import javax.swing.JComponent
+
+interface TextEditor {
+
+    fun getContent(): JComponent
+
+    fun setText(path: Path, text: String)
+
+    fun save()
+
+    fun setCaretPosition(position: Int)
+
+    fun caretPosition(): Int
+
+    fun currentText(): String?
+
+    fun cancel()
+
+}
