8f69d26c5ca577fb058f75b66381bcc4f9f4254e
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-06 08:37:19 +0900

    feature Add vertical scroll bar.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolView.kt
@@ -1,10 +1,15 @@
 package jp.toastkid.yobidashi4.presentation.tool.file
 
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
@@ -18,6 +23,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -51,61 +57,66 @@ fun FileRenameToolView() {
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
         elevation = 4.dp
     ) {
-        LazyColumn {
-            item {
-                TextField(
-                    firstInput.value,
-                    maxLines = 1,
-                    colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
-                    label = { Text("Base file name") },
-                    onValueChange = {
-                        firstInput.value = TextFieldValue(it.text, it.selection, it.composition)
-                    },
-                    trailingIcon = {
-                        Icon(
-                            painterResource("images/icon/ic_clear_form.xml"),
-                            contentDescription = "Clear input.",
-                            tint = MaterialTheme.colors.primary,
-                            modifier = Modifier.clickable {
-                                firstInput.value = TextFieldValue()
+        Box {
+            val verticalScrollState = rememberLazyListState()
+            LazyColumn(state = verticalScrollState) {
+                item {
+                    TextField(
+                        firstInput.value,
+                        maxLines = 1,
+                        colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
+                        label = { Text("Base file name") },
+                        onValueChange = {
+                            firstInput.value = TextFieldValue(it.text, it.selection, it.composition)
+                        },
+                        trailingIcon = {
+                            Icon(
+                                painterResource("images/icon/ic_clear_form.xml"),
+                                contentDescription = "Clear input.",
+                                tint = MaterialTheme.colors.primary,
+                                modifier = Modifier.clickable {
+                                    firstInput.value = TextFieldValue()
+                                }
+                            )
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier.onKeyEvent {
+                            if (it.type == KeyEventType.KeyDown && it.key == Key.Enter
+                                && firstInput.value.composition == null
+                                && firstInput.value.text.isNotBlank()
+                            ) {
+                                rename(paths, firstInput.value.text)
                             }
-                        )
-                    },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.onKeyEvent {
-                        if (it.type == KeyEventType.KeyDown && it.key == Key.Enter
-                            && firstInput.value.composition == null
-                            && firstInput.value.text.isNotBlank()
-                        ) {
-                            rename(paths, firstInput.value.text)
+                            true
                         }
-                        true
-                    }
-                )
-            }
+                    )
+                }
 
-            item {
-                Row {
-                    Button(onClick = {
-                        paths.clear()
-                    },
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Text("Clear files")
-                    }
-                    Button(onClick = {
-                        rename(paths, firstInput.value.text)
-                    },
-                        modifier = Modifier.padding(8.dp)
+                item {
+                    Row {
+                        Button(onClick = {
+                            paths.clear()
+                        },
+                            modifier = Modifier.padding(8.dp)
                         ) {
-                        Text("Rename")
+                            Text("Clear files")
+                        }
+                        Button(onClick = {
+                            rename(paths, firstInput.value.text)
+                        },
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            Text("Rename")
+                        }
                     }
                 }
-            }
 
-            items(paths) { path ->
-                Text(path.fileName.toString())
+                items(paths) { path ->
+                    Text(path.fileName.toString())
+                }
             }
+            VerticalScrollbar(adapter = rememberScrollbarAdapter(verticalScrollState), modifier = Modifier.fillMaxHeight().align(
+                Alignment.CenterEnd))
         }
     }
 
