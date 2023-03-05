32c19d287a3d6cd68d6c2d59fd2c0954f7587cd4
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-05 10:16:24 +0900

    refactor Add starting and stopping Koin.

--- src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/CommandReceiverServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/CommandReceiverServiceTest.kt
@@ -4,13 +4,19 @@ import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.unmockkAll
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.presentation.editor.legacy.view.EditorAreaView
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 internal class CommandReceiverServiceTest {
 
@@ -20,13 +26,28 @@ internal class CommandReceiverServiceTest {
     @MockK
     private lateinit var editorAreaView: EditorAreaView
 
+    @MockK
+    private lateinit var viewModel: MainViewModel
+
+    @MockK
+    private lateinit var setting: Setting
+
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { viewModel } bind(MainViewModel::class)
+                    single(qualifier=null) { setting } bind(Setting::class)
+                }
+            )
+        }
     }
 
     @AfterEach
     fun tearDown() {
+        stopKoin()
         unmockkAll()
     }
 
