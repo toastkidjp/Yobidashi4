89489a5803d56cdebdce8bc6339a87319c1a35f0
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-22 08:41:26 +0900

    feature Set tab icon.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/LoanCalculatorTab.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/LoanCalculatorTab.kt
@@ -4,4 +4,8 @@ class LoanCalculatorTab : Tab {
     override fun title(): String = "Loan Calculator"
 
     override fun closeable(): Boolean = true
+
+    override fun iconPath(): String? {
+        return "images/icon/ic_home.xml"
+    }
 }
