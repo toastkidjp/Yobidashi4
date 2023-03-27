a1306358ac17a512901347895fb8cab4fa86ac99
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-27 09:39:07 +0900

    refactor Move to companion object.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/service/table/TableContentExporter.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/table/TableContentExporter.kt
@@ -6,8 +6,6 @@ import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
 
 class TableContentExporter {
 
-    private val COLUMN_DELIMITER = "\t"
-
     operator fun invoke(aggregationResult: AggregationResult) {
         val outputFolder = Paths.get(EXPORT_TO)
         if (Files.exists(outputFolder).not()) {
@@ -20,6 +18,8 @@ class TableContentExporter {
     }
 
     companion object {
+
+        private val COLUMN_DELIMITER = "\t"
 
         private const val EXPORT_TO = "user/table"
 
