217d1c0eb8aec9863ba94e91ead3fe3372deb767
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-29 09:09:44 +0900

    feature Enable to show tab-icon.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/TableTab.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/TableTab.kt
@@ -1,6 +1,7 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
+import jp.toastkid.yobidashi4.domain.model.aggregation.MovieMemoExtractorResult
 
 data class TableTab(
     private val title: String,
@@ -11,5 +12,9 @@ data class TableTab(
     override fun title(): String = title
 
     override fun closeable(): Boolean = closeable
+
+    override fun iconPath(): String? {
+        return if (items is MovieMemoExtractorResult) "images/icon/ic_movie.xml" else null
+    }
 
 }
