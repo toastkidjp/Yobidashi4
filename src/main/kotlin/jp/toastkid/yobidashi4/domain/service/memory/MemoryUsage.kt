c7dee426f2b13e70ec5ec88707cea043c50e1def
Toast Kid <super_q1m@yahoo.co.jp>
2023-03-30 09:15:19 +0900

    feature Add new service.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/memory/MemoryUsage.kt
@@ -0,0 +1,19 @@
+package jp.toastkid.yobidashi4.domain.service.memory
+
+import java.text.NumberFormat
+
+class MemoryUsage(private val runtime: Runtime = Runtime.getRuntime()) {
+
+    fun maxMemory() = format(runtime.maxMemory())
+
+    fun freeMemory() = format(runtime.freeMemory())
+
+    fun totalMemory() = format(runtime.totalMemory())
+
+    fun usedMemory() = format(runtime.totalMemory() - runtime.freeMemory())
+
+    private fun format(l: Long) = NumberFormat.getInstance().format(l / MEGA_BYTE)
+
+}
+
+private val MEGA_BYTE = 1024 * 1024
