fab363df51b446aa3df418d7b3eca7261c5ab260
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-03 07:52:36 +0900

    refactor Extract to field.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/preview/InternalLinkScheme.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/preview/InternalLinkScheme.kt
@@ -4,7 +4,9 @@ class InternalLinkScheme {
 
     private val WHITE_SPACE = " "
 
-    fun makeLink(title: String?) = "[$title]($INTERNAL_LINK_SCHEME${title?.replace(WHITE_SPACE, "%20")})"
+    private val ESCAPED_WHITE_SPACE = "%20"
+
+    fun makeLink(title: String?) = "[$title]($INTERNAL_LINK_SCHEME${title?.replace(WHITE_SPACE, ESCAPED_WHITE_SPACE)})"
 
     fun isInternalLink(url: String): Boolean {
         return url.startsWith(INTERNAL_LINK_SCHEME)
@@ -14,7 +16,7 @@ class InternalLinkScheme {
         if (url.length <= INTERNAL_LINK_SCHEME.length || !url.startsWith(INTERNAL_LINK_SCHEME)) {
             return url
         }
-        return url.substring(INTERNAL_LINK_SCHEME.length).replace("%20", WHITE_SPACE)
+        return url.substring(INTERNAL_LINK_SCHEME.length).replace(ESCAPED_WHITE_SPACE, WHITE_SPACE)
     }
 
     companion object {
