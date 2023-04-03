361f2afddbf2af1998c9b83851b9c326c88c6b00
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-04 07:26:01 +0900

    refactor Divide to other function.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/drop/DropTargetFactory.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/drop/DropTargetFactory.kt
@@ -18,30 +18,37 @@ class DropTargetFactory {
         dropTarget.addDropTargetListener(
             object : DropTargetAdapter() {
                 override fun drop(dtde: DropTargetDropEvent?) {
-                    dtde ?: return
-                    try {
-                        if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
-                            dtde.acceptDrop(DnDConstants.ACTION_COPY)
-                            val transferable = dtde.transferable
-                            val list = transferable.getTransferData(
-                                DataFlavor.javaFileListFlavor
-                            ) as List<*>
-                            val files = list.filterIsInstance<File>().map { it.toPath() }.sortedBy { it.fileName.toString() }
-                            consumer(files)
-                            dtde.dropComplete(true)
-                            return
-                        }
-                    } catch (ex: UnsupportedFlavorException) {
-                        LoggerFactory.getLogger(javaClass).warn("I/O error.", ex)
-                    } catch (ex: IOException) {
-                        LoggerFactory.getLogger(javaClass).warn("I/O error.", ex)
-                    }
-                    dtde!!.rejectDrop()
+                    onFilesDropped(dtde, consumer)
                 }
             }
         )
 
         return dropTarget
+    }
+
+    private fun onFilesDropped(
+        dtde: DropTargetDropEvent?,
+        consumer: (List<Path>) -> Unit
+    ) {
+        dtde ?: return
+        try {
+            if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                dtde.acceptDrop(DnDConstants.ACTION_COPY)
+                val transferable = dtde.transferable
+                val list = transferable.getTransferData(
+                    DataFlavor.javaFileListFlavor
+                ) as List<*>
+                val files = list.filterIsInstance<File>().map { it.toPath() }.sortedBy { it.fileName.toString() }
+                consumer(files)
+                dtde.dropComplete(true)
+                return
+            }
+        } catch (ex: UnsupportedFlavorException) {
+            LoggerFactory.getLogger(javaClass).warn("I/O error.", ex)
+        } catch (ex: IOException) {
+            LoggerFactory.getLogger(javaClass).warn("I/O error.", ex)
+        }
+        dtde!!.rejectDrop()
     }
 
 }
