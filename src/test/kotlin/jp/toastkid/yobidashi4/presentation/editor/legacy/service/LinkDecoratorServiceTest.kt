75210180f82396d5fdad2d5349804106d6b902b1
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-07 08:08:07 +0900

    refactor Fix incorrect unit test.

--- src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/LinkDecoratorServiceTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/service/LinkDecoratorServiceTest.kt
@@ -1,10 +1,13 @@
 package jp.toastkid.yobidashi4.presentation.editor.legacy.service
 
+import io.mockk.clearAllMocks
 import io.mockk.every
+import io.mockk.mockk
 import io.mockk.mockkStatic
 import java.net.MalformedURLException
 import java.net.URL
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -13,7 +16,12 @@ internal class LinkDecoratorServiceTest {
     @Test
     fun test() {
         assertEquals("www.yahoo.co.jp", LinkDecoratorService().invoke("www.yahoo.co.jp"))
+        mockkStatic(Jsoup::class)
+        val document = mockk<Document>()
+        every { Jsoup.parse(any<URL>(), any()) }.returns(document)
+        every { document.title() } returns "Yahoo! JAPAN"
         assertEquals("[Yahoo! JAPAN](https://www.yahoo.co.jp)", LinkDecoratorService().invoke("https://www.yahoo.co.jp"))
+        clearAllMocks()
     }
 
     @Test
