3e4fd599d88dcc86075dcc94607c07cde7d418d1
Toast Kid <super_q1m@yahoo.co.jp>
2023-04-10 08:32:18 +0900

    refactor Use currentTab().

--- src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/CefClientFactory.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/CefClientFactory.kt
@@ -358,7 +358,7 @@ class CefClientFactory(
         val item = when {
             params?.linkUrl != null && params.linkUrl.isNotBlank() -> makeBookmarkItemWithUrl(params.linkUrl)
             params?.sourceUrl != null && params.sourceUrl.isNotBlank() -> makeBookmarkItemWithUrl(params.sourceUrl)
-            params?.pageUrl != null && params.pageUrl.isNotBlank() -> Bookmark(mainViewModel.tabs[mainViewModel.selected.value].title(), url = params.pageUrl)
+            params?.pageUrl != null && params.pageUrl.isNotBlank() -> Bookmark(mainViewModel.currentTab()?.title() ?: "", url = params.pageUrl)
             else -> Bookmark(mainViewModel.tabs[mainViewModel.selected.value].title(), url = latestBrowser()?.url ?: "")
         }
         object : KoinComponent{ val repository: BookmarkRepository by inject() }.repository.add(item)
