66bbc479b4c4746cc95f6b6f0739b31f752bf865
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-08 08:50:36 +0900

    refactor Extract to field.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/text/CommaInserter.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/text/CommaInserter.kt
@@ -2,11 +2,13 @@ package jp.toastkid.yobidashi4.presentation.editor.legacy.text
 
 class CommaInserter {
 
+    private val NUMBER_OF_DIGITS = 3
+
     operator fun invoke(text: String?): String? {
         val toCharArray = text?.toCharArray() ?: return null
         return with(StringBuilder()) {
             (toCharArray.indices).forEach { index ->
-                if (toCharArray.size > 3 && isNotEmpty() && index % 3 == (toCharArray.size % 3)) {
+                if (toCharArray.size > NUMBER_OF_DIGITS && isNotEmpty() && index % NUMBER_OF_DIGITS == (toCharArray.size % NUMBER_OF_DIGITS)) {
                     append(",")
                 }
                 append(toCharArray[index])
