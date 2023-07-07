f903e76cad66943a40e2ac9b653810e251692577
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-08 08:48:06 +0900

    refactor Implement Unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/legacy/text/CommaInserterTest.kt
@@ -0,0 +1,30 @@
+package jp.toastkid.yobidashi4.presentation.editor.legacy.text
+
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.InjectMockKs
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CommaInserterTest {
+
+    @InjectMockKs
+    private lateinit var commaInserter: CommaInserter
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun invoke() {
+        assertEquals("100", commaInserter.invoke("100"))
+        assertEquals("1,000", commaInserter.invoke("1000"))
+        assertEquals("556,090", commaInserter.invoke("556090"))
+        assertEquals("1,000,556,090", commaInserter.invoke("1000556090"))
+        assertEquals("10,556,090", commaInserter.invoke("10556090"))
+        assertEquals("1,556,090", commaInserter.invoke("1556090"))
+        assertEquals("ora,nge", commaInserter.invoke("orange"))
+    }
+
+}
