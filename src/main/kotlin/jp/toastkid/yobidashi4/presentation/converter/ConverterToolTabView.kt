ea71ae9fcc167ec2727eed365751342e02943f7e
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-09 08:11:28 +0900

    feature Add new converter.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import jp.toastkid.yobidashi4.domain.service.converter.TatamiCountConverterService
 import jp.toastkid.yobidashi4.domain.service.converter.UnixTimeConverterService
 import jp.toastkid.yobidashi4.domain.service.converter.UrlEncodeConverterService
 
@@ -19,6 +20,7 @@ fun ConverterToolTabView() {
         Column(modifier = Modifier.padding(8.dp)) {
             TwoValueConverterBox(UnixTimeConverterService())
             TwoValueConverterBox(UrlEncodeConverterService())
+            TwoValueConverterBox(TatamiCountConverterService())
         }
     }
 }
