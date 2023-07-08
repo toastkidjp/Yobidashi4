bf30c959a445f5718c12270a87d56bdce03402ec
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-09 08:10:49 +0900

    feature Implement new converter.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/converter/TatamiCountConverterService.kt
@@ -0,0 +1,33 @@
+package jp.toastkid.yobidashi4.domain.service.converter
+
+class TatamiCountConverterService : TwoStringConverterService {
+
+    override fun title(): String {
+        return "Tatami counts(畳) <-> Square meter(㎡)"
+    }
+
+    override fun defaultFirstInputValue(): String {
+        return "8"
+    }
+
+    override fun defaultSecondInputValue(): String {
+        return "12.96"
+    }
+
+    override fun firstInputAction(input: String): String? {
+        val d = input.toDoubleOrNull() ?: return null
+        return String.format("%.2f", d * FACTOR)
+    }
+
+    override fun secondInputAction(input: String): String? {
+        val d = input.toDoubleOrNull() ?: return null
+        return String.format("%.2f", d / FACTOR)
+    }
+
+    companion object {
+
+        private const val FACTOR = 1.62f
+
+    }
+
+}
