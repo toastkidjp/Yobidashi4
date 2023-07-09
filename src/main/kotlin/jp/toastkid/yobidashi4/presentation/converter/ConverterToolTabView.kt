314682c8ad88d9ba49c0824d9a7f401e5db56a31
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-10 07:04:31 +0900

    feature Enable to vertical scrolling.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
@@ -1,10 +1,17 @@
 package jp.toastkid.yobidashi4.presentation.converter
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import jp.toastkid.yobidashi4.domain.service.converter.DistanceConverterService
@@ -19,12 +26,20 @@ fun ConverterToolTabView() {
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
         elevation = 4.dp
     ) {
-        Column(modifier = Modifier.padding(8.dp)) {
-            TwoValueConverterBox(UnixTimeConverterService())
-            TwoValueConverterBox(UrlEncodeConverterService())
-            TwoValueConverterBox(TatamiCountConverterService())
-            TwoValueConverterBox(TemperatureConverterService())
-            TwoValueConverterBox(DistanceConverterService())
+        Box {
+            val state = rememberScrollState()
+            Column(modifier = Modifier.padding(8.dp).verticalScroll(state)) {
+                TwoValueConverterBox(UnixTimeConverterService())
+                TwoValueConverterBox(UrlEncodeConverterService())
+                TwoValueConverterBox(TatamiCountConverterService())
+                TwoValueConverterBox(TemperatureConverterService())
+                TwoValueConverterBox(DistanceConverterService())
+            }
+
+            VerticalScrollbar(
+                adapter = rememberScrollbarAdapter(state),
+                modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd)
+            )
         }
     }
 }
