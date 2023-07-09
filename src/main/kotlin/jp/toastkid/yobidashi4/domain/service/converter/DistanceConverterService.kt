981e53a3754e1304a2af9605f028205ce015864b
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-10 07:00:20 +0900

    feature Implement new converter.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/converter/DistanceConverterService.kt
@@ -0,0 +1,32 @@
+package jp.toastkid.yobidashi4.domain.service.converter
+
+class DistanceConverterService : TwoStringConverterService {
+    override fun title(): String {
+        return "Minutes and Distance"
+    }
+
+    override fun defaultFirstInputValue(): String {
+        return "20"
+    }
+
+    override fun defaultSecondInputValue(): String {
+        return "1600"
+    }
+
+    override fun firstInputAction(input: String): String? {
+        val d = input.toDoubleOrNull() ?: return null
+        return String.format("%.2f", d * FACTOR)
+    }
+
+    override fun secondInputAction(input: String): String? {
+        val d = input.toDoubleOrNull() ?: return null
+        return String.format("%.2f", d / FACTOR)
+    }
+
+    companion object {
+
+        private const val FACTOR = 80f
+
+    }
+
+}
