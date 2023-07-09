a89ab4a156eead2a55c49a5f6caf405ae155bc18
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-10 06:51:24 +0900

    feature Implement new converter.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/converter/TemperatureConverterService.kt
@@ -0,0 +1,43 @@
+package jp.toastkid.yobidashi4.domain.service.converter
+
+class TemperatureConverterService : TwoStringConverterService {
+
+    /**
+     * Fahrenheit temperature convert to Celsius temperature.
+     * @param fahrenheit Fahrenheit temperature
+     * @return Celsius temperature
+     */
+    private fun fToC(fahrenheit: Double): Double {
+        return (fahrenheit - 32.0) / 1.8
+    }
+
+    /**
+     * Celsius temperature convert to Fahrenheit temperature.
+     * @param celsius Celsius temperature
+     * @return Fahrenheit temperature
+     */
+    private fun cToF(celsius: Double): Double {
+        return celsius * 1.8 + 32.0
+    }
+
+    override fun title(): String {
+        return "Temperature"
+    }
+
+    override fun defaultFirstInputValue(): String {
+        return "77.0"
+    }
+
+    override fun defaultSecondInputValue(): String {
+        return "25"
+    }
+
+    override fun firstInputAction(input: String): String? {
+        return fToC(input.toDoubleOrNull() ?: 0.0).toString()
+    }
+
+    override fun secondInputAction(input: String): String? {
+        return cToF(input.toDoubleOrNull() ?: 0.0).toString()
+    }
+
+}
