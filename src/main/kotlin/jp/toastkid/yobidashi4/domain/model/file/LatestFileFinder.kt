0565a914443d30ddb8ca0449417bb7d3ac58d1f4
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-16 12:46:58 +0900

    refactor Divide to other class.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/file/LatestFileFinder.kt
@@ -0,0 +1,22 @@
+package jp.toastkid.yobidashi4.domain.model.file
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.util.stream.Collectors
+
+class LatestFileFinder {
+
+    operator fun invoke(path: Path, latest: LocalDateTime): MutableList<Path> {
+        val toEpochMilli =  latest.toInstant(OffsetDateTime.now().offset).toEpochMilli()
+        val paths = Files.list(path)
+            .sorted { p1, p2 -> Files.getLastModifiedTime(p1).compareTo(Files.getLastModifiedTime(p2)) * -1 }
+            .filter {
+                Files.getLastModifiedTime(it).toMillis() > toEpochMilli
+            }
+            .collect(Collectors.toList())
+        return paths
+    }
+
+}
