98a21e57f00494ba2630ff38d9247c2598a0af67
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-17 10:18:02 +0900

    refactor Add unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi4/domain/model/file/ArticleFilesFinderTest.kt
@@ -0,0 +1,51 @@
+package jp.toastkid.yobidashi4.domain.model.file
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.FileTime
+import java.util.stream.Stream
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ArticleFilesFinderTest {
+
+    @InjectMockKs
+    private lateinit var subject: ArticleFilesFinder
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        mockkStatic(Files::class)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test() {
+        val path = mockk<Path>()
+        every { path.fileName.toString() } returns "2021-01-02"
+        val path2 = mockk<Path>()
+        every { path2.fileName.toString() } returns "Test"
+        val path3 = mockk<Path>()
+        every { path3.fileName.toString() } returns "『2021-01-02』"
+
+        every { Files.list(any()) }.returns(Stream.of(path, path2, path3))
+        every { Files.getLastModifiedTime(any()) }.returns(FileTime.fromMillis(System.currentTimeMillis()))
+
+        val paths = subject.invoke(mockk())
+
+        Assertions.assertEquals(2, paths.size)
+    }
+
+}
