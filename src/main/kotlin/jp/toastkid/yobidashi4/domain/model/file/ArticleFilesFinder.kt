2f117c61ca94414547ab4e640095447edc5d7ef9
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-17 10:12:13 +0900

    refactor Divide to other class.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/file/ArticleFilesFinder.kt
@@ -0,0 +1,20 @@
+package jp.toastkid.yobidashi4.domain.model.file
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.stream.Collectors
+
+class ArticleFilesFinder {
+
+    operator fun invoke(path: Path): MutableList<Path> {
+        val paths = Files.list(path)
+            .sorted { p1, p2 -> Files.getLastModifiedTime(p1).compareTo(Files.getLastModifiedTime(p2)) * -1 }
+            .filter {
+                val name = it.fileName.toString()
+                name.startsWith("20") || name.startsWith("『")
+            }
+            .collect(Collectors.toList())
+        return paths
+    }
+
+}
