7b3c5b0b229af6733890f7047b8dc1daeb876ba0
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-19 09:42:22 +0900

    refactor Return directly.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/model/file/LatestFileFinder.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/file/LatestFileFinder.kt
@@ -10,13 +10,12 @@ class LatestFileFinder {
 
     operator fun invoke(path: Path, latest: LocalDateTime): MutableList<Path> {
         val toEpochMilli =  latest.toInstant(OffsetDateTime.now().offset).toEpochMilli()
-        val paths = Files.list(path)
+        return Files.list(path)
             .sorted { p1, p2 -> Files.getLastModifiedTime(p1).compareTo(Files.getLastModifiedTime(p2)) * -1 }
             .filter {
                 Files.getLastModifiedTime(it).toMillis() > toEpochMilli
             }
             .collect(Collectors.toList())
-        return paths
     }
 
 }
