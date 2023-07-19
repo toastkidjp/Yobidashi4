2276d4358843344f31a377753b7323bd6c87ed2e
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-19 09:42:40 +0900

    refactor Return directly.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/model/file/ArticleFilesFinder.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/model/file/ArticleFilesFinder.kt
@@ -7,14 +7,13 @@ import java.util.stream.Collectors
 class ArticleFilesFinder {
 
     operator fun invoke(path: Path): MutableList<Path> {
-        val paths = Files.list(path)
+        return Files.list(path)
             .sorted { p1, p2 -> Files.getLastModifiedTime(p1).compareTo(Files.getLastModifiedTime(p2)) * -1 }
             .filter {
                 val name = it.fileName.toString()
                 name.startsWith("20") || name.startsWith("『")
             }
             .collect(Collectors.toList())
-        return paths
     }
 
 }
