4f0fcfe07a095abd68f5438f6a3de5b9b69f2fc0
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-20 09:26:18 +0900

    refactor Use Path.of instead of Paths.get.

--- src/main/kotlin/jp/toastkid/yobidashi4/domain/service/article/UserTemplateStreamReader.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/domain/service/article/UserTemplateStreamReader.kt
@@ -2,11 +2,11 @@ package jp.toastkid.yobidashi4.domain.service.article
 
 import java.io.InputStream
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
 
 class UserTemplateStreamReader {
     operator fun invoke(): InputStream? {
-        val userTemplatePath = Paths.get("user/article_template.txt")
+        val userTemplatePath = Path.of("user/article_template.txt")
         return if (Files.exists(userTemplatePath)) {
             Files.newInputStream(userTemplatePath)
         } else {
