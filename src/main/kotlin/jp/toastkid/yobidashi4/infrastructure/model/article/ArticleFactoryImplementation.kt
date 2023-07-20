32e328fac14b9db34cd73cb22cfb16ae5fb4d153
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-20 09:26:34 +0900

    refactor Use Path.of instead of Paths.get.

--- src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/model/article/ArticleFactoryImplementation.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/model/article/ArticleFactoryImplementation.kt
@@ -1,6 +1,6 @@
 package jp.toastkid.yobidashi4.infrastructure.model.article
 
-import java.nio.file.Paths
+import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.model.article.Article
 import jp.toastkid.yobidashi4.domain.model.article.ArticleFactory
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
@@ -14,7 +14,7 @@ class ArticleFactoryImplementation : KoinComponent, ArticleFactory {
     private val setting: Setting by inject()
 
     override fun withTitle(title: String): Article {
-        val file = Paths.get(setting.articleFolder(), "$title.md")
+        val file = Path.of(setting.articleFolder(), "$title.md")
         return Article(file)
     }
 
