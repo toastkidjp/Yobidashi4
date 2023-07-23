58186d0f168673c8b24115022eaca7b67aa0e17c
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-23 10:06:51 +0900

    feature Update JCef Maven.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation(compose.desktop.currentOs)
 
     implementation("com.fifesoft:rsyntaxtextarea:3.3.2")
-    implementation("me.friwi:jcefmaven:108.4.13")
+    implementation("me.friwi:jcefmaven:110.0.25")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 
