1c279a89bc5c0a209316f69be6eb770c049f43d2
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-26 09:28:18 +0900

    refactor Use SimpleTextEditor.

--- src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTabView.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTabView.kt
@@ -1,15 +1,18 @@
 package jp.toastkid.yobidashi4.presentation.editor
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
 
 @Composable
 fun EditorTabView(tab: EditorTab) {
-    /*SimpleTextEditor(
-        tab.path,
-        Modifier
-    )*/
+    /*
     EditorView(
         Editor(tab.path)
+    )
+     */
+    SimpleTextEditor(
+        tab.path,
+        Modifier
     )
 }
