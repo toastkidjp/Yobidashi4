a3d96009051f3671ab7b7c3167b057b7f5f30631
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-26 09:51:23 +0900

    refactor Implement context menu factory.

---
+++ src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefContextMenuFactory.kt
@@ -0,0 +1,32 @@
+package jp.toastkid.yobidashi4.infrastructure.service.web
+
+import jp.toastkid.yobidashi4.infrastructure.model.web.Context
+import jp.toastkid.yobidashi4.infrastructure.model.web.ContextMenu
+import org.cef.callback.CefContextMenuParams
+import org.cef.callback.CefMenuModel
+
+class CefContextMenuFactory {
+
+    operator fun invoke(
+        params: CefContextMenuParams?,
+        model: CefMenuModel?
+    ) {
+        ContextMenu.values().filter {
+            when (it.context) {
+                Context.IMAGE -> {
+                    return@filter params?.linkUrl.isNullOrBlank().not()
+                }
+                Context.LINK -> {
+                    return@filter params?.sourceUrl.isNullOrBlank().not()
+                }
+                Context.PLAIN_TEXT -> {
+                    return@filter params?.linkUrl.isNullOrBlank() && params?.sourceUrl.isNullOrBlank()
+                }
+                else -> return@filter true
+            }
+        }.forEach {
+            model?.addItem(it.id, it.text)
+        }
+    }
+
+}
