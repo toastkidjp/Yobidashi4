487ccdbe107030ed04ce5549bbbcd5070d52da5e
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-27 09:05:02 +0900

    refactor Add unit test.

---
+++ src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefContextMenuFactoryTest.kt
@@ -0,0 +1,44 @@
+package jp.toastkid.yobidashi4.infrastructure.service.web
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import jp.toastkid.yobidashi4.infrastructure.model.web.Context
+import jp.toastkid.yobidashi4.infrastructure.model.web.ContextMenu
+import org.cef.callback.CefContextMenuParams
+import org.cef.callback.CefMenuModel
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CefContextMenuFactoryTest {
+
+    @InjectMockKs
+    private lateinit var cefContextMenuFactory: CefContextMenuFactory
+
+    @MockK
+    private lateinit var model: CefMenuModel
+
+    @MockK
+    private lateinit var params: CefContextMenuParams
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        every { model.addItem(any(), any()) } returns true
+        every { params.sourceUrl } returns "https://www.yahoo.co.jp/favicon.ico"
+        every { params.linkUrl } returns "https://www.yahoo.co.jp"
+    }
+
+    @Test
+    fun test() {
+        cefContextMenuFactory.invoke(params, model)
+
+        ContextMenu.values().filter { it.context != Context.PLAIN_TEXT }.forEach {
+            verify(exactly = 1) { model.addItem(it.id, it.text) }
+        }
+    }
+
+}
