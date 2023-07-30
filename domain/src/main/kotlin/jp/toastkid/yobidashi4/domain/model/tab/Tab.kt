c9334e609c1741e1708eb6a6b04aba9bf9eb1225
Toast Kid <super_q1m@yahoo.co.jp>
2023-07-31 07:52:20 +0900

    feature Update implementation.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/Tab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/Tab.kt
@@ -1,5 +1,8 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
 interface Tab {
 
     fun title(): String
@@ -7,5 +10,7 @@ interface Tab {
     fun closeable(): Boolean = true
 
     fun iconPath(): String? = null
+
+    fun update(): Flow<Long> = emptyFlow()
 
 }
