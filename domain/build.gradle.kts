cb9430b23ba1091e5e5d7ca6f3373d929a22376f
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-02 09:19:11 +0900

    refactor Delete useless dependencies.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -15,11 +15,6 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("io.mockk:mockk:1.10.6")
-    testRuntimeOnly("net.bytebuddy:byte-buddy:1.12.22")
 }
 
 tasks.test {
