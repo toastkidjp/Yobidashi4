db681d8ca9a39be0a06863c11efa9a30d0a8f8a7
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-03 09:11:12 +0900

    refactor Delete duplicated settings.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -8,10 +8,6 @@ plugins {
 group = "jp.toastkid.yobidashi4"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
