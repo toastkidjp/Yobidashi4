a14b83ba7550b3675b2eb68a797fdb10a91d51e2
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-04 09:16:36 +0900

    refactor Modify interface.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/TodayArticleGeneratorImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/TodayArticleGeneratorImplementation.kt
@@ -5,6 +5,7 @@ import jp.toastkid.yobidashi4.domain.model.article.ArticleFactory
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.service.article.ArticleTemplate
 import jp.toastkid.yobidashi4.domain.service.article.ArticleTitleGenerator
+import jp.toastkid.yobidashi4.domain.service.article.OffDayFinderService
 import jp.toastkid.yobidashi4.domain.service.article.TodayArticleGenerator
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
@@ -17,6 +18,8 @@ class TodayArticleGeneratorImplementation : KoinComponent, TodayArticleGenerator
 
     private val articleFactory: ArticleFactory by inject()
 
+    private val offDayFinderService: OffDayFinderService by inject()
+
     override operator fun invoke() {
         val title = ArticleTitleGenerator().invoke() ?: return
         val path = setting.articleFolderPath().resolve("${title}.md")
@@ -25,7 +28,7 @@ class TodayArticleGeneratorImplementation : KoinComponent, TodayArticleGenerator
         }
 
         val article = articleFactory.withTitle(title)
-        article.makeFile { ArticleTemplate()(article.getTitle()) }
+        article.makeFile { ArticleTemplate(offDayFinderService = offDayFinderService)(article.getTitle()) }
     }
 
 }
