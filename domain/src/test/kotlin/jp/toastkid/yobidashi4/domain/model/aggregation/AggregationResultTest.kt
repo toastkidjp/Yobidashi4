020d1d5daa33ccc6c1b2162b683ccd2acd733241
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-09 09:30:42 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/aggregation/AggregationResultTest.kt
@@ -0,0 +1,32 @@
+package jp.toastkid.yobidashi4.domain.model.aggregation
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class AggregationResultTest {
+
+    @Test
+    fun test() {
+        val aggregationResult = object : AggregationResult {
+            override fun header(): Array<Any> {
+                return emptyArray()
+            }
+
+            override fun itemArrays(): Collection<Array<Any>> {
+                return listOf(arrayOf(), arrayOf())
+            }
+
+            override fun title(): String {
+                return "test"
+            }
+
+            override fun isEmpty(): Boolean {
+                return false
+            }
+        }
+
+        assertEquals(String::class.java, aggregationResult.columnClass(0))
+        assertEquals(Integer::class.java, aggregationResult.columnClass(1))
+    }
+
+}
