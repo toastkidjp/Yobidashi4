881ea0319e1dcba7d0f295bcef327173edc7a66c
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-13 09:58:38 +0900

    feature Define new function.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/UserOffDayService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/UserOffDayService.kt
@@ -1,6 +1,10 @@
 package jp.toastkid.yobidashi4.domain.service.calendar
 
+import jp.toastkid.yobidashi4.domain.model.calendar.holiday.Holiday
+
 interface UserOffDayService {
     operator fun invoke(month: Int, day: Int): Boolean
     fun contains(month: Int): Boolean
+
+    fun findBy(month: Int): Set<Holiday>
 }
