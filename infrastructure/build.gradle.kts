039ad66f0fffa11e90cf69b70e5243046a6c2814
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-13 10:59:13 +0900

    feature Add ZXing dependencies.

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("org.slf4j:slf4j-reload4j:2.0.6")
+
+    implementation("com.google.zxing:core:3.4.1")
 }
 
 // KSP - To use generated sources
