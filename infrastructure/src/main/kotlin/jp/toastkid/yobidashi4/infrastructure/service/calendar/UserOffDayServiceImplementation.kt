99eca3ce85e33c148b8d9baa434b6eb0bc8f26af
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-13 09:58:53 +0900

    feature Implement new function.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/calendar/UserOffDayServiceImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/calendar/UserOffDayServiceImplementation.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.infrastructure.service.calendar
 
+import jp.toastkid.yobidashi4.domain.model.calendar.holiday.Holiday
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.service.calendar.UserOffDayService
 import org.koin.core.annotation.Single
@@ -19,5 +20,9 @@ class UserOffDayServiceImplementation: KoinComponent, UserOffDayService {
         contains(month) && userOffDays.firstOrNull { it.first == month && it.second == day } != null
 
     override fun contains(month: Int): Boolean = targetMonths.contains(month)
+
+    override fun findBy(month: Int): Set<Holiday> {
+        return userOffDays.filter { it.first == month }.map { Holiday("User off-day", it.first, it.second) }.toSet()
+    }
 
 }
