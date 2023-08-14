d6371e417477e57da58de8602b7e704e3d84fefb
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-15 08:32:16 +0900

    feature Set icon.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/BarcodeToolTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/BarcodeToolTab.kt
@@ -4,4 +4,9 @@ class BarcodeToolTab : Tab {
     override fun title(): String {
         return "Barcode tool"
     }
+
+    override fun iconPath(): String? {
+        return "images/icon/ic_barcode.xml"
+    }
+
 }
