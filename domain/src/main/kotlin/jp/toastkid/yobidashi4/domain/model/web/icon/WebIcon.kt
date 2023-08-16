9aba1b3442c650bb8201398482a8438fcae75236
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-16 09:05:15 +0900

    refactor Specify model for web icon.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/web/icon/WebIcon.kt
@@ -0,0 +1,30 @@
+package jp.toastkid.yobidashi4.domain.model.web.icon
+
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.stream.Collectors
+import kotlin.io.path.pathString
+
+class WebIcon {
+
+    private val faviconFolder = Path.of("temporary/web/icon")
+
+    fun makeFolderIfNeed() {
+        if (Files.exists(faviconFolder).not()) {
+            Files.createDirectories(faviconFolder)
+        }
+    }
+
+    fun faviconFolder(): Path = faviconFolder
+
+    fun find(url: String): Path? =
+        Files.list(faviconFolder).collect(Collectors.toList()).firstOrNull {
+            val startsWith = it.fileName.pathString.startsWith(URL(url).host.trim())
+            startsWith
+        }
+
+    fun readAll(): List<Path> =
+        Files.list(faviconFolder).collect(Collectors.toList())
+
+}
