ca8d24f009aa8fcba1f7085329fdb0734ea23966
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-17 07:39:01 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/web/ad/AdHostsTest.kt
@@ -0,0 +1,30 @@
+package jp.toastkid.yobidashi4.domain.model.web.ad
+
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AdHostsTest {
+
+    private lateinit var adHosts: AdHosts
+
+    @BeforeEach
+    fun setUp() {
+        adHosts = AdHosts(setOf("test-ad.info"))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun contains() {
+        assertTrue(adHosts.contains("https://test-ad.info/content.txt"))
+        assertFalse(adHosts.contains("https://normal-site.com/content.txt"))
+    }
+
+}
