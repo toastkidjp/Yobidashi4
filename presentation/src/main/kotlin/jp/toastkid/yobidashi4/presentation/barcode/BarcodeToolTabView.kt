48c32b1087e92d2ae4a8ff8fd8a2f7679d6c4f24
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-16 09:03:55 +0900

    feature Enable to copy barcode to clipboard.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabView.kt
@@ -15,11 +15,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toAwtImage
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import jp.toastkid.yobidashi4.presentation.editor.legacy.service.ClipboardPutterService
 import jp.toastkid.yobidashi4.presentation.viewmodel.barcode.BarcodeToolTabViewModel
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 @Composable
 fun BarcodeToolTabView() {
@@ -37,6 +42,11 @@ fun BarcodeToolTabView() {
                     it,
                     "Generated barcode",
                     modifier = Modifier.sizeIn(maxWidth = 400.dp, maxHeight = 400.dp)
+                        .clickable {
+                            ClipboardPutterService().invoke(it.toAwtImage())
+                            object : KoinComponent { val vm: MainViewModel by inject() }.vm
+                                .showSnackbar("Copy barcode to clipboard.")
+                        }
                 )
             }
 
