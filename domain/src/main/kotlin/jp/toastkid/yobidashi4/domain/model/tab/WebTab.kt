7e7840a5d03578776dd2ec9b4f9e3bcd65dc27ba
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-17 07:36:40 +0900

    refactor Use WebIcon.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebTab.kt
@@ -1,10 +1,8 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
-import java.net.URL
 import java.nio.file.Files
-import java.nio.file.Path
 import java.util.UUID
-import java.util.stream.Collectors
+import jp.toastkid.yobidashi4.domain.model.web.icon.WebIcon
 import kotlin.io.path.pathString
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -35,11 +33,9 @@ data class WebTab(
             return "images/icon/ic_web.xml"
         }
 
-        val faviconFolder = Path.of("temporary/web/icon")
-        val iconPath = Files.list(faviconFolder).filter {
-            val startsWith = it.fileName.pathString.startsWith(URL(url).host)
-            startsWith
-        }.collect(Collectors.toList()).firstOrNull() ?: return "images/icon/ic_web.xml"
+        val faviconFolder = WebIcon()
+        faviconFolder.makeFolderIfNeed()
+        val iconPath = faviconFolder.find(url) ?: return "images/icon/ic_web.xml"
         if (Files.exists(iconPath)) {
             return iconPath.pathString
         }
