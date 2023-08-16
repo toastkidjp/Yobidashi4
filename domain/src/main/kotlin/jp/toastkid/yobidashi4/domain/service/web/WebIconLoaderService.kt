0f2c1e4f9994a5fc8ce38aae1c7d193d373f536a
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-16 09:04:58 +0900

    refactor Rename interface function's parameter name.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/WebIconLoaderService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/WebIconLoaderService.kt
@@ -5,7 +5,7 @@ import java.nio.file.Path
 interface WebIconLoaderService {
     operator fun invoke(htmlSource: String, browserUrl: String?)
     fun download(
-        it: String,
+        iconUrl: String,
         faviconFolder: Path,
         targetHost: String?
     )
