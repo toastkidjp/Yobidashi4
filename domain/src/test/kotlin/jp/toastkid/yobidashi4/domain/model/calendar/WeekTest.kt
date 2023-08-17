64483a23a5b475f7af2d359ff381ac4858c37fd9
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-18 08:36:25 +0900

    refactor Fix unit test.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/WeekTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/WeekTest.kt
@@ -1,6 +1,7 @@
 package jp.toastkid.yobidashi4.domain.model.calendar
 
 import java.time.LocalDate
+import jp.toastkid.yobidashi4.domain.model.calendar.holiday.Holiday
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -11,13 +12,13 @@ class WeekTest {
 
     @Test
     fun test() {
-        week.add(LocalDate.of(2023, 8, 6), false)
-        week.add(LocalDate.of(2023, 8, 7), false)
-        week.add(LocalDate.of(2023, 8, 8), false)
-        week.add(LocalDate.of(2023, 8, 9), false)
-        week.add(LocalDate.of(2023, 8, 10), false)
-        week.add(LocalDate.of(2023, 8, 11), true)
-        week.add(LocalDate.of(2023, 8, 12), false)
+        week.add(LocalDate.of(2023, 8, 6))
+        week.add(LocalDate.of(2023, 8, 7))
+        week.add(LocalDate.of(2023, 8, 8))
+        week.add(LocalDate.of(2023, 8, 9))
+        week.add(LocalDate.of(2023, 8, 10))
+        week.add(LocalDate.of(2023, 8, 11), Holiday("", 8, 11))
+        week.add(LocalDate.of(2023, 8, 12))
         week.addEmpty()
 
         assertEquals(8, week.days().size)
