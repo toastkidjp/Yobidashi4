6fb6f2a2264849759dcce71c49d47958bfc070a7
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-18 08:33:11 +0900

    refactor Add unit test.

---
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/calendar/CalendarViewModelImplementationTest.kt
@@ -0,0 +1,74 @@
+package jp.toastkid.yobidashi4.infrastructure.viewmodel.calendar
+
+import androidx.compose.ui.text.input.TextFieldValue
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CalendarViewModelImplementationTest {
+
+    private lateinit var viewModel: CalendarViewModelImplementation
+
+    @BeforeEach
+    fun setUp() {
+        viewModel = CalendarViewModelImplementation()
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun localDate() {
+        assertNotNull(viewModel.localDate())
+    }
+
+    @Test
+    fun plusMonths() {
+        val localDate = viewModel.localDate().month
+        viewModel.plusMonths(1)
+        assertEquals(localDate + 1, viewModel.localDate().month)
+    }
+
+    @Test
+    fun setYear() {
+        viewModel.setYear(2022)
+
+        assertEquals(2022, viewModel.localDate().year)
+    }
+
+    @Test
+    fun moveMonth() {
+        viewModel.moveMonth(1)
+        assertEquals(1, viewModel.localDate().month.value)
+    }
+
+    @Test
+    fun moveToCurrentMonth() {
+        val localDate = viewModel.localDate().month
+        viewModel.moveMonth(1)
+
+        viewModel.moveToCurrentMonth()
+
+        assertEquals(localDate, viewModel.localDate().month)
+    }
+
+    @Test
+    fun getFirstDay() {
+        assertEquals(1, viewModel.getFirstDay().dayOfMonth)
+    }
+
+    @Test
+    fun yearInput() {
+        println(viewModel.yearInput())
+    }
+
+    @Test
+    fun setYearInput() {
+        viewModel.setYearInput(TextFieldValue("2023"))
+
+        assertEquals("2023", viewModel.yearInput().text)
+    }
+}
