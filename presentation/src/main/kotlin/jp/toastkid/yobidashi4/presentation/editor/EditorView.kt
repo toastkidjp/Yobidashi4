f470986ef6199582c6cdb7537a41f397036098ff
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-21 08:54:48 +0900

    refactor Safe delete 'LineNumber'.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorView.kt
@@ -1,59 +1,21 @@
 package jp.toastkid.yobidashi4.presentation.editor
 
-import androidx.compose.foundation.HorizontalScrollbar
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlin.text.Regex.Companion.fromLiteral
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
-
-@Composable
-private fun LineNumber(number: String, modifier: Modifier) = Text(
-    text = number,
-    fontSize = 16.sp,
-    //fontFamily = Fonts.jetbrainsMono(),
-    color = LocalContentColor.current,//.copy(alpha = 0.30f),
-    modifier = modifier.padding(start = 12.dp)
-)
 
 @Composable
 private fun LineContent(content: Editor.Content, modifier: Modifier) =
