579893f5d14bb27189cd5819354265db14cc488c
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-22 00:09:16 +0900

    bugfix Fix crash on loading web history.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/repository/web/WebHistoryFileStore.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/repository/web/WebHistoryFileStore.kt
@@ -46,7 +46,7 @@ class WebHistoryFileStore : WebHistoryRepository {
             WebHistory(
                 split[0],
                 split[1],
-                split[2].toLongOrNull() ?: 0,
+                if (split.size >= 3) split[2].toLongOrNull() ?: 0 else 0,
                 if (split.size >= 4) split[3].toIntOrNull() ?: 0 else 0
             )
         }
