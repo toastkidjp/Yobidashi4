53d2173b8f4ade9d34e1c0c6fe66a6ae4cadda93
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-23 09:34:53 +0900

    refactor Use divided file.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/Slideshow.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/Slideshow.kt
@@ -1,15 +1,9 @@
 package jp.toastkid.yobidashi4.presentation.slideshow
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.HorizontalScrollbar
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,24 +14,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.relocation.BringIntoViewRequester
-import androidx.compose.foundation.relocation.bringIntoViewRequester
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -48,23 +33,9 @@ import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.OffsetMapping
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import java.net.URL
 import javax.imageio.ImageIO
@@ -74,7 +45,7 @@ import jp.toastkid.yobidashi4.domain.model.slideshow.data.CodeBlockLine
 import jp.toastkid.yobidashi4.domain.model.slideshow.data.ImageLine
 import jp.toastkid.yobidashi4.domain.model.slideshow.data.TableLine
 import jp.toastkid.yobidashi4.domain.model.slideshow.data.TextLine
-import jp.toastkid.yobidashi4.presentation.editor.EditorTheme
+import jp.toastkid.yobidashi4.presentation.slideshow.view.CodeBlockView
 import kotlin.math.max
 import kotlin.math.min
 import kotlinx.coroutines.launch
@@ -273,143 +244,5 @@ private fun TableLine(line: TableLine) {
             }
 
         }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
-@Composable
-fun CodeBlockView(line: CodeBlockLine) {
-    val coroutineScope = rememberCoroutineScope()
-    val verticalScrollState = rememberScrollState()
-    val horizontalScrollState = rememberScrollState()
-
-    val bringIntoViewRequester = remember { BringIntoViewRequester() }
-    val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
-    val content = remember { mutableStateOf(TextFieldValue(line.code)) }
-
-    Box(modifier = Modifier.background(MaterialTheme.colors.surface)) {
-        BasicTextField(
-            value = content.value,
-            onValueChange = {
-                content.value = it
-            },
-            visualTransformation = {
-                val t = codeString(content.value.text)
-                TransformedText(t, OffsetMapping.Identity)
-            },
-            decorationBox = {
-                Row {
-                    Column(
-                        modifier = Modifier
-                            .scrollable(verticalScrollState, Orientation.Vertical)
-                            .padding(end = 8.dp)
-                            .wrapContentSize(unbounded = true)
-                            .background(MaterialTheme.colors.surface.copy(alpha = 0.75f))
-                    ) {
-                        val textLines = content.value.text.split("\n")
-                        val max = textLines.size
-                        val length = max.toString().length
-                        repeat(max) {
-                            val lineNumberCount = it + 1
-                            val fillCount = length - lineNumberCount.toString().length
-                            val lineNumberText = with(StringBuilder()) {
-                                repeat(fillCount) {
-                                    append(" ")
-                                }
-                                append(lineNumberCount)
-                            }.toString()
-                            Box(
-                                contentAlignment = Alignment.CenterEnd
-                            ) {
-                                Text(
-                                    lineNumberText,
-                                    fontSize = 28.sp,
-                                    fontFamily = FontFamily.Monospace,
-                                    textAlign = TextAlign.End,
-                                    lineHeight = 1.5.em
-                                )
-                            }
-                        }
-                    }
-                    it()
-                }
-            },
-            onTextLayout = {
-                textLayoutResultState.value = it
-            },
-            textStyle = TextStyle(
-                fontSize = 28.sp,
-                fontFamily = FontFamily.Monospace,
-                lineHeight = 1.5.em
-            ),
-            modifier = Modifier
-                .verticalScroll(verticalScrollState)
-                .horizontalScroll(horizontalScrollState)
-                .onPointerEvent(PointerEventType.Scroll, PointerEventPass.Main) {
-                    coroutineScope.launch {
-                        verticalScrollState.scrollBy(it.changes.first().scrollDelta.y)
-                    }
-                }
-                .bringIntoViewRequester(bringIntoViewRequester)
-        )
-
-        VerticalScrollbar(
-            adapter = rememberScrollbarAdapter(verticalScrollState),
-            modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd)
-        )
-        HorizontalScrollbar(
-            adapter = rememberScrollbarAdapter(horizontalScrollState),
-            modifier = Modifier.fillMaxWidth().align(
-                Alignment.BottomCenter
-            )
-        )
-    }
-}
-
-
-private fun codeString(str: String) = buildAnnotatedString {
-    val theme = EditorTheme.get()
-    withStyle(theme.code.simple) {
-        append(str)
-        addStyle(theme.code.punctuation, str, ":")
-        addStyle(theme.code.punctuation, str, "=")
-        addStyle(theme.code.punctuation, str, "\"")
-        addStyle(theme.code.punctuation, str, "[")
-        addStyle(theme.code.punctuation, str, "]")
-        addStyle(theme.code.punctuation, str, "{")
-        addStyle(theme.code.punctuation, str, "}")
-        addStyle(theme.code.punctuation, str, "(")
-        addStyle(theme.code.punctuation, str, ")")
-        addStyle(theme.code.punctuation, str, ",")
-        addStyle(theme.code.keyword, str, "fun ")
-        addStyle(theme.code.keyword, str, "---")
-        addStyle(theme.code.keyword, str, "val ")
-        addStyle(theme.code.keyword, str, "var ")
-        addStyle(theme.code.keyword, str, "private ")
-        addStyle(theme.code.keyword, str, "internal ")
-        addStyle(theme.code.keyword, str, "for ")
-        addStyle(theme.code.keyword, str, "expect ")
-        addStyle(theme.code.keyword, str, "actual ")
-        addStyle(theme.code.keyword, str, "import ")
-        addStyle(theme.code.keyword, str, "package ")
-        addStyle(theme.code.value, str, "true")
-        addStyle(theme.code.value, str, "false")
-        addStyle(theme.code.value, str, Regex("[0-9]*"))
-        addStyle(theme.code.header, str, Regex("\\n#.*"))
-        addStyle(theme.code.table, str, Regex("\\n\\|.*"))
-        addStyle(theme.code.quote, str, Regex("\\n>.*"))
-        addStyle(theme.code.quote, str, Regex("\\n-.*"))
-        addStyle(theme.code.annotation, str, Regex("^@[a-zA-Z_]*"))
-        addStyle(theme.code.comment, str, Regex("^\\s*//.*"))
-    }
-}
-
-private fun AnnotatedString.Builder.addStyle(style: SpanStyle, text: String, regexp: String) {
-    addStyle(style, text, Regex.fromLiteral(regexp))
-}
-
-private fun AnnotatedString.Builder.addStyle(style: SpanStyle, text: String, regexp: Regex) {
-    for (result in regexp.findAll(text)) {
-        addStyle(style, result.range.first, result.range.last + 1)
     }
 }
