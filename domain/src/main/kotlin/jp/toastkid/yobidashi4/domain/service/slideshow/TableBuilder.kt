c9b015d82c5db7a0bae1aed7d3aa26b643bdddd5
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-24 09:14:24 +0900

    refactor Clean up useless comment out.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/TableBuilder.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/TableBuilder.kt
@@ -13,30 +13,15 @@ class TableBuilder {
     fun setColumns(line: String) {
         table.clear()
         columnNames = line.split("|").drop(1)
-        /*TODO val tableModel = DefaultTableModel(columnNames, 0)
-        table = JTable(tableModel)
-        val font = Font(Font.SANS_SERIF, Font.PLAIN, 40)
-        table?.tableHeader?.font = font
-        table?.font = font
-        table?.rowHeight = 120
-        table?.isFocusable = false
-        table?.isEnabled = false*/
     }
 
     fun addTableLines(line: String) {
         line.split("|").drop(1).also {
             table.add(it)
-            //(table?.model as? DefaultTableModel)?.addRow(it.toTypedArray())
         }
     }
 
     fun build() = TableLine(columnNames ?: emptyList(), table)
-
-    /*fun get(): JComponent? = JPanel().also {
-        it.layout = BoxLayout(it, BoxLayout.PAGE_AXIS)
-        it.add(table?.tableHeader)
-        it.add(table)
-    }*/
 
     companion object {
 
