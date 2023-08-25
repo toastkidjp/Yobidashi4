4e3f5f2b51aa5b1cf2b8ebafcb0276d03c5e2d8e
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-25 09:55:54 +0900

    refactor Delete useless brackets.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/slideshow/data/CodeBlockLine.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/slideshow/data/CodeBlockLine.kt
@@ -1,4 +1,3 @@
 package jp.toastkid.yobidashi4.domain.model.slideshow.data
 
-data class CodeBlockLine(val code: String, val format: String = "") : Line {
-}
+data class CodeBlockLine(val code: String, val format: String = "") : Line
