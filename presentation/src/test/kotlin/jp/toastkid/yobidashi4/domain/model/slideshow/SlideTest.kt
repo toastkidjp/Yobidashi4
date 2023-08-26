f69f92a3c678e0387674aee07b3aeac44eebe56f
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-26 09:29:03 +0900

    refactor Add test case.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/slideshow/SlideTest.kt
@@ -0,0 +1,27 @@
+package jp.toastkid.yobidashi4.domain.model.slideshow
+
+import jp.toastkid.yobidashi4.domain.model.slideshow.data.ImageLine
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class SlideTest {
+
+    private lateinit var slide: Slide
+
+    @BeforeEach
+    fun setUp() {
+        slide = Slide()
+        slide.setBackground("https://test.yahoo.co.jp/background.png")
+        slide.addLine(ImageLine("https://test.yahoo.co.jp/test.png"))
+        slide.addLine(ImageLine("https://test.yahoo.co.jp/test.jpg"))
+        slide.addLine(ImageLine("https://test.yahoo.co.jp/test.webp"))
+    }
+
+    @Test
+    fun extractImageUrls() {
+        val imageUrls = slide.extractImageUrls()
+
+        assertEquals(4, imageUrls.size)
+    }
+}
