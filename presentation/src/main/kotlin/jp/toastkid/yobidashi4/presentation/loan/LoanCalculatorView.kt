71ace5dbf3d98c81428b86663ff6feeffe1edb7b
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-29 08:55:56 +0900

    refactor Replace with dot call.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/loan/LoanCalculatorView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/loan/LoanCalculatorView.kt
@@ -272,7 +272,7 @@ private fun extractLong(editText: String) =
     editText.replace(",", "").toLongOrNull() ?: 0
 
 private fun extractInt(editText: String) =
-    editText.replace(",", "")?.toIntOrNull() ?: 0
+    editText.replace(",", "").toIntOrNull() ?: 0
 
 private fun extractDouble(editText: String) =
     editText.replace(",", "")?.toDoubleOrNull() ?: 0.0
