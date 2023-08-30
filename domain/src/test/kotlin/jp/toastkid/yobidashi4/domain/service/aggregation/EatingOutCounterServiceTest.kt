fa17fa23bd20ec8cd0850ffbcd474e3d9e52e75e
Toast Kid <super_q1m@yahoo.co.jp>
2023-08-30 09:09:00 +0900

    refactor Improve verification.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/EatingOutCounterServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/EatingOutCounterServiceTest.kt
@@ -14,6 +14,7 @@ import java.util.stream.Stream
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 import kotlin.io.path.nameWithoutExtension
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -32,6 +33,21 @@ class EatingOutCounterServiceTest {
 
         mockkStatic(Files::class)
         every { Files.readAllLines(any()) }.returns(listOf("test content"))
+        val lines =
+
+        every { Files.readAllLines(any()) }.returns(
+            """
+## 家計簿
+| 品目 | 金額 |
+|:---|:---|
+| (外食) マッシュルームとひき肉のカレー | 1000円
+| 玉ねぎ8 | 218円
+| にんじん | 129円
+| いちごジャム | 118円
+| (外食) マッシュルームとひき肉のスパゲッティ | 1100円
+| ユニバースターゴールド | 268円
+""".split("\n").map { it.trim() }
+        )
 
         MockKAnnotations.init(this)
         every { articlesReaderService.invoke() }.returns(Stream.of(path))
@@ -44,9 +60,11 @@ class EatingOutCounterServiceTest {
 
     @Test
     fun invoke() {
-        aggregatorService.invoke("test")
+        val result = aggregatorService.invoke("test")
 
         verify(exactly = 1) { Files.readAllLines(any()) }
         verify(exactly = 1) { articlesReaderService.invoke() }
+        assertEquals(2, result.itemArrays().size)
+        assertEquals(2100, result.sum())
     }
 }
