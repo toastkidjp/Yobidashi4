734d9f9b0dd50bb2da841a0ab06d31329d6f79e4
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-05 08:06:26 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/number/NumberBoardTest.kt
@@ -0,0 +1,63 @@
+package jp.toastkid.yobidashi4.domain.model.number
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class NumberBoardTest {
+
+    private lateinit var numberBoard: NumberBoard
+
+    @BeforeEach
+    fun setUp() {
+        numberBoard = NumberBoard()
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun fillZero() {
+        assertTrue(numberBoard.fulfilled())
+    }
+
+    @Test
+    fun masked() {
+        // Given
+        while (!numberBoard.fulfilled()) {
+            numberBoard.placeRandom()
+        }
+        assertTrue(numberBoard.fulfilled())
+        assertTrue(numberBoard.isCorrect(numberBoard))
+
+        // When
+        val masked = numberBoard.masked(20)
+
+        // Then
+        assertFalse(masked.fulfilled())
+        assertFalse(numberBoard.isCorrect(masked))
+    }
+
+    @Test
+    fun copyFrom() {
+    }
+
+    @Test
+    fun rows() {
+    }
+
+    @Test
+    fun place() {
+    }
+
+    @Test
+    fun pick() {
+    }
+
+    @Test
+    fun copy() {
+    }
+}
