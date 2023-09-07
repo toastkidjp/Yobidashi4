d770cadf5ac0a2c91d83353a521be82c4621e70e
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-08 08:41:48 +0900

    refactor Use nameWithoutExtension directly.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/OutgoAggregatorService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/OutgoAggregatorService.kt
@@ -4,6 +4,7 @@ import java.nio.file.Files
 import java.util.stream.Collectors
 import jp.toastkid.yobidashi4.domain.model.aggregation.OutgoAggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
+import kotlin.io.path.nameWithoutExtension
 
 class OutgoAggregatorService(private val articlesReaderService: ArticlesReaderService) {
 
@@ -11,7 +12,7 @@ class OutgoAggregatorService(private val articlesReaderService: ArticlesReaderSe
         val aggregationResult = OutgoAggregationResult(keyword)
         articlesReaderService.invoke()
                 .parallel()
-                .map { it.toFile().nameWithoutExtension to Files.readAllLines(it) }
+                .map { it.nameWithoutExtension to Files.readAllLines(it) }
                 .filter { it.first.startsWith(keyword) }
                 .map {
                     var isOutGoLine = false
