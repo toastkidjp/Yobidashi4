94ba199824aa24447f0c4df4ee09a7c44df496b9
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-08 08:42:27 +0900

    refactor Add default parameter value.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/Week.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/Week.kt
@@ -7,7 +7,7 @@ import jp.toastkid.yobidashi4.domain.model.calendar.holiday.Holiday
 class Week {
     private val days: MutableList<CalendarDate> = mutableListOf()
 
-    fun add(date: LocalDate, holiday: Holiday?) {
+    fun add(date: LocalDate, holiday: Holiday? = null) {
         days.add(CalendarDate(date.dayOfMonth, date.dayOfWeek, holiday?.title ?: "", offDay = holiday != null))
     }
 
