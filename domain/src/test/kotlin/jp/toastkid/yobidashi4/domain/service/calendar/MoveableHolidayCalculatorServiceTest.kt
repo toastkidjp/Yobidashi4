b7553094d2e907906c38f88881f56e1d0a72bf95
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-08 09:12:58 +0900

    refactor Use assertAll.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/MoveableHolidayCalculatorServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/MoveableHolidayCalculatorServiceTest.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.domain.service.calendar
 
+import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -16,18 +17,20 @@ internal class MoveableHolidayCalculatorServiceTest {
 
     @Test
     fun test() {
-        assertFalse(moveableHolidayCalculatorService.invoke(2020, 1, 12))
-        assertTrue(moveableHolidayCalculatorService.invoke(2020, 1, 13))
-        assertFalse(moveableHolidayCalculatorService.invoke(2020, 1, 14))
-        assertFalse(moveableHolidayCalculatorService.invoke(2019, 7, 14))
-        assertTrue(moveableHolidayCalculatorService.invoke(2019, 7, 15))
-        assertFalse(moveableHolidayCalculatorService.invoke(2019, 7, 16))
-        assertFalse(moveableHolidayCalculatorService.invoke(2020, 9, 20))
-        assertTrue(moveableHolidayCalculatorService.invoke(2020, 9, 21))
-        assertFalse(moveableHolidayCalculatorService.invoke(2019, 10, 13))
-        assertTrue(moveableHolidayCalculatorService.invoke(2019, 10, 14))
-        assertFalse(moveableHolidayCalculatorService.invoke(2019, 10, 15))
-        assertFalse(moveableHolidayCalculatorService.invoke(2023, 8, 14))
+        assertAll(
+            { assertFalse(moveableHolidayCalculatorService.invoke(2020, 1, 12)) },
+            { assertTrue(moveableHolidayCalculatorService.invoke(2020, 1, 13)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2020, 1, 14)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2019, 7, 14)) },
+            { assertTrue(moveableHolidayCalculatorService.invoke(2019, 7, 15)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2019, 7, 16)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2020, 9, 20)) },
+            { assertTrue(moveableHolidayCalculatorService.invoke(2020, 9, 21)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2019, 10, 13)) },
+            { assertTrue(moveableHolidayCalculatorService.invoke(2019, 10, 14)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2019, 10, 15)) },
+            { assertFalse(moveableHolidayCalculatorService.invoke(2023, 8, 14)) }
+        )
     }
 
     @Test
