b34b1957f5ac7e57c0336dd164527aa8ab4250a7
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-14 09:29:11 +0900

    feature Modify scrolling.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/lib/KeyboardScrollAction.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/lib/KeyboardScrollAction.kt
@@ -2,6 +2,7 @@ package jp.toastkid.yobidashi4.presentation.lib
 
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.key.Key
 import kotlinx.coroutines.CoroutineScope
@@ -13,6 +14,11 @@ class KeyboardScrollAction(private val state: ScrollableState) {
     operator fun invoke(coroutineScope: CoroutineScope, key: Key, controlDown: Boolean) = when (key) {
         Key.DirectionUp -> {
             coroutineScope.launch {
+                if (state is LazyListState) {
+                    state.scrollToItem(0)
+                    return@launch
+                }
+
                 val max = if (controlDown) Float.MAX_VALUE else 50f
                 state.animateScrollBy(-1f * max)
             }
@@ -20,6 +26,11 @@ class KeyboardScrollAction(private val state: ScrollableState) {
         }
         Key.DirectionDown -> {
             coroutineScope.launch {
+                if (state is LazyListState) {
+                    state.scrollToItem(state.layoutInfo.totalItemsCount)
+                    return@launch
+                }
+
                 val max = if (controlDown) Float.MAX_VALUE else 50f
                 state.animateScrollBy(max)
             }
