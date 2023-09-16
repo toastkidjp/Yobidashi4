182e081630b7e162fa5c98e35e466a591efdda94
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-17 00:08:21 +0900

    feature Modify scrolling implementation.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/SimpleTextEditor.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/SimpleTextEditor.kt
@@ -3,10 +3,6 @@ package jp.toastkid.yobidashi4.presentation.editor
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.HorizontalScrollbar
 import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,27 +10,24 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.relocation.BringIntoViewRequester
-import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.rememberTextFieldVerticalScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
@@ -47,9 +40,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import java.nio.file.Path
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlin.io.path.readText
-import kotlin.math.max
-import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
@@ -58,12 +52,17 @@ fun SimpleTextEditor(
     modifier: Modifier = Modifier
 ) {
     val content = remember { mutableStateOf(TextFieldValue(path.readText())) }
-    val verticalScrollState = rememberScrollState()
+    val verticalScrollState = rememberTextFieldVerticalScrollState()
+    val lineNumberScrollState = rememberScrollState()
     val horizontalScrollState = rememberScrollState()
+    val focusRequester = remember { FocusRequester() }
 
-    val coroutineScope = rememberCoroutineScope()
-    val bringIntoViewRequester = remember { BringIntoViewRequester() }
-    val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
+    val textColor = remember {
+        if (object : KoinComponent { val vm : MainViewModel by inject() }.vm.darkMode())
+            Color(0xFFF0F0F0)
+        else
+            Color(0xFF000B00)
+    }
 
     Box {
         BasicTextField(
@@ -72,14 +71,16 @@ fun SimpleTextEditor(
                 content.value = it
             },
             visualTransformation = {
-                val t = codeString(content.value.text)
+                val start = System.currentTimeMillis()
+                val t = codeString(content.value.text, textColor)
+                println("convert ${t.length} time ${System.currentTimeMillis() - start} [ms]")
                 TransformedText(t, OffsetMapping.Identity)
             },
             decorationBox = {
                 Row {
                     Column(
                         modifier = Modifier
-                            .scrollable(verticalScrollState, Orientation.Vertical)
+                            .verticalScroll(lineNumberScrollState)
                             .padding(end = 8.dp)
                             .wrapContentSize(unbounded = true)
                     ) {
@@ -106,19 +107,9 @@ fun SimpleTextEditor(
                     it()
                 }
             },
-            onTextLayout = {
-                textLayoutResultState.value = it
-            },
             textStyle = TextStyle(fontSize = 16.sp, fontFamily = FontFamily.Monospace, lineHeight = 1.5.em),
-            modifier = modifier
-                .verticalScroll(verticalScrollState)
-                .horizontalScroll(horizontalScrollState)
-                .onPointerEvent(PointerEventType.Scroll, PointerEventPass.Main) {
-                    coroutineScope.launch {
-                        verticalScrollState.scrollBy(it.changes.first().scrollDelta.y)
-                    }
-                }
-                .bringIntoViewRequester(bringIntoViewRequester)
+            scrollState = verticalScrollState,
+            modifier = modifier.focusRequester(focusRequester)
         )
 
         VerticalScrollbar(adapter = rememberScrollbarAdapter(verticalScrollState), modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd))
@@ -126,18 +117,18 @@ fun SimpleTextEditor(
             Alignment.BottomCenter))
     }
 
-    LaunchedEffect(content.value.selection) {
-        if (content.value.text.isBlank()) {
-            return@LaunchedEffect
-        }
-        val rect = textLayoutResultState.value?.getBoundingBox(max(0, content.value.selection.end - 1))
-        bringIntoViewRequester.bringIntoView(rect)
+    LaunchedEffect(verticalScrollState.offset) {
+        lineNumberScrollState.scrollTo(verticalScrollState.offset.toInt())
+    }
+
+    LaunchedEffect(path) {
+        focusRequester.requestFocus()
     }
 }
 
-private fun codeString(str: String) = buildAnnotatedString {
+private fun codeString(str: String, textColor: Color) = buildAnnotatedString {
     val theme = EditorTheme.get()
-    withStyle(theme.code.simple) {
+    withStyle(SpanStyle(textColor)) {
         append(str)
         addStyle(theme.code.punctuation, str, ":")
         addStyle(theme.code.punctuation, str, "=")
@@ -149,8 +140,8 @@ private fun codeString(str: String) = buildAnnotatedString {
         addStyle(theme.code.punctuation, str, "(")
         addStyle(theme.code.punctuation, str, ")")
         addStyle(theme.code.punctuation, str, ",")
-        addStyle(theme.code.keyword, str, "fun ")
         addStyle(theme.code.keyword, str, "---")
+        addStyle(theme.code.keyword, str, "fun ")
         addStyle(theme.code.keyword, str, "val ")
         addStyle(theme.code.keyword, str, "var ")
         addStyle(theme.code.keyword, str, "private ")
