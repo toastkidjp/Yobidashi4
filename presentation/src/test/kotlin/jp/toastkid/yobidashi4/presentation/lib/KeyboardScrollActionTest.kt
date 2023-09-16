c26f2c030b83e86737c2229b9facc423b8a02e70
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-16 09:00:40 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/KeyboardScrollActionTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/KeyboardScrollActionTest.kt
@@ -2,13 +2,16 @@ package jp.toastkid.yobidashi4.presentation.lib
 
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.key.Key
 import io.mockk.MockKAnnotations
+import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.just
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import kotlinx.coroutines.CoroutineScope
@@ -26,12 +29,19 @@ class KeyboardScrollActionTest {
     @MockK
     private lateinit var state: ScrollableState
 
+    @MockK
+    private lateinit var lazyListState: LazyListState
+
     private lateinit var coroutineScope: CoroutineScope
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
         coroutineScope = CoroutineScope(Dispatchers.Unconfined)
+
+        coEvery { lazyListState.layoutInfo.totalItemsCount } returns 20
+        coEvery { lazyListState.scrollToItem(any()) } just Runs
+
         mockkStatic("androidx.compose.foundation.gestures.ScrollExtensionsKt")
         coEvery { state.animateScrollBy(any()) } returns 0f
     }
@@ -67,6 +77,16 @@ class KeyboardScrollActionTest {
         keyboardScrollAction.invoke(coroutineScope, Key.PageDown, false)
 
         coVerify(exactly = 1) { state.animateScrollBy(300f) }
+    }
+
+    @Test
+    fun toBottomForLazyListStateCase() {
+        keyboardScrollAction = KeyboardScrollAction(lazyListState)
+
+        keyboardScrollAction.invoke(coroutineScope, Key.DirectionDown, true)
+
+        coVerify(inverse = true) { state.animateScrollBy(any()) }
+        coVerify(exactly = 1) { lazyListState.scrollToItem(20) }
     }
 
     @Test
