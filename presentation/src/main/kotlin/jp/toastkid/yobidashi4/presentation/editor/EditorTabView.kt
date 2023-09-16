543c49232e8e83f0272e2ce2c4242a15fbdee863
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-17 00:08:39 +0900

    refactor Clean up code.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTabView.kt
@@ -1,18 +1,17 @@
 package jp.toastkid.yobidashi4.presentation.editor
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
 
 @Composable
 fun EditorTabView(tab: EditorTab) {
-    /*
-    EditorView(
-        Editor(tab.path)
-    )
-     */
-    SimpleTextEditor(
-        tab.path,
-        Modifier
-    )
+    Surface(color = MaterialTheme.colors.surface.copy(alpha = 0.5f)) {
+        SimpleTextEditor(
+            tab.path,
+            Modifier
+        )
+    }
 }
