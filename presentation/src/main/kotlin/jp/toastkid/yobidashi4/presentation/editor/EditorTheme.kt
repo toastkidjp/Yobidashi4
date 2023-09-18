2c6c87daf9384eaab5716e3a89e30d7cd2bba046
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-19 07:55:55 +0900

    refactor Move making code strings.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTheme.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTheme.kt
@@ -1,10 +1,58 @@
 package jp.toastkid.yobidashi4.presentation.editor
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import jp.toastkid.yobidashi4.presentation.editor.style.EditorStyle
 
 class EditorTheme {
+
+    private val patterns = listOf(
+        EditorStyle(
+            Regex("[0-9]*"),
+            SpanStyle(Color(0xFF6897BB)),
+            SpanStyle(Color(0xFF6897BB))
+        ),
+        EditorStyle(
+            Regex("\\n#.*"),
+            SpanStyle(Color(0xFF00DD00), fontWeight = FontWeight.Bold),
+            SpanStyle(Color(0xFF00DD00), fontWeight = FontWeight.Bold)
+        ),
+        EditorStyle(
+            Regex("\\n\\|.*"),
+            SpanStyle(Color(0xFF68BB97)),
+            SpanStyle(Color(0xFF68BB97))
+        ),
+        EditorStyle(
+            Regex("\\n>.*"),
+            SpanStyle(Color(0xFFCCAAFF)),
+            SpanStyle(Color(0xFFCCAAFF))
+        ),
+        EditorStyle(
+            Regex("\\n-.*"),
+            SpanStyle(Color(0xFFCCAAFF)),
+            SpanStyle(Color(0xFFCCAAFF))
+        )
+    )
+
+    fun codeString(str: String, textColor: Color, darkTheme: Boolean) = buildAnnotatedString {
+        withStyle(SpanStyle(textColor)) {
+            append(str)
+
+            patterns.forEach {
+                addStyle(if (darkTheme) it.darkStyle else it.lightStyle, str, it.regex)
+            }
+        }
+    }
+
+    private fun AnnotatedString.Builder.addStyle(style: SpanStyle, text: String, regexp: Regex) {
+        for (result in regexp.findAll(text)) {
+            addStyle(style, result.range.first, result.range.last + 1)
+        }
+    }
 
     class Code(
         val simple: SpanStyle = SpanStyle(Color(0xFF000B00)),
