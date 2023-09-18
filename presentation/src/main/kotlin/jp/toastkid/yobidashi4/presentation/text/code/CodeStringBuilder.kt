41973b6c28cfcf1bdf0c959fee12160539135ccb
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-19 07:52:59 +0900

    refactor Move theme styles.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/text/code/CodeStringBuilder.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/text/code/CodeStringBuilder.kt
@@ -1,47 +1,65 @@
 package jp.toastkid.yobidashi4.presentation.text.code
 
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
-import jp.toastkid.yobidashi4.presentation.editor.EditorTheme
 
 class CodeStringBuilder {
 
+    private val simple: SpanStyle = SpanStyle(Color(0xFF000B00))
+
+    private val header: SpanStyle = SpanStyle(Color(0xFF00DD00), fontWeight = FontWeight.Bold)
+
+    private val table: SpanStyle = SpanStyle(Color(0xFF68BB97))
+
+    private val quote: SpanStyle = SpanStyle(Color(0xFFCCAAFF))
+
+    private val value: SpanStyle = SpanStyle(Color(0xFF6897BB))
+
+    private val keyword: SpanStyle = SpanStyle(Color(0xFFCC7832))
+
+    private val punctuation: SpanStyle = SpanStyle(Color(0xFFA1C17E))
+
+    private val annotation: SpanStyle = SpanStyle(Color(0xFFBBB529))
+
+    private val comment: SpanStyle = SpanStyle(Color(0xFF808080))
+    
     operator fun invoke(str: String) = buildAnnotatedString {
-        val theme = EditorTheme.get()
-        withStyle(theme.code.simple) {
+        withStyle(simple) {
             append(str)
-            addStyle(theme.code.punctuation, str, ":")
-            addStyle(theme.code.punctuation, str, "=")
-            addStyle(theme.code.punctuation, str, "\"")
-            addStyle(theme.code.punctuation, str, "[")
-            addStyle(theme.code.punctuation, str, "]")
-            addStyle(theme.code.punctuation, str, "{")
-            addStyle(theme.code.punctuation, str, "}")
-            addStyle(theme.code.punctuation, str, "(")
-            addStyle(theme.code.punctuation, str, ")")
-            addStyle(theme.code.punctuation, str, ",")
-            addStyle(theme.code.keyword, str, "fun ")
-            addStyle(theme.code.keyword, str, "---")
-            addStyle(theme.code.keyword, str, "val ")
-            addStyle(theme.code.keyword, str, "var ")
-            addStyle(theme.code.keyword, str, "private ")
-            addStyle(theme.code.keyword, str, "internal ")
-            addStyle(theme.code.keyword, str, "for ")
-            addStyle(theme.code.keyword, str, "expect ")
-            addStyle(theme.code.keyword, str, "actual ")
-            addStyle(theme.code.keyword, str, "import ")
-            addStyle(theme.code.keyword, str, "package ")
-            addStyle(theme.code.value, str, "true")
-            addStyle(theme.code.value, str, "false")
-            addStyle(theme.code.value, str, Regex("[0-9]*"))
-            addStyle(theme.code.header, str, Regex("\\n#.*"))
-            addStyle(theme.code.table, str, Regex("\\n\\|.*"))
-            addStyle(theme.code.quote, str, Regex("\\n>.*"))
-            addStyle(theme.code.quote, str, Regex("\\n-.*"))
-            addStyle(theme.code.annotation, str, Regex("^@[a-zA-Z_]*"))
-            addStyle(theme.code.comment, str, Regex("^\\s*//.*"))
+            addStyle(punctuation, str, ":")
+            addStyle(punctuation, str, "=")
+            addStyle(punctuation, str, "\"")
+            addStyle(punctuation, str, "[")
+            addStyle(punctuation, str, "]")
+            addStyle(punctuation, str, "{")
+            addStyle(punctuation, str, "}")
+            addStyle(punctuation, str, "(")
+            addStyle(punctuation, str, ")")
+            addStyle(punctuation, str, ",")
+            addStyle(keyword, str, "fun ")
+            addStyle(keyword, str, "---")
+            addStyle(keyword, str, "val ")
+            addStyle(keyword, str, "var ")
+            addStyle(keyword, str, "private ")
+            addStyle(keyword, str, "internal ")
+            addStyle(keyword, str, "for ")
+            addStyle(keyword, str, "expect ")
+            addStyle(keyword, str, "actual ")
+            addStyle(keyword, str, "import ")
+            addStyle(keyword, str, "package ")
+            addStyle(value, str, "true")
+            addStyle(value, str, "false")
+            addStyle(value, str, Regex("[0-9]*"))
+            addStyle(header, str, Regex("\\n#.*"))
+            addStyle(table, str, Regex("\\n\\|.*"))
+            addStyle(quote, str, Regex("\\n>.*"))
+            addStyle(quote, str, Regex("\\n-.*"))
+            addStyle(annotation, str, Regex("^@[a-zA-Z_]*"))
+            addStyle(comment, str, Regex("^\\s*//.*"))
         }
     }
 
