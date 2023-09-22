e210eb8b7440a1c0225772c3a7a2e27be1e28492
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-23 08:44:10 +0900

    feature Add factory function.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/MarkdownPreviewTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/MarkdownPreviewTab.kt
@@ -1,8 +1,11 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
+import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.model.markdown.Markdown
+import jp.toastkid.yobidashi4.domain.service.markdown.MarkdownParser
 
 data class MarkdownPreviewTab(
+    private val path: Path,
     private val markdown: Markdown
 ) : Tab {
 
@@ -23,5 +26,15 @@ data class MarkdownPreviewTab(
     fun scrollPosition() = scrollPosition
 
     fun markdown() = markdown
+
+    fun slideshowSourcePath() = path
+
+    companion object {
+        fun with(path: Path) = MarkdownPreviewTab(
+            path,
+            MarkdownParser().invoke(path)
+        )
+
+    }
 
 }
