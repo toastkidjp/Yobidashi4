9fe91dffd1fd433c4d65c02654b0e5e232e7b3ee
Toast Kid <super_q1m@yahoo.co.jp>
2023-09-28 09:29:41 +0900

    feature Enable to scroll with keyboard.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabView.kt
@@ -12,8 +12,16 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.unit.dp
 import jp.toastkid.yobidashi4.domain.service.converter.DistanceConverterService
 import jp.toastkid.yobidashi4.domain.service.converter.JapaneseAgeConverterService
@@ -23,15 +31,23 @@ import jp.toastkid.yobidashi4.domain.service.converter.TemperatureConverterServi
 import jp.toastkid.yobidashi4.domain.service.converter.TsuboCountConverterService
 import jp.toastkid.yobidashi4.domain.service.converter.UnixTimeConverterService
 import jp.toastkid.yobidashi4.domain.service.converter.UrlEncodeConverterService
+import jp.toastkid.yobidashi4.presentation.lib.KeyboardScrollAction
 
 @Composable
 fun ConverterToolTabView() {
+    val state = rememberScrollState()
+    val keyboardScrollAction = KeyboardScrollAction(state)
+    val coroutineScope = rememberCoroutineScope()
+    val focusRequester = remember { FocusRequester() }
+
     Surface(
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
-        elevation = 4.dp
+        elevation = 4.dp,
+        modifier = Modifier
+            .onKeyEvent { keyboardScrollAction(coroutineScope, it.key, it.isCtrlPressed) }
+            .focusRequester(focusRequester)
     ) {
         Box {
-            val state = rememberScrollState()
             Column(modifier = Modifier.padding(8.dp).verticalScroll(state).fillMaxWidth()) {
                 TwoValueConverterBox(UnixTimeConverterService())
                 TwoValueConverterBox(UrlEncodeConverterService())
@@ -47,6 +63,10 @@ fun ConverterToolTabView() {
                 adapter = rememberScrollbarAdapter(state),
                 modifier = Modifier.fillMaxHeight().align(Alignment.CenterEnd)
             )
+
+            LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+            }
         }
     }
 }
