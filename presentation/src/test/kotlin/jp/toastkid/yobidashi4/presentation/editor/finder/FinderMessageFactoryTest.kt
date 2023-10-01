8372f1a2e56c87d161d60f05e4036a42701dc1bd
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-02 07:45:12 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FinderMessageFactoryTest.kt
@@ -0,0 +1,26 @@
+package jp.toastkid.yobidashi4.presentation.editor.finder
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FinderMessageFactoryTest {
+
+    private lateinit var finderMessageFactory: FinderMessageFactory
+
+    @BeforeEach
+    fun setUp() {
+        finderMessageFactory = FinderMessageFactory()
+    }
+
+    @Test
+    fun invoke() {
+        assertTrue(finderMessageFactory.invoke("", 2).isEmpty())
+        assertEquals("\"test\" was not found.", finderMessageFactory.invoke("test", -1))
+        assertEquals("\"test\" was not found.", finderMessageFactory.invoke("test", 0))
+        assertEquals("\"test\" was found. 1", finderMessageFactory.invoke("test", 1))
+        assertEquals("\"test\" was found. 2", finderMessageFactory.invoke("test", 2))
+    }
+
+}
