5ec26e691453da4261e670bf7c2c1321e8ac9be4
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-08 07:34:32 +0900

    feature Modify table conversion.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/TableFormConverter.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/TableFormConverter.kt
@@ -2,7 +2,10 @@ package jp.toastkid.yobidashi4.presentation.editor.markdown.text
 
 class TableFormConverter {
 
-    operator fun invoke(text: String) =
-            "| ${text.trim().replace(" ", " | ").replace("\n", "\n| ")}\n"
+    operator fun invoke(text: String): String {
+        val putEndLineBreak = if (text.endsWith("\n")) "\n" else ""
+
+        return "| ${text.trim().replace(" ", " | ").replace("\n", "\n| ")}$putEndLineBreak"
+    }
 
 }
