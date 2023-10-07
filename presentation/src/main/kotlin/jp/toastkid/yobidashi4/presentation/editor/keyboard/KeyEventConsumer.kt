4ebb6bc4d72ee129814c40518dd108c4095d3042
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-08 07:30:05 +0900

    bugfix Enable to calculate if target contains line break.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumer.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumer.kt
@@ -18,13 +18,13 @@ import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.service.tool.calculator.SimpleCalculator
-import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.BlockQuotation
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.CommaInserter
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.LinkDecoratorService
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.ListHeadAdder
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.NumberedListHeadAdder
 import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardFetcher
+import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlin.math.max
 import kotlin.math.min
@@ -285,7 +285,8 @@ class KeyEventConsumer(
             }
             it.isCtrlPressed && it.isShiftPressed && it.key == Key.C -> {
                 convertSelectedText(content, selectionStartIndex, selectionEndIndex) {
-                    SimpleCalculator().invoke(it)?.toString()
+                    val appendLineBreakIfNeed = if (it.endsWith("\n")) "\n" else ""
+                    "${SimpleCalculator().invoke(it.trimEnd())?.toString()}$appendLineBreakIfNeed"
                 }?.let(setNewContent)
                 true
             }
