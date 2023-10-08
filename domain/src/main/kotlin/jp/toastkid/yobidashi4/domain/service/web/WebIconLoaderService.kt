e167ac6baef792074caf8e43b5fbdd8d426c3477
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-09 06:18:14 +0900

    refactor Delete useless interface.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/WebIconLoaderService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/web/WebIconLoaderService.kt
@@ -1,12 +1,6 @@
 package jp.toastkid.yobidashi4.domain.service.web
 
-import java.nio.file.Path
-
 interface WebIconLoaderService {
     operator fun invoke(htmlSource: String, browserUrl: String?)
-    fun download(
-        iconUrl: String,
-        faviconFolder: Path,
-        targetHost: String?
-    )
+
 }
