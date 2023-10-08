2f6d9da5556bf330affb2da8022e31b837764611
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-09 06:18:53 +0900

    refactor Divide to other class.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/icon/WebIconDownloader.kt
@@ -0,0 +1,33 @@
+package jp.toastkid.yobidashi4.infrastructure.service.web.icon
+
+import java.io.BufferedInputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Path
+
+class WebIconDownloader {
+
+    operator fun invoke(
+        iconUrl: URL,
+        faviconFolder: Path,
+        targetHost: String?
+    ) {
+        val fileExtension = iconUrl.path.split(".").lastOrNull() ?: "png"
+
+        val iconPath = faviconFolder.resolve("$targetHost.$fileExtension")
+        if (Files.exists(iconPath)) {
+            return
+        }
+        val urlConnection = urlConnection(iconUrl) ?: return
+        if (urlConnection.responseCode != 200) {
+            return
+        }
+        BufferedInputStream(urlConnection.inputStream).use {
+            Files.write(iconPath, it.readAllBytes())
+        }
+    }
+
+    fun urlConnection(iconUrl: URL) = iconUrl.openConnection() as? HttpURLConnection
+
+}
