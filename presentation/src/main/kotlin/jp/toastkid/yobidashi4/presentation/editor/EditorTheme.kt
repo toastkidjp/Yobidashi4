ded1e7d5762c00a6e63bd1b80083621f2cbbd0b9
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-14 01:13:41 +0900

    refactor Modify building AnnotatedString.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTheme.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTheme.kt
@@ -3,7 +3,6 @@ package jp.toastkid.yobidashi4.presentation.editor
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import jp.toastkid.yobidashi4.presentation.editor.style.EditorStyle
 
@@ -42,13 +41,13 @@ class EditorTheme {
         )
     )
 
-    fun codeString(str: String, darkTheme: Boolean) = buildAnnotatedString {
-        append(str)
-
+    fun codeString(str: AnnotatedString, darkTheme: Boolean) = AnnotatedString.Builder(str).apply {
         patterns.forEach {
-            addStyle(if (darkTheme) it.darkStyle else it.lightStyle, str, it.regex)
+            for (result in it.regex.findAll(str)) {
+                addStyle(if (darkTheme) it.darkStyle else it.lightStyle, result.range.first, result.range.last + 1)
+            }
         }
-    }
+    }.toAnnotatedString()
 
     private fun AnnotatedString.Builder.addStyle(style: SpanStyle, text: String, regexp: Regex) {
         for (result in regexp.findAll(text)) {
