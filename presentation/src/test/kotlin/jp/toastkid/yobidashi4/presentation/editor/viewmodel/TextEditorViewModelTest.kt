2842f58caad417c1960635ddbdc9eda19e64c649
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-16 08:35:07 +0900

    refactor Add disposing.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/viewmodel/TextEditorViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/viewmodel/TextEditorViewModelTest.kt
@@ -3,12 +3,14 @@ package jp.toastkid.yobidashi4.presentation.editor.viewmodel
 import androidx.compose.ui.text.input.VisualTransformation
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -35,6 +37,8 @@ class TextEditorViewModelTest {
 
     @AfterEach
     fun tearDown() {
+        unmockkAll()
+        stopKoin()
     }
 
     @Test
