ff3ae4d4ec9afdb6e3ead17b3a415afca481914f
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-20 09:18:52 +0900

    refactor Divide factory to other class.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/menu/TextContextMenuFactory.kt
@@ -0,0 +1,60 @@
+package jp.toastkid.yobidashi4.presentation.main.menu
+
+import androidx.compose.foundation.ContextMenuArea
+import androidx.compose.foundation.ContextMenuItem
+import androidx.compose.foundation.ContextMenuState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.text.TextContextMenu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLocalization
+import jp.toastkid.yobidashi4.domain.service.text.TextCountMessageFactory
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class TextContextMenuFactory {
+
+    @OptIn(ExperimentalFoundationApi::class)
+    operator fun invoke(): TextContextMenu {
+        val mainViewModel = object : KoinComponent { val viewModel: MainViewModel by inject() }.viewModel
+
+        return object : TextContextMenu {
+            @Composable
+            override fun Area(
+                textManager: TextContextMenu.TextManager,
+                state: ContextMenuState,
+                content: @Composable () -> Unit
+            ) {
+                val localization = LocalLocalization.current
+                mainViewModel.setTextManager(textManager)
+                val items = {
+                    listOfNotNull(
+                        textManager.cut?.let {
+                            ContextMenuItem(localization.cut, it)
+                        },
+                        textManager.copy?.let {
+                            ContextMenuItem(localization.copy, it)
+                        },
+                        textManager.paste?.let {
+                            ContextMenuItem(localization.paste, it)
+                        },
+                        textManager.selectAll?.let {
+                            ContextMenuItem(localization.selectAll, it)
+                        },
+                        ContextMenuItem("Search") {
+                            mainViewModel
+                                .openUrl("https://search.yahoo.co.jp/search?p=${textManager.selectedText.text}", false)
+                        },
+                        ContextMenuItem("Count") {
+                            mainViewModel
+                                .showSnackbar(TextCountMessageFactory().invoke(textManager.selectedText.text))
+                        }
+                    )
+                }
+
+                ContextMenuArea(items, state, content = content)
+            }
+        }
+    }
+
+}
