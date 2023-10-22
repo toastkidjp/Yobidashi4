a549bcbbe17eb543edb3999389fde486d30a2237
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-23 07:54:34 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/ListHeadAdderTest.kt
@@ -0,0 +1,20 @@
+package jp.toastkid.yobidashi4.presentation.editor.markdown.text
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ListHeadAdderTest {
+
+    private val target = """1st line
+2nd line""".trimIndent()
+
+    private val expected = """- 1st line
+- 2nd line
+""".trimIndent()
+
+    @Test
+    fun test() {
+        assertEquals(expected, ListHeadAdder().invoke(target, "-"))
+    }
+
+}
