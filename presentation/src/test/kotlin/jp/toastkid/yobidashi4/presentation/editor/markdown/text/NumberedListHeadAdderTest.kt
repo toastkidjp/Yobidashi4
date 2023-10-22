38cd7e6b7711c730dce5055e059cc3c380bf93f6
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-23 07:54:47 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/NumberedListHeadAdderTest.kt
@@ -0,0 +1,25 @@
+package jp.toastkid.yobidashi4.presentation.editor.markdown.text
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+class NumberedListHeadAdderTest {
+
+    private val target = """1st line
+2nd line""".trimIndent()
+
+    private val expected = """1. 1st line
+2. 2nd line
+""".trimIndent()
+
+    @Test
+    fun test() {
+        assertEquals(expected, NumberedListHeadAdder().invoke(target))
+    }
+
+    @Test
+    fun testNullCase() {
+        assertNull(NumberedListHeadAdder().invoke(null))
+    }
+}
