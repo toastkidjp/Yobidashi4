6aa8bca6d4a2f637d7d3b75b3cb4ae5d5ff54506
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-26 08:40:52 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/barcode/BarcodeToolTabViewModelTest.kt
@@ -0,0 +1,107 @@
+package jp.toastkid.yobidashi4.presentation.viewmodel.barcode
+
+import androidx.compose.ui.text.input.TextFieldValue
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeDecoder
+import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeEncoder
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class BarcodeToolTabViewModelTest {
+
+    private lateinit var barcodeToolTabViewModel: BarcodeToolTabViewModel
+
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @MockK
+    private lateinit var barcodeEncoder: BarcodeEncoder
+
+    @MockK
+    private lateinit var barcodeDecoder: BarcodeDecoder
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
+                    single(qualifier=null) { barcodeEncoder } bind(BarcodeEncoder::class)
+                    single(qualifier=null) { barcodeDecoder } bind(BarcodeDecoder::class)
+                }
+            )
+        }
+
+        every { mainViewModel.showSnackbar(any()) } just Runs
+        every { mainViewModel.openUrl(any(), any()) } just Runs
+        every { barcodeEncoder.invoke(any(), any(), any()) } returns mockk()
+
+        barcodeToolTabViewModel = BarcodeToolTabViewModel()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+        stopKoin()
+    }
+
+    @Test
+    fun barcodeImage() {
+        assertNull(barcodeToolTabViewModel.barcodeImage())
+    }
+
+    @Test
+    fun setEncodeInputValue() {
+        assertTrue(barcodeToolTabViewModel.encodeInputValue().text.isEmpty())
+
+        barcodeToolTabViewModel.setEncodeInputValue(TextFieldValue("test"))
+
+        assertEquals("test", barcodeToolTabViewModel.encodeInputValue().text)
+        verify { barcodeEncoder.invoke(any(), any(), any()) }
+    }
+
+    @Test
+    fun setDecodeInputValue() {
+        assertTrue(barcodeToolTabViewModel.decodeInputValue().text.isEmpty())
+
+        barcodeToolTabViewModel.setDecodeInputValue(TextFieldValue("test"))
+
+        assertEquals("test", barcodeToolTabViewModel.decodeInputValue().text)
+    }
+
+    @Test
+    fun decodeResult() {
+        assertTrue(barcodeToolTabViewModel.decodeResult().isEmpty())
+    }
+
+    @Test
+    fun onClickDecodeResult() {
+        barcodeToolTabViewModel.onClickDecodeResult()
+
+        verify { mainViewModel.openUrl(any(), any()) }
+    }
+
+    @Test
+    fun onClickImage() {
+        barcodeToolTabViewModel.onClickImage()
+
+        verify(inverse = true) { mainViewModel.showSnackbar(any()) }
+    }
+}
