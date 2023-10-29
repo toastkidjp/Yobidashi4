c308c34e02a0b5edd2898461dffeded0c84ff99f
Toast Kid <super_q1m@yahoo.co.jp>
2023-10-29 11:40:40 +0900

    refactor Make composable function.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
@@ -2,7 +2,6 @@ package jp.toastkid.yobidashi4.presentation.web
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import javax.swing.JDialog
 import javax.swing.WindowConstants
 import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
@@ -16,51 +15,44 @@ import org.koin.core.component.inject
 
 @Composable
 fun WebTabView(tab: WebTab) {
-    val viewModel = remember {
-        object : KoinComponent {
-            val webTabViewModel: WebTabViewModel by inject()
-        }.webTabViewModel
-    }
-
-    val webViewProvider = remember { WebViewProvider() }
-
     if (tab.isReadableUrl().not()) {
         return
     }
 
-    webViewProvider.view(tab.id(), tab.url())
+    WebView(tab.id(), tab.url())
 
     LaunchedEffect(Unit) {
-        receiveEvent(viewModel)
+        receiveEvent()
     }
 }
 
-private suspend fun receiveEvent(
-    viewModel: WebTabViewModel
-) {
+private suspend fun receiveEvent() {
     val webViewPool = object : KoinComponent { val pool: WebViewPool by inject() }.pool
 
-    viewModel.event().collect {
-        when (it) {
-            is FindEvent -> {
-                if (it.upward) {
-                    webViewPool.find(it.id, it.text, false)
-                } else {
-                    webViewPool.find(it.id, it.text, true)
+    object : KoinComponent { val webTabViewModel: WebTabViewModel by inject() }
+        .webTabViewModel
+        .event()
+        .collect {
+            when (it) {
+                is FindEvent -> {
+                    if (it.upward) {
+                        webViewPool.find(it.id, it.text, false)
+                    } else {
+                        webViewPool.find(it.id, it.text, true)
+                    }
+                }
+
+                is ReloadEvent -> {
+                    webViewPool.reload(it.id)
+                }
+
+                is SwitchDeveloperToolEvent -> {
+                    val devToolsDialog = JDialog()
+                    devToolsDialog.defaultCloseOperation = WindowConstants.HIDE_ON_CLOSE
+                    devToolsDialog.setSize(800, 600)
+                    devToolsDialog.add(webViewPool.devTools(it.id))
+                    devToolsDialog.isVisible = true
                 }
             }
-
-            is ReloadEvent -> {
-                webViewPool.reload(it.id)
-            }
-
-            is SwitchDeveloperToolEvent -> {
-                val devToolsDialog = JDialog()
-                devToolsDialog.defaultCloseOperation = WindowConstants.HIDE_ON_CLOSE
-                devToolsDialog.setSize(800, 600)
-                devToolsDialog.add(webViewPool.devTools(it.id))
-                devToolsDialog.isVisible = true
-            }
         }
-    }
 }
