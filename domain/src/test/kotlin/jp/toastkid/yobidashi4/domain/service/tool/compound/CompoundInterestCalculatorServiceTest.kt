9d5b1bd24eaaca232a8743f0332861eb6ac60262
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-02 09:56:18 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorServiceTest.kt
@@ -1,6 +1,8 @@
 package jp.toastkid.yobidashi4.domain.service.tool.compound
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -17,6 +19,12 @@ internal class CompoundInterestCalculatorServiceTest {
     @Test
     fun test() {
         val result = compoundInterestCalculatorService.invoke(120000, 0.01, 10)
+        assertEquals(3, result.header().size)
+        assertEquals(10, result.itemArrays().size)
+        assertEquals(Int::class.java, result.columnClass(1))
+        assertNotNull(result.title())
+        assertFalse(result.isEmpty())
+
         val tenYearsLater = result.get(10) ?: fail("This case does not allow null.")
         assertEquals(1212000, tenYearsLater.second)
         assertEquals(1255466, tenYearsLater.third)
