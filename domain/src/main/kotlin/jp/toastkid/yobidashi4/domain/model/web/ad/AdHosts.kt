f67f7735717e1e6de7c7a17bbd5b3b2687a8de14
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-07 09:15:01 +0900

    refactor Modify implementation.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/web/ad/AdHosts.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/web/ad/AdHosts.kt
@@ -4,17 +4,23 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.stream.Collectors
 
-class AdHosts(
-    private val adHosts: Set<String> =
-        AdHosts::class.java.classLoader.getResourceAsStream("web/ad_hosts.txt")?.use { stream ->
-            return@use BufferedReader(InputStreamReader(stream)).use { reader ->
-                reader.lines().collect(Collectors.toUnmodifiableSet())
-            }
-        } ?: emptySet()
-) {
+class AdHosts(private val adHosts: Set<String>) {
 
     fun contains(url: String?): Boolean {
         return adHosts.any { url?.contains(it) == true }
+    }
+
+    companion object {
+
+        fun make() =
+            AdHosts(
+                AdHosts::class.java.classLoader.getResourceAsStream("web/ad_hosts.txt")?.use { stream ->
+                    return@use BufferedReader(InputStreamReader(stream)).use { reader ->
+                        reader.lines().collect(Collectors.toUnmodifiableSet())
+                    }
+                } ?: emptySet()
+            )
+
     }
 
 }
