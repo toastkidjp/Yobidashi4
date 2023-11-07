df110094974895f5971747724842e08812044117
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-08 08:41:40 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/tab/EditorSettingTabTest.kt
@@ -0,0 +1,13 @@
+package jp.toastkid.yobidashi4.domain.model.tab
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+class EditorSettingTabTest {
+
+    @Test
+    fun test() {
+        assertNotNull(EditorSettingTab().title())
+    }
+
+}
