4174f6215edb318c947e36b4e834f6b1f87f9e5f
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-13 09:39:47 +0900

    refactor Add unit test.

---
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefKeyboardShortcutProcessorTest.kt
@@ -0,0 +1,157 @@
+package jp.toastkid.yobidashi4.infrastructure.service.web
+
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import java.awt.event.KeyEvent
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import jp.toastkid.yobidashi4.presentation.viewmodel.web.WebTabViewModel
+import org.cef.browser.CefBrowser
+import org.cef.handler.CefKeyboardHandler
+import org.cef.misc.EventFlags
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class CefKeyboardShortcutProcessorTest {
+
+    @InjectMockKs
+    private lateinit var subject: CefKeyboardShortcutProcessor
+
+    @MockK
+    private lateinit var selectedText: () -> String
+
+    @MockK
+    private lateinit var viewModel: MainViewModel
+
+    @MockK
+    private lateinit var webTabViewModel: WebTabViewModel
+
+    @MockK
+    private lateinit var browser: CefBrowser
+
+    @BeforeEach
+    fun setUp() {
+        startKoin {
+            modules(
+                module {
+                    single(qualifier = null) { viewModel } bind(MainViewModel::class)
+                    single(qualifier = null) { webTabViewModel } bind(WebTabViewModel::class)
+                }
+            )
+        }
+        MockKAnnotations.init(this)
+
+        every { viewModel.openUrl(any(), any()) } just Runs
+        every { viewModel.webSearch(any(), any()) } just Runs
+        every { viewModel.browseUri(any()) } just Runs
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @Test
+    fun notConsumeOnDown() {
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYDOWN, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_F)
+
+        assertFalse(consumed)
+    }
+
+    @Test
+    fun elseCase() {
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_CIRCUMFLEX)
+
+        assertFalse(consumed)
+    }
+
+    @Test
+    fun find() {
+        every { webTabViewModel.switchFind() } just Runs
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_F)
+
+        assertTrue(consumed)
+        verify { webTabViewModel.switchFind() }
+    }
+
+    @Test
+    fun closeCurrent() {
+        every { viewModel.closeCurrent() } just Runs
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_W)
+
+        assertTrue(consumed)
+        verify { viewModel.closeCurrent() }
+    }
+
+    @Test
+    fun printToPDF() {
+        every { browser.identifier } returns 2
+        every { browser.printToPDF(any(), any(), any()) } just Runs
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_P)
+
+        assertTrue(consumed)
+        verify { browser.identifier }
+        verify { browser.printToPDF(any(), any(), any()) }
+    }
+
+    @Test
+    fun pageUp() {
+        every { browser.executeJavaScript(any(), any(), any()) } just Runs
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_UP)
+
+        assertTrue(consumed)
+        verify { browser.executeJavaScript(any(), any(), any()) }
+    }
+
+    @Test
+    fun pageDown() {
+        every { browser.executeJavaScript(any(), any(), any()) } just Runs
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_DOWN)
+
+        assertTrue(consumed)
+        verify { browser.executeJavaScript(any(), any(), any()) }
+    }
+
+    @Test
+    fun bookmark() {
+        mockkConstructor(BookmarkInsertion::class)
+        every { anyConstructed<BookmarkInsertion>().invoke(any(), any()) } just Runs
+        every { browser.url } returns "https://www.yahoo.co.jp"
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_B)
+
+        assertTrue(consumed)
+        verify { browser.url }
+        verify { anyConstructed<BookmarkInsertion>().invoke(any(), any()) }
+    }
+
+    @Test
+    fun reload() {
+        every { browser.reload() } just Runs
+
+        val consumed = subject.invoke(browser, CefKeyboardHandler.CefKeyEvent.EventType.KEYEVENT_KEYUP, EventFlags.EVENTFLAG_CONTROL_DOWN, KeyEvent.VK_F5)
+
+        assertTrue(consumed)
+        verify { browser.reload() }
+    }
+
+}
