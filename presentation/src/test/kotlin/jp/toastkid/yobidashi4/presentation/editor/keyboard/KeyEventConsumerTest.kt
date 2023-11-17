729f7390ff2b1bca6545cc34bf014ecfbc83e7e4
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-17 09:12:17 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumerTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumerTest.kt
@@ -509,6 +509,27 @@ class KeyEventConsumerTest {
     }
 
     @Test
+    fun surroundBraces() {
+        awtKeyEvent = java.awt.event.KeyEvent(
+            mockk(),
+            java.awt.event.KeyEvent.KEY_PRESSED,
+            1,
+            java.awt.event.KeyEvent.CTRL_DOWN_MASK,
+            java.awt.event.KeyEvent.VK_CLOSE_BRACKET,
+            '}'
+        )
+
+        val consumed = subject.invoke(
+            KeyEvent(awtKeyEvent),
+            TextFieldValue("test", TextRange(0)),
+            mockk(),
+            { assertEquals("{test}", it.getSelectedText().text) }
+        )
+
+        assertTrue(consumed)
+    }
+
+    @Test
     fun combineLines() {
         awtKeyEvent = java.awt.event.KeyEvent(
             mockk(),
