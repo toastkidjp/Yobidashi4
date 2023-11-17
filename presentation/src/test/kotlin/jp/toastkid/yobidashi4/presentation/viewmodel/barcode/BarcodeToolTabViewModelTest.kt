f88d7c726bd528e9ee6cc64d76dd767602b8acd6
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-18 08:15:28 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/barcode/BarcodeToolTabViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/barcode/BarcodeToolTabViewModelTest.kt
@@ -7,10 +7,13 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
+import java.awt.Image
 import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeDecoder
 import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeEncoder
+import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -104,4 +107,17 @@ class BarcodeToolTabViewModelTest {
 
         verify(inverse = true) { mainViewModel.showSnackbar(any()) }
     }
+
+    @Test
+    fun onClickImageOnSetImage() {
+        mockkConstructor(ClipboardPutterService::class)
+        every { anyConstructed<ClipboardPutterService>().invoke(any<Image>()) } just Runs
+        barcodeToolTabViewModel.setEncodeInputValue(TextFieldValue("https://www.yahoo.co.jp"))
+
+        barcodeToolTabViewModel.onClickImage()
+
+        verify { mainViewModel.showSnackbar(any()) }
+        verify { anyConstructed<ClipboardPutterService>().invoke(any<Image>())  }
+    }
+
 }
