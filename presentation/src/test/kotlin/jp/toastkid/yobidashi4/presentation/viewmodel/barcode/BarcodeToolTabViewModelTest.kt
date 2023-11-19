d0cf2d182a51320b8da88036065a1e4be189f726
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-19 09:20:31 +0900

    refactor Add verification wasNot called.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/barcode/BarcodeToolTabViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/barcode/BarcodeToolTabViewModelTest.kt
@@ -3,6 +3,7 @@ package jp.toastkid.yobidashi4.presentation.viewmodel.barcode
 import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
+import io.mockk.called
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
@@ -93,6 +94,7 @@ class BarcodeToolTabViewModelTest {
         barcodeToolTabViewModel.setDecodeInputValue(TextFieldValue("test"))
 
         assertEquals("test", barcodeToolTabViewModel.decodeInputValue().text)
+        verify { barcodeDecoder wasNot called }
     }
 
     @Test
