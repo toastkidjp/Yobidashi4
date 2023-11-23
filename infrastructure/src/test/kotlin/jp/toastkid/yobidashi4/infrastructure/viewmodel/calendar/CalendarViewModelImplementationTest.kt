3d71ea7fc2481dbffed7b3b576421a90dd19bdc0
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-24 07:55:21 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/calendar/CalendarViewModelImplementationTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/calendar/CalendarViewModelImplementationTest.kt
@@ -1,7 +1,18 @@
 package jp.toastkid.yobidashi4.infrastructure.viewmodel.calendar
 
 import androidx.compose.ui.text.input.TextFieldValue
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import java.nio.file.Path
 import java.time.LocalDate
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -10,18 +21,44 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 class CalendarViewModelImplementationTest {
 
     private lateinit var viewModel: CalendarViewModelImplementation
 
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @MockK
+    private lateinit var setting: Setting
+
     @BeforeEach
     fun setUp() {
+        MockKAnnotations.init(this)
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
+                    single(qualifier=null) { setting } bind(Setting::class)
+                }
+            )
+        }
+        every { mainViewModel.edit(any(), any()) } just Runs
+        val path = mockk<Path>()
+        every { setting.articleFolderPath() } returns path
+        every { path.resolve(any<String>()) } returns mockk()
+
         viewModel = CalendarViewModelImplementation()
     }
 
     @AfterEach
     fun tearDown() {
+        stopKoin()
+        unmockkAll()
     }
 
     @Test
@@ -120,6 +157,24 @@ class CalendarViewModelImplementationTest {
                 viewModel.moveToCurrentMonth()
                 viewModel.setYear(2020)
                 assertFalse(viewModel.isToday(LocalDate.now().dayOfMonth))
+            }
+        )
+    }
+
+    @Test
+    fun openDateArticle() {
+        assertAll(
+            {
+                viewModel.openDateArticle(20, true)
+
+                verify { setting.articleFolderPath() }
+                verify { mainViewModel.edit(any(), true) }
+            },
+            {
+                viewModel.openDateArticle(20, false)
+
+                verify { setting.articleFolderPath() }
+                verify { mainViewModel.edit(any(), false) }
             }
         )
     }
