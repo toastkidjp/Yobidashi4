997adbeda32c10a831870881cf2b7d2fa92e8989
Toast Kid <super_q1m@yahoo.co.jp>
2023-11-27 08:20:50 +0900

    refactor Divide to viewModel.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/compound/viewmodel/CompoundInterestCalculatorViewModel.kt
@@ -0,0 +1,65 @@
+package jp.toastkid.yobidashi4.presentation.compound.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.text.input.TextFieldValue
+import jp.toastkid.yobidashi4.domain.model.aggregation.CompoundInterestCalculationResult
+import jp.toastkid.yobidashi4.domain.service.tool.compound.CompoundInterestCalculatorInput
+import jp.toastkid.yobidashi4.domain.service.tool.compound.CompoundInterestCalculatorService
+
+class CompoundInterestCalculatorViewModel {
+
+    private val calculator = CompoundInterestCalculatorService()
+
+    private val capitalInput = mutableStateOf(TextFieldValue("0"))
+
+    fun capitalInput() = capitalInput.value
+
+    fun setCapitalInput(value: TextFieldValue) {
+        capitalInput.value = value
+
+        CompoundInterestCalculatorInput.from(capitalInput.value.text,  installmentInput.value.text, annualInterestInput.value.text, yearInput.value.text)?.let {
+            result.value = calculator.invoke(it)
+        }
+    }
+
+    private val installmentInput = mutableStateOf(TextFieldValue("40000"))
+
+    fun installmentInput() = installmentInput.value
+
+    fun setInstallmentInput(value: TextFieldValue) {
+        installmentInput.value = value
+
+        CompoundInterestCalculatorInput.from(capitalInput.value.text,  installmentInput.value.text, annualInterestInput.value.text, yearInput.value.text)?.let {
+            result.value = calculator.invoke(it)
+        }
+    }
+
+    private val annualInterestInput = mutableStateOf(TextFieldValue("0.03"))
+
+    fun annualInterestInput() = annualInterestInput.value
+
+    fun setAnnualInterestInput(value: TextFieldValue) {
+        annualInterestInput.value = value
+
+        CompoundInterestCalculatorInput.from(capitalInput.value.text,  installmentInput.value.text, annualInterestInput.value.text, yearInput.value.text)?.let {
+            result.value = calculator.invoke(it)
+        }
+    }
+
+    private val yearInput = mutableStateOf(TextFieldValue("20"))
+
+    fun yearInput() = yearInput.value
+
+    fun setYearInput(value: TextFieldValue) {
+        yearInput.value = value
+
+        CompoundInterestCalculatorInput.from(capitalInput.value.text,  installmentInput.value.text, annualInterestInput.value.text, yearInput.value.text)?.let {
+            result.value = calculator.invoke(it)
+        }
+    }
+
+    private val result = mutableStateOf(CompoundInterestCalculationResult())
+
+    fun result() = result.value
+
+}
