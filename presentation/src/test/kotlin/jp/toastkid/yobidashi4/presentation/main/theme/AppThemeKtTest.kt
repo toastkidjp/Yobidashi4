f7a959b28a80a064d1aa5206e1ecb33b90588d04
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-04 08:38:51 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/theme/AppThemeKtTest.kt
@@ -0,0 +1,21 @@
+package jp.toastkid.yobidashi4.presentation.main.theme
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runDesktopComposeUiTest
+import org.junit.jupiter.api.Test
+
+class AppThemeKtTest {
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun appTheme() {
+        runDesktopComposeUiTest {
+            setContent {
+                AppTheme(darkTheme = false) {
+
+                }
+            }
+        }
+    }
+
+}
