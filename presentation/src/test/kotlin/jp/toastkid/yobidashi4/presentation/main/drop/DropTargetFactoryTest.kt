66d0e47f74f8c7a28357e436faefd359be1f0ed8
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-03 09:01:52 +0900

    refactor Modify unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/drop/DropTargetFactoryTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/drop/DropTargetFactoryTest.kt
@@ -2,13 +2,20 @@ package jp.toastkid.yobidashi4.presentation.main.drop
 
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
+import io.mockk.called
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
 import io.mockk.just
+import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
+import java.awt.datatransfer.Transferable
 import java.awt.dnd.DropTarget
+import java.awt.dnd.DropTargetDropEvent
+import java.io.File
+import java.nio.file.Path
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -18,12 +25,33 @@ class DropTargetFactoryTest {
     @InjectMockKs
     private lateinit var dropTargetFactory: DropTargetFactory
 
+    @MockK
+    private lateinit var event: DropTargetDropEvent
+
+    @MockK
+    private lateinit var transferable: Transferable
+
+    @MockK
+    private lateinit var consumer: (List<Path>) -> Unit
+
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
 
         mockkConstructor(DropTarget::class)
         every { anyConstructed<DropTarget>().addDropTargetListener(any()) }.just(Runs)
+
+        every { event.acceptDrop(any()) } just Runs
+        every { event.dropComplete(any()) } just Runs
+        every { event.isDataFlavorSupported(any()) } returns true
+        every { event.rejectDrop() } just Runs
+        every { event.transferable } returns transferable
+        val file = mockk<File>()
+        every { transferable.getTransferData(any()) } returns listOf(file)
+        val path = mockk<Path>()
+        every { file.toPath() } returns path
+        every { path.fileName.toString() } returns "test"
+        every { consumer.invoke(any()) } just Runs
     }
 
     @AfterEach
@@ -33,8 +61,11 @@ class DropTargetFactoryTest {
 
     @Test
     fun invoke() {
-        dropTargetFactory.invoke {  }
+        val target = dropTargetFactory.invoke(consumer)
+        target.drop(event)
 
         verify { anyConstructed<DropTarget>().addDropTargetListener(any()) }
+        verify { consumer wasNot called }
     }
+
 }
