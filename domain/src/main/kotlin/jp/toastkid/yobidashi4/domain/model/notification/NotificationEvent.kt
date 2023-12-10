e7edd37f91a616c5b17a8e296c5b8fb9b8c25bf2
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-11 08:34:30 +0900

    feature Add new functions.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/notification/NotificationEvent.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/notification/NotificationEvent.kt
@@ -1,9 +1,29 @@
 package jp.toastkid.yobidashi4.domain.model.notification
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 data class NotificationEvent(
     val title: String,
     val text: String,
     val date: LocalDateTime
-)
+) {
+
+    fun dateTimeString(): String = date.format(DATE_TIME_FORMATTER)
+
+    fun toTsv() = "${title}\t${text}\t${dateTimeString()}"
+
+    companion object {
+        fun parse(text: String): LocalDateTime? {
+            return try {
+                LocalDateTime.from(DATE_TIME_FORMATTER.parse(text))
+            } catch (e: DateTimeParseException) {
+                null
+            }
+        }
+    }
+
+}
+
+private val DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
