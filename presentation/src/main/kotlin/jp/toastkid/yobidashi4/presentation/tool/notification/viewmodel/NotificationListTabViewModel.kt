d662b00caffd5e0cb63c7d000dd4752548dbfddf
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-13 08:43:00 +0900

    refactor Extract states to viewModel.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/notification/viewmodel/NotificationListTabViewModel.kt
@@ -0,0 +1,68 @@
+package jp.toastkid.yobidashi4.presentation.tool.notification.viewmodel
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
+import jp.toastkid.yobidashi4.domain.model.notification.NotificationEvent
+import jp.toastkid.yobidashi4.domain.repository.notification.NotificationEventRepository
+import jp.toastkid.yobidashi4.presentation.lib.KeyboardScrollAction
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class NotificationListTabViewModel : KoinComponent {
+
+    private val focusRequester = FocusRequester()
+    fun focusRequester() = focusRequester
+
+    private val state = LazyListState()
+    fun listState() = state
+
+    private val scrollAction = KeyboardScrollAction(state)
+    fun onKeyEvent(coroutineScope: CoroutineScope, it: KeyEvent): Boolean {
+        return scrollAction.invoke(coroutineScope, it.key, it.isCtrlPressed)
+    }
+
+    private val notificationEvents = mutableStateListOf<NotificationEvent>()
+    fun items() = notificationEvents
+
+    private val repository: NotificationEventRepository by inject()
+
+    private val mainViewModel: MainViewModel by inject()
+
+    fun add() {
+        val new = NotificationEvent.makeDefault()
+        repository
+            .add(new)
+        notificationEvents.add(new)
+    }
+
+    fun update(index: Int, notificationEvent: NotificationEvent) {
+        repository
+            .update(index, notificationEvent)
+        mainViewModel
+            .showSnackbar("Update notification event.")
+    }
+
+    fun deleteAt(index: Int) {
+        repository
+            .deleteAt(index)
+        notificationEvents.removeAt(index)
+        mainViewModel
+            .showSnackbar("Delete notification event.")
+    }
+
+    fun start(dispatcher: CoroutineDispatcher) {
+        CoroutineScope(dispatcher).launch {
+            notificationEvents.addAll(repository.readAll())
+        }
+        focusRequester().requestFocus()
+    }
+
+}
