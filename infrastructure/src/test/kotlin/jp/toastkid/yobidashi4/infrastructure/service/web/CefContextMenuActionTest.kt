670f2fb953d2f2a06f7cd971bfd1d1c566887a0d
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-15 08:58:05 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefContextMenuActionTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefContextMenuActionTest.kt
@@ -421,6 +421,16 @@ class CefContextMenuActionTest {
     }
 
     @Test
+    fun openWithOtherBrowserWithParamWhichDoesNotHasLinks() {
+        every { param.linkUrl } returns null
+        every { param.sourceUrl } returns null
+
+        subject.invoke(browser, param, "test", ContextMenu.OPEN_WITH_OTHER_BROWSER.id)
+
+        verify { viewModel.browseUri(any()) }
+    }
+
+    @Test
     fun openWithOtherBrowserUseSelectedText() {
         subject.invoke(browser, null, "test", ContextMenu.OPEN_WITH_OTHER_BROWSER.id)
 
