daae15eb0be97574b3ca7cdea7e41dd6bec6137d
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-17 07:47:12 +0900

    refactor Add test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/MainScaffoldKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/MainScaffoldKtTest.kt
@@ -2,6 +2,7 @@ package jp.toastkid.yobidashi4.presentation.main
 
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runDesktopComposeUiTest
@@ -13,11 +14,15 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.unmockkAll
+import jp.toastkid.yobidashi4.domain.model.tab.BarcodeToolTab
+import jp.toastkid.yobidashi4.domain.model.tab.FileRenameToolTab
+import jp.toastkid.yobidashi4.domain.model.tab.LoanCalculatorTab
 import jp.toastkid.yobidashi4.domain.model.tab.MarkdownPreviewTab
 import jp.toastkid.yobidashi4.domain.model.tab.Tab
 import jp.toastkid.yobidashi4.domain.service.archive.KeywordArticleFinder
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlinx.coroutines.flow.emptyFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -101,6 +106,7 @@ class MainScaffoldKtTest {
         every { mainViewModel.initialAggregationType() } returns 0
         every { mainViewModel.inputValue() } returns TextFieldValue("search")
         every { mainViewModel.findStatus() } returns "test"
+        every { mainViewModel.caseSensitive() } returns true
 
         runDesktopComposeUiTest {
             setContent {
@@ -108,4 +114,43 @@ class MainScaffoldKtTest {
             }
         }
     }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun useTabContents() {
+        every { mainViewModel.selected } returns mutableStateOf(0)
+        every { mainViewModel.currentTab() } returns LoanCalculatorTab()
+        every { mainViewModel.tabs } returns mutableStateListOf(
+            LoanCalculatorTab(),
+            BarcodeToolTab(),
+            FileRenameToolTab()
+        )
+
+        runDesktopComposeUiTest {
+            setContent {
+                MainScaffold()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun useFileRenameToolTabContents() {
+        every { mainViewModel.selected } returns mutableStateOf(2)
+        every { mainViewModel.currentTab() } returns FileRenameToolTab()
+        every { mainViewModel.droppedPathFlow() } returns emptyFlow()
+        every { mainViewModel.showSnackbar(any(), any(), any()) } just Runs
+        every { mainViewModel.tabs } returns mutableStateListOf(
+            LoanCalculatorTab(),
+            BarcodeToolTab(),
+            FileRenameToolTab()
+        )
+
+        runDesktopComposeUiTest {
+            setContent {
+                MainScaffold()
+            }
+        }
+    }
+
 }
