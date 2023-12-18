bf5b141d3cdd558bef9d6e464a0a81d2cd7a0655
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-19 08:09:13 +0900

    refactor Delete useless find implementation.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/browser/WebTabViewModelImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/browser/WebTabViewModelImplementation.kt
@@ -1,9 +1,6 @@
 package jp.toastkid.yobidashi4.infrastructure.viewmodel.browser
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.text.input.TextFieldValue
 import jp.toastkid.yobidashi4.presentation.viewmodel.web.WebTabViewModel
-import jp.toastkid.yobidashi4.presentation.web.event.FindEvent
 import jp.toastkid.yobidashi4.presentation.web.event.ReloadEvent
 import jp.toastkid.yobidashi4.presentation.web.event.SwitchDeveloperToolEvent
 import jp.toastkid.yobidashi4.presentation.web.event.WebTabEvent
@@ -24,35 +21,6 @@ class WebTabViewModelImplementation : WebTabViewModel {
     override fun switchDevTools(id: String) {
         CoroutineScope(Dispatchers.Default).launch {
             _event.emit(SwitchDeveloperToolEvent(id))
-        }
-    }
-
-    private val openFind = mutableStateOf(false)
-
-    private val findInput = mutableStateOf(TextFieldValue())
-
-    override fun openFind() = openFind.value
-
-    override fun switchFind() {
-        openFind.value = openFind.value.not()
-    }
-
-    override fun inputValue() = findInput.value
-
-    override fun onFindInputChange(id: String, value: TextFieldValue) {
-        findInput.value = TextFieldValue(value.text, value.selection, value.composition)
-        findDown(id)
-    }
-
-    override fun findUp(id: String) {
-        CoroutineScope(Dispatchers.Default).launch {
-            _event.emit(FindEvent(id, inputValue().text, true))
-        }
-    }
-
-    override fun findDown(id: String) {
-        CoroutineScope(Dispatchers.Default).launch {
-            _event.emit(FindEvent(id, inputValue().text))
         }
     }
 
