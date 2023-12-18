8b0871ece1dc5925c6bb847f1c9c7c01571b209c
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-19 08:08:53 +0900

    refactor Delete useless find interfaces.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/web/WebTabViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/viewmodel/web/WebTabViewModel.kt
@@ -1,17 +1,10 @@
 package jp.toastkid.yobidashi4.presentation.viewmodel.web
 
-import androidx.compose.ui.text.input.TextFieldValue
 import jp.toastkid.yobidashi4.presentation.web.event.WebTabEvent
 import kotlinx.coroutines.flow.SharedFlow
 
 interface WebTabViewModel {
     fun event(): SharedFlow<WebTabEvent>
-    fun openFind(): Boolean
-    fun switchFind()
-    fun inputValue(): TextFieldValue
-    fun onFindInputChange(id: String, value: TextFieldValue)
-    fun findUp(id: String)
-    fun findDown(id: String)
     fun switchDevTools(id: String)
     fun reload(id: String)
 }
