c71fc00762f0d2640fc509716d9f1e53e4b067ac
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-19 08:09:37 +0900

    refactor Clean up test code.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/browser/WebTabViewModelImplementationTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/browser/WebTabViewModelImplementationTest.kt
@@ -1,18 +1,14 @@
 package jp.toastkid.yobidashi4.infrastructure.viewmodel.browser
 
-import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.unmockkAll
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
-import jp.toastkid.yobidashi4.presentation.web.event.FindEvent
 import jp.toastkid.yobidashi4.presentation.web.event.ReloadEvent
 import jp.toastkid.yobidashi4.presentation.web.event.SwitchDeveloperToolEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -42,50 +38,6 @@ class WebTabViewModelImplementationTest {
         }
 
         subject.switchDevTools("test")
-
-        countDownLatch.await(5, TimeUnit.SECONDS)
-    }
-
-    @Test
-    fun switchFind() {
-        assertFalse(subject.openFind())
-
-        subject.switchFind()
-
-        assertTrue(subject.openFind())
-    }
-
-    @Test
-    fun onFindInputChange() {
-        val textFieldValue = TextFieldValue("test")
-        val countDownLatch = CountDownLatch(1)
-        CoroutineScope(Dispatchers.Unconfined).launch {
-            subject.event().collect {
-                assertTrue(it is FindEvent)
-                assertEquals(textFieldValue.text, subject.inputValue().text)
-                countDownLatch.countDown()
-            }
-        }
-
-        subject.onFindInputChange("1", textFieldValue)
-
-        countDownLatch.await(5, TimeUnit.SECONDS)
-    }
-
-    @Test
-    fun findUp() {
-        val textFieldValue = TextFieldValue("test")
-        subject.onFindInputChange("1", textFieldValue)
-
-        val countDownLatch = CountDownLatch(1)
-        CoroutineScope(Dispatchers.Unconfined).launch {
-            subject.event().collect {
-                assertTrue((it as FindEvent).upward)
-                countDownLatch.countDown()
-            }
-        }
-
-        subject.findUp("1")
 
         countDownLatch.await(5, TimeUnit.SECONDS)
     }
