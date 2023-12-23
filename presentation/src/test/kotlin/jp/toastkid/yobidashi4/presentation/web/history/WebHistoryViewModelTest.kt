d42c46eb5679fc5faaefdb5c547b56d063e99651
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-23 09:58:55 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/history/WebHistoryViewModelTest.kt
@@ -0,0 +1,151 @@
+package jp.toastkid.yobidashi4.presentation.web.history
+
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.input.key.Key
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.spyk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.web.history.WebHistory
+import jp.toastkid.yobidashi4.domain.model.web.icon.WebIcon
+import jp.toastkid.yobidashi4.domain.repository.web.history.WebHistoryRepository
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class WebHistoryViewModelTest {
+
+    private lateinit var subject: WebHistoryViewModel
+
+    @MockK
+    private lateinit var viewModel: MainViewModel
+
+    @MockK
+    private lateinit var repository: WebHistoryRepository
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier = null) { viewModel } bind(MainViewModel::class)
+                    single(qualifier = null) { repository } bind(WebHistoryRepository::class)
+                }
+            )
+        }
+
+        mockkConstructor(WebIcon::class)
+        every { anyConstructed<WebIcon>().readAll() } returns listOf(mockk())
+
+        subject = WebHistoryViewModel()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @Test
+    fun listState() {
+        assertNotNull(subject.listState())
+    }
+
+    @Test
+    fun focusRequester() {
+        assertNotNull(subject.focusRequester())
+    }
+
+    @Test
+    fun scrollAction() {
+        subject.scrollAction(CoroutineScope(Dispatchers.Unconfined), Key.DirectionUp, false)
+    }
+
+    @Test
+    fun launch() {
+        assertTrue(subject.list().isEmpty())
+
+        subject = spyk(subject)
+        val focusRequester = mockk<FocusRequester>()
+        every { subject.focusRequester() } returns focusRequester
+        every { focusRequester.requestFocus() } just Runs
+        every { repository.readAll() } returns listOf(
+            WebHistory(
+                "test",
+                "test",
+                1697462064796
+            ),
+            WebHistory(
+                "test",
+                "test",
+                1697462064797
+            )
+        )
+
+        subject.launch()
+
+        verify { repository.readAll() }
+        assertEquals(2, subject.list().size)
+    }
+
+    @Test
+    fun openUrl() {
+        every { viewModel.openUrl(any(), any()) } just Runs
+
+        subject.openUrl("https://www.yahoo.co.jp", true)
+
+        verify { viewModel.openUrl(any(), any()) }
+    }
+
+    @Test
+    fun findIconPath() {
+        subject.findIconPath(
+            WebHistory(
+                "test",
+                "test",
+                1697462064796
+            )
+        )
+    }
+
+    @Test
+    fun dateTimeString() {
+        assertEquals(
+            "2023-10-16(Mon)22:14:24",
+            subject.dateTimeString(
+                WebHistory(
+                    "test",
+                    "test",
+                    1697462064796
+                )
+            )
+        )
+    }
+
+    @Test
+    fun browseUri() {
+        every { viewModel.browseUri(any()) } just Runs
+
+        subject.browseUri("https://www.yahoo.co.jp")
+
+        verify { viewModel.browseUri(any()) }
+    }
+}
