d838d307e0a992a1fd39af201ca1efb897285583
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-24 09:39:43 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewModelTest.kt
@@ -0,0 +1,153 @@
+package jp.toastkid.yobidashi4.presentation.tool.file
+
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.text.input.TextFieldValue
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import java.nio.file.Files
+import java.nio.file.Path
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlin.io.path.extension
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class FileRenameToolViewModelTest {
+
+    private lateinit var subject: FileRenameToolViewModel
+
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier = null) { mainViewModel } bind(MainViewModel::class)
+                }
+            )
+        }
+
+        every { mainViewModel.droppedPathFlow() } returns emptyFlow()
+        every { mainViewModel.showSnackbar(any(), any(), any()) } just Runs
+
+        mockkStatic(Files::class)
+        every { Files.copy(any<Path>(), any<Path>()) } returns mockk()
+
+        subject = FileRenameToolViewModel()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @Test
+    fun onValueChange() {
+        assertEquals("img_", subject.input().text)
+
+        subject.onValueChange(TextFieldValue("ABC"))
+
+        assertEquals("ABC", subject.input().text)
+
+        subject.clearInput()
+
+        assertTrue(subject.input().text.isEmpty())
+    }
+
+    @Test
+    fun rename() {
+        subject.onValueChange(TextFieldValue("ABC"))
+        val value = mockk<Path>()
+        every { value.resolveSibling(any<String>()) } returns mockk()
+        every { value.extension } returns "png"
+        every { mainViewModel.droppedPathFlow() } returns flowOf(value, value)
+
+        runBlocking {
+            subject.collectDroppedPaths()
+
+            subject.rename()
+
+            verify(exactly = 2) { Files.copy(any<Path>(), any<Path>()) }
+            verify(exactly = 1) { mainViewModel.showSnackbar(any(), any(), any()) }
+        }
+    }
+
+    @Test
+    fun onKeyEvent() {
+        subject.onValueChange(TextFieldValue("ABC"))
+
+        subject.onKeyEvent(
+            KeyEvent(
+                java.awt.event.KeyEvent(
+                    mockk(),
+                    java.awt.event.KeyEvent.KEY_PRESSED,
+                    1,
+                    0,
+                    java.awt.event.KeyEvent.VK_ENTER,
+                    '-'
+                )
+            )
+        )
+
+        verify(inverse = true) { Files.copy(any<Path>(), any<Path>()) }
+        verify(inverse = true) { mainViewModel.showSnackbar(any(), any(), any()) }
+    }
+
+    @Test
+    fun onKeyEventNotConsumed() {
+        val consumed = subject.onKeyEvent(
+            KeyEvent(
+                java.awt.event.KeyEvent(
+                    mockk(),
+                    java.awt.event.KeyEvent.KEY_PRESSED,
+                    1,
+                    0,
+                    java.awt.event.KeyEvent.VK_0,
+                    '0'
+                )
+            )
+        )
+
+        assertFalse(consumed)
+    }
+
+    @Test
+    fun collectDroppedPaths() {
+        runBlocking {
+            assertTrue(subject.items().isEmpty())
+
+            every { mainViewModel.droppedPathFlow() } returns flowOf(mockk())
+
+            subject.collectDroppedPaths()
+
+            assertEquals(1, subject.items().size)
+
+            subject.clearPaths()
+
+            assertTrue(subject.items().isEmpty())
+        }
+    }
+
+}
