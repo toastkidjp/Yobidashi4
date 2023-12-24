8a723525ab041ac9836c0de5f30447c2b37f82db
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-25 08:34:47 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceViewModelTest.kt
@@ -11,9 +11,10 @@ import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.repository.number.GameRepository
 import jp.toastkid.yobidashi4.domain.service.number.GameFileProvider
-import jp.toastkid.yobidashi4.presentation.number.NumberPlaceViewModel
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNotNull
@@ -29,7 +30,13 @@ class NumberPlaceViewModelTest {
     private lateinit var numberPlaceViewModel: NumberPlaceViewModel
 
     @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @MockK
     private lateinit var repository: GameRepository
+
+    @MockK
+    private lateinit var setting: Setting
 
     @BeforeEach
     fun setUp() {
@@ -37,7 +44,9 @@ class NumberPlaceViewModelTest {
         startKoin {
             modules(
                 module {
+                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
                     single(qualifier=null) { repository } bind(GameRepository::class)
+                    single(qualifier=null) { setting } bind(Setting::class)
                 }
             )
         }
@@ -122,6 +131,15 @@ class NumberPlaceViewModelTest {
 
         verify { anyConstructed<GameFileProvider>().invoke() }
         verify { repository wasNot called }
+    }
+
+    @Test
+    fun setMaskingCount() {
+        every { setting.setMaskingCount(any()) } just Runs
+
+        numberPlaceViewModel.setMaskingCount(30)
+
+        verify { setting.setMaskingCount(30) }
     }
 
 }
