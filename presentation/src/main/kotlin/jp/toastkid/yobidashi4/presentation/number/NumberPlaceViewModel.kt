ce3ab0dbe75337d1efec5a9cdea8530608bac168
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-25 08:35:39 +0900

    refactor Add functions.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceViewModel.kt
@@ -11,14 +11,24 @@ package jp.toastkid.yobidashi4.presentation.number
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.unit.sp
+import java.nio.file.Files
 import jp.toastkid.yobidashi4.domain.model.number.NumberBoard
 import jp.toastkid.yobidashi4.domain.model.number.NumberPlaceGame
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.repository.number.GameRepository
 import jp.toastkid.yobidashi4.domain.service.number.GameFileProvider
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class NumberPlaceViewModel : KoinComponent {
+
+    private val mainViewModel: MainViewModel by inject()
+
+    private val setting: Setting by inject()
 
     private val gameRepository: GameRepository by inject()
 
@@ -27,6 +37,10 @@ class NumberPlaceViewModel : KoinComponent {
     private val _mask = mutableStateOf(NumberBoard())
 
     private val _loading = mutableStateOf(false)
+
+    private val numberStates = mutableListOf<MutableState<String>>()
+
+    private val openOption = mutableStateOf(false)
 
     fun initialize(maskingCount: Int) {
         _loading.value = true
@@ -90,5 +104,73 @@ class NumberPlaceViewModel : KoinComponent {
             gameRepository.delete(file)
         }
     }
+
+    fun renewGame() {
+        val game = NumberPlaceGame()
+        game.initialize(setting.getMaskingCount())
+        setGame(game)
+        initializeSolving()
+        initialize(setting.getMaskingCount())
+        saveCurrentGame()
+    }
+
+    fun startNewGame() {
+        deleteGame()
+        initializeSolving()
+        initialize(setting.getMaskingCount())
+        saveCurrentGame()
+    }
+
+    suspend fun start() {
+        val file = GameFileProvider().invoke()
+        if (file != null && Files.size(file) != 0L) {
+            val game = gameRepository.load(file)
+            if (game != null) {
+                setGame(game)
+                return
+            }
+        }
+
+        withContext(Dispatchers.IO) {
+            initialize(setting.getMaskingCount())
+            saveCurrentGame()
+        }
+    }
+
+    fun showMessageSnackbar(
+        done: Boolean,
+        onAction: () -> Unit = {}
+    ) {
+        mainViewModel.showSnackbar(
+            if (done) "Well done!" else "Incorrect...",
+            if (done) "Next game" else ""
+        ) {
+            onAction()
+        }
+    }
+
+    fun fontSize() = 32.sp
+
+    fun getMaskingCount(): Int = setting.getMaskingCount()
+
+    fun setMaskingCount(it: Int) {
+        setting.setMaskingCount(it)
+    }
+
+    fun onCellLongClick(rowIndex: Int, columnIndex: Int, number: MutableState<String>) {
+        mainViewModel.showSnackbar(
+            "Would you like to use hint?",
+            "Use"
+        ) {
+            useHint(
+                rowIndex,
+                columnIndex,
+                number
+            ) { done ->
+                showMessageSnackbar(done)
+            }
+        }
+    }
+
 
 }
