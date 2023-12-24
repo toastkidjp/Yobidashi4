0da37467b703540949e6b392159bfd8b092992e3
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-24 09:39:25 +0900

    refactor Extract to viewModel.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewModel.kt
@@ -0,0 +1,79 @@
+package jp.toastkid.yobidashi4.presentation.tool.file
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.text.input.TextFieldValue
+import java.nio.file.Files
+import java.nio.file.Path
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlin.io.path.extension
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class FileRenameToolViewModel : KoinComponent {
+
+    private val viewModel: MainViewModel by inject()
+
+    private val paths = mutableStateListOf<Path>()
+
+    private val input = mutableStateOf(TextFieldValue("img_"))
+
+    fun items() = paths
+
+    fun input() = input.value
+
+    fun onValueChange(it: TextFieldValue) {
+        input.value = TextFieldValue(it.text, it.selection, it.composition)
+    }
+
+    fun rename() {
+        if (paths.isEmpty()) {
+            return
+        }
+
+        paths.forEachIndexed { i, p ->
+            Files.copy(p, p.resolveSibling("${input.value.text}_${i + 1}.${p.extension}"))
+        }
+
+        viewModel
+            .showSnackbar(
+                "Rename completed!",
+                "Open folder",
+                { viewModel.openFile(paths.first().parent, false) }
+            )
+    }
+
+    fun onKeyEvent(it: KeyEvent): Boolean {
+        if (it.type == KeyEventType.KeyDown && it.key == Key.Enter
+            && input.value.composition == null
+            && input.value.text.isNotBlank()
+        ) {
+            rename()
+            return true
+        }
+
+        return false
+    }
+
+    fun clearPaths() {
+        paths.clear()
+    }
+
+    fun launchedEffectKey() = viewModel.droppedPathFlow().hashCode()
+
+    suspend fun collectDroppedPaths() {
+        viewModel.droppedPathFlow().collect {
+            paths.add(it)
+        }
+    }
+
+    fun clearInput() {
+        input.value = TextFieldValue()
+    }
+
+}
