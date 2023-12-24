ed7f89c12fd25485d1c3250642bf771913dfcd1d
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-24 10:41:35 +0900

    refactor Delete useless classes.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/viewmodel/browser/WebTabViewModelImplementation.kt
+++
@@ -1,9 +0,0 @@
-package jp.toastkid.yobidashi4.infrastructure.viewmodel.browser
-
-import jp.toastkid.yobidashi4.presentation.viewmodel.web.WebTabViewModel
-import org.koin.core.annotation.Single
-
-@Single
-class WebTabViewModelImplementation : WebTabViewModel {
-
-}
