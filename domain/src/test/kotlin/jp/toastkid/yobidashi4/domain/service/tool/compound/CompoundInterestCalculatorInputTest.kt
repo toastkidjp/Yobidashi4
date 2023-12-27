87dcbea885b90ac420d305cabe08e918e28b9442
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-28 00:07:31 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorInputTest.kt
@@ -0,0 +1,16 @@
+package jp.toastkid.yobidashi4.domain.service.tool.compound
+
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+class CompoundInterestCalculatorInputTest {
+
+    @Test
+    fun test() {
+        assertNull(CompoundInterestCalculatorInput.from(null, null, null, null))
+        assertNull(CompoundInterestCalculatorInput.from("", null, null, null))
+        assertNull(CompoundInterestCalculatorInput.from("", "", null, null))
+        assertNull(CompoundInterestCalculatorInput.from("", "", "", null))
+    }
+
+}
