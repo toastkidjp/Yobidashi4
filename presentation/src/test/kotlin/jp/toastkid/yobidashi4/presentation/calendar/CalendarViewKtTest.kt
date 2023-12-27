17d6da4abd0221e959a8f532275ce197b9b8d42b
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-28 00:09:04 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/calendar/CalendarViewKtTest.kt
@@ -0,0 +1,65 @@
+package jp.toastkid.yobidashi4.presentation.calendar
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.text.input.TextFieldValue
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.unmockkAll
+import java.time.LocalDate
+import jp.toastkid.yobidashi4.domain.model.calendar.Week
+import jp.toastkid.yobidashi4.presentation.viewmodel.calendar.CalendarViewModel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class CalendarViewKtTest {
+
+    @MockK
+    private lateinit var calendarViewModel: CalendarViewModel
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { calendarViewModel } bind(CalendarViewModel::class)
+                }
+            )
+        }
+
+        every { calendarViewModel.makeMonth(any()) } returns mutableListOf(Week().also { it.add(LocalDate.now()) })
+        every { calendarViewModel.isToday(any()) } returns false
+        every { calendarViewModel.openDateArticle(any(), any()) } just Runs
+        every { calendarViewModel.plusMonths(any()) } just Runs
+        every { calendarViewModel.yearInput() } returns TextFieldValue()
+        every { calendarViewModel.setYearInput(any()) } just Runs
+        every { calendarViewModel.moveMonth(any()) } just Runs
+        every { calendarViewModel.moveToCurrentMonth() } just Runs
+        every { calendarViewModel.localDate() } returns LocalDate.now()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+        stopKoin()
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun calendarView() {
+        runDesktopComposeUiTest {
+            setContent {
+                CalendarView()
+            }
+        }
+    }
+}
