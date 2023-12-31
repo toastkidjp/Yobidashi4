4b47d84c529769c50e4cdec88afbcfda13f69f18
Toast Kid <super_q1m@yahoo.co.jp>
2023-12-31 18:59:58 +0900

    feature Add function.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/web/bookmark/Bookmark.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/web/bookmark/Bookmark.kt
@@ -1,5 +1,7 @@
 package jp.toastkid.yobidashi4.domain.model.web.bookmark
 
+import jp.toastkid.yobidashi4.domain.model.tab.WebTab
+
 data class Bookmark(
     val title: String = "",
     val url: String = "",
@@ -7,4 +9,12 @@ data class Bookmark(
     val parent: String = "root",
     val folder: Boolean = false
 ) {
+
+    companion object {
+        fun fromWebTab(webTab: WebTab): Bookmark {
+            return Bookmark(webTab.title(), webTab.url())
+        }
+
+    }
+
 }
