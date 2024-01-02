3ce093db526cc60ad1afd82af8c97a461ad2cd73
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-02 09:52:20 +0900

    refactor Delete useless mocking.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewKtTest.kt
@@ -8,11 +8,12 @@ import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
+import io.mockk.mockk
 import io.mockk.unmockkAll
+import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.emptyFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -43,7 +44,6 @@ class WebTabViewKtTest {
             )
         }
 
-        every { webViewPool.event() } returns MutableSharedFlow()
         every { webViewPool.find(any(), any(), any()) } just Runs
         every { webViewPool.reload(any()) } just Runs
         every { webViewPool.clearFind(any()) } just Runs
@@ -64,6 +64,27 @@ class WebTabViewKtTest {
             setContent {
                 ComposePanel().setContent {
                     WebTabView(WebTab("test", "https://www.yahoo.com"))
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun isNotReadableUrl() {
+        runDesktopComposeUiTest {
+            setContent {
+                ComposePanel().setContent {
+                    val tab = mockk<WebTab>()
+                    every { tab.isReadableUrl() } returns false
+                    every { tab.id() } returns "test"
+                    every { tab.url() } returns "test"
+
+                    WebTabView(tab)
+
+                    verify { tab.isReadableUrl() }
+                    verify(inverse = true) { tab.id() }
+                    verify(inverse = true) { tab.url() }
                 }
             }
         }
