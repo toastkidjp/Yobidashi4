3e1b64484437d8ac263e4538547c90bfb33f90f4
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-02 09:50:16 +0900

    refactor Use viewModel.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
@@ -1,7 +1,21 @@
 package jp.toastkid.yobidashi4.presentation.web
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.SwingPanel
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.toArgb
+import java.awt.Color
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 internal fun WebTabView(tab: WebTab) {
@@ -9,5 +23,27 @@ internal fun WebTabView(tab: WebTab) {
         return
     }
 
-    WebView(tab.id(), tab.url())
+    val background = Color(MaterialTheme.colors.surface.toArgb())
+
+    val viewModel = remember { WebTabViewModel() }
+
+    Column {
+        SwingPanel(
+            factory = {
+                viewModel.component(tab, background)
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .focusRequester(viewModel.focusRequester())
+        )
+
+        Spacer(modifier = Modifier.height(viewModel.spacerHeight()))
+    }
+
+    LaunchedEffect(tab.id()) {
+        withContext(Dispatchers.IO) {
+            viewModel.start(tab.id())
+        }
+    }
 }
