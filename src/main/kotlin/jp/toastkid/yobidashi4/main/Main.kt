65f0aaf97e3672f04447e0651cf4029c7821c3f5
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-02 09:49:25 +0900

    refactor Use divided factory.

--- src/main/kotlin/jp/toastkid/yobidashi4/main/Main.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/main/Main.kt
@@ -1,15 +1,12 @@
 package jp.toastkid.yobidashi4.main
 
-import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
-import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.infrastructure.di.DependencyInjectionContainer
 import jp.toastkid.yobidashi4.infrastructure.service.article.TodayArticleGeneratorImplementation
+import jp.toastkid.yobidashi4.infrastructure.service.main.AppCloserThreadFactory
 import jp.toastkid.yobidashi4.presentation.main.launchMainApplication
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 fun main() {
     DependencyInjectionContainer.start()
@@ -20,15 +17,5 @@ fun main() {
 
     launchMainApplication()
 
-    Runtime.getRuntime().addShutdownHook(Thread {
-        val koin = object : KoinComponent {
-            val setting: Setting by inject()
-            val webViewPool: WebViewPool by inject()
-        }
-
-        koin.setting.save()
-
-        DependencyInjectionContainer.stop()
-        koin.webViewPool.disposeAll()
-    })
+    Runtime.getRuntime().addShutdownHook(AppCloserThreadFactory().invoke())
 }
