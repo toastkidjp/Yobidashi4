ce675e15c20a0e97ba7b4571480d4f2244b004c7
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-02 09:48:33 +0900

    refactor Divide to other factory.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/main/AppCloserThreadFactory.kt
@@ -0,0 +1,23 @@
+package jp.toastkid.yobidashi4.infrastructure.service.main
+
+import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import jp.toastkid.yobidashi4.infrastructure.di.DependencyInjectionContainer
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class AppCloserThreadFactory : KoinComponent {
+
+    private val setting: Setting by inject()
+
+    private val webViewPool: WebViewPool by inject()
+
+    operator fun invoke() = Thread {
+        setting.save()
+
+        DependencyInjectionContainer.stop()
+
+        webViewPool.disposeAll()
+    }
+
+}
