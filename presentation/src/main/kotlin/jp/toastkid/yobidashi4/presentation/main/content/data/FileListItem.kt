e305aab25a3cdaf38ee069154fa8fabf74c5fbdf
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-04 00:11:13 +0900

    refactor Move function.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/data/FileListItem.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/data/FileListItem.kt
@@ -1,6 +1,11 @@
 package jp.toastkid.yobidashi4.presentation.main.content.data
 
+import java.nio.file.Files
 import java.nio.file.Path
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 data class FileListItem(
     val path: Path,
@@ -12,4 +17,19 @@ data class FileListItem(
 
     fun unselect() = FileListItem(path, false, editable)
 
+    fun subText(): String? {
+        if (Files.exists(path).not()) {
+            return null
+        }
+
+        return "${Files.size(path) / 1000} KB | ${
+            LocalDateTime
+                .ofInstant(Files.getLastModifiedTime(path).toInstant(), ZoneId.systemDefault())
+                .format(dateTimeFormatter)
+        }"
+    }
+
 }
+
+private val dateTimeFormatter =
+    DateTimeFormatter.ofPattern("yyyy-MM-dd(E) HH:mm:ss").withLocale(Locale.ENGLISH)
