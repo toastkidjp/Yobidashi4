63b9555d427b9a6dbd81e5a47ee09de94b5efc5c
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-07 18:13:20 +0900

    refactor Use mockkConstructor.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/roulette/RouletteToolTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/roulette/RouletteToolTabViewKtTest.kt
@@ -2,6 +2,10 @@ package jp.toastkid.yobidashi4.presentation.tool.roulette
 
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.text.input.TextFieldValue
+import io.mockk.every
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,10 +14,15 @@ class RouletteToolTabViewKtTest {
 
     @BeforeEach
     fun setUp() {
+        mockkConstructor(RouletteToolTabViewModel::class)
+
+        every { anyConstructed<RouletteToolTabViewModel>().input() } returns TextFieldValue()
+        every { anyConstructed<RouletteToolTabViewModel>().result() } returns "test"
     }
 
     @AfterEach
     fun tearDown() {
+        unmockkAll()
     }
 
     @OptIn(ExperimentalTestApi::class)
