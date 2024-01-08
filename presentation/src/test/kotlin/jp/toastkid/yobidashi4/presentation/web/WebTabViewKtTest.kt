e43b8bc8932d6f595e8ef795b23380fcb4659ff6
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-09 08:29:13 +0900

    refactor Fix incorrect test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewKtTest.kt
@@ -1,16 +1,19 @@
 package jp.toastkid.yobidashi4.presentation.web
 
-import androidx.compose.ui.awt.ComposePanel
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.window.Window
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
+import java.awt.Panel
 import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
@@ -47,8 +50,11 @@ class WebTabViewKtTest {
         every { webViewPool.find(any(), any(), any()) } just Runs
         every { webViewPool.reload(any()) } just Runs
         every { webViewPool.clearFind(any()) } just Runs
+        every { webViewPool.component(any(), any()) } returns Panel()
         every { mainViewModel.showingSnackbar() } returns false
         every { mainViewModel.finderFlow() } returns emptyFlow()
+        mockkConstructor(WebTabViewModel::class)
+        coEvery { anyConstructed<WebTabViewModel>().start(any()) } just Runs
     }
 
     @AfterEach
@@ -62,10 +68,12 @@ class WebTabViewKtTest {
     fun webTabView() {
         runDesktopComposeUiTest {
             setContent {
-                ComposePanel().setContent {
+                Window({}, visible = false) {
                     WebTabView(WebTab("test", "https://www.yahoo.com"))
                 }
             }
+
+            verify { webViewPool.component(any(), any()) }
         }
     }
 
@@ -74,18 +82,16 @@ class WebTabViewKtTest {
     fun isNotReadableUrl() {
         runDesktopComposeUiTest {
             setContent {
-                ComposePanel().setContent {
-                    val tab = mockk<WebTab>()
-                    every { tab.isReadableUrl() } returns false
-                    every { tab.id() } returns "test"
-                    every { tab.url() } returns "test"
+                val tab = mockk<WebTab>()
+                every { tab.isReadableUrl() } returns false
+                every { tab.id() } returns "test"
+                every { tab.url() } returns "test"
 
-                    WebTabView(tab)
+                WebTabView(tab)
 
-                    verify { tab.isReadableUrl() }
-                    verify(inverse = true) { tab.id() }
-                    verify(inverse = true) { tab.url() }
-                }
+                verify { tab.isReadableUrl() }
+                verify(inverse = true) { tab.id() }
+                verify(inverse = true) { tab.url() }
             }
         }
     }
