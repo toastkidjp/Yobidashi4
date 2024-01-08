4a54abad61e4986f126a7175a36512d07f08d781
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-08 09:41:02 +0900

    refactor Modify unit test.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/model/browser/WebViewPoolImplementationTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/model/browser/WebViewPoolImplementationTest.kt
@@ -10,22 +10,14 @@ import io.mockk.mockkConstructor
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import jp.toastkid.yobidashi4.domain.service.web.event.ReloadEvent
-import jp.toastkid.yobidashi4.domain.service.web.event.SwitchDeveloperToolEvent
+import java.awt.Component
+import javax.swing.JDialog
 import jp.toastkid.yobidashi4.infrastructure.service.web.CefClientFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.cef.CefApp
 import org.cef.CefClient
 import org.cef.browser.CefBrowser
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -91,25 +83,6 @@ class WebViewPoolImplementationTest {
         subject.reload("1")
 
         verify { cefBrowser.reload() }
-    }
-
-    @Test
-    fun reloadViaFlow() {
-        val countDownLatch = CountDownLatch(1)
-        CoroutineScope(Dispatchers.Unconfined).launch {
-            subject.event().collect {
-                if (it !is ReloadEvent) {
-                    fail<Unit>("Unexpected event type.")
-                    return@collect
-                }
-                assertEquals("1", it.id)
-                countDownLatch.countDown()
-            }
-        }
-
-        subject.reload("1")
-
-        countDownLatch.await(5, TimeUnit.SECONDS)
     }
 
     @Test
@@ -184,17 +157,15 @@ class WebViewPoolImplementationTest {
 
     @Test
     fun switchDevTools() {
-        val countDownLatch = CountDownLatch(1)
-        CoroutineScope(Dispatchers.Unconfined).launch {
-            subject.event().collect {
-                Assertions.assertTrue(it is SwitchDeveloperToolEvent)
-                countDownLatch.countDown()
-            }
-        }
+        mockkConstructor(JDialog::class)
+        every { anyConstructed<JDialog>().defaultCloseOperation = any() } just Runs
+        every { anyConstructed<JDialog>().setSize(any(), any()) } just Runs
+        every { anyConstructed<JDialog>().add(any<Component>()) } returns mockk()
+        every { anyConstructed<JDialog>().isVisible = any() } just Runs
 
         subject.switchDevTools("test")
 
-        countDownLatch.await(5, TimeUnit.SECONDS)
+        verify { anyConstructed<JDialog>().isVisible = any() }
     }
 
 }
