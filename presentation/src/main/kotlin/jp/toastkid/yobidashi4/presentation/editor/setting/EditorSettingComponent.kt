e0961ba264f33b82c63703a00e39d65fe5360a2b
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-09 09:08:04 +0900

    refactor Use viewModel.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/setting/EditorSettingComponent.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/setting/EditorSettingComponent.kt
@@ -16,17 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.godaddy.android.colorpicker.ClassicColorPicker
 import java.awt.GraphicsEnvironment
-import jp.toastkid.yobidashi4.domain.model.setting.Setting
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 @Composable
 internal fun EditorSettingComponent(modifier: Modifier) {
-    val setting = object : KoinComponent { val s: Setting by inject() }.s
+    val viewModel = remember { EditorSettingViewModel() }
 
     Surface(
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
@@ -35,56 +31,28 @@ internal fun EditorSettingComponent(modifier: Modifier) {
     ) {
         Column {
             Text("Background color")
-            val currentBackgroundColor = remember {
-                mutableStateOf(
-                    setting.editorBackgroundColor()?.let {
-                        Color(it.red, it.green, it.blue, it.alpha)
-                    } ?: Color.LightGray
-                )
-            }
-            val currentFontColor = remember {
-                mutableStateOf(
-                    setting.editorForegroundColor()?.let {
-                        Color(it.red, it.green, it.blue, it.alpha)
-                    } ?: Color.Black
-                )
-            }
+
             ClassicColorPicker(
-                color = currentBackgroundColor.value,
+                color = viewModel.currentBackgroundColor(),
                 onColorChanged = { hsvColor ->
-                    currentBackgroundColor.value = hsvColor.toColor()
+                    viewModel.onBackgroundColorChanged(hsvColor)
                 },
                 modifier = Modifier.height(200.dp)
             )
 
             Text("Font color")
             ClassicColorPicker(
-                color = currentFontColor.value,
+                color = viewModel.currentFontColor(),
                 onColorChanged = { hsvColor ->
-                    currentFontColor.value = hsvColor.toColor()
+                    viewModel.onFontColorChanged(hsvColor)
                 },
                 modifier = Modifier.height(200.dp)
             )
 
             Button(onClick = {
-                setting.setEditorBackgroundColor(
-                    java.awt.Color(
-                        currentBackgroundColor.value.red,
-                        currentBackgroundColor.value.green,
-                        currentBackgroundColor.value.blue,
-                        currentBackgroundColor.value.alpha
-                    )
-                )
-                setting.setEditorForegroundColor(
-                    java.awt.Color(
-                        currentFontColor.value.red,
-                        currentFontColor.value.green,
-                        currentFontColor.value.blue,
-                        currentFontColor.value.alpha
-                    )
-                )
+                viewModel.commit()
             },
-                colors = ButtonDefaults.buttonColors(backgroundColor = currentBackgroundColor.value, contentColor = currentFontColor.value)
+                colors = ButtonDefaults.buttonColors(backgroundColor = viewModel.currentBackgroundColor(), contentColor = viewModel.currentFontColor())
             ) {
                 Text("Commit colors")
             }
@@ -92,33 +60,31 @@ internal fun EditorSettingComponent(modifier: Modifier) {
             val openFontFamily = remember { mutableStateOf(false) }
             EditorSettingDropdown(
                 GraphicsEnvironment.getLocalGraphicsEnvironment().availableFontFamilyNames.toList(),
-                "Font family: ${setting.editorFontFamily()}",
+                "Font family: ${viewModel.editorFontFamily()}",
                 openFontFamily.value,
                 {
                     openFontFamily.value = it
                 },
                 {
-                    setting.setEditorFontFamily(it.toString())
+                    viewModel.setEditorFontFamily(it.toString())
                 }
             )
 
             val openFontSize = remember { mutableStateOf(false) }
             EditorSettingDropdown(
                 (12 .. 24).toList(),
-                "Font Size: ${setting.editorFontSize()}",
+                "Font Size: ${viewModel.editorFontSize()}",
                 openFontSize.value,
                 {
                     openFontSize.value = it
                 },
                 {
-                    if (it is Int) {
-                        setting.setEditorFontSize(it)
-                    }
+                    viewModel.setEditorFontSize(it)
                 }
             )
 
             Text("Reset color setting", modifier = Modifier.clickable {
-                setting.resetEditorColorSetting()
+                viewModel.reset()
             })
         }
     }
