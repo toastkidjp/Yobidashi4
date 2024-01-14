c30c56f75d5b2051e18c2fbdaef687b8774ad407
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-15 08:37:53 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/InputBoxViewModelTest.kt
@@ -0,0 +1,162 @@
+package jp.toastkid.yobidashi4.presentation.main.component
+
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.text.input.TextFieldValue
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.called
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import java.awt.event.KeyEvent
+import jp.toastkid.yobidashi4.domain.model.tab.WebTab
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class InputBoxViewModelTest {
+
+    private lateinit var subject: InputBoxViewModel
+
+    @MockK
+    private lateinit var viewModel: MainViewModel
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier = null) { viewModel } bind(MainViewModel::class)
+                }
+            )
+        }
+
+        subject = InputBoxViewModel()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @Test
+    fun noopInvokeActionWhenQueryIsEmpty() {
+        subject.invokeAction()
+
+        verify { viewModel wasNot called }
+    }
+
+    @Test
+    fun invokeAction() {
+        every { viewModel.invokeInputAction(any()) } just Runs
+        every { viewModel.setShowInputBox() } just Runs
+        subject.onValueChange(TextFieldValue("test"))
+
+        subject.invokeAction()
+
+        verify { viewModel.invokeInputAction(any()) }
+        verify { viewModel.setShowInputBox() }
+    }
+
+    @Test
+    fun onValueChange() {
+        subject.onValueChange(TextFieldValue("test"))
+
+        assertEquals("test", subject.query().text)
+
+        subject.clearInput()
+
+        assertTrue(subject.query().text.isEmpty())
+    }
+
+    @Test
+    fun onKeyEvent() {
+        every { viewModel.setShowInputBox(any()) } just Runs
+
+        val consumed = subject.onKeyEvent(
+            androidx.compose.ui.input.key.KeyEvent(
+                KeyEvent(
+                    mockk(),
+                    KeyEvent.KEY_PRESSED,
+                    1,
+                    -1,
+                    KeyEvent.VK_ESCAPE,
+                    'A'
+                )
+            )
+        )
+
+        assertTrue(consumed)
+        verify { viewModel.setShowInputBox(any()) }
+    }
+
+    @Test
+    fun notConsumedOnKeyEvent() {
+        every { viewModel.setShowInputBox(any()) } just Runs
+
+        val consumed = subject.onKeyEvent(
+            androidx.compose.ui.input.key.KeyEvent(
+                KeyEvent(
+                    mockk(),
+                    KeyEvent.KEY_PRESSED,
+                    1,
+                    KeyEvent.CTRL_DOWN_MASK,
+                    KeyEvent.VK_UP,
+                    'A'
+                )
+            )
+        )
+
+        assertFalse(consumed)
+        verify { viewModel wasNot called }
+    }
+
+    @Test
+    fun start() {
+        every { viewModel.showInputBox() } returns true
+        val tab = mockk<WebTab>()
+        every { viewModel.currentTab() } returns tab
+        val url = "https://www.yahoo.co.jp"
+        every { tab.url() } returns url
+        subject = spyk(subject)
+        val focusRequester = mockk<FocusRequester>()
+        every { subject.focusRequester() } returns focusRequester
+        every { focusRequester.requestFocus() } just Runs
+
+        subject.start()
+
+        verify { focusRequester.requestFocus() }
+        assertEquals(url, subject.query().text)
+    }
+
+    @Test
+    fun startIfBoxIsClosed() {
+        every { viewModel.showInputBox() } returns false
+        every { viewModel.currentTab() } returns mockk()
+        subject = spyk(subject)
+        val focusRequester = mockk<FocusRequester>()
+        every { subject.focusRequester() } returns focusRequester
+        every { focusRequester.requestFocus() } just Runs
+
+        subject.start()
+
+        verify { focusRequester wasNot called }
+        assertTrue(subject.query().text.isEmpty())
+    }
+
+}
