9f9ffd1aa45a092d81815c11c9b7a2718b307ca6
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-16 08:43:12 +0900

    feature Add service for cut endsWith '.0'.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/ExpressionTextCalculatorService.kt
@@ -0,0 +1,19 @@
+package jp.toastkid.yobidashi4.presentation.editor.markdown.text
+
+import java.util.regex.Pattern
+import jp.toastkid.yobidashi4.domain.service.tool.calculator.SimpleCalculator
+
+class ExpressionTextCalculatorService {
+
+    operator fun invoke(it: String): String {
+        val appendLineBreakIfNeed = if (it.endsWith("\n")) "\n" else ""
+        val doubleResult = SimpleCalculator().invoke(it.trimEnd())?.toString() ?: return it
+        val converted =
+            if (pattern.matcher(doubleResult).find()) doubleResult.substring(0, doubleResult.lastIndexOf("."))
+            else doubleResult
+        return "${converted}$appendLineBreakIfNeed"
+    }
+
+}
+
+private val pattern = Pattern.compile("\\.0*0$")
