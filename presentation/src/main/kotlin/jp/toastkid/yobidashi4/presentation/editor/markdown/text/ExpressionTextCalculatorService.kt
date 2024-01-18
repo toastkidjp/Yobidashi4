a96b6f043c6e4eead9ea5d59279058d9daa011d4
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-18 09:24:34 +0900

    refactor Extract to field.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/ExpressionTextCalculatorService.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/ExpressionTextCalculatorService.kt
@@ -5,9 +5,11 @@ import jp.toastkid.yobidashi4.domain.service.tool.calculator.SimpleCalculator
 
 class ExpressionTextCalculatorService {
 
+    private val calculator = SimpleCalculator()
+
     operator fun invoke(it: String): String {
         val appendLineBreakIfNeed = if (it.endsWith("\n")) "\n" else ""
-        val doubleResult = SimpleCalculator().invoke(it.trimEnd())?.toString() ?: return it
+        val doubleResult = calculator.invoke(it.trimEnd())?.toString() ?: return it
         val converted =
             if (pattern.matcher(doubleResult).find()) doubleResult.substring(0, doubleResult.lastIndexOf("."))
             else doubleResult
