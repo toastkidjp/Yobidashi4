b2a75d17aa7f5f4e9e67dadd117611786fe0e215
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-19 09:03:58 +0900

    refactor Delegate other class.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/OutgoAggregatorService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/OutgoAggregatorService.kt
@@ -1,44 +1,14 @@
 package jp.toastkid.yobidashi4.domain.service.aggregation
 
-import java.nio.file.Files
-import java.util.stream.Collectors
 import jp.toastkid.yobidashi4.domain.model.aggregation.OutgoAggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
-import kotlin.io.path.nameWithoutExtension
 
 class OutgoAggregatorService(private val articlesReaderService: ArticlesReaderService) {
 
+    private val service = EatingOutCounterService(articlesReaderService, { false })
+
     operator fun invoke(keyword: String): OutgoAggregationResult {
-        val aggregationResult = OutgoAggregationResult(keyword)
-        articlesReaderService.invoke()
-                .parallel()
-                .map { it.nameWithoutExtension to Files.readAllLines(it) }
-                .filter { it.first.startsWith(keyword) }
-                .map {
-                    var isOutGoLine = false
-                    for (line in it.second) {
-                        if (line.startsWith("#") && line.endsWith("家計簿")) {
-                            isOutGoLine = true
-                        }
-
-                        if (!isOutGoLine || !line.startsWith("|")) {
-                            continue
-                        }
-
-                        val items = line.split("|")
-                        val target = items[2]
-                        var price = 0
-                        if (target.endsWith(YEN_UNIT)) {
-                            val priceStr = target.substring(0, target.indexOf(YEN_UNIT)).trim().replace(",", "")
-                            if (priceStr.isNotBlank()) {
-                                price = Integer.parseInt(priceStr)
-                            }
-                            aggregationResult.add(it.first, items[0] + items[1].trim(), price)
-                        }
-                    }
-                }
-                .collect(Collectors.toList())
-        return aggregationResult
+        return service.invoke(keyword)
     }
 
     companion object {
