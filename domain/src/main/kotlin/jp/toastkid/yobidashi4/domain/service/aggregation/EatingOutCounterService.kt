5d922f462901ba8683f6f5b6b750c2695c357873
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-21 00:02:51 +0900

    refactor Modify initialization.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/EatingOutCounterService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/EatingOutCounterService.kt
@@ -3,12 +3,9 @@ package jp.toastkid.yobidashi4.domain.service.aggregation
 import jp.toastkid.yobidashi4.domain.model.aggregation.OutgoAggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 
-class EatingOutCounterService(
-    articlesReaderService: ArticlesReaderService,
-    additionalLineFilter: (String) -> Boolean = { it.contains(TARGET_LINE_LABEL).not() }
-) {
+class EatingOutCounterService(articlesReaderService: ArticlesReaderService) {
 
-    private val behavior = OutgoCalculationBehavior(articlesReaderService, additionalLineFilter)
+    private val behavior = OutgoCalculationBehavior(articlesReaderService, { it.contains(TARGET_LINE_LABEL).not() })
 
     operator fun invoke(keyword: String): OutgoAggregationResult {
         return behavior.invoke(keyword)
