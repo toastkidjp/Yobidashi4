8690863794612f1aa89ae0114eaf0a57a14f0be5
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-23 09:37:15 +0900

    refactor Add test cases.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefClientFactoryTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/CefClientFactoryTest.kt
@@ -3,23 +3,24 @@ package jp.toastkid.yobidashi4.infrastructure.service.web
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
-import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkConstructor
-import io.mockk.mockkObject
 import io.mockk.mockkStatic
+import io.mockk.slot
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
-import jp.toastkid.yobidashi4.domain.model.web.ad.AdHosts
 import jp.toastkid.yobidashi4.domain.model.web.user_agent.UserAgent
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import me.friwi.jcefmaven.CefAppBuilder
 import org.cef.CefApp
 import org.cef.CefClient
 import org.cef.browser.CefBrowser
+import org.cef.callback.CefStringVisitor
+import org.cef.handler.CefLifeSpanHandler
+import org.cef.handler.CefLoadHandler
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
@@ -31,11 +32,13 @@ import org.koin.dsl.module
 
 class CefClientFactoryTest {
 
-    @InjectMockKs
     private lateinit var subject: CefClientFactory
 
     @MockK
     private lateinit var findId: (CefBrowser?) -> String?
+
+    @MockK
+    private lateinit var cefAppFactory: CefAppFactory
 
     @MockK
     private lateinit var appSetting : Setting
@@ -56,6 +59,7 @@ class CefClientFactoryTest {
         startKoin {
             modules(
                 module {
+                    single(qualifier = null) { cefAppFactory } bind(CefAppFactory::class)
                     single(qualifier = null) { appSetting } bind(Setting::class)
                     single(qualifier = null) { viewModel } bind(MainViewModel::class)
                 }
@@ -63,13 +67,15 @@ class CefClientFactoryTest {
         }
         every { appSetting.darkMode() } returns true
         every { appSetting.userAgentName() } returns UserAgent.DEFAULT.name
+        every { cefAppFactory.invoke() } returns cefApp
 
-        mockkConstructor(CefSettingsApplier::class, CefAppBuilder::class)
+        mockkConstructor(CefSettingsApplier::class, CefAppBuilder::class, WebIconLoaderServiceImplementation::class)
         every { anyConstructed<CefSettingsApplier>().invoke(any(), any()) } just Runs
         every { anyConstructed<CefAppBuilder>().setInstallDir(any()) } just Runs
         every { anyConstructed<CefAppBuilder>().setProgressHandler(any()) } just Runs
         every { anyConstructed<CefAppBuilder>().cefSettings } returns mockk()
         every { anyConstructed<CefAppBuilder>().build() } returns cefApp
+        every { anyConstructed<WebIconLoaderServiceImplementation>().invoke(any(), any()) } just Runs
         every { cefApp.createClient() } returns client
         every { client.addLoadHandler(any()) } returns client
         every { client.addLifeSpanHandler(any()) } returns client
@@ -79,11 +85,10 @@ class CefClientFactoryTest {
         every { client.addKeyboardHandler(any()) } returns client
         every { client.addContextMenuHandler(any()) } returns client
 
-        mockkObject(AdHosts)
-        every { AdHosts.make() } returns AdHosts(emptySet())
-
         mockkStatic(CefApp::class)
         every { CefApp.addAppHandler(any()) } just Runs
+
+        subject = CefClientFactory()
     }
 
     @AfterEach
@@ -93,13 +98,36 @@ class CefClientFactoryTest {
     }
 
     @Test
-    fun invoke() {
+    fun checkAddLoadHandler() {
+        val loadHandlerSlot = slot<CefLoadHandler>()
+        every { client.addLoadHandler(capture(loadHandlerSlot)) } returns client
+
         val client = subject.invoke()
+        val browser = mockk<CefBrowser>()
+        every { browser.url } returns "https://www.yahoo.co.jp"
+        val sourceSlot = slot<CefStringVisitor>()
+        every { browser.getSource(capture(sourceSlot)) } just Runs
+        loadHandlerSlot.captured.onLoadingStateChange(browser, false, false, false)
+        sourceSlot.captured.visit("test")
 
         assertNotNull(client)
-        verify { anyConstructed<CefSettingsApplier>().invoke(any(), any()) }
-        verify { AdHosts.make() }
-        verify { CefApp.addAppHandler(any()) }
+        verify { client.addLoadHandler(any()) }
+        verify { browser.getSource(any()) }
+        verify { anyConstructed<WebIconLoaderServiceImplementation>().invoke(any(), any()) }
+    }
+
+    @Test
+    fun checkAddLifeSpanHandler() {
+        val loadHandlerSlot = slot<CefLifeSpanHandler>()
+        every { client.addLifeSpanHandler(capture(loadHandlerSlot)) } returns client
+        every { viewModel.openUrl(any(), any()) } just Runs
+
+        val client = subject.invoke()
+        loadHandlerSlot.captured.onBeforePopup(mockk(), mockk(), "https://www.yahoo.co.jp", "test")
+
+        assertNotNull(client)
+        verify { client.addLifeSpanHandler(any()) }
+        verify { viewModel.openUrl(any(), any()) }
     }
 
 }
