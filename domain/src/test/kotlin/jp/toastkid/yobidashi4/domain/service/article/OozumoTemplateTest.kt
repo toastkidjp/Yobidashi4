a1f4fe438d7e451cdf2e958e3ec41962142189bb
Toast Kid <super_q1m@yahoo.co.jp>
2024-01-29 08:44:26 +0900

    refactor Add test cases.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/article/OozumoTemplateTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/article/OozumoTemplateTest.kt
@@ -1,6 +1,7 @@
 package jp.toastkid.yobidashi4.domain.service.article
 
 import java.time.LocalDate
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -25,6 +26,20 @@ class OozumoTemplateTest {
         assertTrue(finalDate?.contains("### これより三役") ?: false)
         assertNull(subject.invoke(LocalDate.of(2024, 1, 29)))
         assertNull(subject.invoke(LocalDate.of(2024, 2, 15)))
+    }
+
+    @Test
+    fun aroundDays() {
+        (14..28).map { LocalDate.of(2024, 7, it) }.forEach {
+            assertTrue(subject.invoke(it)?.contains("大相撲七月場所") ?: false)
+        }
+    }
+
+    @Test
+    fun aroundMonths() {
+        val months = (1..12).map { subject.invoke(LocalDate.of(2024, it, 20)) }.filterNotNull()
+        assertEquals(6, months.size)
+        months.forEach { assertTrue(it.startsWith("## 大相撲")) }
     }
 
 }
