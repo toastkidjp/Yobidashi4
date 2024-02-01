30094512b5bcecfd90da3b0a9443e5648eb9b5f7
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-01 09:23:54 +0900

    refactor Remove annotation.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/component/WebSearchBox.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/component/WebSearchBox.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
@@ -45,7 +44,6 @@ import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import jp.toastkid.yobidashi4.domain.model.web.search.SearchSite
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 internal fun WebSearchBox() {
     val viewModel = remember { WebSearchBoxViewModel() }
