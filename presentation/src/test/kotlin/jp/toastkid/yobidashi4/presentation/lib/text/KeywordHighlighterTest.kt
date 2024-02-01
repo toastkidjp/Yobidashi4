5ad13b6c39bb9dd9565ac2337d121af521928b5c
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-01 09:28:09 +0900

    refactor Add test case.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/text/KeywordHighlighterTest.kt
@@ -0,0 +1,40 @@
+package jp.toastkid.yobidashi4.presentation.lib.text
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class KeywordHighlighterTest {
+
+    private lateinit var subject: KeywordHighlighter
+
+    @BeforeEach
+    fun setUp() {
+        subject = KeywordHighlighter()
+    }
+
+    @Test
+    fun annotate() {
+        val annotate = subject.invoke("It longs to ~~make~~ it.", "long")
+
+        Assertions.assertEquals(2, annotate.spanStyles.size)
+        Assertions.assertEquals("It longs to make it.", annotate.text)
+    }
+
+    @Test
+    fun annotate2() {
+        val annotate = subject.invoke("It **longs** to make it.", "long")
+
+        Assertions.assertEquals(2, annotate.spanStyles.size)
+        Assertions.assertEquals("It longs to make it.", annotate.text)
+    }
+
+    @Test
+    fun annotate3() {
+        val annotate = subject.invoke("It longs ***to*** make it.", "long")
+
+        Assertions.assertEquals(2, annotate.spanStyles.size)
+        Assertions.assertEquals("It longs to make it.", annotate.text)
+    }
+
+}
