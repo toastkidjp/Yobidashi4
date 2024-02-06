b4520ec9499b2da5a155a2054b39f307759db1b4
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-07 00:06:12 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/ImageExtractorTest.kt
@@ -0,0 +1,13 @@
+package jp.toastkid.yobidashi4.domain.service.slideshow
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ImageExtractorTest {
+
+    @Test
+    fun elseCase() {
+        assertTrue(ImageExtractor().invoke(null).isEmpty())
+    }
+
+}
