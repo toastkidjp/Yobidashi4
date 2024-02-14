8218619d892bb95ef64e1ce4261d4d4f80fa202c
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-14 09:18:56 +0900

    refactor Format code.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FindOrder.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FindOrder.kt
@@ -1,13 +1,13 @@
 package jp.toastkid.yobidashi4.presentation.editor.finder
 
 data class FindOrder(
-        val target: String,
-        val replace: String,
-        val upper: Boolean = false,
-        val invokeReplace: Boolean = false,
-        val caseSensitive: Boolean = true
+    val target: String,
+    val replace: String,
+    val upper: Boolean = false,
+    val invokeReplace: Boolean = false,
+    val caseSensitive: Boolean = true
 ) {
-        companion object {
-                val EMPTY = FindOrder("", "")
-        }
+    companion object {
+        val EMPTY = FindOrder("", "")
+    }
 }
