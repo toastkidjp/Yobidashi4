ccffa3ad44faff960f95a85e8f4baa444439992b
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-15 08:38:59 +0900

    feature Make masking-count dropdownMenu scrollable.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceView.kt
@@ -9,6 +9,7 @@
 package jp.toastkid.yobidashi4.presentation.number
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.awaitEachGesture
@@ -20,7 +21,10 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
@@ -228,22 +232,30 @@ private fun AppBarContent(
             DropdownMenu(
                 openingMaskingCount,
                 onDismissRequest = { closeMaskingCount() }) {
-                (1..64).forEach {
-                    DropdownMenuItem(
-                        onClick = {
-                            setMaskingCount(it)
-                        closeMaskingCount()
-                            viewModel.setMaskingCount(it)
-                            deleteGame()
-                        renewGame()
-                        //contentViewModel?.nextRoute("tool/number/place")
-                    }) {
-                        Text(
-                            text = "$it",
-                            fontSize = viewModel.fontSize(),
-                            textAlign = TextAlign.Center
-                        )
+                val lazyListState = rememberLazyListState()
+                Box(modifier = Modifier.width(100.dp).height(600.dp)) {
+                    LazyColumn(state = lazyListState) {
+                        items(64) {
+                            val count = it + 1
+                            DropdownMenuItem(
+                                onClick = {
+                                    setMaskingCount(count)
+                                    closeMaskingCount()
+                                    viewModel.setMaskingCount(count)
+                                    deleteGame()
+                                    renewGame()
+                                    //contentViewModel?.nextRoute("tool/number/place")
+                                }) {
+                                Text(
+                                    text = "$count",
+                                    fontSize = viewModel.fontSize(),
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        }
                     }
+                    VerticalScrollbar(adapter = rememberScrollbarAdapter(lazyListState), modifier = Modifier.fillMaxHeight().align(
+                        Alignment.CenterEnd))
                 }
             }
         }
