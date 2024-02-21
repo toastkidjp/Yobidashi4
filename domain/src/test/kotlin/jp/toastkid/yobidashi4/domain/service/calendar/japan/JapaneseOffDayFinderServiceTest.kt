9033b5bff44dd462b070ef864f369c4a9d7879bb
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-22 00:02:10 +0900

    refactor Add test case

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/japan/JapaneseOffDayFinderServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/japan/JapaneseOffDayFinderServiceTest.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.domain.service.calendar.japan
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
@@ -20,5 +21,9 @@ class JapaneseOffDayFinderServiceTest {
         assertNotNull(japaneseOffDayFinderService.invoke(2014, 5).firstOrNull { it.day == 6 })
         assertNotNull(japaneseOffDayFinderService.invoke(2015, 5).firstOrNull { it.day == 6 })
         assertNull(japaneseOffDayFinderService.invoke(2016, 5).firstOrNull { it.day == 6 })
+
+        assertEquals(10, japaneseOffDayFinderService.invoke(2020, 8).firstOrNull()?.day)
+        assertEquals(9, japaneseOffDayFinderService.invoke(2021, 8).firstOrNull()?.day)
+        assertEquals(11, japaneseOffDayFinderService.invoke(2022, 8).firstOrNull()?.day)
     }
 }
