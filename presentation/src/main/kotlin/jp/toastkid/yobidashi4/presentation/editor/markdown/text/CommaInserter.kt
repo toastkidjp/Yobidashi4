699a72f4ee364b2285a0e61cb546ab974429fc6a
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-24 09:15:11 +0900

    refactor Add guard.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/CommaInserter.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/CommaInserter.kt
@@ -3,7 +3,11 @@ package jp.toastkid.yobidashi4.presentation.editor.markdown.text
 class CommaInserter {
 
     operator fun invoke(text: String?): String? {
-        val toCharArray = text?.toCharArray() ?: return null
+        if (text == null) {
+            return null
+        }
+
+        val toCharArray = text.toCharArray()
         return with(StringBuilder()) {
             (toCharArray.indices).forEach { index ->
                 if (toCharArray.size > NUMBER_OF_DIGITS && isNotEmpty() && index % NUMBER_OF_DIGITS == (toCharArray.size % NUMBER_OF_DIGITS)) {
