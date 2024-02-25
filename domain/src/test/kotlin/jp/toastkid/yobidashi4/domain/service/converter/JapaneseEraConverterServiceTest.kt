116a4f4a3101d0fcbad35502d9f303dc843dc964
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-26 08:34:08 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/JapaneseEraConverterServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/JapaneseEraConverterServiceTest.kt
@@ -3,6 +3,7 @@ package jp.toastkid.yobidashi4.domain.service.converter
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -46,12 +47,18 @@ class JapaneseEraConverterServiceTest {
 
     @Test
     fun firstInputAction() {
+        assertNull(japaneseEraConverterService.firstInputAction("63"))
+        assertNull(japaneseEraConverterService.firstInputAction("63昭和"))
+        assertEquals("1900", japaneseEraConverterService.firstInputAction("明治33"))
+        assertEquals("1914", japaneseEraConverterService.firstInputAction("大正2"))
         assertEquals("1988", japaneseEraConverterService.firstInputAction("昭和63"))
+        assertEquals("2018", japaneseEraConverterService.firstInputAction("平成30"))
         assertEquals("2019", japaneseEraConverterService.firstInputAction("令和1"))
     }
 
     @Test
     fun secondInputAction() {
+        assertNull(japaneseEraConverterService.secondInputAction("昭和"))
         assertEquals("明治44", japaneseEraConverterService.secondInputAction("1911"))
         assertEquals("大正1", japaneseEraConverterService.secondInputAction("1912"))
         assertEquals("大正14", japaneseEraConverterService.secondInputAction("1925"))
