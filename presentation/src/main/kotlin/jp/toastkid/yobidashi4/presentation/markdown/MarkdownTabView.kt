a040868a918d1daf2de8ffb91c2bf5aede4796ab
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-27 00:03:53 +0900

    refactor Merge 2 effect.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabView.kt
@@ -5,13 +5,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import jp.toastkid.yobidashi4.domain.model.tab.MarkdownPreviewTab
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -19,6 +20,7 @@ import org.koin.core.component.inject
 internal fun MarkdownTabView(tab: MarkdownPreviewTab, modifier: Modifier) {
     val scrollState = rememberScrollState()
     val focusRequester = remember { FocusRequester() }
+    val coroutineScope = rememberCoroutineScope()
 
     Surface(
         color = MaterialTheme.colors.surface.copy(alpha = 0.5f),
@@ -26,13 +28,13 @@ internal fun MarkdownTabView(tab: MarkdownPreviewTab, modifier: Modifier) {
     ) {
         MarkdownPreview(tab.markdown(), scrollState, modifier)
 
-        LaunchedEffect(tab) {
-            scrollState.scrollTo(tab.scrollPosition())
+        DisposableEffect(tab) {
+            coroutineScope.launch {
+                scrollState.scrollTo(tab.scrollPosition())
+            }
 
             focusRequester.requestFocus()
-        }
 
-        DisposableEffect(tab) {
             onDispose {
                 val mainViewModel = object : KoinComponent { val vm: MainViewModel by inject() }.vm
 
