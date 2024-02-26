bc246ab25263ce77c5779dddb5cae826ae1b5ac1
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-27 00:02:08 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/JapaneseAgeConverterServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/JapaneseAgeConverterServiceTest.kt
@@ -4,9 +4,11 @@ import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import java.time.LocalDate
+import java.time.chrono.JapaneseDate
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -18,6 +20,8 @@ class JapaneseAgeConverterServiceTest {
     fun setUp() {
         japaneseAgeConverterService = JapaneseAgeConverterService()
 
+        mockkStatic(JapaneseDate::class)
+        every { JapaneseDate.now() }.returns(JapaneseDate.of(2023, 8, 27))
         mockkStatic(LocalDate::class)
         every { LocalDate.now() }.returns(LocalDate.of(2023, 8, 27))
     }
@@ -54,12 +58,19 @@ class JapaneseAgeConverterServiceTest {
 
     @Test
     fun firstInputAction() {
+        assertNull(japaneseAgeConverterService.firstInputAction("大正"))
+        assertNull(japaneseAgeConverterService.firstInputAction("1984"))
         assertEquals("35", japaneseAgeConverterService.firstInputAction("昭和63"))
+        assertEquals("33", japaneseAgeConverterService.firstInputAction("平成2"))
         assertEquals("4", japaneseAgeConverterService.firstInputAction("令和1"))
+        assertEquals("113", japaneseAgeConverterService.firstInputAction("明治43"))
+        assertEquals("99", japaneseAgeConverterService.firstInputAction("大正13"))
+
     }
 
     @Test
     fun secondInputAction() {
+        assertNull(japaneseAgeConverterService.secondInputAction("大正"))
         assertEquals("大正14", japaneseAgeConverterService.secondInputAction("98"))
         assertEquals("大正15", japaneseAgeConverterService.secondInputAction("97"))
         assertEquals("昭和2", japaneseAgeConverterService.secondInputAction("96"))
