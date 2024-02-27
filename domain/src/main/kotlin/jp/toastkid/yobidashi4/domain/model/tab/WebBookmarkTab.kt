b0370283291537a552af98e24e5d355cc11f3dc0
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-28 00:03:26 +0900

    refactor Clean up code.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebBookmarkTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebBookmarkTab.kt
@@ -1,6 +1,6 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
-class WebBookmarkTab(private val scrollPosition:Int = 0) : ScrollableContentTab {
+class WebBookmarkTab(private val scrollPosition: Int = 0) : ScrollableContentTab {
 
     override fun title(): String = "Bookmark"
 
