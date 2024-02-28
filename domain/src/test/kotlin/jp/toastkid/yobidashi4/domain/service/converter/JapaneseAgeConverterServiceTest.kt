977684c2e766b741387e4151e2316e8af0a9432d
Toast Kid <super_q1m@yahoo.co.jp>
2024-02-29 00:02:34 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/JapaneseAgeConverterServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/JapaneseAgeConverterServiceTest.kt
@@ -60,12 +60,12 @@ class JapaneseAgeConverterServiceTest {
     fun firstInputAction() {
         assertNull(japaneseAgeConverterService.firstInputAction("大正"))
         assertNull(japaneseAgeConverterService.firstInputAction("1984"))
+        assertNull(japaneseAgeConverterService.firstInputAction("大正浪漫"))
         assertEquals("35", japaneseAgeConverterService.firstInputAction("昭和63"))
         assertEquals("33", japaneseAgeConverterService.firstInputAction("平成2"))
         assertEquals("4", japaneseAgeConverterService.firstInputAction("令和1"))
         assertEquals("113", japaneseAgeConverterService.firstInputAction("明治43"))
         assertEquals("99", japaneseAgeConverterService.firstInputAction("大正13"))
-
     }
 
     @Test
