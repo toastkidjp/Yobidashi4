af14bbaa4b203b5309d51b280c6e1eca2d5fd859
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-01 00:01:41 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/text/KeywordHighlighterTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/text/KeywordHighlighterTest.kt
@@ -1,6 +1,8 @@
 package jp.toastkid.yobidashi4.presentation.lib.text
 
+import androidx.compose.ui.text.style.TextDecoration.Companion.Underline
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -43,6 +45,26 @@ class KeywordHighlighterTest {
 
         Assertions.assertEquals(1, annotate.spanStyles.size)
         Assertions.assertEquals("It longs to make it.", annotate.text)
+    }
+
+    @Test
+    fun link() {
+        val annotate = subject.invoke("It [longs](https://www.yahoo.co.jp) to make it.", null)
+
+        Assertions.assertEquals(1, annotate.spanStyles.size)
+        assertTrue(annotate.spanStyles.any { it.item.textDecoration == Underline })
+        Assertions.assertEquals("It longs to make it.", annotate.text)
+    }
+
+    @Test
+    fun link2() {
+        val annotate = subject.invoke(
+            "- [Easter Egg in APK Files: What Is Frosting](https://bi-zone.medium.com/easter-egg-in-apk-files-what-is-frosting-f356aa9f4d1)……いわゆる雪エフェクトの実装方法についてだった",
+            "雪"
+        )
+        Assertions.assertEquals(2, annotate.spanStyles.size)
+        assertTrue(annotate.spanStyles.any { it.item.textDecoration == Underline })
+        Assertions.assertEquals("- Easter Egg in APK Files: What Is Frosting……いわゆる雪エフェクトの実装方法についてだった", annotate.text)
     }
 
 }
