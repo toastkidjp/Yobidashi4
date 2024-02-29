358f5f5ace149d4fed4dcd4918ada3013317feb0
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-01 00:03:22 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/DistanceConverterServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/converter/DistanceConverterServiceTest.kt
@@ -3,6 +3,7 @@ package jp.toastkid.yobidashi4.domain.service.converter
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.InjectMockKs
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -30,6 +31,12 @@ class DistanceConverterServiceTest {
     fun test() {
         assertEquals("1600.00", distanceConverterService.firstInputAction("20"))
         assertEquals("40.00", distanceConverterService.secondInputAction("3200"))
+    }
+
+    @Test
+    fun incorrectInput() {
+        assertNull(distanceConverterService.firstInputAction("あ"))
+        assertNull(distanceConverterService.secondInputAction("あ"))
     }
 
 }
