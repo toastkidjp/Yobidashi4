224aa08687dbe7f81380635885cd938fd657512b
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-04 08:32:45 +0900

    feature Improve webView switching.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
@@ -12,8 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.onSizeChanged
 import java.awt.Color
+import java.awt.Container
+import java.awt.Dimension
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
+import kotlin.math.max
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -27,23 +31,39 @@ internal fun WebTabView(tab: WebTab) {
 
     val viewModel = remember { WebTabViewModel() }
 
+    val component = remember { val panel = Container()
+        panel
+    }
+
+    LaunchedEffect(tab.id()) {
+        component.background = background
+        component.removeAll()
+        val component1 = viewModel.component(tab, background)
+        //component1.size = Dimension(component.parent.width, component.parent.height)
+        component.add(component1)
+
+        withContext(Dispatchers.IO) {
+            viewModel.start(tab.id())
+        }
+    }
+
     Column {
         SwingPanel(
             factory = {
-                viewModel.component(tab, background)
+                component
             },
             modifier = Modifier
                 .fillMaxSize()
                 .weight(1f)
                 .focusRequester(viewModel.focusRequester())
+                .onSizeChanged {
+                    if (component.components.isEmpty()) {
+                        return@onSizeChanged
+                    }
+                    component.getComponent(0).size = Dimension(max(1, it.width - 40), it.height)
+                }
         )
 
         Spacer(modifier = Modifier.height(viewModel.spacerHeight()))
-    }
-
-    LaunchedEffect(tab.id()) {
-        withContext(Dispatchers.IO) {
-            viewModel.start(tab.id())
-        }
     }
 }
