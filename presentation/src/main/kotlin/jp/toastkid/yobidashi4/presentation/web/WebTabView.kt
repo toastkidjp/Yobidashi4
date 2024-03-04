35582cd3facd7cb884fd48c3cf7f2a8421f6b478
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-05 00:21:36 +0900

    refactor Return component directly.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
@@ -30,8 +30,7 @@ internal fun WebTabView(tab: WebTab) {
     val viewModel = remember { WebTabViewModel() }
 
     val component = remember {
-        val container = JPanel(CardLayout())
-        container
+        JPanel(CardLayout())
     }
 
     LaunchedEffect(tab.id()) {
