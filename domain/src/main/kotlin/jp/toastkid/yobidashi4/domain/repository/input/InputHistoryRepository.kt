8d3334e2b6fe101b7970fbd65b15d6bb3179c2ac
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-13 08:53:42 +0900

    feature Add repository interface.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/input/InputHistoryRepository.kt
@@ -0,0 +1,17 @@
+package jp.toastkid.yobidashi4.domain.repository.input
+
+import jp.toastkid.yobidashi4.domain.model.input.InputHistory
+
+interface InputHistoryRepository {
+
+    fun list(): List<InputHistory>
+
+    fun filter(query: String?): List<InputHistory>
+
+    fun add(item: InputHistory)
+
+    fun delete(item: InputHistory)
+
+    fun clear()
+
+}
