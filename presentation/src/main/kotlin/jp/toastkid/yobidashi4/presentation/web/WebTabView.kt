eca7b3600116f7bdbd7a7157fb29553e8b0cebf4
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-21 08:19:06 +0900

    feature Fix WebTabView's implementation.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabView.kt
@@ -12,10 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.layout.onPlaced
-import java.awt.CardLayout
 import java.awt.Color
-import javax.swing.JPanel
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -30,32 +27,23 @@ internal fun WebTabView(tab: WebTab) {
 
     val viewModel = remember { WebTabViewModel() }
 
-    val component = remember { JPanel(CardLayout()) }
-
     Column {
         SwingPanel(
             factory = {
-                component
+                val webUiComponent = viewModel.component(tab)
+                webUiComponent.background = background
+                webUiComponent
             },
             modifier = Modifier
                 .fillMaxSize()
                 .weight(1f)
                 .focusRequester(viewModel.focusRequester())
-                .onPlaced {
-                    component.revalidate()
-                }
         )
 
         Spacer(modifier = Modifier.height(viewModel.spacerHeight()))
     }
 
     LaunchedEffect(tab.id()) {
-        component.background = background
-        component.removeAll()
-        val webUiComponent = viewModel.component(tab)
-        webUiComponent.background = background
-        component.add(webUiComponent)
-
         withContext(Dispatchers.Unconfined) {
             viewModel.start(tab.id())
         }
