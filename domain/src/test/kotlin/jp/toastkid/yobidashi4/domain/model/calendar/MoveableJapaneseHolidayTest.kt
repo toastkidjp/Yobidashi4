6a37c78d5a27aa07ec32de044fe61c6890c3b000
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-22 08:34:03 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/MoveableJapaneseHolidayTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/MoveableJapaneseHolidayTest.kt
@@ -34,6 +34,34 @@ internal class MoveableJapaneseHolidayTest {
                 assertEquals(21, agedDay2015.day)
             },
             {
+                val holidays = MoveableJapaneseHoliday.find(2020, 7)
+                val marineDay = holidays.first()
+                assertEquals(MoveableJapaneseHoliday.MARINE_DAY.title, marineDay.title)
+                assertEquals(23, marineDay.day)
+                val sportsDay = holidays.last()
+                assertEquals(MoveableJapaneseHoliday.SPORTS_DAY.title, sportsDay.title)
+                assertEquals(24, sportsDay.day)
+            },
+            {
+                val holidays = MoveableJapaneseHoliday.find(2021, 7)
+                val marineDay = holidays.first()
+                assertEquals(MoveableJapaneseHoliday.MARINE_DAY.title, marineDay.title)
+                assertEquals(22, marineDay.day)
+                val sportsDay = holidays.last()
+                assertEquals(MoveableJapaneseHoliday.SPORTS_DAY.title, sportsDay.title)
+                assertEquals(23, sportsDay.day)
+            },
+            {
+                assertTrue(MoveableJapaneseHoliday.find(2020, 10).isEmpty())
+            },
+            {
+                assertTrue(MoveableJapaneseHoliday.find(2021, 10).isEmpty())
+            },
+            {
+                val holidays = MoveableJapaneseHoliday.find(2022, 7)
+                assertEquals(1, holidays.size)
+            },
+            {
                 val agedDay2023 = MoveableJapaneseHoliday.find(2023, 9).first()
                 assertEquals(MoveableJapaneseHoliday.RESPECT_FOR_THE_AGED_DAY.title, agedDay2023.title)
                 assertEquals(18, agedDay2023.day)
