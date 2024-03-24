18d56060f4dcbed91bd3d12590bd8d35dcd12be3
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-24 10:06:02 +0900

    refactor Delete ueless field.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownPreviewViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownPreviewViewModel.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.isCtrlPressed
 import androidx.compose.ui.input.key.key
 import jp.toastkid.yobidashi4.presentation.lib.KeyboardScrollAction
-import jp.toastkid.yobidashi4.presentation.lib.text.KeywordHighlighter
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import org.koin.core.component.KoinComponent
@@ -17,12 +16,8 @@ class MarkdownPreviewViewModel(scrollState: ScrollableState) : KoinComponent {
 
     private val scrollAction = KeyboardScrollAction(scrollState)
 
-    private val keywordHighlighter = KeywordHighlighter()
-
     fun onKeyEvent(coroutineScope: CoroutineScope, it: KeyEvent): Boolean {
         return scrollAction(coroutineScope, it.key, it.isCtrlPressed)
     }
-
-    fun annotate(text: String, finderTarget: String?) = keywordHighlighter(text, finderTarget)
 
 }
