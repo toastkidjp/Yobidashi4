4c09ab5b2bcc0dd700957e1e71da5e0b0535f5c9
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-24 10:08:02 +0900

    refactor Safe delete 'mainViewModel'.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownPreviewViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownPreviewViewModel.kt
@@ -5,14 +5,10 @@ import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.isCtrlPressed
 import androidx.compose.ui.input.key.key
 import jp.toastkid.yobidashi4.presentation.lib.KeyboardScrollAction
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 class MarkdownPreviewViewModel(scrollState: ScrollableState) : KoinComponent {
-
-    private val mainViewModel: MainViewModel by inject()
 
     private val scrollAction = KeyboardScrollAction(scrollState)
 
