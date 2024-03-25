4c067a6dccc79edf87c2858e0d6f0021b3138d24
Toast Kid <super_q1m@yahoo.co.jp>
2024-03-26 08:49:06 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownPreviewViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownPreviewViewModelTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.text.font.FontWeight
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.called
@@ -13,14 +14,23 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkConstructor
+import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
+import java.awt.Image
+import java.awt.image.BufferedImage
+import java.io.IOException
+import java.net.URL
+import javax.imageio.ImageIO
 import jp.toastkid.yobidashi4.presentation.lib.text.KeywordHighlighter
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -51,6 +61,9 @@ class MarkdownPreviewViewModelTest {
 
         mockkConstructor(KeywordHighlighter::class)
         every { anyConstructed<KeywordHighlighter>().invoke(any(), any()) } returns mockk()
+
+        mockkStatic(ImageIO::class)
+        every { ImageIO.read(any<URL>()) } returns BufferedImage(1, 1, Image.SCALE_FAST)
 
         subject = MarkdownPreviewViewModel(ScrollState(0))
     }
@@ -123,6 +136,45 @@ class MarkdownPreviewViewModelTest {
 
         assertFalse(consumed)
         verify { mainViewModel wasNot called }
+    }
+
+    @Test
+    fun extractText() {
+        assertEquals("test", subject.extractText("test", false))
+        assertEquals("- [ ] test", subject.extractText("- [ ] test", false))
+        assertEquals(" test", subject.extractText("- [ ] test", true))
+    }
+
+    @Test
+    fun loadBitmapWithIncorrectUrl() {
+        val bitmap = subject.loadBitmap("test")
+
+        assertNull(bitmap)
+        verify(inverse = true) { ImageIO.read(any<URL>()) }
+    }
+
+    @Test
+    fun loadBitmap() {
+        val bitmap = subject.loadBitmap("https://test")
+
+        assertNotNull(bitmap)
+        verify { ImageIO.read(any<URL>()) }
+    }
+
+    @Test
+    fun loadBitmapWithExceptionCase() {
+        every { ImageIO.read(any<URL>()) } throws mockk<IOException>()
+
+        val bitmap = subject.loadBitmap("https://test")
+
+        assertNull(bitmap)
+        verify { ImageIO.read(any<URL>()) }
+    }
+
+    @Test
+    fun makeFontWeight() {
+        assertEquals(FontWeight.Normal, subject.makeFontWeight(-1))
+        assertEquals(FontWeight.Bold, subject.makeFontWeight(0))
     }
 
 }
