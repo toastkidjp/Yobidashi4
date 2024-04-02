0e793c457c2e94f4092125179dba83675cf648ac
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-03 08:36:00 +0900

    feature Implement service.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatServiceImplementation.kt
@@ -0,0 +1,48 @@
+package jp.toastkid.yobidashi4.infrastructure.service.chat
+
+import java.util.concurrent.atomic.AtomicReference
+import jp.toastkid.yobidashi4.domain.model.chat.Chat
+import jp.toastkid.yobidashi4.domain.model.chat.ChatMessage
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import jp.toastkid.yobidashi4.domain.repository.chat.ChatRepository
+import jp.toastkid.yobidashi4.domain.service.chat.ChatService
+import org.koin.core.annotation.Single
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.parameter.ParametersHolder
+
+@Single
+class ChatServiceImplementation : ChatService, KoinComponent {
+
+    private val chatHolder: AtomicReference<Chat> = AtomicReference(Chat())
+
+    private val setting: Setting by inject()
+
+    private val repository: ChatRepository by inject(parameters = { ParametersHolder(mutableListOf(setting.chatApiKey())) })
+
+    override fun send(text: String): String? {
+        val chat = chatHolder.get()
+        chat.addUserText(text)
+
+        val response = repository.request(chat.makeContent())
+
+        if (response != null) {
+            chat.addModelText(response)
+        }
+
+        return response
+    }
+
+    override fun setChat(chat: Chat) {
+        chatHolder.set(chat)
+    }
+
+    override fun getChat(): Chat {
+        return chatHolder.get()
+    }
+
+    override fun messages(): List<ChatMessage> {
+        return chatHolder.get().list()
+    }
+
+}
