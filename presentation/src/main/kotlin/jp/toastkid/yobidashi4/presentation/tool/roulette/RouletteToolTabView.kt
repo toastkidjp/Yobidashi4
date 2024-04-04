df4b810deddaf08f3da1521131850b57831e063a
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-05 08:31:49 +0900

    refactor Use common component.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/roulette/RouletteToolTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/roulette/RouletteToolTabView.kt
@@ -4,23 +4,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import jp.toastkid.yobidashi4.presentation.component.MultiLineTextField
 
 @Composable
 fun RouletteToolTabView() {
@@ -54,25 +49,12 @@ fun RouletteToolTabView() {
                 Text("Start")
             }
 
-            TextField(
+            MultiLineTextField(
                 viewModel.input(),
-                colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.Transparent),
-                label = { Text("Base file name") },
-                onValueChange = viewModel::onValueChange,
-                trailingIcon = {
-                    Icon(
-                        painterResource("images/icon/ic_clear_form.xml"),
-                        contentDescription = "Clear input.",
-                        tint = MaterialTheme.colors.primary,
-                        modifier = Modifier.clickable(onClick = viewModel::clearInput)
-                    )
-                },
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        viewModel.roulette()
-                    }
-                ),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                "Base file name",
+                Int.MAX_VALUE,
+                viewModel::onValueChange,
+                viewModel::clearInput
             )
         }
     }
