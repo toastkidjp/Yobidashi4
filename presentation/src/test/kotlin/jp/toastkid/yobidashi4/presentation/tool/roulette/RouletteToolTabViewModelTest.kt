7298f9360b49c53f0e9b139c2082490f9af7f569
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-06 09:38:59 +0900

    refactor Add test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/roulette/RouletteToolTabViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/roulette/RouletteToolTabViewModelTest.kt
@@ -2,14 +2,19 @@ package jp.toastkid.yobidashi4.presentation.tool.roulette
 
 import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.Runs
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.just
+import io.mockk.mockk
 import io.mockk.mockkConstructor
+import io.mockk.spyk
 import io.mockk.unmockkAll
 import io.mockk.verify
+import java.awt.event.KeyEvent
 import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -60,6 +65,45 @@ class RouletteToolTabViewModelTest {
         subject.clipResult()
 
         verify(inverse = true) { anyConstructed<ClipboardPutterService>().invoke(any<String>()) }
+    }
+
+    @Test
+    fun onKeyEvent() {
+        subject = spyk(subject)
+        coEvery { subject.roulette() } just Runs
+        subject.onValueChange(TextFieldValue("test"))
+
+        val consumed = subject.onKeyEvent(
+            androidx.compose.ui.input.key.KeyEvent(
+                KeyEvent(
+                    mockk(),
+                    KeyEvent.KEY_RELEASED,
+                    1,
+                    KeyEvent.CTRL_DOWN_MASK,
+                    KeyEvent.VK_ENTER,
+                    'E'
+                )
+            )
+        )
+
+        assertTrue(consumed)
+    }
+
+    @Test
+    fun onKeyEventOtherKey() {
+        val consumed = subject.onKeyEvent(
+            androidx.compose.ui.input.key.KeyEvent(
+                KeyEvent(
+                    mockk(),
+                    KeyEvent.KEY_RELEASED,
+                    1,
+                    KeyEvent.CTRL_DOWN_MASK,
+                    KeyEvent.VK_1,
+                    'A'
+                )
+            )
+        )
+        assertFalse(consumed)
     }
 
 }
