7613224b1cb35ab2b6576821b925cf29500dcf06
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-08 08:03:05 +0900

    feature Add safetySettings.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/chat/Chat.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/chat/Chat.kt
@@ -2,12 +2,24 @@ package jp.toastkid.yobidashi4.domain.model.chat
 
 data class Chat(private val texts: MutableList<ChatMessage> = mutableListOf()) {
 
+    private val contentsHolder = mutableListOf<Pair<String, List<String>>>()
+
     fun addUserText(text: String) {
         texts.add(ChatMessage("user", text))
+        contentsHolder.add("user" to listOf(text))
     }
 
     fun addModelText(text: String) {
-        texts.add(ChatMessage("model", text))
+        val element = ChatMessage("model", text)
+        texts.add(element)
+    }
+
+    fun addModelTexts(texts: List<String>) {
+        if (texts.isEmpty()) {
+            contentsHolder.removeLast()
+            return
+        }
+        contentsHolder.add("model" to texts)
     }
 
     fun list(): List<ChatMessage> = texts
@@ -15,7 +27,13 @@ data class Chat(private val texts: MutableList<ChatMessage> = mutableListOf()) {
     fun makeContent() = """
       {
         "contents": [
-          ${list().map { "{\"role\":\"${it.role}\", \"parts\":[{ \"text\": \"${it.text}\"}]}" }.joinToString(",")}
+          ${contentsHolder.map { "{\"role\":\"${it.first}\", \"parts\":[ ${it.second.map { "{ \"text\": \"${it}\"}" }.joinToString(",")} ]}" }.joinToString(",")}
+        ],
+        "safetySettings": [
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_ONLY_HIGH"
+            }
         ]
       }
     """.trimIndent()
