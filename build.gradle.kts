80e080fc3e0d9026f0e04cb59aa1a494f0104208
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-13 07:02:28 +0900

    feature Upgrade Compose to 1.6.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.10"
-    id("org.jetbrains.compose") version "1.6.1"
+    id("org.jetbrains.compose") version "1.6.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
     // Apply the application plugin to add support for building a CLI application in Java.
     id("com.google.devtools.ksp") version "1.9.10-1.0.13"
