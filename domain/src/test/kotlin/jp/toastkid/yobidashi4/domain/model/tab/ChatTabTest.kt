30bd5c9e544af76838aa32427148480b6fb00082
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-16 08:13:48 +0900

    refactor Add unit test.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/tab/ChatTabTest.kt
@@ -0,0 +1,47 @@
+package jp.toastkid.yobidashi4.domain.model.tab
+
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import jp.toastkid.yobidashi4.domain.model.chat.Chat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ChatTabTest {
+
+    private lateinit var subject: ChatTab
+
+    @MockK
+    private lateinit var chat: Chat
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        subject = ChatTab(chat)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun title() {
+        assertTrue(subject.title().isNotEmpty())
+    }
+
+    @Test
+    fun iconPath() {
+        assertTrue(subject.iconPath()?.isNotEmpty() ?: false)
+    }
+
+    @Test
+    fun chat() {
+        assertSame(chat, subject.chat())
+    }
+
+}
