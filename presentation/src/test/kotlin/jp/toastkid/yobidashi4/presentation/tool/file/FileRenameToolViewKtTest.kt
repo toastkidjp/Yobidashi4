95bd5c762500850a5e1ec7041d64aaafe6bd5fbf
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-16 08:13:33 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewKtTest.kt
@@ -0,0 +1,44 @@
+package jp.toastkid.yobidashi4.presentation.tool.file
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runDesktopComposeUiTest
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import java.nio.file.Path
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FileRenameToolViewKtTest {
+
+    @BeforeEach
+    fun setUp() {
+        mockkConstructor(FileRenameToolViewModel::class)
+        val element = mockk<Path>()
+        every { element.fileName } returns element
+        every { element.toString() } returns "test"
+        every { anyConstructed<FileRenameToolViewModel>().items() } returns listOf(element)
+        every { anyConstructed<FileRenameToolViewModel>().launchedEffectKey() } returns 1
+        coEvery { anyConstructed<FileRenameToolViewModel>().collectDroppedPaths() } just Runs
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun fileRenameToolView() {
+        runDesktopComposeUiTest {
+            setContent {
+                FileRenameToolView()
+            }
+        }
+    }
+}
