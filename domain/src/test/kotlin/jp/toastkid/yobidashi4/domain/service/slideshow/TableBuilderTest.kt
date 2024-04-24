1381b2234e600d0a81539d9be57a144cdadf5a8b
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-25 08:42:41 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/TableBuilderTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/TableBuilderTest.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.domain.service.slideshow
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -8,6 +9,19 @@ class TableBuilderTest {
     @Test
     fun elseCase() {
         assertTrue(TableBuilder().build().header.isEmpty())
+    }
+
+    @Test
+    fun active() {
+        val tableBuilder = TableBuilder()
+
+        assertFalse(tableBuilder.active())
+
+        tableBuilder.setActive()
+        assertTrue(tableBuilder.active())
+
+        tableBuilder.setInactive()
+        assertFalse(tableBuilder.active())
     }
 
 }
