0d88a8d6f53ed6c542e754a5bcb119f89dd288ed
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-26 07:14:00 +0900

    refactor Use divided transformation.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/loan/viewmodel/LoanCalculatorViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/loan/viewmodel/LoanCalculatorViewModel.kt
@@ -3,15 +3,11 @@ package jp.toastkid.yobidashi4.presentation.loan.viewmodel
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.OffsetMapping
-import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
-import java.math.BigDecimal
-import java.text.DecimalFormat
 import jp.toastkid.yobidashi4.domain.model.loan.Factor
 import jp.toastkid.yobidashi4.domain.model.loan.PaymentDetail
 import jp.toastkid.yobidashi4.domain.service.loan.DebouncedCalculatorService
+import jp.toastkid.yobidashi4.presentation.component.DecimalVisualTransformation
 import kotlin.math.roundToInt
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -121,37 +117,8 @@ class LoanCalculatorViewModel {
             inputChannel.send(text)
         }
     }
-    
-    private val formatter = DecimalFormat("#,###.##")
 
-    private val visualTransformation: VisualTransformation = VisualTransformation {
-        val decimal = toDecimal(it.text)
-        val useFormatter = decimal != null && decimal != BigDecimal.ZERO && !it.contains(".")
-        val formatted = if (useFormatter) AnnotatedString(formatter.format(decimal)) else it
-        val offsetMapping = if (!useFormatter) OffsetMapping.Identity else object : OffsetMapping {
-
-            override fun originalToTransformed(offset: Int): Int {
-                val totalSeparatorCount = (it.length - 1) / 3
-                val rightSeparatorCount = (it.length - 1 - offset) / 3
-                val leftSeparatorCount = totalSeparatorCount - rightSeparatorCount
-                return offset + leftSeparatorCount
-            }
-
-            override fun transformedToOriginal(offset: Int): Int {
-                val totalSeparatorCount = (it.length - 1) / 3
-                val rightSeparatorCount = (formatted.length - offset) / 4
-                val leftSeparatorCount = totalSeparatorCount - rightSeparatorCount
-                return offset - leftSeparatorCount
-            }
-
-        }
-
-        TransformedText(formatted, offsetMapping = offsetMapping)
-    }
-
-    private fun toDecimal(input: String): BigDecimal? {
-        return input.toBigDecimalOrNull();
-    }
+    private val visualTransformation: VisualTransformation = DecimalVisualTransformation()
 
     fun visualTransformation() = visualTransformation
 
