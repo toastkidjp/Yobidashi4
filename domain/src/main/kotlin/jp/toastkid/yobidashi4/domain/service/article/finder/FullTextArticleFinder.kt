c82d3c481d464ed31d9bcf9c1970a1295783646d
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-29 07:16:27 +0900

    feature Add interface.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/article/finder/FullTextArticleFinder.kt
@@ -0,0 +1,7 @@
+package jp.toastkid.yobidashi4.domain.service.article.finder
+
+import jp.toastkid.yobidashi4.domain.service.archive.KeywordArticleFinder
+
+interface FullTextArticleFinder : KeywordArticleFinder {
+
+}
