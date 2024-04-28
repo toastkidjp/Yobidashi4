cebd70e9a403076e96f4ae4b9c7d9cede2cc094b
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-29 07:16:45 +0900

    feature Implement searcher.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/finder/FullTextSearch.kt
@@ -0,0 +1,38 @@
+package jp.toastkid.yobidashi4.infrastructure.service.article.finder
+
+import java.io.IOException
+import java.nio.file.Path
+import java.text.ParseException
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.index.CorruptIndexException
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.ScoreDoc
+import org.apache.lucene.search.TopDocs
+import org.apache.lucene.store.FSDirectory
+
+class FullTextSearch constructor(private val indexSearcher: IndexSearcher) {
+
+    private val queryParser: QueryParser = QueryParser("contents", StandardAnalyzer())
+
+    @Throws(IOException::class, ParseException::class)
+    fun search(searchQuery: String): TopDocs? {
+        return indexSearcher.search(queryParser.parse(searchQuery), 300)
+    }
+
+    @Throws(CorruptIndexException::class, IOException::class)
+    fun getDocument(scoreDoc: ScoreDoc): Document? {
+        return indexSearcher.doc(scoreDoc.doc)
+    }
+
+    companion object {
+
+        fun make(indexDirectoryPath: Path) = FullTextSearch(
+            IndexSearcher(DirectoryReader.open(FSDirectory.open(indexDirectoryPath)))
+        )
+
+    }
+
+}
