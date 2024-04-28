7277fdbbb83a1b969f26d58e0d4da61bd2d1ef36
Toast Kid <super_q1m@yahoo.co.jp>
2024-04-28 09:05:31 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FinderMessageFactoryTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FinderMessageFactoryTest.kt
@@ -17,6 +17,7 @@ class FinderMessageFactoryTest {
     @Test
     fun invoke() {
         assertTrue(finderMessageFactory.invoke("", 2).isEmpty())
+        assertTrue(finderMessageFactory.invoke(" ", 2).isEmpty())
         assertEquals("\"test\" was not found.", finderMessageFactory.invoke("test", -1))
         assertEquals("\"test\" was not found.", finderMessageFactory.invoke("test", 0))
         assertEquals("\"test\" was found. 1", finderMessageFactory.invoke("test", 1))
