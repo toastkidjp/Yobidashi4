d54e5289f60b87c57267e0f9f3b59c6fbe30377b
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-01 00:10:35 +0900

    refactor Modify test case.

--- src/test/kotlin/jp/toastkid/yobidashi4/main/MainTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/main/MainTest.kt
@@ -1,7 +1,9 @@
 package jp.toastkid.yobidashi4.main
 
+import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkConstructor
@@ -9,23 +11,47 @@ import io.mockk.mockkObject
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.infrastructure.di.DependencyInjectionContainer
 import jp.toastkid.yobidashi4.infrastructure.service.article.TodayArticleGeneratorImplementation
+import jp.toastkid.yobidashi4.infrastructure.service.article.finder.AsynchronousArticleIndexerServiceImplementation
 import jp.toastkid.yobidashi4.infrastructure.service.main.AppCloserThreadFactory
 import jp.toastkid.yobidashi4.presentation.main.launchMainApplication
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 class MainTest {
 
+    @MockK
+    private lateinit var setting: Setting
+
     @BeforeEach
     fun setUp() {
+        MockKAnnotations.init(this)
+        every { setting.articleFolderPath() } returns mockk()
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { setting } bind(Setting::class)
+                }
+            )
+        }
+
         mockkObject(DependencyInjectionContainer)
         every { DependencyInjectionContainer.start() } just Runs
-        mockkConstructor(TodayArticleGeneratorImplementation::class, AppCloserThreadFactory::class)
+        mockkConstructor(
+            TodayArticleGeneratorImplementation::class,
+            AppCloserThreadFactory::class,
+            AsynchronousArticleIndexerServiceImplementation::class
+        )
         every { anyConstructed<TodayArticleGeneratorImplementation>().invoke() } just Runs
         every { anyConstructed<AppCloserThreadFactory>().invoke() } returns mockk()
+        every { anyConstructed<AsynchronousArticleIndexerServiceImplementation>().invoke() } just Runs
         mockkStatic("jp.toastkid.yobidashi4.presentation.main.MainApplicationKt")
         every { launchMainApplication() } just Runs
         mockkStatic(Runtime::class)
@@ -34,6 +60,7 @@ class MainTest {
 
     @AfterEach
     fun tearDown() {
+        stopKoin()
         unmockkAll()
     }
 
@@ -45,6 +72,7 @@ class MainTest {
         verify { launchMainApplication() }
         verify { Runtime.getRuntime().addShutdownHook(any()) }
         verify { anyConstructed<AppCloserThreadFactory>().invoke() }
+        verify { anyConstructed<AsynchronousArticleIndexerServiceImplementation>().invoke() }
     }
 
 }
