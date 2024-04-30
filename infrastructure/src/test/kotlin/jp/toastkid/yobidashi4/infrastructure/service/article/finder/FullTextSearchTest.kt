bc21a677a159475f66195886a81830211ff974ae
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-01 00:09:58 +0900

    refactor Add unit test.

---
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/finder/FullTextSearchTest.kt
@@ -0,0 +1,62 @@
+package jp.toastkid.yobidashi4.infrastructure.service.article.finder
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.apache.lucene.index.IndexReader
+import org.apache.lucene.index.StoredFields
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.ScoreDoc
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class FullTextSearchTest {
+
+    private lateinit var subject: FullTextSearch
+
+    @MockK
+    private lateinit var indexSearcher: IndexSearcher
+
+    @MockK
+    private lateinit var indexReader: IndexReader
+
+    @MockK
+    private lateinit var storedFields: StoredFields
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        every { indexSearcher.search(any(), any<Int>()) } returns mockk()
+        every { indexSearcher.indexReader } returns indexReader
+        every { indexReader.storedFields() } returns storedFields
+        every { storedFields.document(any()) } returns mockk()
+
+        subject = FullTextSearch(indexSearcher)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun search() {
+        subject.search("test")
+
+        verify { indexSearcher.search(any(), any<Int>()) }
+    }
+
+    @Test
+    fun getDocument() {
+        val scoreDoc = ScoreDoc(1, 1.0f)
+
+        subject.getDocument(scoreDoc)
+
+        verify { storedFields.document(any()) }
+    }
+
+}
