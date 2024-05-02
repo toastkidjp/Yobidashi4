a047dac0c53d85cfccf1d6b12e1fe48b0b5c2dff
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-03 06:55:52 +0900

    refactor Delete useless classes.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/archive/KeywordArticleFinder.kt
+++
@@ -1,7 +0,0 @@
-package jp.toastkid.yobidashi4.domain.service.archive
-
-import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
-
-interface KeywordArticleFinder {
-    operator fun invoke(keyword: String, fileFilter: String?): AggregationResult
-}
