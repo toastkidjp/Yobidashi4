1d14fd30e3caacc1facc4a815b2f726e062573d5
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-07 08:00:19 +0900

    feature Add world time content.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/MultiTabContent.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/MultiTabContent.kt
@@ -1,9 +1,11 @@
 package jp.toastkid.yobidashi4.presentation.main.content
 
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
@@ -18,6 +20,7 @@ import jp.toastkid.yobidashi4.presentation.main.component.FindInPageBox
 import jp.toastkid.yobidashi4.presentation.main.component.InputBox
 import jp.toastkid.yobidashi4.presentation.main.component.MemoryUsageBox
 import jp.toastkid.yobidashi4.presentation.main.component.WebSearchBox
+import jp.toastkid.yobidashi4.presentation.time.WorldTimeView
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -49,10 +52,14 @@ fun MultiTabContent() {
             MemoryUsageBox()
         }
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            ArticleListView(viewModel.openArticleList(), viewModel.articles())
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                ArticleListView(viewModel.openArticleList(), viewModel.articles())
 
-            TabsView(modifier = Modifier.fillMaxHeight().weight(1f))
+                TabsView(modifier = Modifier.fillMaxHeight().weight(1f))
+            }
+
+            WorldTimeArea(viewModel.openWorldTime(), modifier = Modifier.align(Alignment.CenterEnd).wrapContentWidth(Alignment.End))
         }
     }
 
@@ -73,4 +80,13 @@ private fun ArticleListView(openArticleList: Boolean, articles: List<Path>) {
             modifier = Modifier.widthIn(max = width.value).wrapContentWidth(Alignment.Start)
         )
     }
+}
+
+@Composable
+private fun WorldTimeArea(open: Boolean, modifier: Modifier) {
+    val width = animateDpAsState(if (open) 330.dp else 0.dp)
+
+    WorldTimeView(
+        modifier = modifier.widthIn(max = width.value)
+    )
 }
