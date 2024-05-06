ea4e2fa1af6a234a0d6a845abc890c33c20a11fc
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-07 07:57:20 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TableViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TableViewKtTest.kt
@@ -2,20 +2,52 @@ package jp.toastkid.yobidashi4.presentation.main.content
 
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.unmockkAll
+import jp.toastkid.yobidashi4.domain.model.aggregation.FindResult
 import jp.toastkid.yobidashi4.domain.model.aggregation.StepsAggregationResult
+import jp.toastkid.yobidashi4.domain.model.article.ArticleFactory
 import jp.toastkid.yobidashi4.domain.model.tab.TableTab
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 class TableViewKtTest {
 
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @MockK
+    private lateinit var articleFactory: ArticleFactory
+
     @BeforeEach
     fun setUp() {
+        MockKAnnotations.init(this)
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
+                    single(qualifier=null) { articleFactory } bind(ArticleFactory::class)
+                }
+            )
+        }
+
+        every { mainViewModel.updateScrollableTab(any(), any()) } just Runs
     }
 
     @AfterEach
     fun tearDown() {
+        stopKoin()
+        unmockkAll()
     }
 
     @OptIn(ExperimentalTestApi::class)
@@ -33,4 +65,17 @@ class TableViewKtTest {
         }
     }
 
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun findResultCase() {
+        runDesktopComposeUiTest {
+            setContent {
+                val result = FindResult("test")
+                result.add("2022-12-22", listOf("1st", "2nd"))
+                result.add("2022-12-22", listOf("1st", "2nd"))
+
+                TableView(TableTab("test", result))
+            }
+        }
+    }
 }
