0e329c139c9fa8e6080a65fff6a2c3da639da7d2
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-09 06:48:27 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TableViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TableViewKtTest.kt
@@ -1,13 +1,22 @@
 package jp.toastkid.yobidashi4.presentation.main.content
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.click
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performMouseInput
 import androidx.compose.ui.test.runDesktopComposeUiTest
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
+import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
+import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.aggregation.FindResult
 import jp.toastkid.yobidashi4.domain.model.aggregation.StepsAggregationResult
 import jp.toastkid.yobidashi4.domain.model.article.ArticleFactory
@@ -42,6 +51,11 @@ class TableViewKtTest {
         }
 
         every { mainViewModel.updateScrollableTab(any(), any()) } just Runs
+
+        mockkConstructor(TableViewModel::class)
+        every { anyConstructed<TableViewModel>().sort(any(), any()) } just Runs
+        every { anyConstructed<TableViewModel>().openMarkdownPreview(any()) } just Runs
+        every { anyConstructed<TableViewModel>().edit(any()) } just Runs
     }
 
     @AfterEach
@@ -53,15 +67,41 @@ class TableViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun tableView() {
+        val result = StepsAggregationResult()
+        result.put("2022-12-22", 1200, 12)
+        result.put("2022-12-23", 1240, 12)
+        result.put("2022-12-24", 1230, 12)
+
         runDesktopComposeUiTest {
             setContent {
-                val result = StepsAggregationResult()
-                result.put("2022-12-22", 1200, 12)
-                result.put("2022-12-23", 1240, 12)
-                result.put("2022-12-24", 1230, 12)
-
                 TableView(TableTab("test", result))
             }
+
+            onNode(hasText("Steps"), useUnmergedTree = true).onParent().performMouseInput {
+                enter()
+                click()
+                click()
+                exit()
+            }
+            verify { anyConstructed<TableViewModel>().sort(any(), any()) }
+
+            onNode(hasText("Steps"), useUnmergedTree = true).onParent().performMouseInput {
+                click()
+                click()
+                click()
+            }
+            verify { anyConstructed<TableViewModel>().sort(any(), any()) }
+
+            val previewButton = onAllNodesWithContentDescription("Open preview", useUnmergedTree = true).onFirst()
+            previewButton.performClick()
+            verify { anyConstructed<TableViewModel>().openMarkdownPreview(any()) }
+            previewButton.onParent().performMouseInput {
+                enter()
+                exit()
+            }
+
+            onAllNodesWithContentDescription("Open file", useUnmergedTree = true).onFirst().performClick()
+            verify { anyConstructed<TableViewModel>().edit(any()) }
         }
     }
 
