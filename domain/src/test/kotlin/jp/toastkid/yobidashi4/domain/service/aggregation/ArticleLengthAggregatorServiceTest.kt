891ac1a397bc067c0ecc519dce02a7063c83ec39
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-13 08:16:38 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/ArticleLengthAggregatorServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/ArticleLengthAggregatorServiceTest.kt
@@ -14,6 +14,8 @@ import java.util.stream.Stream
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 import kotlin.io.path.nameWithoutExtension
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -44,10 +46,16 @@ class ArticleLengthAggregatorServiceTest {
 
     @Test
     fun invoke() {
-        articleLengthAggregatorService.invoke("file")
+        val aggregationResult = articleLengthAggregatorService.invoke("file")
+        assertEquals("Total: 12", aggregationResult.title())
+        assertEquals(1, aggregationResult.itemArrays().size)
 
         verify(exactly = 1) { Files.readAllBytes(any()) }
         verify(exactly = 1) { articlesReaderService.invoke() }
+        assertFalse(aggregationResult.isEmpty())
+        assertEquals(2, aggregationResult.header().size)
+        assertEquals(String::class.java, aggregationResult.columnClass(0))
+        assertEquals(Int::class.java, aggregationResult.columnClass(1))
     }
 
 }
