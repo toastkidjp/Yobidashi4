831c07c39e06eb8923c0f4d47463eb80e40d83c1
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-13 08:16:04 +0900

    refactor Add aggregator interface.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/ArticleAggregator.kt
@@ -0,0 +1,13 @@
+package jp.toastkid.yobidashi4.domain.service.aggregation
+
+import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
+
+interface ArticleAggregator {
+
+    operator fun invoke(keyword: String): AggregationResult
+
+    fun label(): String
+
+    fun iconPath(): String
+
+}
