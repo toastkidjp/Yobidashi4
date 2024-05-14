fc5bcc58fe95ab9a43bca4b00596967f0e98cae6
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-15 08:35:45 +0900

    refactor Modify test code.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/converter/ConverterToolTabViewKtTest.kt
@@ -1,6 +1,9 @@
 package jp.toastkid.yobidashi4.presentation.converter
 
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.test.runDesktopComposeUiTest
 import org.junit.jupiter.api.Test
 
@@ -12,6 +15,11 @@ class ConverterToolTabViewKtTest {
         runDesktopComposeUiTest {
             setContent {
                 ConverterToolTabView()
+            }
+
+            onNodeWithTag("surface").performKeyInput {
+                keyDown(Key.DirectionUp)
+                keyUp(Key.DirectionUp)
             }
         }
     }
