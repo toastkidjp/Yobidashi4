2a5cd5cfe8fac84a6067b2d4c84908e633c822ec
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-16 07:08:58 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/handler/UncaughtExceptionHandlerTest.kt
@@ -0,0 +1,58 @@
+package jp.toastkid.yobidashi4.presentation.main.handler
+
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class UncaughtExceptionHandlerTest {
+
+    private lateinit var subject: UncaughtExceptionHandler
+
+    @MockK
+    private lateinit var logger: Logger
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        mockkStatic(LoggerFactory::class)
+        every { LoggerFactory.getLogger(any<Class<Any>>()) } returns logger
+        every { logger.error(any<String>(), any()) } just Runs
+
+        subject = UncaughtExceptionHandler()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun uncaughtException() {
+        val throwable = mockk<Throwable>()
+
+        subject.uncaughtException(Thread.currentThread(), throwable)
+
+        verify { logger.error(any(), throwable) }
+    }
+
+    @Test
+    fun nullCase() {
+        val throwable = mockk<Throwable>()
+
+        subject.uncaughtException(null, throwable)
+
+        verify { logger.error(null, throwable) }
+    }
+
+}
