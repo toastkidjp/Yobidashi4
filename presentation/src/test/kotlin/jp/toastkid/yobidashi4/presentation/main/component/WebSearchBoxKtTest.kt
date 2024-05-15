72edf94a7bb4446f391c5d6d7caba4af892c72dd
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-16 07:08:18 +0900

    refactor Modify UI test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/WebSearchBoxKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/WebSearchBoxKtTest.kt
@@ -1,13 +1,21 @@
 package jp.toastkid.yobidashi4.presentation.main.component
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.web.search.SearchSite
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -17,10 +25,14 @@ class WebSearchBoxKtTest {
     @BeforeEach
     fun setUp() {
         mockkConstructor(WebSearchBoxViewModel::class)
-        every { anyConstructed<WebSearchBoxViewModel>().openingDropdown() } returns true
+        every { anyConstructed<WebSearchBoxViewModel>().openingDropdown() } returns false
         every { anyConstructed<WebSearchBoxViewModel>().containsSwingContent() } returns false
         every { anyConstructed<WebSearchBoxViewModel>().existsResult() } returns true
         every { anyConstructed<WebSearchBoxViewModel>().showWebSearch() } returns true
+        every { anyConstructed<WebSearchBoxViewModel>().setShowWebSearch(any()) } just Runs
+        every { anyConstructed<WebSearchBoxViewModel>().setOpenDropdown() } just Runs
+        every { anyConstructed<WebSearchBoxViewModel>().closeDropdown() } just Runs
+        every { anyConstructed<WebSearchBoxViewModel>().choose(any()) } just Runs
         every { anyConstructed<WebSearchBoxViewModel>().start() } just Runs
     }
 
@@ -32,28 +44,65 @@ class WebSearchBoxKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun webSearchBox() {
+        val text = "web-search-input"
+        every { anyConstructed<WebSearchBoxViewModel>().query() } returns TextFieldValue(text)
+        every { anyConstructed<WebSearchBoxViewModel>().invokeSearch() } just Runs
+
         runDesktopComposeUiTest {
             setContent {
                 WebSearchBox()
-
-                verify { anyConstructed<WebSearchBoxViewModel>().openingDropdown() }
-                verify { anyConstructed<WebSearchBoxViewModel>().showWebSearch() }
             }
+
+            verify { anyConstructed<WebSearchBoxViewModel>().showWebSearch() }
+
+            onNodeWithTag("simple_close_button", true).performClick()
+            verify { anyConstructed<WebSearchBoxViewModel>().setShowWebSearch(false) }
+
+            onNodeWithTag("dropdown_switch", true).performClick()
+            verify { anyConstructed<WebSearchBoxViewModel>().setOpenDropdown() }
+
+            onNode(hasText(text), useUnmergedTree = true).performImeAction()
+            verify { anyConstructed<WebSearchBoxViewModel>().invokeSearch() }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun withDropdown() {
+        every { anyConstructed<WebSearchBoxViewModel>().openingDropdown() } returns true
+        runDesktopComposeUiTest {
+            setContent {
+                WebSearchBox()
+            }
+
+            verify { anyConstructed<WebSearchBoxViewModel>().openingDropdown() }
+
+            onNodeWithContentDescription(SearchSite.SEARCH_WITH_IMAGE.siteName, useUnmergedTree = true)
+                .onParent()
+                .performClick()
+            verify { anyConstructed<WebSearchBoxViewModel>().choose(any()) }
         }
     }
 
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun containsSwingContent() {
+        every { anyConstructed<WebSearchBoxViewModel>().openingDropdown() } returns true
         every { anyConstructed<WebSearchBoxViewModel>().containsSwingContent() } returns true
 
         runDesktopComposeUiTest {
             setContent {
                 WebSearchBox()
-
-                verify { anyConstructed<WebSearchBoxViewModel>().openingDropdown() }
-                verify { anyConstructed<WebSearchBoxViewModel>().showWebSearch() }
             }
+
+            verify { anyConstructed<WebSearchBoxViewModel>().openingDropdown() }
+            verify { anyConstructed<WebSearchBoxViewModel>().showWebSearch() }
+
+            onNodeWithContentDescription(SearchSite.SEARCH_WITH_IMAGE.siteName, useUnmergedTree = true).performClick()
+            verify { anyConstructed<WebSearchBoxViewModel>().choose(any()) }
+
+            onNodeWithTag("dropdown_switch", true).performClick()
+            verify { anyConstructed<WebSearchBoxViewModel>().closeDropdown() }
         }
     }
 }
