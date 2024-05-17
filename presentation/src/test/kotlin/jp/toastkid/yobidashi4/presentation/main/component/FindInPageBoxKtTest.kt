96b3190b14a96126d00e9f38ed9efc959dabc39c
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-18 08:17:23 +0900

    refactor Add test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/FindInPageBoxKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/FindInPageBoxKtTest.kt
@@ -1,12 +1,23 @@
 package jp.toastkid.yobidashi4.presentation.main.component
 
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performKeyInput
+import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runDesktopComposeUiTest
 import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.MockKAnnotations
+import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
@@ -42,6 +53,14 @@ class FindInPageBoxKtTest {
         every { mainViewModel.replaceInputValue() } returns TextFieldValue()
         every { mainViewModel.caseSensitive() } returns false
         every { mainViewModel.openFind() } returns false
+
+        mockkConstructor(FindInPageBoxViewModel::class)
+        every { anyConstructed<FindInPageBoxViewModel>().launch() } just Runs
+        every { anyConstructed<FindInPageBoxViewModel>().onFindInputChange(any()) } just Runs
+        every { anyConstructed<FindInPageBoxViewModel>().findUp() } just Runs
+        every { anyConstructed<FindInPageBoxViewModel>().findDown() } just Runs
+        every { anyConstructed<FindInPageBoxViewModel>().switchCaseSensitive() } just Runs
+        every { anyConstructed<FindInPageBoxViewModel>().switchFind() } just Runs
     }
 
     @AfterEach
@@ -53,14 +72,37 @@ class FindInPageBoxKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun findInPageBox() {
+        val text = "input_field"
+        every { anyConstructed<FindInPageBoxViewModel>().inputValue() } returns TextFieldValue(text)
         runDesktopComposeUiTest {
             setContent {
                 FindInPageBox()
-
-                verify { mainViewModel.currentTab() }
-                verify { mainViewModel.findStatus() }
-                verify { mainViewModel.openFind() }
             }
+
+            verify { mainViewModel.currentTab() }
+            verify { mainViewModel.findStatus() }
+            verify { mainViewModel.openFind() }
+
+            onNode(hasText("x")).performClick()
+            verify { anyConstructed<FindInPageBoxViewModel>().switchFind() }
+
+            onNode(hasText("↑")).performClick()
+            verify { anyConstructed<FindInPageBoxViewModel>().findUp() }
+
+            onNode(hasText("↓")).performClick()
+            verify { anyConstructed<FindInPageBoxViewModel>().findDown() }
+
+            onNodeWithContentDescription("Find input").performKeyInput {
+                keyDown(Key.K)
+                keyUp(Key.K)
+            }.performTextInput("test")
+            verify { anyConstructed<FindInPageBoxViewModel>().onFindInputChange(any()) }
+
+            onNodeWithContentDescription("Case sensitive checkbox", useUnmergedTree = true).performClick()
+            verify { anyConstructed<FindInPageBoxViewModel>().switchCaseSensitive() }
+
+            onAllNodesWithContentDescription("Clear input.").onFirst().performClick()
+            verify { anyConstructed<FindInPageBoxViewModel>().onFindInputChange(any()) }
         }
     }
 }
