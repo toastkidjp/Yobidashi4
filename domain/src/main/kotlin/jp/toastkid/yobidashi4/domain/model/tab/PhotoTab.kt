e118502212f3881d42469ba77c1f3e585f89f690
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-23 08:59:55 +0900

    feature Add icon path.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/PhotoTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/PhotoTab.kt
@@ -1,6 +1,7 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 import java.nio.file.Path
+import kotlin.io.path.absolutePathString
 import kotlin.io.path.name
 
 class PhotoTab(
@@ -10,6 +11,8 @@ class PhotoTab(
     override fun title(): String {
         return path.name
     }
+
+    override fun iconPath() = path.absolutePathString()
 
     fun path() = path
 
