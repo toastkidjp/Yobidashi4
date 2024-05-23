b51f159cf1420db3f24b5a44233823f5c58a4c39
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-23 09:01:37 +0900

    refactor Extract sorting to service.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/TableSortService.kt
@@ -0,0 +1,31 @@
+package jp.toastkid.yobidashi4.presentation.markdown
+
+class TableSortService {
+
+    operator fun invoke(
+        lastSortOrder: Boolean,
+        index: Int,
+        snapshot: List<List<Any>>
+    ): List<List<Any>>? {
+        val first = snapshot.firstOrNull() ?: return null
+        val swap = if (lastSortOrder)
+            if (first[index].toString().toDoubleOrNull() != null) {
+                snapshot.sortedBy { it[index].toString().toDoubleOrNull() ?: 0.0 }
+            } else if (first[index].toString().toIntOrNull() != null) {
+                snapshot.sortedBy { it[index].toString().toIntOrNull() ?: 0 }
+            } else {
+                snapshot.sortedBy { it[index].toString() }
+            }
+        else
+            if (first[index].toString().toDoubleOrNull() != null) {
+                snapshot.sortedByDescending { it[index].toString().toDoubleOrNull() ?: 0.0 }
+            } else if (first[index].toString().toIntOrNull() != null) {
+                snapshot.sortedByDescending { it[index].toString().toIntOrNull() ?: 0 }
+            } else {
+                snapshot.sortedByDescending { it[index].toString() }
+            }
+
+        return swap
+    }
+
+}
