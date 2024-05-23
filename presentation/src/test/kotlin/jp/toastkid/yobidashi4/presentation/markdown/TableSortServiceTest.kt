60ff7ce2b1559cade00bea2c5846ea783f3b9b0a
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-24 07:26:24 +0900

    refactor Add test case.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/markdown/TableSortServiceTest.kt
@@ -0,0 +1,44 @@
+package jp.toastkid.yobidashi4.presentation.markdown
+
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class TableSortServiceTest {
+
+    private lateinit var subject: TableSortService
+
+    @BeforeEach
+    fun setUp() {
+        subject = TableSortService()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun invoke() {
+        val items = listOf(
+            listOf("test", 1, 2.2),
+            listOf("test2", -1, -1.2),
+        )
+        subject.invoke(true, 0, items)
+        subject.invoke(true, 0, items)
+        subject.invoke(true, 1, items)
+        subject.invoke(true, 1, items)
+        subject.invoke(true, 2, items)
+        subject.invoke(true, 2, items)
+        subject.invoke(false, 0, items)
+        subject.invoke(false, 0, items)
+        subject.invoke(false, 1, items)
+        subject.invoke(false, 1, items)
+        subject.invoke(false, 2, items)
+        subject.invoke(false, 2, items)
+        assertNull(subject.invoke(false, 2, emptyList()))
+    }
+
+}
