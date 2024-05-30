f696c459923f62dda7d9e892e3a5f9dc3715fa6f
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-31 08:53:04 +0900

    refactor Modify test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/notification/NotificationListTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/notification/NotificationListTabViewKtTest.kt
@@ -2,7 +2,15 @@ package jp.toastkid.yobidashi4.presentation.tool.notification
 
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performKeyInput
+import androidx.compose.ui.test.performMouseInput
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.pressKey
 import androidx.compose.ui.test.runDesktopComposeUiTest
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
@@ -11,6 +19,7 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
+import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.notification.NotificationEvent
 import jp.toastkid.yobidashi4.domain.repository.notification.NotificationEventRepository
 import jp.toastkid.yobidashi4.presentation.tool.notification.viewmodel.NotificationListTabViewModel
@@ -48,7 +57,8 @@ class NotificationListTabViewKtTest {
         mockkConstructor(NotificationListTabViewModel::class)
         every { anyConstructed<NotificationListTabViewModel>().listState() } returns LazyListState()
         every { anyConstructed<NotificationListTabViewModel>().start(Dispatchers.IO) } just Runs
-        every { anyConstructed<NotificationListTabViewModel>().items() } returns mutableListOf(NotificationEvent.makeDefault())
+        every { anyConstructed<NotificationListTabViewModel>().deleteAt(any()) } just Runs
+        every { anyConstructed<NotificationListTabViewModel>().update(any(), any(), any(), any()) } just Runs
         every { anyConstructed<NotificationListTabViewModel>().focusRequester() } returns FocusRequester()
     }
 
@@ -61,10 +71,54 @@ class NotificationListTabViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun notificationListTabView() {
+        val item = NotificationEvent.makeDefault()
+        every { anyConstructed<NotificationListTabViewModel>().items() } returns mutableListOf(item)
+
         runDesktopComposeUiTest {
             setContent {
                 NotificationListTabView()
             }
+
+            onNode(hasText("Update"), true).onParent().performClick()
+            verify { anyConstructed<NotificationListTabViewModel>().update(any(), any(), any(), any()) }
+
+            onNode(hasText("x"), true).onParent().performClick().performKeyInput { pressKey(Key.DirectionUp) }
+            verify { anyConstructed<NotificationListTabViewModel>().deleteAt(any()) }
+
+            onNode(hasText(item.title), true)
+                .performMouseInput {
+                    enter()
+                    exit()
+                }
+                .performTextInput("NewTitle")
+
+            onNode(hasText(item.text), true)
+                .performTextInput("NewText")
+
+            onNode(hasText(item.dateTimeString()), true)
+                .performTextInput("2024-02-02")
         }
     }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun notFirst() {
+        every { anyConstructed<NotificationListTabViewModel>().listState() } returns LazyListState(2)
+        val item = NotificationEvent.makeDefault()
+        every { anyConstructed<NotificationListTabViewModel>().items() } returns mutableListOf(item)
+
+        runDesktopComposeUiTest {
+            setContent {
+                NotificationListTabView()
+            }
+
+            onNode(hasText("Update"), true)
+                .onParent()
+                .performClick()
+                .performKeyInput {
+                    pressKey(Key.DirectionUp, 1000L)
+                }
+        }
+    }
+
 }
