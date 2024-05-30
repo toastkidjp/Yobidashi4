b5d4cf9e58b0176460c41eb926b18bef4c0bb514
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-31 08:50:23 +0900

    refactor Modify test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/snackbar/MainSnackbarKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/snackbar/MainSnackbarKtTest.kt
@@ -6,7 +6,11 @@ import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.Test
 
@@ -40,4 +44,68 @@ class MainSnackbarKtTest {
             }
         }
     }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun swipeToEnd() {
+        runDesktopComposeUiTest {
+            setContent {
+                val snackbarHostState = SnackbarHostState()
+
+                Scaffold(
+                    snackbarHost = {
+                        SnackbarHost(
+                            hostState = snackbarHostState,
+                            snackbar = {
+                                MainSnackbar(it) { snackbarHostState.currentSnackbarData?.dismiss() }
+                            }
+                        )
+                    }
+                ) {
+
+                }
+
+                rememberCoroutineScope().launch {
+                    snackbarHostState.showSnackbar("test-snackbar", "OK", SnackbarDuration.Indefinite)
+                }
+            }
+
+            onNode(hasText("test-snackbar",)).performTouchInput {
+                swipeRight(0f, 400f)
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun swipeLeft() {
+        runDesktopComposeUiTest {
+            setContent {
+                val snackbarHostState = SnackbarHostState()
+
+                Scaffold(
+                    snackbarHost = {
+                        SnackbarHost(
+                            hostState = snackbarHostState,
+                            snackbar = {
+                                MainSnackbar(it) { snackbarHostState.currentSnackbarData?.dismiss() }
+                            }
+                        )
+                    }
+                ) {
+
+                }
+
+                rememberCoroutineScope().launch {
+                    snackbarHostState.showSnackbar("test-snackbar", "OK", SnackbarDuration.Indefinite)
+                }
+            }
+
+            onNode(hasText("test-snackbar",)).performTouchInput {
+                swipeLeft(0f, -400f)
+            }
+        }
+    }
+
 }
