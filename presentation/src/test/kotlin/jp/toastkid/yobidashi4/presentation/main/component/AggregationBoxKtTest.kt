b32e06d057fdd6338dc95b19fe29884dcf115dff
Toast Kid <super_q1m@yahoo.co.jp>
2024-05-31 08:49:59 +0900

    refactor Add test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/AggregationBoxKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/AggregationBoxKtTest.kt
@@ -1,7 +1,13 @@
 package jp.toastkid.yobidashi4.presentation.main.component
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
@@ -9,6 +15,7 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
+import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.aggregation.StepsAggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
@@ -45,10 +52,14 @@ class AggregationBoxKtTest {
 
         mockkConstructor(AggregationBoxViewModel::class)
         every { anyConstructed<AggregationBoxViewModel>().isCurrentSwingContent() } returns false
-        every { anyConstructed<AggregationBoxViewModel>().isOpeningChooser() } returns true
+        every { anyConstructed<AggregationBoxViewModel>().isOpeningChooser() } returns false
         every { anyConstructed<AggregationBoxViewModel>().selectedCategoryName() } returns "test"
         every { anyConstructed<AggregationBoxViewModel>().showAggregationBox() } returns true
         every { anyConstructed<AggregationBoxViewModel>().start() } just Runs
+        every { anyConstructed<AggregationBoxViewModel>().choose(any()) } just Runs
+        every { anyConstructed<AggregationBoxViewModel>().closeChooser() } just Runs
+        every { anyConstructed<AggregationBoxViewModel>().onSearch() } just Runs
+        every { anyConstructed<AggregationBoxViewModel>().switchAggregationBox(any()) } just Runs
         every { anyConstructed<AggregationBoxViewModel>().categories() } returns mapOf(
             "test" to { StepsAggregationResult() },
             "test2" to { StepsAggregationResult() }
@@ -64,21 +75,60 @@ class AggregationBoxKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun aggregationBox() {
+        val label = "test input label"
+        every { anyConstructed<AggregationBoxViewModel>().dateInput() } returns TextFieldValue(label)
+        every { anyConstructed<AggregationBoxViewModel>().onDateInputValueChange(any()) } just Runs
+
         runDesktopComposeUiTest {
             setContent {
                 AggregationBox()
             }
+
+            val closeButton = onNode(hasText("x"), true)
+            closeButton.assertExists()
+            closeButton.performClick()
+            verify { anyConstructed<AggregationBoxViewModel>().switchAggregationBox(any()) }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun withDropdown() {
+        every { anyConstructed<AggregationBoxViewModel>().isOpeningChooser() } returns true
+        val label = "test input label"
+        every { anyConstructed<AggregationBoxViewModel>().dateInput() } returns TextFieldValue(label)
+        every { anyConstructed<AggregationBoxViewModel>().onDateInputValueChange(any()) } just Runs
+
+        runDesktopComposeUiTest {
+            setContent {
+                AggregationBox()
+            }
+
+            onNode(hasText("test2"), true).onParent().performClick()
+            verify { anyConstructed<AggregationBoxViewModel>().choose(any()) }
+
+            val input = onNode(hasText(label), true)
+            input.performClick()
+            verify { anyConstructed<AggregationBoxViewModel>().closeChooser() }
+            input.performTextInput("test")
+            input.performImeAction()
+            verify { anyConstructed<AggregationBoxViewModel>().onDateInputValueChange(any()) }
+            verify { anyConstructed<AggregationBoxViewModel>().onSearch() }
         }
     }
 
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun currentSwingContentCase() {
+        every { anyConstructed<AggregationBoxViewModel>().isOpeningChooser() } returns true
         every { anyConstructed<AggregationBoxViewModel>().isCurrentSwingContent() } returns true
         runDesktopComposeUiTest {
             setContent {
                 AggregationBox()
             }
+
+            onNode(hasText("Stock"), true).performClick()
+            verify { anyConstructed<AggregationBoxViewModel>().choose(any()) }
         }
     }
 
