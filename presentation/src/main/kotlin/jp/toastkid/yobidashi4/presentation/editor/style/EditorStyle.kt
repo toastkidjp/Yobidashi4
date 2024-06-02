3a16d8bd2e4b20a7a6814b832b40fb5c5e6a47a7
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-02 16:15:29 +0900

    refactor Add @Immutable.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/style/EditorStyle.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/style/EditorStyle.kt
@@ -1,7 +1,9 @@
 package jp.toastkid.yobidashi4.presentation.editor.style
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.text.SpanStyle
 
+@Immutable
 data class EditorStyle(
     val regex: Regex,
     val lightStyle: SpanStyle,
