f7fe7c68560886199a6ad403a5affd69abeb9e96
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-04 08:43:01 +0900

    refactor Modify test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabViewKtTest.kt
@@ -1,6 +1,17 @@
 package jp.toastkid.yobidashi4.presentation.web.bookmark
 
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.longClick
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performKeyInput
+import androidx.compose.ui.test.performMouseInput
+import androidx.compose.ui.test.pressKey
 import androidx.compose.ui.test.runDesktopComposeUiTest
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
@@ -10,6 +21,7 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
+import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.tab.WebBookmarkTab
 import jp.toastkid.yobidashi4.domain.model.web.bookmark.Bookmark
 import jp.toastkid.yobidashi4.domain.model.web.icon.WebIcon
@@ -45,14 +57,23 @@ class WebBookmarkTabViewKtTest {
                 }
             )
         }
-        every { repository.list() } returns listOf(Bookmark("test", "https://www.yahoo.co.jp"))
-        mockkConstructor(WebIcon::class)
+        mockkConstructor(WebIcon::class, WebBookmarkTabViewModel::class)
         every { anyConstructed<WebIcon>().readAll() } returns emptyList()
         every { anyConstructed<WebIcon>().makeFolderIfNeed() } just Runs
         every { anyConstructed<WebIcon>().find(any()) } returns null
         every { webBookmarkTab.scrollPosition() } returns 0
         every { webBookmarkTab.withNewPosition(any()) } returns mockk()
         every { mainViewModel.updateScrollableTab(any(), any()) } just Runs
+
+        every { anyConstructed<WebBookmarkTabViewModel>().launch(any(), any()) } just Runs
+        every { anyConstructed<WebBookmarkTabViewModel>().openUrl(any(), any()) } just Runs
+        every { anyConstructed<WebBookmarkTabViewModel>().browseUri(any()) } just Runs
+        every { anyConstructed<WebBookmarkTabViewModel>().clipText(any()) } just Runs
+        every { anyConstructed<WebBookmarkTabViewModel>().delete(any()) } just Runs
+        every { anyConstructed<WebBookmarkTabViewModel>().focusRequester() } returns FocusRequester()
+        every { anyConstructed<WebBookmarkTabViewModel>().listState() } returns LazyListState(0)
+        every { anyConstructed<WebBookmarkTabViewModel>().openingDropdown(any()) } returns true
+        every { anyConstructed<WebBookmarkTabViewModel>().bookmarks() } returns listOf(Bookmark("test item", "https://www.yahoo.co.jp"))
     }
 
     @AfterEach
@@ -64,9 +85,23 @@ class WebBookmarkTabViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun webBookmarkTabView() {
+        every { anyConstructed<WebBookmarkTabViewModel>().openingDropdown(any()) } returns false
+
         runDesktopComposeUiTest {
             setContent {
                 WebBookmarkTabView(webBookmarkTab)
+            }
+
+            val item = onNodeWithText("test item")
+            item.performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().openUrl(any(), false) }
+            item.performMouseInput {
+                longClick()
+                enter()
+                exit()
+            }
+            item.performKeyInput {
+                pressKey(Key.DirectionUp, 1000L)
             }
         }
     }
@@ -74,13 +109,33 @@ class WebBookmarkTabViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun webBookmarkTabViewWithDropdown() {
-        mockkConstructor(WebBookmarkTabViewModel::class)
-        every { anyConstructed<WebBookmarkTabViewModel>().openingDropdown(any()) } returns true
-
         runDesktopComposeUiTest {
             setContent {
                 WebBookmarkTabView(webBookmarkTab)
             }
+
+            verify { anyConstructed<WebBookmarkTabViewModel>().bookmarks() }
+
+            onNode(hasText("Open"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().openUrl(any(), false) }
+
+            onNode(hasText("Open background"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().openUrl(any(), true) }
+
+            onNode(hasText("Open with browser"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().browseUri(any()) }
+
+            onNode(hasText("Copy title"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().clipText(any()) }
+
+            onNode(hasText("Copy URL"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().clipText(any()) }
+
+            onNode(hasText("Clip markdown link"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().clipText(any()) }
+
+            onNode(hasText("Delete"), true).onParent().performClick()
+            verify { anyConstructed<WebBookmarkTabViewModel>().delete(any()) }
         }
     }
 
