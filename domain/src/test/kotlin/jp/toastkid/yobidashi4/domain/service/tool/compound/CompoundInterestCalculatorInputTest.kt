a046e6da611a92021495e8e2587a74fe5674b30f
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-05 07:17:58 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorInputTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorInputTest.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.domain.service.tool.compound
 
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
@@ -12,6 +13,10 @@ class CompoundInterestCalculatorInputTest {
         assertNull(CompoundInterestCalculatorInput.from("", "", null, null))
         assertNull(CompoundInterestCalculatorInput.from("", "", "", null))
         assertNull(CompoundInterestCalculatorInput.from("", "", "", ""))
+        assertNull(CompoundInterestCalculatorInput.from("1", null, null, null))
+        assertNull(CompoundInterestCalculatorInput.from("2", "2", null, null))
+        assertNull(CompoundInterestCalculatorInput.from("3", "3", "2", null))
+        assertNotNull(CompoundInterestCalculatorInput.from("4", "4", "2", "1"))
     }
 
 }
