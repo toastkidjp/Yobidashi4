b7496e7884c88a697702dc31ae852cf734816dc6
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-06 09:07:54 +0900

    refactor Adapt interface.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/EatingOutCounterService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/EatingOutCounterService.kt
@@ -1,15 +1,19 @@
 package jp.toastkid.yobidashi4.domain.service.aggregation
 
-import jp.toastkid.yobidashi4.domain.model.aggregation.OutgoAggregationResult
+import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 
-class EatingOutCounterService(articlesReaderService: ArticlesReaderService) {
+class EatingOutCounterService(articlesReaderService: ArticlesReaderService) : ArticleAggregator {
 
     private val behavior = OutgoCalculationBehavior(articlesReaderService, { it.contains(TARGET_LINE_LABEL).not() })
 
-    operator fun invoke(keyword: String): OutgoAggregationResult {
+    override operator fun invoke(keyword: String): AggregationResult {
         return behavior.invoke(keyword)
     }
+
+    override fun label() = "Eat out"
+
+    override fun iconPath() = "images/icon/ic_payments.xml"
 
 }
 
