125e3e567914613ad46d88b76a8302a36fe69096
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-06 09:10:15 +0900

    refactor Extend interface.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/article/finder/FullTextArticleFinder.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/article/finder/FullTextArticleFinder.kt
@@ -1,9 +1,5 @@
 package jp.toastkid.yobidashi4.domain.service.article.finder
 
-import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
+import jp.toastkid.yobidashi4.domain.service.aggregation.ArticleAggregator
 
-interface FullTextArticleFinder {
-
-    operator fun invoke(keyword: String): AggregationResult
-
-}
+interface FullTextArticleFinder : ArticleAggregator
