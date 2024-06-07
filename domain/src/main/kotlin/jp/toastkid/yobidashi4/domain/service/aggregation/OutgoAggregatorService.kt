2e21cf2b9181f279b5c08e395fe1a071d329c966
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-07 09:25:18 +0900

    refactor Adapt interface.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/OutgoAggregatorService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/OutgoAggregatorService.kt
@@ -3,12 +3,17 @@ package jp.toastkid.yobidashi4.domain.service.aggregation
 import jp.toastkid.yobidashi4.domain.model.aggregation.OutgoAggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 
-class OutgoAggregatorService(private val articlesReaderService: ArticlesReaderService) {
+class OutgoAggregatorService(private val articlesReaderService: ArticlesReaderService) : ArticleAggregator {
 
     private val service = OutgoCalculationBehavior(articlesReaderService, { false })
 
-    operator fun invoke(keyword: String): OutgoAggregationResult {
+    override operator fun invoke(keyword: String): OutgoAggregationResult {
         return service.invoke(keyword)
     }
+
+    override fun label() = "Outgo"
+
+    override fun iconPath() = "images/icon/ic_payments.xml"
+
 
 }
