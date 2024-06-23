b05e1f8e00313082a44349339624b2d30845439a
Toast Kid <super_q1m@yahoo.co.jp>
2024-06-24 08:35:17 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatStreamParserTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatStreamParserTest.kt
@@ -39,4 +39,9 @@ class ChatStreamParserTest {
         assertNull(subject.invoke("test"))
     }
 
+    @Test
+    fun error() {
+        assertEquals("[ERROR]", subject.invoke("data: {\"candidates\": [{\"finishReason\": \"SAFETY\",\"index\": 0,\"safetyRatings\": [{\"category\": \"HARM_CATEGORY_SEXUALLY_EXPLICIT\",\"probability\": \"NEGLIGIBLE\"},{\"category\": \"HARM_CATEGORY_HATE_SPEECH\",\"probability\": \"NEGLIGIBLE\"},{\"category\": \"HARM_CATEGORY_HARASSMENT\",\"probability\": \"NEGLIGIBLE\"},{\"category\": \"HARM_CATEGORY_DANGEROUS_CONTENT\",\"probability\": \"LOW\"}]}],\"usageMetadata\": {\"promptTokenCount\": 73,\"totalTokenCount\": 73}}\n"))
+    }
+
 }
