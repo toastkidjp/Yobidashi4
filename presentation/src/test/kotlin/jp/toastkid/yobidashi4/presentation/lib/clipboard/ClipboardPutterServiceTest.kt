067c1a52e3f71d04c4e0459d8f7a578a95bf8a92
Toast Kid <super_q1m@yahoo.co.jp>
2024-07-03 09:11:31 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/clipboard/ClipboardPutterServiceTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/clipboard/ClipboardPutterServiceTest.kt
@@ -8,6 +8,7 @@ import io.mockk.unmockkAll
 import io.mockk.verify
 import java.awt.Image
 import java.awt.datatransfer.Clipboard
+import java.io.IOException
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -44,6 +45,15 @@ internal class ClipboardPutterServiceTest {
         val image: Image = mockk()
 
         clipboardPutterService(image)
+
+        verify(exactly = 1) { clipboard.setContents(any(), any()) }
+    }
+
+    @Test
+    fun exceptionCase() {
+        every { clipboard.setContents(any(), any()) } throws IOException()
+
+        clipboardPutterService(mockk<Image>())
 
         verify(exactly = 1) { clipboard.setContents(any(), any()) }
     }
