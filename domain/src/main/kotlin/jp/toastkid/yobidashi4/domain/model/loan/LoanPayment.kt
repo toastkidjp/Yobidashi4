8408a3ddc9101aa396e1c3c5b14eafa06caf951d
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-15 07:07:23 +0900

    refactor Add function.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/loan/LoanPayment.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/loan/LoanPayment.kt
@@ -3,4 +3,8 @@ package jp.toastkid.yobidashi4.domain.model.loan
 data class LoanPayment(
     val monthlyPayment: Long,
     val paymentSchedule: List<PaymentDetail>
-)
+) {
+
+    fun totalInterestAmount() = paymentSchedule.sumOf { paymentDetail -> paymentDetail.interest }.toLong()
+
+}
