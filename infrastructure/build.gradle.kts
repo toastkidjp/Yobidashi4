94a6a4749f0d2ac9b378f956bdc8dc52c9da78cb
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-16 07:10:27 +0900

    feature Add plugin dependencies.

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.devtools.ksp")
     id("org.jetbrains.kotlinx.kover")
