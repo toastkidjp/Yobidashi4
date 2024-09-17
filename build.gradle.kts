5a49746adb23e74996c8ac13dcbb1bacae4ee0d3
Toast Kid <super_q1m@yahoo.co.jp>
2024-09-18 06:52:12 +0900

    refactor Add dependency for using parameterized test.

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ allprojects {
 
     dependencies {
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+        testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
         testImplementation("io.mockk:mockk:1.10.6")
         testRuntimeOnly("net.bytebuddy:byte-buddy:1.12.22")
