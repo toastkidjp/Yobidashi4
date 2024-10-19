6bdc48c1613c02e170c5649c48c6603b0063a2f9
Toast Kid <super_q1m@yahoo.co.jp>
2024-10-20 08:04:31 +0900

    feature Upgrade JCEF Maven to 127.

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 
-    implementation("me.friwi:jcefmaven:122.1.10")
+    implementation("me.friwi:jcefmaven:127.3.1")
 
     implementation("io.insert-koin:koin-core:3.3.2")
     implementation("io.insert-koin:koin-annotations:1.0.3")
