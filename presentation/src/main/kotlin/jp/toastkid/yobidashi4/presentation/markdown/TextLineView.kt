70d7e53591ecdadb887060d27a75cde3746b85ee
Toast Kid <super_q1m@yahoo.co.jp>
2024-10-28 08:26:25 +0900

    feature Enable to handle secondary click.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/TextLineView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/TextLineView.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.presentation.markdown
 
+import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -8,6 +9,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -21,7 +23,11 @@ fun TextLineView(text: String, textStyle: TextStyle, modifier: Modifier) {
         viewModel.annotatedString(),
         style = textStyle,
         modifier = modifier.onPointerEvent(PointerEventType.Release) {
-            viewModel.onPointerReleased(it)
+            //viewModel.onPointerReleased(it)
+        }.pointerInput(Unit) {
+            awaitEachGesture {
+                viewModel.onPointerReleased(awaitPointerEvent())
+            }
         },
         onTextLayout = viewModel::putLayoutResult
     )
