8fe923ef083d9a5fb0d9423da275415e3118da4f
Toast Kid <super_q1m@yahoo.co.jp>
2024-10-29 09:28:18 +0900

    refactor Upgrade Koin to 4.0.0.

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -20,9 +20,9 @@ dependencies {
 
     implementation("me.friwi:jcefmaven:127.3.1")
 
-    implementation("io.insert-koin:koin-core:3.3.2")
-    implementation("io.insert-koin:koin-annotations:1.0.3")
-    ksp("io.insert-koin:koin-ksp-compiler:1.0.3")
+    implementation("io.insert-koin:koin-core:4.0.0")
+    implementation("io.insert-koin:koin-annotations:1.4.0")
+    ksp("io.insert-koin:koin-ksp-compiler:1.4.0")
 
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("org.slf4j:slf4j-api:2.0.6")
