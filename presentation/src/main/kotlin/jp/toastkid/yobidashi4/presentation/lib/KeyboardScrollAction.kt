b1507f5b60dd8f159fe4d0b8722005ddb24ee83e
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-02 08:42:18 +0900

    refactor Remove annotation ExperimentalComposeUiApi.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/lib/KeyboardScrollAction.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/lib/KeyboardScrollAction.kt
@@ -3,14 +3,12 @@ package jp.toastkid.yobidashi4.presentation.lib
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.key.Key
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class KeyboardScrollAction(private val state: ScrollableState) {
 
-    @OptIn(ExperimentalComposeUiApi::class)
     operator fun invoke(coroutineScope: CoroutineScope, key: Key, controlDown: Boolean) = when (key) {
         Key.DirectionUp -> {
             coroutineScope.launch {
