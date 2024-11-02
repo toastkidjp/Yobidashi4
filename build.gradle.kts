b7035682e9f2583ba0e5fbc7375595a414ad7395
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-03 07:52:09 +0900

    refactor Use catalog.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,9 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("org.slf4j:slf4j-reload4j:2.0.6")
 
-    implementation("io.insert-koin:koin-core:4.0.0")
-    implementation("io.insert-koin:koin-annotations:1.4.0")
-    ksp("io.insert-koin:koin-ksp-compiler:1.4.0")
+    implementation(libraries.koin)
+    implementation(libraries.koinAnnotations)
+    ksp(libraries.koinKspCompiler)
 
     // Kover
     kover(project(path = ":domain"))
