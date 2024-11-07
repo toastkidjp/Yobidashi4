7ac0f657e0effd4089ea6524544e41b8d956adf8
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-07 09:30:09 +0900

    refactor Extract to catalog.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ dependencyResolutionManagement {
             library("koinAnnotations", "io.insert-koin", "koin-annotations").version("1.4.0")
             library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("1.4.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.3.3")
+            library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.6.1")
         }
     }
 }
