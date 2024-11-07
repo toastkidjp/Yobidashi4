6ab1a84119688600591f8e964babbe3a4d5875e5
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-07 09:30:44 +0900

    refactor Use catalog.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,6 +9,6 @@ group = "jp.toastkid.yobidashi4"
 version = "1.0.0"
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.1")
+    implementation(libraries.coroutines)
     implementation(libraries.kotlinSerialization)
 }
