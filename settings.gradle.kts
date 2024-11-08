a76b74c9f8c0881d15eedd7c95b26bcdd70c80f6
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-08 09:08:26 +0900

    refactor Add catalog.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,8 @@ dependencyResolutionManagement {
             library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("1.4.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.3.3")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.6.1")
+            library("slf4j", "org.slf4j", "slf4j-api").version("2.0.6")
+            library("reload4j", "org.slf4j", "slf4j-reload4j").version("2.0.6")
         }
     }
 }
