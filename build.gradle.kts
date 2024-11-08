400ab6acd9aa56bcd12ed1cf87a6fcb891eb5619
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-08 09:08:57 +0900

    refactor Use catalog.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ dependencies {
     implementation(libraries.kotlinSerialization)
     implementation(libraries.coroutines)
 
-    implementation("org.slf4j:slf4j-api:2.0.6")
-    implementation("org.slf4j:slf4j-reload4j:2.0.6")
+    implementation(libraries.slf4j)
+    implementation(libraries.reload4j)
 
     implementation(libraries.koin)
 
