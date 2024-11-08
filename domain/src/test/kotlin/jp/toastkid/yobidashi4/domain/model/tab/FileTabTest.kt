08e90dd05428be13e00f3c628df5c6c278b30fbd
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-09 07:47:54 +0900

    refactor Modify test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/tab/FileTabTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/tab/FileTabTest.kt
@@ -2,6 +2,7 @@ package jp.toastkid.yobidashi4.domain.model.tab
 
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,7 +20,7 @@ class FileTabTest {
     fun test() {
         assertNotNull(tab.title())
         assertTrue(tab.closeable())
-        assertTrue(tab.iconPath()?.contains("search") ?: false)
+        assertNull(tab.iconPath())
     }
 
     @Test
@@ -27,7 +28,7 @@ class FileTabTest {
         tab = FileTab("music", emptyList(), false, FileTab.Type.MUSIC)
         assertNotNull(tab.title())
         assertFalse(tab.closeable())
-        assertTrue(tab.iconPath()?.contains("music") ?: false)
+        assertNull(tab.iconPath())
     }
 
 }
