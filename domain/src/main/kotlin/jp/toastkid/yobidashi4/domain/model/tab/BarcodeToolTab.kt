29f482b9ff8e0354a6544c0d35d891263714f22e
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-09 07:48:53 +0900

    refactor Delete useless override.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/BarcodeToolTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/BarcodeToolTab.kt
@@ -1,12 +1,9 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 class BarcodeToolTab : Tab {
+
     override fun title(): String {
         return "Barcode tool"
-    }
-
-    override fun iconPath(): String? {
-        return "images/icon/ic_barcode.xml"
     }
 
 }
