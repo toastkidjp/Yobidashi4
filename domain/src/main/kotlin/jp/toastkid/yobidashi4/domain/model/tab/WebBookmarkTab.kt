e5cf5e788d3573f79277f2f99d4cea65daa876c8
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-10 08:27:22 +0900

    refactor Delete useless override.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebBookmarkTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebBookmarkTab.kt
@@ -6,10 +6,6 @@ class WebBookmarkTab(private val scrollPosition: Int = 0) : ScrollableContentTab
 
     override fun closeable(): Boolean = true
 
-    override fun iconPath(): String {
-        return "images/icon/ic_bookmark.xml"
-    }
-
     override fun scrollPosition(): Int = scrollPosition
 
     override fun withNewPosition(scrollPosition: Int): ScrollableContentTab {
