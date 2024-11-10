5efdc6217056100f71c8a127e713d6d1aa84903c
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-11 08:33:34 +0900

    refactor Delete useless default argument.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/theme/AppTheme.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/theme/AppTheme.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.graphics.Color
 
 @Composable
 fun AppTheme(
-    darkTheme: Boolean = false,
+    darkTheme: Boolean,
     content: @Composable  () -> Unit
 ) {
     val primary = Color(0xFF444499)
