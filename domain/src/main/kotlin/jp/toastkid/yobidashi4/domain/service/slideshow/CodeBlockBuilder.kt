165ff3d757345922dd90dded6ead581e53022c78
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-12 07:34:21 +0900

    refactor Use AtomicBoolean.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/CodeBlockBuilder.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/CodeBlockBuilder.kt
@@ -1,11 +1,12 @@
 package jp.toastkid.yobidashi4.domain.service.slideshow
 
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 import jp.toastkid.yobidashi4.domain.model.slideshow.data.CodeBlockLine
 
 class CodeBlockBuilder {
 
-    private var isInCodeBlock = false
+    private val isInCodeBlock = AtomicBoolean(false)
 
     private val code = StringBuilder()
 
@@ -16,23 +17,23 @@ class CodeBlockBuilder {
     }
 
     fun shouldAppend(line: String): Boolean {
-        return isInCodeBlock && !line.startsWith("```")
+        return isInCodeBlock.get() && !line.startsWith("```")
     }
 
     fun build(): CodeBlockLine {
         return CodeBlockLine(code.toString(), codeFormat.get())
     }
 
-    fun inCodeBlock() = isInCodeBlock
+    fun inCodeBlock() = isInCodeBlock.get()
 
     fun initialize() {
         code.setLength(0)
         codeFormat.set("")
-        isInCodeBlock = false
+        isInCodeBlock.set(false)
     }
 
     fun startCodeBlock() {
-        this.isInCodeBlock = true
+        this.isInCodeBlock.set(true)
     }
 
     fun setCodeFormat(codeFormat: String) {
