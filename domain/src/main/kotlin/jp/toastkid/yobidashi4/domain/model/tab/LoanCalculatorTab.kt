7dae72830b59a8e6535c08e5af5bd7b6dba13f9f
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-13 10:15:11 +0900

    refactor Delete useless override.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/LoanCalculatorTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/LoanCalculatorTab.kt
@@ -1,8 +1,7 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 class LoanCalculatorTab : Tab {
-    override fun title(): String = "Loan Calculator"
 
-    override fun closeable(): Boolean = true
+    override fun title(): String = "Loan Calculator"
 
 }
