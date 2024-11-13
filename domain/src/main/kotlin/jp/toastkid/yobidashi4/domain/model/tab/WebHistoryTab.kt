14e2199cfe449d777a8a646275c12cfb712d1fdb
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-13 10:17:43 +0900

    refactor Delete useless override.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebHistoryTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/WebHistoryTab.kt
@@ -6,10 +6,6 @@ class WebHistoryTab(private val scrollPosition: Int = 0) : ScrollableContentTab 
         return "Web history"
     }
 
-    override fun iconPath(): String? {
-        return "images/icon/ic_history.xml"
-    }
-
     override fun scrollPosition(): Int = scrollPosition
 
     override fun withNewPosition(scrollPosition: Int): ScrollableContentTab {
