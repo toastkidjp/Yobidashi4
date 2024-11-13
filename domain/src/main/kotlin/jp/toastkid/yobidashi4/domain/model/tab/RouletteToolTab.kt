c1e803de058875e5f39d3e6d1159cbdf255916aa
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-13 10:16:16 +0900

    refactor Delete useless override.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/RouletteToolTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/RouletteToolTab.kt
@@ -1,10 +1,9 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 class RouletteToolTab : Tab {
+
     override fun title(): String {
         return "Roulette"
     }
-
-    override fun iconPath() = "images/icon/ic_shuffle.xml"
 
 }
