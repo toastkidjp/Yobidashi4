0f45864cbe59afdd511315fdecac4fb0b6e52884
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-15 00:07:02 +0900

    refactor Modify test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/tab/TextFileViewerTabTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/tab/TextFileViewerTabTest.kt
@@ -1,16 +1,23 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 import java.nio.file.Path
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class TextFileViewerTabTest {
 
     @Test
-    fun iconPath() {
-        assertTrue(TextFileViewerTab(Path.of("tomato.txt")).iconPath()?.contains("text") == true)
-        assertTrue(TextFileViewerTab(Path.of("temporary/log1/tomato.txt")).iconPath()?.contains("text") == true)
-        assertTrue(TextFileViewerTab(Path.of("temporary/logs/tomato.txt")).iconPath()?.contains("log") == true)
+    fun test() {
+        val path = Path.of("tomato.txt")
+        val tab = TextFileViewerTab(path)
+
+        assertNotNull(tab.title())
+        assertTrue(tab.closeable())
+        assertSame(path, tab.path())
+        assertNull(tab.iconPath())
     }
 
 }
