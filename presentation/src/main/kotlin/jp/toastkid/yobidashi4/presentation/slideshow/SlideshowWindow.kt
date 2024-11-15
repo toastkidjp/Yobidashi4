50a16d4c32c0988eaaec16182af5333d9ffa70fa
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-16 08:31:24 +0900

    refactor Clean up code.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowWindow.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowWindow.kt
@@ -1,11 +1,8 @@
 package jp.toastkid.yobidashi4.presentation.slideshow
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.window.Window
 import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.service.slideshow.SlideDeckReader
@@ -29,18 +26,12 @@ class SlideshowWindow {
                 visible = viewModel.windowVisible(),
                 title = deck.title,
             ) {
-                val focusRequester = remember { FocusRequester() }
-
                 Slideshow(
                     deck,
                     { viewModel.onEscapeKeyReleased(onCloseWindow) },
                     viewModel::toggleFullscreen,
-                    modifier = Modifier.focusRequester(focusRequester)
+                    modifier = Modifier
                 )
-
-                LaunchedEffect(Unit) {
-                    focusRequester.requestFocus()
-                }
             }
         }
     }
