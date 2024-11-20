f48ee188f4f6bd40d5753e0e71abc52d41d5528d
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-20 09:14:34 +0900

    refactor Remove opt-in marker.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowViewModelTest.kt
@@ -129,7 +129,7 @@ class SlideshowViewModelTest {
         }
     }
 
-    @OptIn(ExperimentalFoundationApi::class, ExperimentalTestApi::class, InternalComposeUiApi::class)
+    @OptIn(ExperimentalTestApi::class, InternalComposeUiApi::class)
     @Test
     fun onKeyEventF5() {
         runDesktopComposeUiTest {
