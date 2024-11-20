165394332fed843ab46ff171e1442aef145a0349
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-20 09:03:16 +0900

    refactor Delete useless import.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/component/InputBox.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/component/InputBox.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.onKeyEvent
