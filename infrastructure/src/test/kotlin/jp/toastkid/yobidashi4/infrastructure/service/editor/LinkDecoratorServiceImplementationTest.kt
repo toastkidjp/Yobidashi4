f6129eed4b07e99038cbba70aa2c9aad25da2d6a
Toast Kid <super_q1m@yahoo.co.jp>
2024-11-21 09:47:40 +0900

    refactor Add unit test.

---
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/editor/LinkDecoratorServiceImplementationTest.kt
@@ -0,0 +1,43 @@
+package jp.toastkid.yobidashi4.infrastructure.service.editor
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import java.net.MalformedURLException
+import java.net.URL
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class LinkDecoratorServiceImplementationTest {
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test() {
+        assertEquals("www.yahoo.co.jp", LinkDecoratorServiceImplementation().invoke("www.yahoo.co.jp"))
+        mockkStatic(Jsoup::class)
+        val document = mockk<Document>()
+        every { Jsoup.parse(any<URL>(), any()) }.returns(document)
+        every { document.title() } returns "Yahoo! JAPAN"
+        assertEquals(
+            "[Yahoo! JAPAN](https://www.yahoo.co.jp)",
+            LinkDecoratorServiceImplementation().invoke("https://www.yahoo.co.jp")
+        )
+    }
+
+    @Test
+    fun testException() {
+        mockkStatic(Jsoup::class)
+        every { Jsoup.parse(any<URL>(), any()) }.throws(MalformedURLException())
+
+        assertEquals("https://www.yahoo.co.jp", LinkDecoratorServiceImplementation().invoke("https://www.yahoo.co.jp"))
+    }
+
+}
