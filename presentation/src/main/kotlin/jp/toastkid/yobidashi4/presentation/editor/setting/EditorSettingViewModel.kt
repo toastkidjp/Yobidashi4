b9020f20dac98624d9c693625335b9ce81b23668
Toast Kid <super_q1m@yahoo.co.jp>
2025-01-01 09:59:35 +0900

    feature Adapt new interface.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/setting/EditorSettingViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/setting/EditorSettingViewModel.kt
@@ -2,7 +2,7 @@ package jp.toastkid.yobidashi4.presentation.editor.setting
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
-import com.godaddy.android.colorpicker.HsvColor
+import com.github.skydoves.colorpicker.compose.ColorPickerController
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -12,44 +12,55 @@ class EditorSettingViewModel : KoinComponent {
     private val setting: Setting by inject()
 
     private val currentBackgroundColor = mutableStateOf(
-        setting.editorBackgroundColor()?.let {
-            Color(it.red, it.green, it.blue, it.alpha)
-        } ?: Color.LightGray
+        ColorPickerController().also {
+            it.selectByColor(
+                setting.editorBackgroundColor()?.let {
+                    Color(it.red, it.green, it.blue, it.alpha)
+                } ?: Color.LightGray,
+                false
+            )
+        }
     )
 
     private val currentFontColor =  mutableStateOf(
-        setting.editorForegroundColor()?.let {
-            Color(it.red, it.green, it.blue, it.alpha)
-        } ?: Color.Black
+        ColorPickerController().also {
+            it.selectByColor(
+                setting.editorForegroundColor()?.let {
+                    Color(it.red, it.green, it.blue, it.alpha)
+                } ?: Color.Black,
+                false
+            )
+        }
+
     )
 
     fun currentBackgroundColor() = currentBackgroundColor.value
 
     fun currentFontColor() = currentFontColor.value
 
-    fun onBackgroundColorChanged(hsvColor: HsvColor) {
-        currentBackgroundColor.value = hsvColor.toColor()
+    fun onBackgroundColorChanged(hsvColor: Color) {
+        //currentBackgroundColor.value = hsvColor
     }
 
-    fun onFontColorChanged(hsvColor: HsvColor) {
-        currentFontColor.value = hsvColor.toColor()
+    fun onFontColorChanged(hsvColor: Color) {
+        //currentFontColor.value = hsvColor.toArgb()
     }
 
     fun commit() {
         setting.setEditorBackgroundColor(
             java.awt.Color(
-                currentBackgroundColor.value.red,
-                currentBackgroundColor.value.green,
-                currentBackgroundColor.value.blue,
-                currentBackgroundColor.value.alpha
+                currentBackgroundColor.value.selectedColor.value.red,
+                currentBackgroundColor.value.selectedColor.value.green,
+                currentBackgroundColor.value.selectedColor.value.blue,
+                currentBackgroundColor.value.selectedColor.value.alpha
             )
         )
         setting.setEditorForegroundColor(
             java.awt.Color(
-                currentFontColor.value.red,
-                currentFontColor.value.green,
-                currentFontColor.value.blue,
-                currentFontColor.value.alpha
+                currentFontColor.value.selectedColor.value.red,
+                currentFontColor.value.selectedColor.value.green,
+                currentFontColor.value.selectedColor.value.blue,
+                currentFontColor.value.selectedColor.value.alpha
             )
         )
     }
