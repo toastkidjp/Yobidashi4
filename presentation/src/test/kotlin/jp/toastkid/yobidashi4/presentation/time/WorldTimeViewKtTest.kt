fa51baf90ad53e58be7bf6bc1026b8f7edf4f819
toastkidjp <10fmi13@gmail.com>
2025-01-03 10:35:45 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/time/WorldTimeViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/time/WorldTimeViewKtTest.kt
@@ -2,10 +2,24 @@ package jp.toastkid.yobidashi4.presentation.time
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockkConstructor
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class WorldTimeViewKtTest {
+
+    @BeforeEach
+    fun setUp() {
+        mockkConstructor(WorldTimeViewModel::class)
+        every { anyConstructed<WorldTimeViewModel>().onClickItem(any()) } just Runs
+    }
 
     @OptIn(ExperimentalTestApi::class)
     @Test
@@ -14,6 +28,11 @@ class WorldTimeViewKtTest {
             setContent {
                 WorldTimeView(Modifier)
             }
+
+            onNodeWithContentDescription("Asia/Tokyo", useUnmergedTree = true)
+                .performClick()
+
+            verify { anyConstructed<WorldTimeViewModel>().onClickItem(any()) }
         }
     }
 }
