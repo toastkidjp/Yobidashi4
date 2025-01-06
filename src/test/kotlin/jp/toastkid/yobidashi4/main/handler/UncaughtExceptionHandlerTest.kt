33da6eebd66cfdf7bb9fb52af7430f80c05692ae
toastkidjp <10fmi13@gmail.com>
2025-01-06 09:06:21 +0900

    refactor Add test case.

--- src/test/kotlin/jp/toastkid/yobidashi4/main/handler/UncaughtExceptionHandlerTest.kt
+++ src/test/kotlin/jp/toastkid/yobidashi4/main/handler/UncaughtExceptionHandlerTest.kt
@@ -6,12 +6,20 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkConstructor
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import jp.toastkid.yobidashi4.infrastructure.service.main.AppCloserAction
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -29,11 +37,23 @@ class UncaughtExceptionHandlerTest {
         every { LoggerFactory.getLogger(any<Class<Any>>()) } returns logger
         every { logger.error(any<String>(), any()) } just Runs
 
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { mockk<Setting>() } bind(Setting::class)
+                    single(qualifier=null) { mockk<WebViewPool>() } bind(WebViewPool::class)
+                }
+            )
+        }
+        mockkConstructor(AppCloserAction::class)
+        every { anyConstructed<AppCloserAction>().invoke() } just Runs
+
         subject = UncaughtExceptionHandler()
     }
 
     @AfterEach
     fun tearDown() {
+        stopKoin()
         unmockkAll()
     }
 
@@ -44,6 +64,7 @@ class UncaughtExceptionHandlerTest {
         subject.uncaughtException(Thread.currentThread(), throwable)
 
         verify { logger.error(any(), throwable) }
+        verify { anyConstructed<AppCloserAction>().invoke() }
     }
 
     @Test
@@ -53,6 +74,7 @@ class UncaughtExceptionHandlerTest {
         subject.uncaughtException(null, throwable)
 
         verify { logger.error(null, throwable) }
+        verify { anyConstructed<AppCloserAction>().invoke() }
     }
 
 }
