8d22696a673a341c311f6f9d7a13acedcd5eb9fa
toastkidjp <10fmi13@gmail.com>
2025-01-07 09:15:27 +0900

    refactor Use returns.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/calendar/OffDayFinderServiceTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/calendar/OffDayFinderServiceTest.kt
@@ -45,7 +45,7 @@ internal class OffDayFinderServiceTest {
         mockkConstructor(SpecialCaseOffDayCalculatorService::class)
         mockkConstructor(MoveableHolidayCalculatorService::class)
         every { anyConstructed<EquinoxDayCalculator>().calculateVernalEquinoxDay(any()) } returns Holiday("", 3, 20)
-        every { anyConstructed<EquinoxDayCalculator>().calculateAutumnalEquinoxDay(any()) }.answers { Holiday("", 9, 22) }
+        every { anyConstructed<EquinoxDayCalculator>().calculateAutumnalEquinoxDay(any()) } returns Holiday("", 9, 22)
         every { anyConstructed<SpecialCaseOffDayCalculatorService>().invoke(any(), any()) }.answers { emptySet() }
         every { anyConstructed<MoveableHolidayCalculatorService>().invoke(any(), any(), any()) } returns false
         every { userOffDayService.invoke(any(), any()) } returns false
