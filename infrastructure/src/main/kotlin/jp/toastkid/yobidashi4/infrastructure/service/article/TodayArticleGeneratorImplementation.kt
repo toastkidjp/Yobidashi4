bf8a2bec5f3ef38721c7566f4200e3030b990d4e
toastkidjp <10fmi13@gmail.com>
2025-01-09 00:04:38 +0900

    feature Add guard.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/TodayArticleGeneratorImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/TodayArticleGeneratorImplementation.kt
@@ -1,6 +1,5 @@
 package jp.toastkid.yobidashi4.infrastructure.service.article
 
-import java.nio.file.Files
 import jp.toastkid.yobidashi4.domain.model.article.ArticleFactory
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.service.article.ArticleTemplate
@@ -10,6 +9,7 @@ import jp.toastkid.yobidashi4.domain.service.article.TodayArticleGenerator
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.nio.file.Files
 
 @Single
 class TodayArticleGeneratorImplementation : KoinComponent, TodayArticleGenerator {
@@ -21,6 +21,10 @@ class TodayArticleGeneratorImplementation : KoinComponent, TodayArticleGenerator
     private val offDayFinderService: OffDayFinderService by inject()
 
     override operator fun invoke() {
+        if (Files.exists(setting.articleFolderPath()).not()) {
+            return
+        }
+
         val title = ArticleTitleGenerator().invoke() ?: return
         val path = setting.articleFolderPath().resolve("${title}.md")
         if (Files.exists(path)) {
