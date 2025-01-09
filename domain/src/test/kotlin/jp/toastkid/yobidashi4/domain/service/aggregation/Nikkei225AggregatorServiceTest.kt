5617da9ffd5c2e03c78e8fdab9629fb9e484d1ea
toastkidjp <10fmi13@gmail.com>
2025-01-09 09:27:31 +0900

    refactor Clean up code.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/Nikkei225AggregatorServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/Nikkei225AggregatorServiceTest.kt
@@ -8,17 +8,17 @@ import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
-import java.nio.file.Files
-import java.nio.file.Path
-import java.util.stream.Stream
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
-import kotlin.io.path.nameWithoutExtension
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.stream.Stream
+import kotlin.io.path.nameWithoutExtension
 
 internal class Nikkei225AggregatorServiceTest {
 
@@ -31,7 +31,7 @@ internal class Nikkei225AggregatorServiceTest {
     @BeforeEach
     fun setUp() {
         val path = mockk<Path>()
-        every { path.nameWithoutExtension }.returns("file.md")
+        every { path.nameWithoutExtension } returns "file.md"
 
         mockkStatic(Files::class)
         val lines = """
