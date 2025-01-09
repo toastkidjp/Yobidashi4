cd6e6577fddf6a0eeec6e8fa0fe6561246d9b333
toastkidjp <10fmi13@gmail.com>
2025-01-09 09:22:39 +0900

    refactor Modify import.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/StocksAggregatorServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/StocksAggregatorServiceTest.kt
@@ -11,6 +11,7 @@ import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
@@ -54,10 +55,10 @@ _
         val outgoAggregationResult = stocksAggregatorService.invoke("file")
 
         outgoAggregationResult.itemArrays().firstOrNull()?.let {
-            Assertions.assertEquals("file", it[0])
-            Assertions.assertEquals(12202, it[1])
-            Assertions.assertEquals(6838, it[2])
-            Assertions.assertEquals(11.12, it[3])
+            assertEquals("file", it[0])
+            assertEquals(12202, it[1])
+            assertEquals(6838, it[2])
+            assertEquals(11.12, it[3])
         }
         verify(exactly = 1) { Files.readAllLines(any()) }
         verify(exactly = 1) { articlesReaderService.invoke() }
