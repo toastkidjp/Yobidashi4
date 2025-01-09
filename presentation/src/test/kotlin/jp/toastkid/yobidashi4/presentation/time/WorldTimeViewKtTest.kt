0079228b4c3abfe77fc353418a439e35892da69c
toastkidjp <10fmi13@gmail.com>
2025-01-10 00:03:52 +0900

    refactor Modify test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/time/WorldTimeViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/time/WorldTimeViewKtTest.kt
@@ -5,20 +5,45 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runDesktopComposeUiTest
+import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import io.mockk.just
-import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 class WorldTimeViewKtTest {
 
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
     @BeforeEach
     fun setUp() {
-        mockkConstructor(WorldTimeViewModel::class)
-        every { anyConstructed<WorldTimeViewModel>().onClickItem(any()) } just Runs
+        MockKAnnotations.init(this)
+        every { mainViewModel.showSnackbar(any(), any(), any()) } just Runs
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
+                }
+            )
+        }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
     }
 
     @OptIn(ExperimentalTestApi::class)
@@ -32,7 +57,7 @@ class WorldTimeViewKtTest {
             onNodeWithContentDescription("Asia/Tokyo", useUnmergedTree = true)
                 .performClick()
 
-            verify { anyConstructed<WorldTimeViewModel>().onClickItem(any()) }
+            verify { mainViewModel.showSnackbar(any(), any(), any()) }
         }
     }
 }
