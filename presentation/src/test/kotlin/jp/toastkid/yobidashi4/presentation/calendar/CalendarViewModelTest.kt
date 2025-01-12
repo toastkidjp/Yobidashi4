0696b6945e78942c1889361fb88145511bd7925a
toastkidjp <10fmi13@gmail.com>
2025-01-12 11:49:03 +0900

    refactor Clean up assertions.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/calendar/CalendarViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/calendar/CalendarViewModelTest.kt
@@ -165,7 +165,7 @@ class CalendarViewModelTest {
 
     @Test
     fun openDateArticle() {
-        Assertions.assertAll(
+        assertAll(
             {
                 viewModel.openDateArticle(20, true)
 
