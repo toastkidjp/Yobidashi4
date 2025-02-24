ce4bde2fbc4c97604e2f86cc6b59ed69edd8ad52
toastkidjp <10fmi13@gmail.com>
2025-02-25 07:21:32 +0900

    feature Update slf4j to 2.0.16.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
             library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("1.4.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.3.3")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.6.1")
-            library("slf4j", "org.slf4j", "slf4j-api").version("2.0.6")
+            library("slf4j", "org.slf4j", "slf4j-api").version("2.0.16")
             library("reload4j", "org.slf4j", "slf4j-reload4j").version("2.0.6")
             library("zxing", "com.google.zxing", "core").version("3.4.1")
             library("jcef", "me.friwi", "jcefmaven").version("132.3.1")
