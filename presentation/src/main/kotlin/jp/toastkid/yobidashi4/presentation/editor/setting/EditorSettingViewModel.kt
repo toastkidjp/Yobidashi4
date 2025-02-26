d86e7fa12374d2bc830ec5de174d12caffc8b919
toastkidjp <10fmi13@gmail.com>
2025-02-27 00:06:51 +0900

    refactor Delete fields and properties.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/setting/EditorSettingViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/setting/EditorSettingViewModel.kt
@@ -1,8 +1,6 @@
 package jp.toastkid.yobidashi4.presentation.editor.setting
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.Color
-import com.github.skydoves.colorpicker.compose.ColorPickerController
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -10,60 +8,6 @@ import org.koin.core.component.inject
 class EditorSettingViewModel : KoinComponent {
 
     private val setting: Setting by inject()
-
-    private val currentBackgroundColor = mutableStateOf(
-        ColorPickerController().also {
-            it.selectByColor(
-                setting.editorBackgroundColor()?.let {
-                    Color(it.red, it.green, it.blue, it.alpha)
-                } ?: Color.LightGray,
-                false
-            )
-        }
-    )
-
-    private val currentFontColor =  mutableStateOf(
-        ColorPickerController().also {
-            it.selectByColor(
-                setting.editorForegroundColor()?.let {
-                    Color(it.red, it.green, it.blue, it.alpha)
-                } ?: Color.Black,
-                false
-            )
-        }
-
-    )
-
-    fun currentBackgroundColor() = currentBackgroundColor.value
-
-    fun currentFontColor() = currentFontColor.value
-
-    fun onBackgroundColorChanged(hsvColor: Color) {
-        //currentBackgroundColor.value = hsvColor
-    }
-
-    fun onFontColorChanged(hsvColor: Color) {
-        //currentFontColor.value = hsvColor.toArgb()
-    }
-
-    fun commit() {
-        setting.setEditorBackgroundColor(
-            java.awt.Color(
-                currentBackgroundColor.value.selectedColor.value.red,
-                currentBackgroundColor.value.selectedColor.value.green,
-                currentBackgroundColor.value.selectedColor.value.blue,
-                currentBackgroundColor.value.selectedColor.value.alpha
-            )
-        )
-        setting.setEditorForegroundColor(
-            java.awt.Color(
-                currentFontColor.value.selectedColor.value.red,
-                currentFontColor.value.selectedColor.value.green,
-                currentFontColor.value.selectedColor.value.blue,
-                currentFontColor.value.selectedColor.value.alpha
-            )
-        )
-    }
 
     private val openFontFamily = mutableStateOf(false)
 
