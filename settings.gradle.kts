ee5ce5d4dbfdd9bd87238ed0ba7db2cd82fdc604
toastkidjp <10fmi13@gmail.com>
2025-03-01 00:10:38 +0900

    feature Upgrade Koin annotation to 2.0.0.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,8 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libraries") {
             library("koin", "io.insert-koin", "koin-core").version("4.0.2")
-            library("koinAnnotations", "io.insert-koin", "koin-annotations").version("1.4.0")
-            library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("1.4.0")
+            library("koinAnnotations", "io.insert-koin", "koin-annotations").version("2.0.0")
+            library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("2.0.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.3.3")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.6.1")
             library("slf4j", "org.slf4j", "slf4j-api").version("2.0.16")
