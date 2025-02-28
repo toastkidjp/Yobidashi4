20b26eb8c1484bccd1cf0b3211275ff08dae2541
toastkidjp <10fmi13@gmail.com>
2025-03-01 00:12:49 +0900

    feature Upgrade Kotlin coroutines to 1.10.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
             library("koinAnnotations", "io.insert-koin", "koin-annotations").version("2.0.0")
             library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("2.0.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.8.0")
-            library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.6.1")
+            library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.10.1")
             library("slf4j", "org.slf4j", "slf4j-api").version("2.0.16")
             library("reload4j", "org.slf4j", "slf4j-reload4j").version("2.0.16")
             library("zxing", "com.google.zxing", "core").version("3.4.1")
