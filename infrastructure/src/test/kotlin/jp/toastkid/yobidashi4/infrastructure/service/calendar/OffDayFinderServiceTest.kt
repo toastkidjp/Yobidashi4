11cea6831e0d52552d8b595042234ccd5feeec7f
toastkidjp <10fmi13@gmail.com>
2025-03-06 09:27:13 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/calendar/OffDayFinderServiceTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/calendar/OffDayFinderServiceTest.kt
@@ -117,6 +117,15 @@ internal class OffDayFinderServiceTest {
     }
 
     @Test
+    fun testAutumnalEquinoxDay1979() {
+        assertFalse(offDayFinderService(1979, 9, 21, DayOfWeek.THURSDAY))
+
+        verify(inverse = true) { anyConstructed<EquinoxDayCalculator>().calculateVernalEquinoxDay(any()) }
+        verify(exactly = 1) { anyConstructed<EquinoxDayCalculator>().calculateAutumnalEquinoxDay(1979) }
+        verify(exactly = 1) { anyConstructed<SpecialCaseOffDayCalculatorService>().invoke(any(), any()) }
+    }
+
+    @Test
     fun testSpecialCase2019ButNotOffDayCase() {
         every { anyConstructed<SpecialCaseOffDayCalculatorService>().invoke(any(), any()) } returns setOf(Holiday("", 5, 1))
 
