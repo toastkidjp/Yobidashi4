6c810addd9129ed53f3edd7075034b9d343ced85
toastkidjp <10fmi13@gmail.com>
2025-03-17 08:49:31 +0900

    refactor Use URI instead of URL.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/editor/LinkDecoratorServiceImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/editor/LinkDecoratorServiceImplementation.kt
@@ -1,19 +1,18 @@
 package jp.toastkid.yobidashi4.infrastructure.service.editor
 
-import java.io.IOException
-import java.net.MalformedURLException
-import java.net.URL
 import jp.toastkid.yobidashi4.domain.service.editor.LinkDecoratorService
 import org.jsoup.Jsoup
 import org.koin.core.annotation.Single
+import java.io.IOException
+import java.net.URI
 
 @Single
 class LinkDecoratorServiceImplementation : LinkDecoratorService {
 
     override operator fun invoke(link: String): String {
         val url = try {
-            URL(link)
-        } catch (e: MalformedURLException) {
+            URI(link).toURL()
+        } catch (e: IllegalArgumentException) {
             return link
         }
 
