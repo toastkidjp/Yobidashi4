e6b659ab24cac2d6b4480e3ea39587de7ad89a7e
toastkidjp <10fmi13@gmail.com>
2025-03-17 08:55:51 +0900

    refactor Use URI instead of URL.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabViewModel.kt
@@ -3,16 +3,16 @@ package jp.toastkid.yobidashi4.presentation.barcode
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.text.input.TextFieldValue
-import java.awt.image.BufferedImage
-import java.io.IOException
-import java.net.URL
-import javax.imageio.ImageIO
 import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeDecoder
 import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeEncoder
 import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.awt.image.BufferedImage
+import java.io.IOException
+import java.net.URI
+import javax.imageio.ImageIO
 
 class BarcodeToolTabViewModel : KoinComponent {
 
@@ -50,7 +50,7 @@ class BarcodeToolTabViewModel : KoinComponent {
 
         if (value.text.isNotBlank()) {
             val image = try {
-                ImageIO.read(URL(value.text))
+                ImageIO.read(URI(value.text).toURL())
             } catch (e: IOException) {
                 e.printStackTrace()
                 null
