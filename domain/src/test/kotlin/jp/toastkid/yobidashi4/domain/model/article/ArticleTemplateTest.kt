31fb78607ca647837aeada6119570101ef47dec2
toastkidjp <10fmi13@gmail.com>
2025-03-19 00:02:15 +0900

    refactor Use answers.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleTemplateTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleTemplateTest.kt
@@ -53,11 +53,13 @@ internal class ArticleTemplateTest {
     @Test
     fun testContainsWorkDay() {
         mockkConstructor(UserTemplateStreamReader::class)
-        every { anyConstructed<UserTemplateStreamReader>().invoke() } returns """
+        every { anyConstructed<UserTemplateStreamReader>().invoke() } answers {
+            """
             {{workday}}
 test
             {{/workday}}
         """.trimIndent().byteInputStream()
+        }
         val content = ArticleTemplate(LocalDate.of(2023, 2, 23), offDayFinderService).invoke("test")
         assertTrue(content.contains("test"))
     }
