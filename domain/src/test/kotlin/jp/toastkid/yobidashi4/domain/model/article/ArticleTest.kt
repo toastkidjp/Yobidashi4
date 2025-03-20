793526244ece85eb74b87c5acaebaa85aadf141f
toastkidjp <10fmi13@gmail.com>
2025-03-21 08:57:44 +0900

    refactor Modify returns.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/article/ArticleTest.kt
@@ -69,7 +69,7 @@ internal class ArticleTest {
     @Test
     fun testLastModified() {
         val fileTime = mockk<FileTime>()
-        every { fileTime.toMillis() }.returns(42L)
+        every { fileTime.toMillis() } returns 42L
         every { Files.getLastModifiedTime(any()) }.returns(fileTime)
 
         val article = Article(path)
