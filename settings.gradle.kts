6b8fa74bf3316f1b3cfb934f7a346bd7b76023ec
toastkidjp <10fmi13@gmail.com>
2025-03-23 08:57:39 +0900

    refactor Add kover version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,7 @@ dependencyResolutionManagement {
             library("jcef", "me.friwi", "jcefmaven").version("132.3.1")
             library("jsoup", "org.jsoup", "jsoup").version("1.18.3")
             version("compose", "1.7.3")
+            version("kover", "0.9.1")
         }
     }
 }
