f66e3971643328153ca9d0bb664cfca5894c22ba
toastkidjp <10fmi13@gmail.com>
2025-03-23 08:56:26 +0900

    refactor Upgrade Kover to 0.9.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
     // Apply the application plugin to add support for building a CLI application in Java.
     id("com.google.devtools.ksp") version "2.1.20-1.0.31"
-    id("org.jetbrains.kotlinx.kover") version "0.8.3"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
 group = "jp.toastkid.yobidashi4"
