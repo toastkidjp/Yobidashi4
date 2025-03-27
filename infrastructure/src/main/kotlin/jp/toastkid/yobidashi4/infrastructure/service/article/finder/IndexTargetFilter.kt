31f19c9b7edac5450ce6258d559a965c49bb0059
toastkidjp <10fmi13@gmail.com>
2025-03-28 07:43:31 +0900

    bugfix Add extension filtering.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/finder/IndexTargetFilter.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/finder/IndexTargetFilter.kt
@@ -2,15 +2,19 @@ package jp.toastkid.yobidashi4.infrastructure.service.article.finder
 
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.extension
 import kotlin.io.path.nameWithoutExtension
 
 class IndexTargetFilter(private val indexDirectoryPath: Path) {
 
     private val lastIndexed = calculateLastUpdated()
 
+    private val targetExtensions = setOf("txt", "md")
+
     operator fun invoke(it: Path): Boolean {
         return Files.isDirectory(it).not()
                 && Files.isReadable(it)
+                && targetExtensions.contains(it.extension)
                 && Files.getLastModifiedTime(it).toMillis() > lastIndexed
     }
 
