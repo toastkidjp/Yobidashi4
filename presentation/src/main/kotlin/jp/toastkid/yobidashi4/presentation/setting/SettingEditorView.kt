3fa1f149f0bcc4d56366503a7b1aaadefdf9628e
toastkidjp <10fmi13@gmail.com>
2025-03-30 07:52:17 +0900

    feature Implement view.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/setting/SettingEditorView.kt
@@ -0,0 +1,98 @@
+package jp.toastkid.yobidashi4.presentation.setting
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import jp.toastkid.yobidashi4.presentation.component.SingleLineTextField
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@Composable
+fun SettingEditorView() {
+    val viewModel = remember { SettingEditorViewModel() }
+    val coroutineScope = rememberCoroutineScope()
+
+    Surface(
+        color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
+        elevation = 4.dp,
+        modifier = Modifier.onKeyEvent {
+            return@onKeyEvent viewModel.onKeyEvent(coroutineScope, it)
+        }.focusRequester(viewModel.focusRequester()).focusable(true)
+    ) {
+        Box {
+            LazyColumn(
+                state = viewModel.listState(),
+                userScrollEnabled = true
+            ) {
+                stickyHeader {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Button(onClick = {}) {
+                            Text("Save")
+                        }
+                    }
+                }
+
+                itemsIndexed(viewModel.items(), { _, item -> item.first }) { index, item ->
+                    val titleState = remember { mutableStateOf(item.second) }
+                    val headerCursorOn = mutableStateOf(false)
+                    val headerColumnBackgroundColor = animateColorAsState(
+                        if (headerCursorOn.value) MaterialTheme.colors.primary
+                        else if (viewModel.listState().firstVisibleItemIndex != 0) MaterialTheme.colors.surface
+                        else Color.Transparent
+                    )
+                    Row(modifier = Modifier.fillMaxWidth().animateItem()
+                        .onPointerEvent(PointerEventType.Enter) {
+                            headerCursorOn.value = true
+                        }
+                        .onPointerEvent(PointerEventType.Exit) {
+                            headerCursorOn.value = false
+                        }
+                        .drawBehind { drawRect(headerColumnBackgroundColor.value) }
+                    ) {
+                        SingleLineTextField(
+                            titleState.value,
+                            item.first,
+                            {
+                                viewModel.update(item.first, it)
+                            },
+                            { viewModel.update(item.first, TextFieldValue()) }
+                        )
+                    }
+                }
+            }
+            VerticalScrollbar(adapter = rememberScrollbarAdapter(viewModel.listState()), modifier = Modifier.fillMaxHeight().align(
+                Alignment.CenterEnd))
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.start()
+    }
+}
