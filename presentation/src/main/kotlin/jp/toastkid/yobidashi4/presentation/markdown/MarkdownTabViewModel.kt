78ab1e0a4c2c0621fd048b88f22ded1e995530a2
toastkidjp <10fmi13@gmail.com>
2025-04-17 09:15:41 +0900

    refactor Modify injection.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewModel.kt
@@ -7,11 +7,13 @@ import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class MarkdownTabViewModel {
+class MarkdownTabViewModel : KoinComponent {
 
     private val scrollState = ScrollState(0)
 
     private val focusRequester = FocusRequester()
+
+    private val mainViewModel: MainViewModel by inject()
 
     suspend fun launch(scrollPosition: Int) {
         scrollState.scrollTo(scrollPosition)
@@ -23,8 +25,6 @@ class MarkdownTabViewModel {
     fun focusRequester() = focusRequester
 
     fun onDispose(tab: ScrollableContentTab) {
-        val mainViewModel = object : KoinComponent { val vm: MainViewModel by inject() }.vm
-
         mainViewModel.updateScrollableTab(tab, scrollState.value)
     }
 
