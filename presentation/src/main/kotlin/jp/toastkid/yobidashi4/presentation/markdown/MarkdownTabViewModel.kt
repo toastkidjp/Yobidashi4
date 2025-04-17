fdde28d90b109a51a5917c791b71d83d0076da6f
toastkidjp <10fmi13@gmail.com>
2025-04-17 09:13:18 +0900

    refactor Add viewModel.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewModel.kt
@@ -0,0 +1,31 @@
+package jp.toastkid.yobidashi4.presentation.markdown
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.ui.focus.FocusRequester
+import jp.toastkid.yobidashi4.domain.model.tab.ScrollableContentTab
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class MarkdownTabViewModel {
+
+    private val scrollState = ScrollState(0)
+
+    private val focusRequester = FocusRequester()
+
+    suspend fun launch(scrollPosition: Int) {
+        scrollState.scrollTo(scrollPosition)
+        focusRequester().requestFocus()
+    }
+
+    fun scrollState() = scrollState
+
+    fun focusRequester() = focusRequester
+
+    fun onDispose(tab: ScrollableContentTab) {
+        val mainViewModel = object : KoinComponent { val vm: MainViewModel by inject() }.vm
+
+        mainViewModel.updateScrollableTab(tab, scrollState.value)
+    }
+
+}
