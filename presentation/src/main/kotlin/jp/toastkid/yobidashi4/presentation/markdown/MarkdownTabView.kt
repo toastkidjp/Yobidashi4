cfb11f802478a617b405e358a2ef6b748703e579
toastkidjp <10fmi13@gmail.com>
2025-04-17 09:13:41 +0900

    refactor Use viewModel.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabView.kt
@@ -1,6 +1,5 @@
 package jp.toastkid.yobidashi4.presentation.markdown
 
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -8,36 +7,28 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import jp.toastkid.yobidashi4.domain.model.tab.MarkdownPreviewTab
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.launch
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 @Composable
 internal fun MarkdownTabView(tab: MarkdownPreviewTab, modifier: Modifier) {
-    val scrollState = rememberScrollState(tab.scrollPosition())
-    val focusRequester = remember { FocusRequester() }
     val coroutineScope = rememberCoroutineScope()
+    val viewModel = remember { MarkdownTabViewModel() }
 
     Surface(
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
-        modifier = modifier.focusRequester(focusRequester)
+        modifier = modifier.focusRequester(viewModel.focusRequester())
     ) {
-        MarkdownPreview(tab.markdown(), scrollState, modifier)
+        MarkdownPreview(tab.markdown(), viewModel.scrollState(), modifier)
 
         DisposableEffect(tab) {
-            focusRequester.requestFocus()
             coroutineScope.launch {
-                scrollState.scrollTo(tab.scrollPosition())
+                viewModel.launch(tab.scrollPosition())
             }
 
             onDispose {
-                val mainViewModel = object : KoinComponent { val vm: MainViewModel by inject() }.vm
-
-                mainViewModel.updateScrollableTab(tab, scrollState.value)
+                viewModel.onDispose(tab)
             }
         }
     }
