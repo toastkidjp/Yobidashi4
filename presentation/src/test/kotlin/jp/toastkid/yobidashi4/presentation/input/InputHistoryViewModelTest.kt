87024728295e502de616738e5d88852dabd79d20
toastkidjp <10fmi13@gmail.com>
2025-04-23 08:37:31 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/input/InputHistoryViewModelTest.kt
@@ -0,0 +1,123 @@
+package jp.toastkid.yobidashi4.presentation.input
+
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.input.InputHistory
+import jp.toastkid.yobidashi4.domain.model.tab.InputHistoryTab
+import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
+import jp.toastkid.yobidashi4.domain.service.article.finder.FullTextArticleFinder
+import jp.toastkid.yobidashi4.presentation.lib.input.InputHistoryService
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import java.util.stream.Stream
+
+class InputHistoryViewModelTest {
+
+    private lateinit var subject: InputHistoryViewModel
+
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @MockK
+    private lateinit var keywordSearch: FullTextArticleFinder
+
+    @MockK
+    private lateinit var articlesReaderService: ArticlesReaderService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier = null) { mainViewModel } bind (MainViewModel::class)
+                    single(qualifier = null) { keywordSearch } bind (FullTextArticleFinder::class)
+                    single(qualifier = null) { articlesReaderService } bind (ArticlesReaderService::class)
+                }
+            )
+        }
+
+        every { mainViewModel.initialAggregationType() } returns 0
+        every { keywordSearch.invoke(any()) } returns mockk()
+        every { articlesReaderService.invoke() } returns Stream.empty()
+        mockkConstructor(InputHistoryService::class)
+        every { anyConstructed<InputHistoryService>().delete(any(), any()) } just Runs
+
+        subject = InputHistoryViewModel()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @Test
+    fun listState() {
+        assertNotNull(subject.listState())
+    }
+
+    @Test
+    fun items() {
+        assertTrue(subject.items().isEmpty())
+    }
+
+    @Test
+    fun onPointerEvent() {
+        subject.onPointerEvent(mockk(), mockk())
+    }
+
+    @Test
+    fun open() {
+    }
+
+    @Test
+    fun openOnBackground() {
+    }
+
+    @Test
+    fun dateTimeString() {
+        println(subject.dateTimeString(InputHistory("test", System.currentTimeMillis())))
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun launch() {
+        runComposeUiTest {
+            setContent {
+                val coroutineScope = rememberCoroutineScope()
+                subject.launch(coroutineScope, InputHistoryTab("test"))
+                subject.delete(InputHistory("test", 1))
+            }
+        }
+    }
+
+    @Test
+    fun onDispose() {
+        every { mainViewModel.updateScrollableTab(any(), any()) } just Runs
+
+        subject.onDispose(InputHistoryTab("test"))
+
+        verify { mainViewModel.updateScrollableTab(any(), any()) }
+    }
+
+}
