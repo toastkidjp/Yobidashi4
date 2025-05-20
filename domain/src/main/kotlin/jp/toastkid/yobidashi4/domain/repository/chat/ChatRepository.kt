fbf92a94c9ea191b246bb782e28a28802212fe2f
toastkidjp <10fmi13@gmail.com>
2025-05-21 08:50:30 +0900

    feature Modify interfaces.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/chat/ChatRepository.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/chat/ChatRepository.kt
@@ -1,7 +1,9 @@
 package jp.toastkid.yobidashi4.domain.repository.chat
 
+import jp.toastkid.yobidashi4.domain.repository.chat.dto.ChatResponseItem
+
 interface ChatRepository {
 
-    fun request(content: String, streamLineConsumer: (String?) -> Unit)
+    fun request(content: String, streamLineConsumer: (ChatResponseItem?) -> Unit)
 
 }
