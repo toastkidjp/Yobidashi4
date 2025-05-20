f4f8a8426487a460c20f9d604d9caba4ce4b964c
toastkidjp <10fmi13@gmail.com>
2025-05-21 08:49:37 +0900

    feature Add API definitions.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/model/chat/ChatApiUrl.kt
@@ -0,0 +1,5 @@
+package jp.toastkid.yobidashi4.infrastructure.model.chat
+
+const val CHAT = "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:streamGenerateContent?alt=sse&key="
+
+const val IMAGE_GENERATOR = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation:streamGenerateContent?alt=sse&key="
