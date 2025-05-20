b45a0f193d2dc15fcc5225fa5b57eb8a43898262
toastkidjp <10fmi13@gmail.com>
2025-05-21 08:53:04 +0900

    feature Modify for image generation.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatServiceImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatServiceImplementation.kt
@@ -5,6 +5,8 @@ import jp.toastkid.yobidashi4.domain.model.chat.ChatMessage
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.repository.chat.ChatRepository
 import jp.toastkid.yobidashi4.domain.service.chat.ChatService
+import jp.toastkid.yobidashi4.infrastructure.model.chat.CHAT
+import jp.toastkid.yobidashi4.infrastructure.model.chat.IMAGE_GENERATOR
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -18,19 +20,31 @@ class ChatServiceImplementation : ChatService, KoinComponent {
 
     private val setting: Setting by inject()
 
-    private val repository: ChatRepository by inject(parameters = { ParametersHolder(mutableListOf(setting.chatApiKey())) })
+    private val repository: ChatRepository by inject(parameters = {
+        ParametersHolder(mutableListOf(setting.chatApiKey(), CHAT))
+    })
 
-    override fun send(text: String, onUpdate: () -> Unit): String? {
+    private val imageGeneratorRepository: ChatRepository by inject(parameters = {
+        ParametersHolder(mutableListOf(setting.chatApiKey(), IMAGE_GENERATOR))
+    })
+
+    override fun send(text: String, image: Boolean, onUpdate: () -> Unit): String? {
         val chat = chatHolder.get()
         chat.addUserText(text)
         onUpdate()
 
-        repository.request(chat.makeContent()) {
+        (if (image) imageGeneratorRepository else repository)
+            .request(chat.makeContent(image)) {
             if (it == null) {
                 return@request
             }
 
-            chat.addModelText(it.replace("\"", ""))
+            if (it.image()) {
+                chat.addModelImage(it.message())
+            } else {
+                chat.addModelText(it.message().replace("\"", ""))
+            }
+
             onUpdate()
         }
 
