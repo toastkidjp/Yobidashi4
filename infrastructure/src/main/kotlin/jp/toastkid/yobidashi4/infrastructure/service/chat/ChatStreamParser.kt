7469ce03bf24fb24fe3a8bf7a078b0cdbacc395b
toastkidjp <10fmi13@gmail.com>
2025-05-21 08:52:06 +0900

    feature Modify for image generation.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatStreamParser.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/chat/ChatStreamParser.kt
@@ -1,21 +1,36 @@
 package jp.toastkid.yobidashi4.infrastructure.service.chat
 
+import jp.toastkid.yobidashi4.domain.repository.chat.dto.ChatResponseItem
 import java.util.regex.Pattern
 
 class ChatStreamParser {
 
-    operator fun invoke(line: String): String? {
+    operator fun invoke(line: String): ChatResponseItem? {
         if (line.contains("\"finishReason\": \"SAFETY\"")) {
-            return "[ERROR]"
+            return ChatResponseItem.error()
         }
         if (line.contains("\"finishReason\": \"OTHER\"")) {
-            return "[OTHER]"
+            return ChatResponseItem.other()
         }
 
         val matcher = pattern.matcher(line)
-        return if (matcher.find()) matcher.group(1).replace("\\n", "\n") else null
+        val message = if (matcher.find()) matcher.group(1).replace("\\n", "\n") else null
+
+        val imageMatcher = imagePattern.matcher(line)
+        val base64 = if (imageMatcher.find()) imageMatcher.group(2) else null
+
+        if (message.isNullOrBlank() && base64.isNullOrBlank()) {
+            return null
+        }
+
+        return ChatResponseItem(
+            message = message ?: base64 ?: "",
+            image = base64 != null
+        )
     }
 
 }
 
 private val pattern = Pattern.compile("\\{\"parts\": \\[\\{\"text\": \"(.+?)\"}]", Pattern.DOTALL)
+
+private val imagePattern = Pattern.compile("\"inlineData\":(.+?)\"data\": \"(.+?)\"", Pattern.DOTALL)
