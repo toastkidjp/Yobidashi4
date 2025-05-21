91de34d018081df9201259a00b6e28111c46e9a3
toastkidjp <10fmi13@gmail.com>
2025-05-22 08:59:45 +0900

    refactor Use ViewModel.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContent.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContent.kt
@@ -15,12 +15,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import jp.toastkid.yobidashi4.presentation.lib.text.KeywordHighlighter
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 import java.io.ByteArrayInputStream
-import java.nio.file.Path
 import java.util.Base64
 import javax.imageio.ImageIO
 
@@ -30,7 +25,8 @@ internal fun MessageContent(
     base64Image: String? = null,
     modifier: Modifier
 ) {
-    val keywordHighlighter = remember { KeywordHighlighter() }
+    val viewModel = remember { MessageContentViewModel() }
+
     Column(modifier) {
         text.split("\n").forEach {
             val listLine = it.startsWith("* ")
@@ -47,7 +43,7 @@ internal fun MessageContent(
                 }
 
                 Text(
-                    keywordHighlighter(if (listLine) it.substring(2) else it),
+                    viewModel.lineText(listLine, it),
                     fontSize = 16.sp
                 )
             }
@@ -60,16 +56,7 @@ internal fun MessageContent(
                 }.toPainter(),
                 contentDescription = text,
                 modifier = Modifier.clickable {
-                    val image = ByteArrayInputStream(Base64.getDecoder().decode(base64Image))
-                        .use { ImageIO.read(it) }
-                    ImageIO.write(image, "png", Path.of("user/download/${System.currentTimeMillis()}.png").toFile())
-                    val mainViewModel = object : KoinComponent {
-                        val vm: MainViewModel by inject()
-                    }.vm
-                    mainViewModel
-                        .showSnackbar("Store image file.", "Open") {
-                            mainViewModel.openFile(Path.of("user/download"))
-                        }
+                    viewModel.storeImage(base64Image)
                 }
             )
         }
