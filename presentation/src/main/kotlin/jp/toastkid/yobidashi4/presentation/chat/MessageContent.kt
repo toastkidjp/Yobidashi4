92dab16b173bfb077501effc4e14217002b1f63e
toastkidjp <10fmi13@gmail.com>
2025-05-22 08:28:39 +0900

    feature Add displaying image.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContent.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContent.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.presentation.chat
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -8,15 +9,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toPainter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jp.toastkid.yobidashi4.presentation.lib.text.KeywordHighlighter
+import java.io.ByteArrayInputStream
+import java.util.Base64
+import javax.imageio.ImageIO
 
 @Composable
 internal fun MessageContent(
     text: String,
+    base64Image: String? = null,
     modifier: Modifier
 ) {
     val keywordHighlighter = remember { KeywordHighlighter() }
@@ -40,6 +46,15 @@ internal fun MessageContent(
                     fontSize = 16.sp
                 )
             }
+        }
+
+        if (base64Image != null) {
+            Image(
+                ByteArrayInputStream(Base64.getDecoder().decode(base64Image)).use {
+                    ImageIO.read(it)
+                }.toPainter(),
+                contentDescription = text
+            )
         }
     }
 }
