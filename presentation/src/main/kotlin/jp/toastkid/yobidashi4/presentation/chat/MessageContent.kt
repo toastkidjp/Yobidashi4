40b26cbc75f759bdc997a21a8c05bb99adf06459
toastkidjp <10fmi13@gmail.com>
2025-05-24 08:30:13 +0900

    refactor Remove annotation.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContent.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContent.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
