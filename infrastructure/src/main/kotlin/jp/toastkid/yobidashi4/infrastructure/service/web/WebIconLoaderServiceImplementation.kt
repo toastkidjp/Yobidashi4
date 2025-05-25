842958435354f9559ba4d2213eeb35808431db68
toastkidjp <10fmi13@gmail.com>
2025-05-26 08:03:55 +0900

    refactor Use function reference.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/WebIconLoaderServiceImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/WebIconLoaderServiceImplementation.kt
@@ -43,7 +43,7 @@ class WebIconLoaderServiceImplementation : WebIconLoaderService {
                 it
             }
         }
-            .mapNotNull { toUrl(it) }
+            .mapNotNull(::toUrl)
             .forEach { webIconDownloader(it, webIcon.faviconFolder(), targetUrl.host) }
     }
 
