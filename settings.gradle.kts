5bfd08bb3f0f06ed05af4b12e5691cd787aaae3c
toastkidjp <10fmi13@gmail.com>
2025-05-25 09:06:33 +0900

    feature Upgrade Compose to 1.8.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
             library("zxing", "com.google.zxing", "core").version("3.4.1")
             library("jcef", "me.friwi", "jcefmaven").version("132.3.1")
             library("jsoup", "org.jsoup", "jsoup").version("1.18.3")
-            version("compose", "1.7.3")
+            version("compose", "1.8.1")
             version("kover", "0.9.1")
             version("kotlin", "2.1.20")
         }
