2438a1e5c2206ca22b8ff3ecf23dd1b49aa064cc
toastkidjp <10fmi13@gmail.com>
2025-06-01 07:13:56 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/repository/notification/NotificationEventFileStoreTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/repository/notification/NotificationEventFileStoreTest.kt
@@ -6,16 +6,16 @@ import io.mockk.mockkStatic
 import io.mockk.slot
 import io.mockk.unmockkAll
 import io.mockk.verify
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.StandardOpenOption
-import java.time.LocalDateTime
 import jp.toastkid.yobidashi4.domain.model.notification.NotificationEvent
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.time.LocalDateTime
 
 class NotificationEventFileStoreTest {
 
@@ -35,6 +35,7 @@ class NotificationEventFileStoreTest {
 Test	This notification is only attempt to send.	2023-12-08 10:51:00
 Test2	This notification is only attempt to send.	2023-12-08 10:52:00
 Test3	This notification is only attempt to send.	2023-12-08 10:49:00
+Test3	This notification is only attempt to send.	2023-13-48 34:49:00
 Test3	This notification is only attempt to send.	
 Test3	This notification is only attempt to send.
 Test3
