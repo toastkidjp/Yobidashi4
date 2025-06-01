a8410a18c7772124e7d873bc136f778181af0adc
toastkidjp <10fmi13@gmail.com>
2025-06-02 08:46:40 +0900

    refactor Modify test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumerTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumerTest.kt
@@ -440,8 +440,10 @@ class KeyEventConsumerTest {
             mockk(),
             { assertEquals("**test**", it.text) }
         )
+        val capturedConversionResult = conversionCapturingSlot.captured.invoke("test")
 
         assertTrue(consumed)
+        assertEquals("**test**", capturedConversionResult)
     }
 
     @Test
