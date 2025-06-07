ba9897dd832f32b1cd9f55287b4ce833beff7c82
toastkidjp <10fmi13@gmail.com>
2025-06-07 10:56:13 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContentKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContentKtTest.kt
@@ -1,16 +1,18 @@
 package jp.toastkid.yobidashi4.presentation.chat
 
+import androidx.compose.foundation.ContextMenuState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.MouseButton
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.performMouseInput
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runComposeUiTest
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
+import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -21,6 +23,9 @@ class MessageContentKtTest {
     fun setUp() {
         mockkConstructor(MessageContentViewModel::class)
         every { anyConstructed<MessageContentViewModel>().storeImage(any()) } just Runs
+        val contextMenuState = ContextMenuState()
+        contextMenuState.status = ContextMenuState.Status.Open(Rect(4f, 25f, 116f, 57f))
+        every { anyConstructed<MessageContentViewModel>().contextMenuState() } returns contextMenuState
     }
 
     @AfterEach
@@ -45,11 +50,9 @@ class MessageContentKtTest {
                 )
             }
 
-            onNodeWithContentDescription("image", useUnmergedTree = true)
-                .performMouseInput {
-                    enter()
-                    press(MouseButton.Secondary)
-                }
+            onNode(hasText("Store image")).performClick()
+
+            verify { anyConstructed<MessageContentViewModel>().storeImage(any()) }
         }
     }
 
