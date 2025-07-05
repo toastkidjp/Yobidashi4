d324614da1edb2bdbe65825308bdb7f11a52ed64
toastkidjp <10fmi13@gmail.com>
2025-07-05 11:02:38 +0900

    feature Modify to scrollable tab.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/ChatTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/ChatTab.kt
@@ -2,12 +2,21 @@ package jp.toastkid.yobidashi4.domain.model.tab
 
 import jp.toastkid.yobidashi4.domain.model.chat.Chat
 
-class ChatTab(private val chat: Chat = Chat()) : Tab {
+class ChatTab(
+    private val chat: Chat = Chat(),
+    private val scrollPosition: Int = 0,
+) : ScrollableContentTab {
 
     fun chat() = chat
 
     override fun title(): String {
         return "Chat"
+    }
+
+    override fun scrollPosition() = scrollPosition
+
+    override fun withNewPosition(scrollPosition: Int): ScrollableContentTab {
+        return ChatTab(this.chat, scrollPosition)
     }
 
 }
