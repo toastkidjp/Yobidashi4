58774f21fa6a7beca109d8e5c00ed34e05acf909
toastkidjp <10fmi13@gmail.com>
2025-07-07 09:39:03 +0900

    refactor Extract to function.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/icon/IconUrlFinder.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/web/icon/IconUrlFinder.kt
@@ -1,13 +1,16 @@
 package jp.toastkid.yobidashi4.infrastructure.service.web.icon
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 class IconUrlFinder {
 
     operator fun invoke(htmlSource: String) =
         Jsoup.parse(htmlSource)
             .select("link")
-            .filter { elem -> elem.attr("rel").contains("icon") }
+            .filter { elem -> extractIcon(elem) }
             .map { it.attr("href") }
+
+    private fun extractIcon(elem: Element) = elem.attr("rel").contains("icon")
 
 }
