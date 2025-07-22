16524657bd8e35a26553833bd691bcc25ffa6ca8
toastkidjp <10fmi13@gmail.com>
2025-07-22 09:36:07 +0900

    refactor Upgrade zxing to 3.5.3.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.10.2")
             library("slf4j", "org.slf4j", "slf4j-api").version("2.0.17")
             library("reload4j", "org.slf4j", "slf4j-reload4j").version("2.0.17")
-            library("zxing", "com.google.zxing", "core").version("3.4.1")
+            library("zxing", "com.google.zxing", "core").version("3.5.3")
             library("jcef", "me.friwi", "jcefmaven").version("135.0.20")
             library("jsoup", "org.jsoup", "jsoup").version("1.21.1")
             version("compose", "1.7.3")
