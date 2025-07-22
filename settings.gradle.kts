bcda6c9ca406dbf861efd1c036e29ec0c9fbef94
toastkidjp <10fmi13@gmail.com>
2025-07-22 09:35:12 +0900

    refactor Upgrade slf4j to 2.0.17.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
             library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("2.1.0")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.9.0")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.10.2")
-            library("slf4j", "org.slf4j", "slf4j-api").version("2.0.16")
+            library("slf4j", "org.slf4j", "slf4j-api").version("2.0.17")
             library("reload4j", "org.slf4j", "slf4j-reload4j").version("2.0.16")
             library("zxing", "com.google.zxing", "core").version("3.4.1")
             library("jcef", "me.friwi", "jcefmaven").version("135.0.20")
