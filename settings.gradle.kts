3f8095b9813130f0a75063a5e5720f05ec52dc33
toastkidjp <10fmi13@gmail.com>
2025-07-25 07:10:30 +0900

    feature Upgrade KSP to 2.2.0-2.0.2.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
             version("compose", "1.7.3")
             version("kover", "0.9.1")
             version("kotlin", "2.2.0")
-            version("ksp", "2.1.21-2.0.1")
+            version("ksp", "2.2.0-2.0.2")
         }
     }
 }
