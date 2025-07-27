9764a9a011e59d971ddc4940fe285d42b5395421
toastkidjp <10fmi13@gmail.com>
2025-07-27 10:48:53 +0900

    feature Implement functions.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/time/WorldTimeViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/time/WorldTimeViewModel.kt
@@ -2,20 +2,25 @@ package jp.toastkid.yobidashi4.presentation.time
 
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import java.time.LocalDateTime
+import java.text.DecimalFormat
 import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.zone.ZoneRulesException
 import java.util.Locale
 import java.util.TimeZone
+import java.util.concurrent.atomic.AtomicReference
 
 class WorldTimeViewModel : KoinComponent {
 
     private val mainViewModel: MainViewModel by inject()
+
+    private val currentTime = AtomicReference(ZonedDateTime.now())
 
     private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd(E)HH:mm:ss", Locale.ENGLISH)
 
@@ -42,13 +47,20 @@ class WorldTimeViewModel : KoinComponent {
 
     private val availableIDs = pickupTimeZone.plus(TimeZone.getAvailableIDs())
 
+    fun pickupTimeZone() = pickupTimeZone
+
     fun start() {
+        updateItems()
+    }
+
+    fun updateItems() {
         items.clear()
 
+        val currentTime = currentTime.get()
         availableIDs.mapNotNull {
             try {
                 val zoneId = ZoneId.of(it)
-                WorldTime(zoneId, formatter.format(LocalDateTime.now(zoneId)))
+                WorldTime(zoneId, formatter.format(currentTime.withZoneSameInstant(zoneId)))
             } catch (e: ZoneRulesException) {
                 null
             }
@@ -76,6 +88,81 @@ class WorldTimeViewModel : KoinComponent {
         ClipboardPutterService().invoke(text)
 
         mainViewModel.showSnackbar("Copy to clipboard.: $text")
+    }
+
+    private val openChooser = mutableStateOf(false)
+
+    fun openingChooser() = openChooser.value
+
+    fun openChooser() {
+        openChooser.value = true
+    }
+
+    fun closeChooser() {
+        openChooser.value = false
+    }
+
+    fun choose(value: String) {
+        val calendar = currentTime.get()
+        setCurrentTime(calendar.withZoneSameInstant(ZoneId.of(value)))
+        closeChooser()
+    }
+
+    private val openHourChooser = mutableStateOf(false)
+
+    fun openingHourChooser() = openHourChooser.value
+
+    fun openHourChooser() {
+        openHourChooser.value = true
+    }
+
+    fun closeHourChooser() {
+        openHourChooser.value = false
+    }
+
+    fun chooseHour(value: Int) {
+        val calendar = currentTime.get()
+        setCurrentTime(calendar.withHour(value))
+        closeHourChooser()
+    }
+
+    private val openMinutesChooser = mutableStateOf(false)
+
+    fun openingMinuteChooser() = openMinutesChooser.value
+
+    fun openMinuteChooser() {
+        openMinutesChooser.value = true
+    }
+
+    fun closeMinuteChooser() {
+        openMinutesChooser.value = false
+    }
+
+    fun chooseMinute(value: Int) {
+        val calendar = currentTime.get()
+        setCurrentTime(calendar.withMinute(value))
+        closeMinuteChooser()
+    }
+
+    private val zeroPaddingFormatter = DecimalFormat("00")
+
+    private val currentHour = mutableStateOf("")
+
+    fun currentHour(): String = currentHour.value
+
+    private val currentMinute = mutableStateOf("")
+
+    fun currentMinute(): String = currentMinute.value
+
+    fun currentTimezoneLabel(): String {
+        return currentTime.get().zone.id
+    }
+
+    fun setCurrentTime(newTime: ZonedDateTime) {
+        currentTime.set(newTime)
+        currentHour.value = zeroPaddingFormatter.format(currentTime.get().hour)
+        currentMinute.value = zeroPaddingFormatter.format(currentTime.get().minute)
+        updateItems()
     }
 
 }
