22edabb783d40bee5ac3cbf2e10d178de8ecd33e
toastkidjp <10fmi13@gmail.com>
2025-08-09 15:34:09 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/chat/GenerativeAiModelTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/chat/GenerativeAiModelTest.kt
@@ -8,6 +8,11 @@ class GenerativeAiModelTest {
     @Test
     fun image() {
         assertTrue(GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE.image())
+
+        assertTrue(
+            GenerativeAiModel.entries.filter { it != GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE }
+                .none { it.image() }
+        )
     }
 
 }
