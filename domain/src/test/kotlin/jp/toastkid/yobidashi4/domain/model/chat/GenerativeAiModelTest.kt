8c802da0eafdd51599ce71a647ac8a67e79044c7
toastkidjp <10fmi13@gmail.com>
2025-08-09 15:32:11 +0900

    refactor Add test case.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/chat/GenerativeAiModelTest.kt
@@ -0,0 +1,13 @@
+package jp.toastkid.yobidashi4.domain.model.chat
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class GenerativeAiModelTest {
+
+    @Test
+    fun image() {
+        assertTrue(GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE.image())
+    }
+
+}
