0e7aa075bb5bcecbd836ff6220cceec47d298d33
toastkidjp <10fmi13@gmail.com>
2025-08-15 07:13:33 +0900

    refactor Use function reference.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/FileListView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/FileListView.kt
@@ -129,7 +129,7 @@ internal fun FileListView(paths: List<Path>, modifier: Modifier = Modifier) {
                         viewModel.openingDropdown(fileListItem),
                         cursorOn,
                         viewModel::closeDropdown,
-                        { viewModel.selectedFiles() },
+                        viewModel::selectedFiles,
                         viewModel::openFile,
                         { viewModel.edit(fileListItem.path) },
                         { viewModel.preview(fileListItem.path) },
