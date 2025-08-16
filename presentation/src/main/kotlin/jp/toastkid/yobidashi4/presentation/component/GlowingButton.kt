23c68dd7322d4c9639889ad28df8704ffd14c5b8
toastkidjp <10fmi13@gmail.com>
2025-08-16 14:49:29 +0900

    refactor Modify interface.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/GlowingButton.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/GlowingButton.kt
@@ -8,19 +8,18 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
-internal fun GlowingButton(text: String, onClick: () -> Unit) {
+internal fun GlowingButton(onClick: () -> Unit, content: @Composable BoxScope.() -> Unit) {
     val infiniteTransition = rememberInfiniteTransition()
     val glow = infiniteTransition.animateFloat(
         initialValue = 1f,
@@ -41,8 +40,6 @@ internal fun GlowingButton(text: String, onClick: () -> Unit) {
             .background(Color(0xFF6200EA), shape = RoundedCornerShape(12.dp))
             .clickable(onClick = onClick)
             .padding(horizontal = 24.dp, vertical = 12.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text, color = Color.White, fontWeight = FontWeight.Bold)
-    }
+        contentAlignment = Alignment.Center, content = content
+    )
 }
