dbe3fa478b2050a74aae41b3485685688129b761
toastkidjp <10fmi13@gmail.com>
2025-08-16 14:52:40 +0900

    refactor Clean up interface.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/GlowingButton.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/GlowingButton.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
@@ -20,7 +19,6 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun GlowingButton(
-    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable BoxScope.() -> Unit,
 ) {
@@ -42,7 +40,6 @@ internal fun GlowingButton(
                 shadowElevation = 12f
             }
             .background(Color(0xFF6200EA), shape = RoundedCornerShape(12.dp))
-            .clickable(onClick = onClick)
             .padding(horizontal = 24.dp, vertical = 12.dp),
         contentAlignment = Alignment.Center, content = content
     )
