62eb242192c86c5611d3408cfaf2fd0e927f3004
toastkidjp <10fmi13@gmail.com>
2025-08-20 09:36:15 +0900

    refactor Use new component.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceView.kt
@@ -37,10 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
@@ -151,19 +148,9 @@ fun NumberPlaceView() {
                 else Color.Transparent
             )
 
-            DropdownMenuItem(
-                onClick = viewModel::renewGame,
-                modifier = Modifier
-                    .drawBehind { drawRect(backgroundColor.value) }
-                    .onPointerEvent(PointerEventType.Enter) {
-                        cursorOn.value = true
-                    }
-                    .onPointerEvent(PointerEventType.Exit) {
-                        cursorOn.value = false
-                    }
-            ) {
-                Text("Other board", color = fontColor.value)
-            }
+            HoverHighlightDropdownMenuItem(
+                "Other board", onClick = viewModel::renewGame
+            )
 
             HoverHighlightDropdownMenuItem(
                 "Set answer", onClick = viewModel::setCorrect
