8257d492dfee1575ff2812124903d84c817c5833
toastkidjp <10fmi13@gmail.com>
2025-08-21 09:17:35 +0900

    feature Enable to display icon.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/HoverHighlighingDropdownMenuItem.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/HoverHighlighingDropdownMenuItem.kt
@@ -1,12 +1,16 @@
 package jp.toastkid.yobidashi4.presentation.component
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -14,7 +18,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -22,6 +29,7 @@ internal fun HoverHighlightDropdownMenuItem(
     labelText: String,
     modifier: Modifier = Modifier,
     fontSize: TextUnit = 16.sp,
+    drawableResource: DrawableResource? = null,
     onClick: () -> Unit
 ) {
     val cursorOn = remember { mutableStateOf(false) }
@@ -45,6 +53,19 @@ internal fun HoverHighlightDropdownMenuItem(
                 cursorOn.value = false
             }
     ) {
-        Text(labelText, fontSize = fontSize, color = fontColor.value)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(4.dp)
+        ) {
+            if (drawableResource != null) {
+                Icon(
+                    painter = painterResource(drawableResource),
+                    contentDescription = labelText,
+                    tint = fontColor.value
+                )
+            }
+
+            Text(labelText, fontSize = fontSize, color = fontColor.value, modifier = Modifier.padding(start = if (drawableResource != null) 4.dp else 0.dp))
+        }
     }
 }
