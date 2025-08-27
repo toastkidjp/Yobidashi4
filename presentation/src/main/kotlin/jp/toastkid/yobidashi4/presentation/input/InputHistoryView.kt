0cba5b8185cad28a70c6501bba24aee05bd33063
toastkidjp <10fmi13@gmail.com>
2025-08-27 09:11:14 +0900

    refactor Use new component.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/input/InputHistoryView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/input/InputHistoryView.kt
@@ -1,13 +1,11 @@
 package jp.toastkid.yobidashi4.presentation.input
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,16 +18,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
@@ -37,6 +30,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jp.toastkid.yobidashi4.domain.model.input.InputHistory
 import jp.toastkid.yobidashi4.domain.model.tab.InputHistoryTab
+import jp.toastkid.yobidashi4.presentation.component.HoverHighlightRow
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 @Composable
@@ -54,52 +48,37 @@ fun InputHistoryView(tab: InputHistoryTab) {
                 modifier = Modifier.padding(end = 16.dp)
             ) {
                 items(viewModel.items(), key = InputHistory::hashCode) { item ->
-                    val cursorOn = remember { mutableStateOf(false) }
-                    val backgroundColor = animateColorAsState(if (cursorOn.value) MaterialTheme.colors.primary else Color.Transparent)
-
-                    Box(
-                        Modifier
-                            .animateItem()
-                            .drawBehind { drawRect(backgroundColor.value) }
-                            .onPointerEvent(PointerEventType.Enter) {
-                                cursorOn.value = true
-                            }
-                            .onPointerEvent(PointerEventType.Exit) {
-                                cursorOn.value = false
-                            }
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Text(
-                                "x",
-                                fontSize = 16.sp,
-                                modifier = Modifier
-                                    .size(44.dp)
-                                    .padding(4.dp)
-                                    .padding(start = 8.dp)
-                                    .clickable { viewModel.delete(item) }
-                                    .semantics { contentDescription = "Delete item ${item.word}" }
+                    HoverHighlightRow(
+                        modifier = Modifier.animateItem()
+                    ) { textColor ->
+                        Text(
+                            "x",
+                            fontSize = 16.sp,
+                            modifier = Modifier
+                                .size(44.dp)
+                                .padding(4.dp)
+                                .padding(start = 8.dp)
+                                .clickable { viewModel.delete(item) }
+                                .semantics { contentDescription = "Delete item ${item.word}" }
+                        )
+                        Column(modifier = Modifier
+                            .combinedClickable(
+                                enabled = true,
+                                onClick = {
+                                    viewModel.open(item)
+                                },
+                                onLongClick = {
+                                    viewModel.openOnBackground(item)
+                                }
                             )
-                            Column(modifier = Modifier
-                                .combinedClickable(
-                                    enabled = true,
-                                    onClick = {
-                                        viewModel.open(item)
-                                    },
-                                    onLongClick = {
-                                        viewModel.openOnBackground(item)
-                                    }
-                                )
-                                .padding(start = 4.dp)
-                            ) {
-                                val textColor = if (cursorOn.value) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
-
-                                Text(item.word, color = textColor)
-                                Text(
-                                    viewModel.dateTimeString(item),
-                                    color = textColor,
-                                    maxLines = 1, overflow = TextOverflow.Ellipsis)
-                                Divider(modifier = Modifier.padding(start = 16.dp, end = 4.dp))
-                            }
+                            .padding(start = 4.dp)
+                        ) {
+                            Text(item.word, color = textColor)
+                            Text(
+                                viewModel.dateTimeString(item),
+                                color = textColor,
+                                maxLines = 1, overflow = TextOverflow.Ellipsis)
+                            Divider(modifier = Modifier.padding(start = 16.dp, end = 4.dp))
                         }
                     }
                 }
