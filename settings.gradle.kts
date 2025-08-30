2dece895a6dc0d1cad7dc476e85db5c386298451
toastkidjp <10fmi13@gmail.com>
2025-08-31 06:25:15 +0900

    feature Upgrade Jsoup to 1.21.2.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
             library("reload4j", "org.slf4j", "slf4j-reload4j").version("2.0.17")
             library("zxing", "com.google.zxing", "core").version("3.5.3")
             library("jcef", "me.friwi", "jcefmaven").version("135.0.20")
-            library("jsoup", "org.jsoup", "jsoup").version("1.21.1")
+            library("jsoup", "org.jsoup", "jsoup").version("1.21.2")
             version("compose", "1.7.3")
             version("kover", "0.9.1")
             version("kotlin", "2.2.0")
