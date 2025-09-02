eeee2c43ab83b5b7e63fe68bfc81a49c6fff43e2
toastkidjp <10fmi13@gmail.com>
2025-09-02 09:36:01 +0900

    refactor Extract to local variable.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/DateCalculatorTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/DateCalculatorTest.kt
@@ -7,11 +7,12 @@ class DateCalculatorTest {
 
     @Test
     fun test() {
-        assertEquals(5, DateCalculator().invoke(2025, 5, 1))
-        assertEquals(26, DateCalculator().invoke(2025, 5, -1))
-        assertEquals(8, DateCalculator().invoke(2025, 9, 2))
-        assertEquals(15, DateCalculator().invoke(2025, 9, 3))
-        assertEquals(24, DateCalculator().invoke(2025, 11, 4))
+        val dateCalculator = DateCalculator()
+        assertEquals(5, dateCalculator.invoke(2025, 5, 1))
+        assertEquals(26, dateCalculator.invoke(2025, 5, -1))
+        assertEquals(8, dateCalculator.invoke(2025, 9, 2))
+        assertEquals(15, dateCalculator.invoke(2025, 9, 3))
+        assertEquals(24, dateCalculator.invoke(2025, 11, 4))
     }
 
 }
