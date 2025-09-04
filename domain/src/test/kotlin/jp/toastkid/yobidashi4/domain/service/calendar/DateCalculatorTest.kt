f5a0d160914ab8df284eb6b5f406ee8f8b12327f
toastkidjp <10fmi13@gmail.com>
2025-09-05 07:12:04 +0900

    refactor Extract to field.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/DateCalculatorTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/DateCalculatorTest.kt
@@ -1,18 +1,25 @@
 package jp.toastkid.yobidashi4.domain.service.calendar
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class DateCalculatorTest {
 
+    private lateinit var subject: MondayDateCalculator
+
+    @BeforeEach
+    fun setUp() {
+        subject = MondayDateCalculator()
+    }
+
     @Test
     fun test() {
-        val dateCalculator = MondayDateCalculator()
-        assertEquals(5, dateCalculator.invoke(2025, 5, 1))
-        assertEquals(26, dateCalculator.invoke(2025, 5, -1))
-        assertEquals(8, dateCalculator.invoke(2025, 9, 2))
-        assertEquals(15, dateCalculator.invoke(2025, 9, 3))
-        assertEquals(24, dateCalculator.invoke(2025, 11, 4))
+        assertEquals(5, subject.invoke(2025, 5, 1))
+        assertEquals(26, subject.invoke(2025, 5, -1))
+        assertEquals(8, subject.invoke(2025, 9, 2))
+        assertEquals(15, subject.invoke(2025, 9, 3))
+        assertEquals(24, subject.invoke(2025, 11, 4))
     }
 
 }
