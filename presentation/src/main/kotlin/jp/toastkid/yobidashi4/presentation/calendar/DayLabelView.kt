7fe3535d1bc5b0958ff3e357e0ee4e6a01ee101d
toastkidjp <10fmi13@gmail.com>
2025-09-05 07:11:08 +0900

    bugfix Modify layout.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/calendar/DayLabelView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/calendar/DayLabelView.kt
@@ -1,13 +1,10 @@
 package jp.toastkid.yobidashi4.presentation.calendar
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jp.toastkid.yobidashi4.presentation.component.HoverHighlightColumn
@@ -24,22 +21,17 @@ fun DayLabelView(
 ) {
     val viewModel = remember { DayLabelViewModel() }
 
-    Surface(
-        color = if (today) MaterialTheme.colors.primary.copy(alpha = 0.5f)
-        else if (viewModel.useOffDayBackground(offDay, dayOfWeek)) Color.White.copy(alpha = 0.8f)
-        else MaterialTheme.colors.surface,
+    HoverHighlightColumn(
         modifier = modifier
-    ) {
-        HoverHighlightColumn { textColor ->
-            Text(
-                viewModel.makeText(date),
-                fontSize = 16.sp,
-                color = viewModel.textColor(dayOfWeek, offDay, today) ?: textColor,
-                modifier = Modifier.padding(bottom = 4.dp)
-            )
-            label.forEach { text ->
-                Text(text, fontSize = viewModel.labelSize(text), color = viewModel.labelColor())
-            }
+    ) { textColor ->
+        Text(
+            viewModel.makeText(date),
+            fontSize = 16.sp,
+            color = viewModel.textColor(dayOfWeek, offDay, today) ?: textColor,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+        label.forEach { text ->
+            Text(text, fontSize = viewModel.labelSize(text), color = viewModel.labelColor())
         }
     }
 }
