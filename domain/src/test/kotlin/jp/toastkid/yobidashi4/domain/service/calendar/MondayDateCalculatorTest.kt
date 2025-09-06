07e1aafc62e5e8fdde1aebfb097f1168d32eedd4
toastkidjp <10fmi13@gmail.com>
2025-09-06 12:07:47 +0900

    refactor Rename test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/MondayDateCalculatorTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/MondayDateCalculatorTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class DateCalculatorTest {
+class MondayDateCalculatorTest {
 
     private lateinit var subject: MondayDateCalculator
 
