91522140e152ef16d4ae0ed5e77c01e915622e2f
toastkidjp <10fmi13@gmail.com>
2025-09-07 07:38:46 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumerTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/KeyEventConsumerTest.kt
@@ -24,6 +24,7 @@ import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
 import jp.toastkid.yobidashi4.domain.model.web.search.SearchUrlFactory
 import jp.toastkid.yobidashi4.domain.service.editor.LinkDecoratorService
+import jp.toastkid.yobidashi4.domain.service.editor.text.TextReformat
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.BlockQuotation
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.CommaInserter
 import jp.toastkid.yobidashi4.presentation.editor.markdown.text.ExpressionTextCalculatorService
@@ -73,6 +74,9 @@ class KeyEventConsumerTest {
     @MockK
     private lateinit var blockQuotation: BlockQuotation
 
+    @MockK
+    private lateinit var textReformat: TextReformat
+
     private val conversionCapturingSlot = slot<(String) -> String?>()
 
     @BeforeEach
@@ -86,7 +90,7 @@ class KeyEventConsumerTest {
             )
         }
 
-        subject = KeyEventConsumer(mainViewModel, controlAndLeftBracketCase, selectedTextConversion, searchUrlFactory, blockQuotation = blockQuotation)
+        subject = KeyEventConsumer(mainViewModel, controlAndLeftBracketCase, selectedTextConversion, searchUrlFactory, blockQuotation = blockQuotation, textReformat = textReformat)
         every { searchUrlFactory.invoke(any()) } returns "https://search.yahoo.co.jp/search?p=test"
         every { expressionTextCalculatorService.invoke(any()) } returns "3"
         every { selectedTextConversion.invoke(any(), any(), any(), capture(conversionCapturingSlot), any()) } returns true
@@ -889,6 +893,22 @@ class KeyEventConsumerTest {
                 { fail() }
             )
         )
+    }
+
+    @Test
+    fun textReformat() {
+        mockkConstructor(ClipboardFetcher::class)
+        every { anyConstructed<ClipboardFetcher>().invoke() } returns "test"
+        every { textReformat.invoke(any()) } returns "reformat"
+
+        val consumed = subject.invoke(
+            KeyEvent(Key.F, KeyEventType.KeyDown, isCtrlPressed = true, isShiftPressed = true),
+            TextFieldValue("", TextRange(0)),
+            mockk(),
+            { assertEquals("reformat", it.text) }
+        )
+
+        assertTrue(consumed)
     }
 
 }
