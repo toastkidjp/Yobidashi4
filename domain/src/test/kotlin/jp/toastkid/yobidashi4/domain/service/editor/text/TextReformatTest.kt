c5654542fb59c980944812289584b88b56eb7ef8
toastkidjp <10fmi13@gmail.com>
2025-09-07 07:31:18 +0900

    refactor Add test case.

---
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/editor/text/TextReformatTest.kt
@@ -0,0 +1,52 @@
+package jp.toastkid.yobidashi4.domain.service.editor.text
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class TextReformatTest {
+
+    private lateinit var subject: TextReformat
+
+    @BeforeEach
+    fun setUp() {
+        subject = TextReformat()
+    }
+
+    @Test
+    fun invoke() {
+        assertTrue(subject.invoke("").isEmpty())
+
+        assertEquals("text", subject.invoke("  text"))
+
+        assertEquals(
+            """
+a
+  b
+    c""",
+            subject.invoke(
+                """
+            a
+              b
+                c"""
+            )
+        )
+
+        assertEquals(
+            """
+{
+    "key": "value",
+    "values": [
+        "a",
+        "b"
+    ],
+    "obj": {
+        "inner": true
+    }
+}""".trimIndent(),
+            subject.invoke("""{"key": "value", "values": ["a", "b"], "obj": { "inner": true }}""")
+        )
+    }
+
+}
