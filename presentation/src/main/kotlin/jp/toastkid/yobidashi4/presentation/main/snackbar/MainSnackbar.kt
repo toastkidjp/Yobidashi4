9685ad05e7688a30f7728ddfed809b4da8652405
toastkidjp <10fmi13@gmail.com>
2025-09-06 13:26:00 +0900

    refactor Use new interfaces.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/snackbar/MainSnackbar.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/snackbar/MainSnackbar.kt
@@ -2,14 +2,16 @@ package jp.toastkid.yobidashi4.presentation.main.snackbar
 
 import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.End
 import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Start
-import androidx.compose.animation.core.exponentialDecay
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.OverscrollEffect
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.AnchoredDraggableDefaults
 import androidx.compose.foundation.gestures.AnchoredDraggableState
 import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
+import androidx.compose.foundation.gestures.snapTo
 import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.offset
@@ -23,8 +25,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.koin.core.component.KoinComponent
@@ -45,17 +50,6 @@ internal fun MainSnackbar(snackbarData: SnackbarData) {
         AnchoredDraggableState(
             initialValue = Center,
             anchors = anchors,
-            positionalThreshold = { it },
-            velocityThreshold = { 124.dp.value },
-            snapAnimationSpec = spring(),
-            decayAnimationSpec = exponentialDecay(),
-            confirmValueChange = {
-                when (it) {
-                    Start, End -> mainViewModel.dismissSnackbar()
-                    else -> return@AnchoredDraggableState false
-                }
-                true
-            }
         )
     }
 
@@ -64,7 +58,37 @@ internal fun MainSnackbar(snackbarData: SnackbarData) {
         contentColor = MaterialTheme.colors.onPrimary, modifier = Modifier
             .anchoredDraggable(
                 state = anchoredDraggableState,
-                orientation = Orientation.Horizontal
+                orientation = Orientation.Horizontal,
+                reverseDirection = false,
+                flingBehavior = AnchoredDraggableDefaults.flingBehavior(
+                    anchoredDraggableState,
+                    positionalThreshold = { it },
+                    animationSpec = spring()
+                ),
+                overscrollEffect = object : OverscrollEffect {
+                    override val isInProgress: Boolean
+                        get() = anchoredDraggableState.currentValue == Center
+
+                    override suspend fun applyToFling(
+                        velocity: Velocity,
+                        performFling: suspend (Velocity) -> Velocity
+                    ) {
+                        when (anchoredDraggableState.currentValue) {
+                            Start, End -> mainViewModel.dismissSnackbar()
+                            else -> anchoredDraggableState.snapTo(Center)
+                        }
+                    }
+
+                    override fun applyToScroll(
+                        delta: Offset,
+                        source: NestedScrollSource,
+                        performScroll: (Offset) -> Offset
+                    ): Offset {
+                        anchoredDraggableState.dispatchRawDelta(delta.x)
+                        return delta
+                    }
+
+                }
             )
             .offset { IntOffset(anchoredDraggableState.requireOffset().toInt(), 0) }
     ) {
