09ee4d760b72ec176ef9dffd2c4155477d4eb789
toastkidjp <10fmi13@gmail.com>
2025-09-12 07:40:29 +0900

    refactor Add license text.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/editor/EditorTabFileStore.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/editor/EditorTabFileStore.kt
@@ -1,12 +1,19 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.service.editor
 
-import java.io.IOException
-import java.nio.file.Files
 import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.IOException
+import java.nio.file.Files
 
 class EditorTabFileStore {
 
