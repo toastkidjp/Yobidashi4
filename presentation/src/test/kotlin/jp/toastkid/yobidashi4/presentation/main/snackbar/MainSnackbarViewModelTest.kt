810ce862600a877565d648b581137a2bb72c2de4
toastkidjp <10fmi13@gmail.com>
2025-09-29 08:26:11 +0900

    refactor Add test cases.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/snackbar/MainSnackbarViewModelTest.kt
@@ -0,0 +1,76 @@
+package jp.toastkid.yobidashi4.presentation.main.snackbar
+
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.End
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Start
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.ui.unit.dp
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.just
+import io.mockk.unmockkAll
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+class MainSnackbarViewModelTest {
+
+    private lateinit var subject: MainSnackbarViewModel
+
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier = null) { mainViewModel } bind (MainViewModel::class)
+                }
+            )
+        }
+        every { mainViewModel.dismissSnackbar() } just Runs
+
+        subject = MainSnackbarViewModel()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @Test
+    fun setAnchor() {
+        assertFalse(subject.isInitialized())
+
+        subject.setAnchor(
+            DraggableAnchors<Any> {
+                Start at -20.dp.value
+                Center at 0f
+                End at 20.dp.value
+            }
+        )
+
+        assertTrue(subject.isInitialized())
+
+        assertNotNull(subject.anchoredDraggableState())
+    }
+
+    @Test
+    fun overscrollEffect() {
+    }
+
+}
