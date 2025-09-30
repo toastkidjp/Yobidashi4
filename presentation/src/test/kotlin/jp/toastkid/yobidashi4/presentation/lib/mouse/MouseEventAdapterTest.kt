e99f7b24991f9c36b80ae247eb63bfedac6143d8
toastkidjp <10fmi13@gmail.com>
2025-09-30 11:31:19 +0900

    refactor Add unit test.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/mouse/MouseEventAdapterTest.kt
@@ -0,0 +1,42 @@
+package jp.toastkid.yobidashi4.presentation.lib.mouse
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.PointerInputChange
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.spyk
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class MouseEventAdapterTest {
+
+    private lateinit var subject: MouseEventAdapter
+
+    @MockK
+    private lateinit var event: PointerEvent
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        subject = MouseEventAdapter()
+    }
+
+    @OptIn(ExperimentalComposeUiApi::class)
+    @Test
+    fun isSecondaryClick() {
+        val pointerInputChange = mockk<PointerInputChange>()
+        every { pointerInputChange.previousPressed } returns false
+        every { pointerInputChange.pressed } returns true
+        event = spyk(PointerEvent(listOf(pointerInputChange)))
+        every { event.button } returns PointerButton.Secondary
+
+        assertTrue(subject.isSecondaryClick(event))
+    }
+
+}
