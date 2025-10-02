c4b970ada941d5f86330d0f8f5c3f113cb84bf8a
toastkidjp <10fmi13@gmail.com>
2025-10-02 09:29:42 +0900

    refactor Use adapter.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TabsViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/TabsViewModel.kt
@@ -2,9 +2,7 @@ package jp.toastkid.yobidashi4.presentation.main.content
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
@@ -14,6 +12,7 @@ import jp.toastkid.yobidashi4.domain.model.tab.WebTab
 import jp.toastkid.yobidashi4.domain.repository.chat.ChatExporter
 import jp.toastkid.yobidashi4.domain.service.table.TableContentExporter
 import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
+import jp.toastkid.yobidashi4.presentation.lib.mouse.PointerEventAdapter
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -43,11 +42,12 @@ class TabsViewModel  : KoinComponent {
         viewModel.setSelectedIndex(index)
     }
 
+    private val pointerEventAdapter = PointerEventAdapter()
+
     @OptIn(ExperimentalComposeUiApi::class)
     fun onPointerEvent(awaitPointerEvent: PointerEvent, tab: Tab) {
-        if (awaitPointerEvent.type == PointerEventType.Press
-            && !openingDropdown(tab)
-            && awaitPointerEvent.button == PointerButton.Secondary) {
+        if (pointerEventAdapter.isSecondaryClick(awaitPointerEvent)
+            && !openingDropdown(tab)) {
             openDropdown(tab)
         }
     }
