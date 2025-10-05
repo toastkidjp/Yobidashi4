fe1b8d95185d3c9e5f237390cf2ad91f5478bbd8
toastkidjp <10fmi13@gmail.com>
2025-10-05 09:16:29 +0900

    refactor Extract to local function.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/SlideDeckReader.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/SlideDeckReader.kt
@@ -8,6 +8,7 @@ import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicReference
 import java.util.regex.Matcher
 import java.util.regex.Pattern
+import java.util.stream.Stream
 
 class SlideDeckReader(private val pathToMarkdown: Path) {
 
@@ -28,126 +29,134 @@ class SlideDeckReader(private val pathToMarkdown: Path) {
      * @return List&lt;Slide&gt;
      */
     operator fun invoke(): SlideDeck {
-        val deck = SlideDeck()
         try {
-            Files.lines(pathToMarkdown).use { lines ->
-                lines.forEach { line: String ->
-                    if (line.startsWith("#")) {
-                        if (this.builder.get().hasTitle()) {
-                            this.builder.get().let {
-                                deck.add(it)
-                            }
-                            this.builder.set(Slide())
-                        }
-
-                        val builder = this.builder.get()
-                        if (line.startsWith("# ")) {
-                            builder.setFront(true)
-                        }
-                        val titleText = line.substring(line.indexOf(" ")).trim { it <= ' ' }
-                        builder.setTitle(titleText)
-                        if (deck.title.isEmpty()) {
-                            deck.title = titleText
-                        }
-                        this.builder.set(builder)
-                        return@forEach
-                    }
-                    if (line.startsWith("![")) {
-                        if (BackgroundExtractor.shouldInvoke(line)) {
-                            backgroundExtractor(line)?.let {
-                                if (deck.slides.isEmpty() && deck.background.isBlank()) {
-                                    deck.background = it
-                                    return@forEach
-                                }
-                                val slide = builder.get()
-                                slide.setBackground(it)
-                                this.builder.set(slide)
-                            }
-                            return@forEach
-                        }
-
-                        val slide = builder.get()
-                        slide.addLines(imageExtractor.invoke(line))
-                        this.builder.set(slide)
-                        return@forEach
-                    }
-                    if (line.startsWith("[footer](")) {
-                        val matcher: Matcher = FOOTER_TEXT.matcher(line)
-                        if (matcher.find()) {
-                            deck.footerText = matcher.group(1)
-                        }
-                        return@forEach
-                    }
-                    if (line.startsWith("> ")) {
-                        val slide = builder.get()
-                        slide.addQuotedLines(line)
-                        this.builder.set(slide)
-                        return@forEach
-                    }
-                    // Adding code block.
-                    if (line.startsWith("```")) {
-                        if (codeBlockBuilder.inCodeBlock()) {
-                            codeBlockBuilder.build().let {
-                                val slide = builder.get()
-                                slide.addLine(it)
-                                this.builder.set(slide)
-                                codeBlockBuilder.initialize()
-                            }
-                            return@forEach
-                        }
-
-                        codeBlockBuilder.startCodeBlock()
-                        val index = line.indexOf(":")
-                        val lastIndex = if (index == -1) line.length else index
-                        codeBlockBuilder.setCodeFormat(line.substring(3, lastIndex))
-                        return@forEach
-                    }
-                    if (codeBlockBuilder.shouldAppend(line)) {
-                        codeBlockBuilder.append(line)
-                        return@forEach
-                    }
-
-                    if (TableBuilder.isTableStart(line)) {
-                        if (!tableBuilder.active()) {
-                            tableBuilder.setActive()
-                        }
-
-                        if (TableBuilder.shouldIgnoreLine(line)) {
-                            return@forEach
-                        }
-
-                        if (tableBuilder.hasColumns() == false) {
-                            tableBuilder.setColumns(line)
-                            return@forEach
-                        }
-
-                        tableBuilder.addTableLines(line)
-                        return@forEach
-                    }
-
-                    if (tableBuilder.active()) {
-                        insertTable()
-                    }
-                    // Not code.
-                    if (line.isNotEmpty()) {
-                        val slide = this.builder.get()
-                        slide.addText(line)
-                        this.builder.set(slide)
-                    }
-                }
-
-                if (tableBuilder.active()) {
-                    insertTable()
-                }
-
-                builder.let {
-                    deck.add(it.get())
-                }
+            return Files.lines(pathToMarkdown).use { lines ->
+                readLines(lines)
             }
 
         } catch (e: IOException) {
             e.printStackTrace()
         }
+        return SlideDeck()
+    }
+
+    private fun readLines(
+        lines: Stream<String>
+    ): SlideDeck {
+        val deck = SlideDeck()
+        lines.forEach { line: String ->
+            if (line.startsWith("#")) {
+                if (this.builder.get().hasTitle()) {
+                    this.builder.get().let {
+                        deck.add(it)
+                    }
+                    this.builder.set(Slide())
+                }
+
+                val builder = this.builder.get()
+                if (line.startsWith("# ")) {
+                    builder.setFront(true)
+                }
+                val titleText = line.substring(line.indexOf(" ")).trim { it <= ' ' }
+                builder.setTitle(titleText)
+                if (deck.title.isEmpty()) {
+                    deck.title = titleText
+                }
+                this.builder.set(builder)
+                return@forEach
+            }
+            if (line.startsWith("![")) {
+                if (BackgroundExtractor.shouldInvoke(line)) {
+                    backgroundExtractor(line)?.let {
+                        if (deck.slides.isEmpty() && deck.background.isBlank()) {
+                            deck.background = it
+                            return@forEach
+                        }
+                        val slide = builder.get()
+                        slide.setBackground(it)
+                        this.builder.set(slide)
+                    }
+                    return@forEach
+                }
+
+                val slide = builder.get()
+                slide.addLines(imageExtractor.invoke(line))
+                this.builder.set(slide)
+                return@forEach
+            }
+            if (line.startsWith("[footer](")) {
+                val matcher: Matcher = FOOTER_TEXT.matcher(line)
+                if (matcher.find()) {
+                    deck.footerText = matcher.group(1)
+                }
+                return@forEach
+            }
+            if (line.startsWith("> ")) {
+                val slide = builder.get()
+                slide.addQuotedLines(line)
+                this.builder.set(slide)
+                return@forEach
+            }
+            // Adding code block.
+            if (line.startsWith("```")) {
+                if (codeBlockBuilder.inCodeBlock()) {
+                    codeBlockBuilder.build().let {
+                        val slide = builder.get()
+                        slide.addLine(it)
+                        this.builder.set(slide)
+                        codeBlockBuilder.initialize()
+                    }
+                    return@forEach
+                }
+
+                codeBlockBuilder.startCodeBlock()
+                val index = line.indexOf(":")
+                val lastIndex = if (index == -1) line.length else index
+                codeBlockBuilder.setCodeFormat(line.substring(3, lastIndex))
+                return@forEach
+            }
+            if (codeBlockBuilder.shouldAppend(line)) {
+                codeBlockBuilder.append(line)
+                return@forEach
+            }
+
+            if (TableBuilder.isTableStart(line)) {
+                if (!tableBuilder.active()) {
+                    tableBuilder.setActive()
+                }
+
+                if (TableBuilder.shouldIgnoreLine(line)) {
+                    return@forEach
+                }
+
+                if (tableBuilder.hasColumns() == false) {
+                    tableBuilder.setColumns(line)
+                    return@forEach
+                }
+
+                tableBuilder.addTableLines(line)
+                return@forEach
+            }
+
+            if (tableBuilder.active()) {
+                insertTable()
+            }
+            // Not code.
+            if (line.isNotEmpty()) {
+                val slide = this.builder.get()
+                slide.addText(line)
+                this.builder.set(slide)
+            }
+        }
+
+        if (tableBuilder.active()) {
+            insertTable()
+        }
+
+        builder.let {
+            deck.add(it.get())
+        }
+
         return deck
     }
 
