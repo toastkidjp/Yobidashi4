ab2618d5452d958a9294a6606bcd6bc31eaa3744
toastkidjp <10fmi13@gmail.com>
2025-10-07 08:19:25 +0900

    refactor Use adapter.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabViewModel.kt
@@ -6,21 +6,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.pointer.PointerButton
 import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.PointerEventType
-import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.model.tab.WebBookmarkTab
 import jp.toastkid.yobidashi4.domain.model.web.bookmark.Bookmark
 import jp.toastkid.yobidashi4.domain.model.web.icon.WebIcon
 import jp.toastkid.yobidashi4.domain.repository.BookmarkRepository
 import jp.toastkid.yobidashi4.presentation.lib.KeyboardScrollAction
 import jp.toastkid.yobidashi4.presentation.lib.clipboard.ClipboardPutterService
+import jp.toastkid.yobidashi4.presentation.lib.mouse.PointerEventAdapter
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.nio.file.Path
 
 class WebBookmarkTabViewModel : KoinComponent {
 
@@ -83,11 +82,12 @@ class WebBookmarkTabViewModel : KoinComponent {
         currentDropdownItem.value = null
     }
 
+    private val pointerEventAdapter = PointerEventAdapter()
+
     @OptIn(ExperimentalComposeUiApi::class)
     fun onPointerEvent(pointerEvent: PointerEvent, bookmark: Bookmark) {
-        if (pointerEvent.type == PointerEventType.Press
+        if (pointerEventAdapter.isSecondaryClick(pointerEvent)
             && openingDropdown(bookmark).not()
-            && pointerEvent.button == PointerButton.Secondary
         ) {
             openDropdown(bookmark)
         }
