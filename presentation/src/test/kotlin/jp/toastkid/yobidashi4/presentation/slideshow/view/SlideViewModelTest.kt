82c0a08741f2b670e16087f059848be231123d2b
toastkidjp <10fmi13@gmail.com>
2025-10-09 09:49:17 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/view/SlideViewModelTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/view/SlideViewModelTest.kt
@@ -1,5 +1,10 @@
 package jp.toastkid.yobidashi4.presentation.slideshow.view
 
+import androidx.compose.ui.focus.FocusRequester
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -26,6 +31,14 @@ class SlideViewModelTest {
 
     @Test
     fun requestFocus() {
+        subject = spyk(subject)
+        val focusRequester = mockk<FocusRequester>()
+        every { subject.focusRequester() } returns focusRequester
+        every { focusRequester.requestFocus() } returns true
+
+        subject.requestFocus()
+
+        verify { focusRequester.requestFocus() }
     }
 
 }
