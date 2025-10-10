73199c53541b273667fdb2e304ab54b3ac9f31c4
toastkidjp <10fmi13@gmail.com>
2025-10-10 23:25:17 +0900

    refactor Add license.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/view/TableLineViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/view/TableLineViewModel.kt
@@ -1,9 +1,16 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.presentation.slideshow.view
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import java.util.concurrent.atomic.AtomicReference
 import jp.toastkid.yobidashi4.presentation.markdown.TableSortService
+import java.util.concurrent.atomic.AtomicReference
 
 class TableLineViewModel {
 
