383fe8927358049c24abe96b44625bd1395a51ee
toastkidjp <10fmi13@gmail.com>
2025-10-11 21:30:53 +0900

    feature Add US.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/holiday/HolidayCalendar.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/holiday/HolidayCalendar.kt
@@ -3,6 +3,7 @@ package jp.toastkid.yobidashi4.domain.model.calendar.holiday
 import jp.toastkid.yobidashi4.domain.service.calendar.OffDayFinderService
 import jp.toastkid.yobidashi4.domain.service.calendar.japan.JapaneseOffDayFinderService
 import jp.toastkid.yobidashi4.domain.service.calendar.uk.UKOffDayFinder
+import jp.toastkid.yobidashi4.domain.service.calendar.us.AmericanOffDayFinder
 
 enum class HolidayCalendar(
     private val offDayFinderService: OffDayFinderService,
@@ -10,7 +11,8 @@ enum class HolidayCalendar(
 ) {
 
     JAPAN(JapaneseOffDayFinderService(), "\uD83C\uDDEF\uD83C\uDDF5"),
-    UK(UKOffDayFinder(), "\uD83C\uDDEC\uD83C\uDDE7");
+    UK(UKOffDayFinder(), "\uD83C\uDDEC\uD83C\uDDE7"),
+    US(AmericanOffDayFinder(), "\uD83C\uDDFA\uD83C\uDDF8");
 
     fun getHolidays(year: Int, month: Int): List<Holiday> {
         return offDayFinderService.invoke(year, month)
