9b06570f90c40a7b1a1839b9f207624efa6b1faa
toastkidjp <10fmi13@gmail.com>
2025-10-19 08:30:48 +0900

    refactor Clean up old test cases.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/SpecialCaseOffDayCalculatorServiceTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/SpecialCaseOffDayCalculatorServiceTest.kt
@@ -8,9 +8,7 @@
 package jp.toastkid.yobidashi4.domain.service.calendar
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -39,83 +37,6 @@ internal class SpecialCaseOffDayCalculatorServiceTest {
     )
     fun test(year: Int, month: Int, expected: Int) {
         assertEquals(expected, specialCaseOffDayCalculatorService.invoke(year, month).size)
-    }
-
-    @Test
-    fun testNormalDay() {
-        val result = specialCaseOffDayCalculatorService.invoke(2019, 1)
-
-        assertTrue(result.isEmpty())
-    }
-
-    @Test
-    fun test2019_4() {
-        val result = specialCaseOffDayCalculatorService.invoke(2019, 4)
-
-        assertEquals(1, result.size)
-    }
-
-    @Test
-    fun test2019_May() {
-        val result = specialCaseOffDayCalculatorService.invoke(2019, 5)
-
-        assertEquals(2, result.size)
-    }
-
-    @Test
-    fun test2019_July() {
-        val result = specialCaseOffDayCalculatorService.invoke(2019, 7)
-
-        assertTrue(result.isEmpty())
-    }
-
-    @Test
-    fun test2020_July() {
-        val result = specialCaseOffDayCalculatorService.invoke(2020, 7)
-
-        assertEquals(2, result.size)
-    }
-
-    @Test
-    fun test2021_July() {
-        val result = specialCaseOffDayCalculatorService.invoke(2021, 7)
-
-        assertEquals(2, result.size)
-    }
-
-    @Test
-    fun test2020_August() {
-        val result = specialCaseOffDayCalculatorService.invoke(2020, 8)
-
-        assertTrue(result.isEmpty())
-    }
-
-    @Test
-    fun test2020_Oct() {
-        val result = specialCaseOffDayCalculatorService.invoke(2020, 10)
-
-        assertTrue(result.isEmpty())
-    }
-
-    @Test
-    fun test2021_August() {
-        val result = specialCaseOffDayCalculatorService.invoke(2021, 8)
-
-        assertTrue(result.isEmpty())
-    }
-
-    @Test
-    fun test2021_Oct() {
-        val result = specialCaseOffDayCalculatorService.invoke(2021, 10)
-
-        assertTrue(result.isEmpty())
-    }
-
-    @Test
-    fun test2021_Apr() {
-        val result = specialCaseOffDayCalculatorService.invoke(2021, 4)
-
-        assertTrue(result.isEmpty())
     }
 
 }
