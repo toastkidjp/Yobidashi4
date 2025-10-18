4570056b8f1365aa3c9736d3f1b30d2b1587565c
toastkidjp <10fmi13@gmail.com>
2025-10-18 09:06:49 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/us/AmericanOffDayFinderTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/calendar/us/AmericanOffDayFinderTest.kt
@@ -35,6 +35,7 @@ class AmericanOffDayFinderTest {
         assertEquals(1, subject.invoke(2025, 9, false).size)
         assertEquals(1, subject.invoke(2025, 10, false).size)
         assertEquals(2, subject.invoke(2025, 11, false).size)
+        assertEquals(1, subject.invoke(2025, 12, false).size)
     }
 
 }
