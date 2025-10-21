05ce82a7c113a2eaf52cab2b461360a1db218460
toastkidjp <10fmi13@gmail.com>
2025-10-22 08:48:09 +0900

    feature Use cache.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/aggregation/FindResult.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/aggregation/FindResult.kt
@@ -1,15 +1,28 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.model.aggregation
 
 class FindResult(private val keyword: String) : AggregationResult {
 
     private val items = mutableMapOf<String, List<String>>()
 
+    private val cache: MutableList<Array<Any>> = mutableListOf()
+
     override fun header(): Array<Any> {
         return arrayOf("Name", "Lines")
     }
 
     override fun itemArrays(): Collection<Array<Any>> {
-        return items.map { arrayOf<Any>(it.key, it.value) }
+        if (cache.size != items.size) {
+            cache.clear()
+            items.map { arrayOf<Any>(it.key, it.value) }.forEach { cache.add(it) }
+        }
+        return cache
     }
 
     override fun title(): String {
