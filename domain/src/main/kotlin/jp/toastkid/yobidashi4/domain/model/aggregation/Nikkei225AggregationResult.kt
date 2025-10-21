828b185e52364f5f3ebd45ce1db92ff60fb6afcf
toastkidjp <10fmi13@gmail.com>
2025-10-22 08:48:27 +0900

    feature Use cache.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/aggregation/Nikkei225AggregationResult.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/aggregation/Nikkei225AggregationResult.kt
@@ -4,12 +4,18 @@ class Nikkei225AggregationResult : AggregationResult {
 
     private val map = mutableMapOf<String, Pair<String, String>>()
 
+    private val cache: MutableList<Array<Any>> = mutableListOf()
+
     override fun header(): Array<Any> {
         return arrayOf("Date", "Price", "Diff")
     }
 
     override fun itemArrays(): Collection<Array<Any>> {
-        return map.map { arrayOf<Any>(it.key, it.value.first, it.value.second) }
+        if (cache.size != map.size) {
+            cache.clear()
+            map.map { arrayOf<Any>(it.key, it.value.first, it.value.second) }.forEach { cache.add(it) }
+        }
+        return cache
     }
 
     override fun title(): String {
