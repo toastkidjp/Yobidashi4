b81806e1972773ce0b06b9191168f919aaef0faf
toastkidjp <10fmi13@gmail.com>
2025-10-26 09:06:18 +0900

    refactor Add functions.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/ChatTab.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/tab/ChatTab.kt
@@ -1,10 +1,13 @@
 package jp.toastkid.yobidashi4.domain.model.tab
 
 import jp.toastkid.yobidashi4.domain.model.chat.Chat
+import jp.toastkid.yobidashi4.domain.model.chat.GenerativeAiModel
 
 class ChatTab(
     private val chat: Chat = Chat(),
     private val scrollPosition: Int = 0,
+    private val initialModel: GenerativeAiModel = GenerativeAiModel.default(),
+    private val initialQuestion: String = "",
 ) : ScrollableContentTab {
 
     fun chat() = chat
@@ -18,5 +21,13 @@ class ChatTab(
     override fun withNewPosition(scrollPosition: Int): ScrollableContentTab {
         return ChatTab(this.chat, scrollPosition)
     }
+
+    fun shouldSendInitialQuestion(): Boolean {
+        return initialQuestion.isNotEmpty()
+    }
+
+    fun initialModel() = initialModel
+
+    fun initialQuestion() = initialQuestion
 
 }
