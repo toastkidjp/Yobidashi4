37b0e03975a7358fd0f49415fb1266706f5db120
toastkidjp <10fmi13@gmail.com>
2025-10-28 09:28:24 +0900

    refactor Add line breaks.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/browser/WebViewPool.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/browser/WebViewPool.kt
@@ -1,11 +1,22 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.model.browser
 
 import java.awt.Component
 
 interface WebViewPool {
+
     fun component(id: String, initialUrl: String): Component
+
     fun dispose(id: String)
+
     fun disposeAll()
+
     fun find(id: String, text: String, forward: Boolean)
 
     fun clearFind(id: String)
