22b922cc2c2129e3640413673c896442db848389
toastkidjp <10fmi13@gmail.com>
2025-11-14 07:21:55 +0900

    refactor Add license.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/clipboard/TransferableImageTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/clipboard/TransferableImageTest.kt
@@ -1,12 +1,16 @@
+/*
+ * Copyright (c) 2025 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.model.clipboard
 
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.unmockkAll
-import java.awt.Image
-import java.awt.datatransfer.DataFlavor
-import java.awt.datatransfer.UnsupportedFlavorException
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
@@ -15,6 +19,9 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.awt.Image
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.UnsupportedFlavorException
 
 class TransferableImageTest {
 
