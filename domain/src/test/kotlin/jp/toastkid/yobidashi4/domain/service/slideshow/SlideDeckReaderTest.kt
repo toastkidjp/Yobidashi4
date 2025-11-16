e9dc7e72dd17df487059246be0fd8dbb0b187d7c
toastkidjp <10fmi13@gmail.com>
2025-11-17 08:57:42 +0900

    refactor Add test case.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/SlideDeckReaderTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/slideshow/SlideDeckReaderTest.kt
@@ -7,6 +7,7 @@ import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.slideshow.data.TableLine
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -98,6 +99,24 @@ result.value = engine.eval(input.value.text).toString()
         assertTrue(background.isEmpty())
         assertTrue(title.isEmpty())
         assertTrue(footerText.isEmpty())
+    }
+
+    @Test
+    fun lastTableCase() {
+        every { Files.lines(any()) } returns
+                """
+## Table
+
+| Time | ID | Title
+|:---|:---|:---
+| 09:30 -11:30 | D1-KY| Java Day Tokyo 2017 基調講演
+| 13:00 -13:50 | D1-A1| Java 9 and Beyond: Java Renaissance in the Cloud
+| 14:05 -14:55 | D1-A2| Modular Development with JDK 9""".split("\n").stream()
+
+        val (slides, background, title, footerText) = slideDeckReader.invoke()
+
+        assertEquals(1, slides.size)
+        assertTrue(slides.last().lines().last() is TableLine)
     }
 
 }
