b7ece4d0adace711038b8635575ed1215783038f
toastkidjp <10fmi13@gmail.com>
2025-12-22 08:44:19 +0900

    feature Improve table conversion.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/TableFormConverter.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/markdown/text/TableFormConverter.kt
@@ -5,7 +5,23 @@ class TableFormConverter {
     operator fun invoke(text: String): String {
         val putEndLineBreak = if (text.endsWith("\n")) "\n" else ""
 
-        return "| ${text.trim().replace(" ", " | ").replace("\n", "\n| ")}$putEndLineBreak"
+        val lines = text.split("\n")
+        return lines.mapNotNull { line ->
+            val tuple = line.split(" ")
+            val trimmedLine = line.trim()
+            if (trimmedLine.isEmpty()) {
+                return@mapNotNull null
+            }
+
+            if (tuple.size != 2 || !(tuple[0].contains("時") && tuple[0].contains("分"))) {
+                return@mapNotNull "| ${trimmedLine.replace(" ", " | ")}"
+            }
+            val fill = with(StringBuilder()) {
+                repeat(6 - tuple[0].length) { append(" ") }
+                toString()
+            }
+            return@mapNotNull "| ${trimmedLine.replace(" ", "$fill | ")}"
+        }.joinToString("\n") + putEndLineBreak
     }
 
 }
