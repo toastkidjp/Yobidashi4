e4fa78f8896be71a7c36bb57215ccc9d45c47007
toastkidjp <10fmi13@gmail.com>
2025-12-23 09:14:26 +0900

    refactor Upgrade Koin KSP Compiler to 2.3.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
         create("libraries") {
             library("koin", "io.insert-koin", "koin-core").version("4.1.1")
             library("koinAnnotations", "io.insert-koin", "koin-annotations").version("2.3.1")
-            library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("2.1.0")
+            library("koinKspCompiler", "io.insert-koin", "koin-ksp-compiler").version("2.3.1")
             library("kotlinSerialization", "org.jetbrains.kotlinx", "kotlinx-serialization-json").version("1.9.0")
             library("coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm").version("1.10.2")
             library("slf4j", "org.slf4j", "slf4j-api").version("2.0.17")
