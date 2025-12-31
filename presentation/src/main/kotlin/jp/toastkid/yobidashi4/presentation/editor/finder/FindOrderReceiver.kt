abffe79eb72a8f3f1f3592fcb24d6d581fd94823
toastkidjp <10fmi13@gmail.com>
2025-12-31 09:57:02 +0900

    feature Rewrite with TextFieldState.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FindOrderReceiver.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/finder/FindOrderReceiver.kt
@@ -1,12 +1,12 @@
 package jp.toastkid.yobidashi4.presentation.editor.finder
 
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
-import java.util.concurrent.atomic.AtomicInteger
 import jp.toastkid.yobidashi4.domain.model.find.FindOrder
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.util.concurrent.atomic.AtomicInteger
 
 class FindOrderReceiver : KoinComponent {
 
@@ -16,13 +16,19 @@ class FindOrderReceiver : KoinComponent {
 
     private val selected = AtomicInteger(-1)
 
-    operator fun invoke(it: FindOrder, content: TextFieldValue, setNewContent: (TextFieldValue) -> Unit) {
+    operator fun invoke(it: FindOrder, content: TextFieldState) {
         if (it == FindOrder.EMPTY) {
             return
         }
 
         if (it.invokeReplace) {
-            setNewContent(TextFieldValue(content.text.replace(it.target, it.replace, it.caseSensitive.not())))
+            content.edit {
+                val start = it.target.indexOf(it.replace, content.selection.start)
+                if (start == -1) {
+                    return@edit
+                }
+                replace(start, start + it.replace.length, it.replace)
+            }
             return
         }
 
@@ -36,11 +42,15 @@ class FindOrderReceiver : KoinComponent {
         mainViewModel.setFindStatus(finderMessageFactory(it.target, foundCount))
 
         if (selected.get() == -1) {
-            setNewContent(content.copy(selection = TextRange(content.selection.start)))
+            content.edit {
+                selection = TextRange(content.selection.start)
+            }
             return
         }
 
-        setNewContent(content.copy(selection = TextRange(selected.get(), selected.get() + it.target.length)))
+        content.edit {
+            selection = TextRange(selected.get(), selected.get() + it.target.length)
+        }
     }
 
 }
