27b2e0582a6413e03ef9207c09acefec1bce480c
toastkidjp <10fmi13@gmail.com>
2026-01-01 09:10:06 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/SelectedTextConversionTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/keyboard/SelectedTextConversionTest.kt
@@ -1,15 +1,14 @@
 package jp.toastkid.yobidashi4.presentation.editor.keyboard
 
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.foundation.text.input.TextFieldState
 import io.mockk.Called
 import io.mockk.MockKAnnotations
-import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.just
 import io.mockk.unmockkAll
 import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -22,14 +21,10 @@ class SelectedTextConversionTest {
     @MockK
     private lateinit var conversion: (String) -> String?
 
-    @MockK
-    private lateinit var setNewContent: (TextFieldValue) -> Unit
-
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
         every { conversion.invoke(any()) } returns "test"
-        every { setNewContent.invoke(any()) } just Runs
 
         subject = SelectedTextConversion()
     }
@@ -42,48 +37,51 @@ class SelectedTextConversionTest {
     @Test
     fun emptySelection() {
         val invoke = subject.invoke(
-            TextFieldValue(),
+            TextFieldState(),
             0,
             0,
             conversion,
-            setNewContent
         )
 
         assertFalse(invoke)
         verify { conversion wasNot Called }
-        verify { setNewContent wasNot Called }
     }
 
     @Test
     fun conversionFailure() {
         every { conversion.invoke(any()) } returns null
+        val content = TextFieldState("Test is good.")
 
         val invoke = subject.invoke(
-            TextFieldValue("Test is good."),
+            content,
             0,
             4,
             conversion,
-            setNewContent
         )
 
         assertFalse(invoke)
         verify { conversion.invoke(any()) }
-        verify { setNewContent wasNot Called }
+        assertEquals("Test is good.", content.text)
+        assertEquals(13, content.selection.start)
+        assertEquals(13, content.selection.end)
     }
 
     @Test
     fun invoke() {
+        val content = TextFieldState("Test is good.")
+
         val invoke = subject.invoke(
-            TextFieldValue("Test is good."),
+            content,
             0,
             4,
             conversion,
-            setNewContent
         )
 
         assertTrue(invoke)
         verify { conversion.invoke("Test") }
-        verify { setNewContent.invoke(any()) }
+        assertEquals("test is good.", content.text)
+        assertEquals(0, content.selection.start)
+        assertEquals(4, content.selection.end)
     }
 
 }
