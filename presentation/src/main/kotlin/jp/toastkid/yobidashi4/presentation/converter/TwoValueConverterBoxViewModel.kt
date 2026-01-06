69e6a54d0cc611050dd1e3687298716ff6433cb6
toastkidjp <10fmi13@gmail.com>
2026-01-07 07:39:46 +0900

    feature Replace to TextFieldState.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/TwoValueConverterBoxViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/converter/TwoValueConverterBoxViewModel.kt
@@ -1,41 +1,38 @@
 package jp.toastkid.yobidashi4.presentation.converter
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import jp.toastkid.yobidashi4.domain.service.converter.TwoStringConverterService
 
 class TwoValueConverterBoxViewModel(private val unixTimeConverterService: TwoStringConverterService) {
 
-    private val firstInput = mutableStateOf(TextFieldValue(unixTimeConverterService.defaultFirstInputValue()))
+    private val firstInput = (TextFieldState(unixTimeConverterService.defaultFirstInputValue()))
 
-    private val secondInput = mutableStateOf(TextFieldValue(unixTimeConverterService.defaultSecondInputValue()))
+    private val secondInput = (TextFieldState(unixTimeConverterService.defaultSecondInputValue()))
 
-    fun firstInput() = firstInput.value
+    fun firstInput() = firstInput
 
-    fun onFirstValueChange(it: TextFieldValue) {
-        firstInput.value = it
-
-        unixTimeConverterService.firstInputAction(firstInput.value.text)?.let { newValue ->
-            secondInput.value = TextFieldValue(newValue)
+    fun onFirstValueChange() {
+        unixTimeConverterService.firstInputAction(firstInput.text.toString())?.let { newValue ->
+            secondInput.setTextAndPlaceCursorAtEnd(newValue)
         }
     }
 
-    fun secondInput() = secondInput.value
+    fun secondInput() = secondInput
 
-    fun onSecondValueChange(it: TextFieldValue) {
-        secondInput.value = it
-
-        unixTimeConverterService.secondInputAction(secondInput.value.text)?.let { input ->
-            firstInput.value = TextFieldValue(input)
+    fun onSecondValueChange() {
+        unixTimeConverterService.secondInputAction(secondInput.text.toString())?.let { input ->
+            firstInput.setTextAndPlaceCursorAtEnd(input)
         }
     }
 
     fun clearFirstInput() {
-        firstInput.value = TextFieldValue()
+        firstInput.clearText()
     }
 
     fun clearSecondInput() {
-        secondInput.value = TextFieldValue()
+        secondInput.clearText()
     }
 
 }
