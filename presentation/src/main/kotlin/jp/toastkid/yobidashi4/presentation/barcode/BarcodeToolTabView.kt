bdaa399b13801f5548174ce8dd2d81cca435abc9
toastkidjp <10fmi13@gmail.com>
2026-01-06 09:21:20 +0900

    feature Replace to TextFieldState.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabView.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -57,6 +58,14 @@ internal fun BarcodeToolTabView() {
                         modifier = Modifier.clickable(onClick = viewModel::onClickDecodeResult)
                     )
                 }
+            }
+
+            LaunchedEffect(viewModel.encodeInputValue().text) {
+                viewModel.setEncodeInputValue()
+            }
+
+            LaunchedEffect(viewModel.decodeInputValue().text) {
+                viewModel.setDecodeInputValue()
             }
         }
     }
