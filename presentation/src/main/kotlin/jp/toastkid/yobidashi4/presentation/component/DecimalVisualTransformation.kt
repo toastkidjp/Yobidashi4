14649778072b872312c82475a1717f58cee56654
toastkidjp <10fmi13@gmail.com>
2026-01-06 09:21:28 +0900

    feature Replace to TextFieldState.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/DecimalVisualTransformation.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/DecimalVisualTransformation.kt
@@ -1,43 +1,33 @@
 package jp.toastkid.yobidashi4.presentation.component
 
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.OffsetMapping
-import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.foundation.text.input.OutputTransformation
+import androidx.compose.foundation.text.input.TextFieldBuffer
+import androidx.compose.foundation.text.input.insert
 import java.math.BigDecimal
 import java.text.DecimalFormat
 
-class DecimalVisualTransformation : VisualTransformation {
+class DecimalVisualTransformation : OutputTransformation {
 
     private val formatter = DecimalFormat("#,###.##")
 
-    override fun filter(text: AnnotatedString): TransformedText {
-        val decimal = toDecimal(text.text)
-        val useFormatter = decimal != null && decimal != BigDecimal.ZERO && !text.contains(".")
-        val formatted = if (useFormatter) AnnotatedString(formatter.format(decimal)) else text
-        val offsetMapping = if (!useFormatter) OffsetMapping.Identity else object : OffsetMapping {
-
-            override fun originalToTransformed(offset: Int): Int {
-                val totalSeparatorCount = (text.length - 1) / 3
-                val rightSeparatorCount = (text.length - 1 - offset) / 3
-                val leftSeparatorCount = totalSeparatorCount - rightSeparatorCount
-                return offset + leftSeparatorCount
-            }
-
-            override fun transformedToOriginal(offset: Int): Int {
-                val totalSeparatorCount = (text.length - 1) / 3
-                val rightSeparatorCount = (formatted.length - offset) / 4
-                val leftSeparatorCount = totalSeparatorCount - rightSeparatorCount
-                return offset - leftSeparatorCount
-            }
-
-        }
-
-        return TransformedText(formatted, offsetMapping = offsetMapping)
-    }
-
     private fun toDecimal(input: String): BigDecimal? {
         return input.toBigDecimalOrNull();
+    }
+
+    override fun TextFieldBuffer.transformOutput() {
+        val input = asCharSequence().toString()
+        val decimal = toDecimal(input)
+        val useFormatter = decimal != null && decimal != BigDecimal.ZERO && !input.contains(".")
+        if (useFormatter.not() || decimal.intValueExact() < 999) {
+            return
+        }
+
+        val formatted = formatter.format(decimal)
+        var index = formatted.indexOf(",")
+        while (index != -1) {
+            insert(index, ",")
+            index = formatted.indexOf(",", index + 1)
+        }
     }
 
 }
