5e210f0e3df2317212676a9a38affbc36de87920
toastkidjp <10fmi13@gmail.com>
2026-01-13 08:25:36 +0900

    feature Implement japanese zodiac calculator.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/calendar/zodiac/JapaneseZodiac.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.domain.model.calendar.zodiac
+
+class JapaneseZodiac {
+
+    private val jikkan = listOf("甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸")
+
+    private val junishi = listOf("子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥")
+
+    fun calculate(year: Int): String {
+        val jikkanIndex = (year - 4) % 10
+        val junishiIndex = (year - 4) % 12
+
+        val finalJikkanIndex = if (jikkanIndex < 0) jikkanIndex + 10 else jikkanIndex
+        val finalJunishiIndex = if (junishiIndex < 0) junishiIndex + 12 else junishiIndex
+
+        return jikkan[finalJikkanIndex] + junishi[finalJunishiIndex]
+    }
+
+}
