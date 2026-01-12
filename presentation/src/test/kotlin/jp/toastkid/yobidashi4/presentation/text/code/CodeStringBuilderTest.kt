d6a1114ecc7e64b9cca15aaf6fba257a8e38bc2d
toastkidjp <10fmi13@gmail.com>
2026-01-12 12:49:00 +0900

    refactor Modify test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/text/code/CodeStringBuilderTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/text/code/CodeStringBuilderTest.kt
@@ -1,8 +1,13 @@
 package jp.toastkid.yobidashi4.presentation.text.code
 
+import androidx.compose.foundation.text.input.TextFieldBuffer
+import androidx.compose.ui.text.SpanStyle
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -31,11 +36,14 @@ public class Code {
 }
             """
 
-        val annotatedString = subject.invoke(code)
+        val textBuffer = mockk<TextFieldBuffer>()
+        every { textBuffer.addStyle(any<SpanStyle>(), any(), any()) } just Runs
+        every { textBuffer.asCharSequence() } returns code
+        with(subject) {
+            textBuffer.transformOutput()
+        }
 
-        assertEquals(code, annotatedString.text)
-        assertEquals(163, annotatedString.spanStyles.size)
-        assertTrue(annotatedString.paragraphStyles.isEmpty())
+        verify { textBuffer.addStyle(any<SpanStyle>(), any(), any()) }
     }
 
 }
