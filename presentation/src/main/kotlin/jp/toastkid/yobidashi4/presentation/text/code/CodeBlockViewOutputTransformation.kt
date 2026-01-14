18395b2ac25454dc5e76ab7a8be5a74a2fea4284
toastkidjp <10fmi13@gmail.com>
2026-01-14 09:28:06 +0900

    refactor Delete useless fields.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/text/code/CodeBlockViewOutputTransformation.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/text/code/CodeBlockViewOutputTransformation.kt
@@ -4,28 +4,15 @@ import androidx.compose.foundation.text.input.OutputTransformation
 import androidx.compose.foundation.text.input.TextFieldBuffer
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.font.FontWeight
 import java.util.regex.Pattern
 
 class CodeBlockViewOutputTransformation : OutputTransformation {
-
-    private val simple: SpanStyle = SpanStyle()
-
-    private val header: SpanStyle = SpanStyle(Color(0xFF00DD00), fontWeight = FontWeight.Bold)
-
-    private val table: SpanStyle = SpanStyle(Color(0xFF68BB97))
-
-    private val quote: SpanStyle = SpanStyle(Color(0xFFCCAAFF))
 
     private val value: SpanStyle = SpanStyle(Color(0xFF6897BB))
 
     private val keyword: SpanStyle = SpanStyle(Color(0xFFCC7832))
 
     private val punctuation: SpanStyle = SpanStyle(Color(0xFFA1C17E))
-
-    private val annotation: SpanStyle = SpanStyle(Color(0xFFBBB529))
-
-    private val comment: SpanStyle = SpanStyle(Color(0xFF808080))
 
     private val punctuationPattern = Pattern.compile("[:=\"\\[\\]\\{\\}\\(\\),]")
 
