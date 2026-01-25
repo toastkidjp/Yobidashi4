82663683411cf4aebcacc8883c736eb6709e46b4
toastkidjp <10fmi13@gmail.com>
2026-01-25 12:33:58 +0900

    refactor Adapt new interface.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/EditorTabView.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -42,7 +42,7 @@ internal fun EditorTabView(tab: EditorTab) {
                 AnimatedVisibility(visible = viewModel.showPreview(), Modifier.weight(1f)) {
                     MarkdownPreview(
                         viewModel.preview(),
-                        rememberScrollState(),
+                        rememberLazyListState(),
                         Modifier
                     )
                 }
