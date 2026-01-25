4bca9a5afca567dc8f8a023e659e2a17ff7cabf6
toastkidjp <10fmi13@gmail.com>
2026-01-25 11:52:05 +0900

    feature Add field for subheadings.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/markdown/Markdown.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/markdown/Markdown.kt
@@ -4,13 +4,18 @@ import jp.toastkid.yobidashi4.domain.model.slideshow.data.Line
 
 data class Markdown(
     private val title: String,
-    private val lines: MutableList<Line> = mutableListOf()
+    private val lines: MutableList<Line> = mutableListOf(),
+    private val subheadings: MutableList<TextBlock> = mutableListOf(),
 ) {
 
     fun title() = title
 
     fun add(line: Line) {
         lines.add(line)
+
+        if (line is TextBlock && line.level != -1) {
+            this.subheadings.add(line)
+        }
     }
 
     fun addAll(lines: List<Line>) {
@@ -18,5 +23,7 @@ data class Markdown(
     }
 
     fun lines(): List<Line> = lines
+
+    fun subheadings() = subheadings
 
 }
