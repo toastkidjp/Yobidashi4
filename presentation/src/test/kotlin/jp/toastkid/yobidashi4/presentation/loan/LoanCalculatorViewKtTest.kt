8e4fc8a6b39d5985a88c7244bc19a6bc84720435
toastkidjp <10fmi13@gmail.com>
2026-01-28 00:02:57 +0900

    refactor Fix incorrect test code.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/loan/LoanCalculatorViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/loan/LoanCalculatorViewKtTest.kt
@@ -1,9 +1,9 @@
 package jp.toastkid.yobidashi4.presentation.loan
 
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runDesktopComposeUiTest
-import androidx.compose.ui.text.input.TextFieldValue
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
@@ -24,12 +24,12 @@ class LoanCalculatorViewKtTest {
             PaymentDetail(1.0, 0.99, 2000),
             PaymentDetail(1.2, 1.0, 2000),
         )
-        every { anyConstructed<LoanCalculatorViewModel>().loanAmount() } returns TextFieldValue("0")
-        every { anyConstructed<LoanCalculatorViewModel>().loanTerm() } returns TextFieldValue("0")
-        every { anyConstructed<LoanCalculatorViewModel>().interestRate() } returns TextFieldValue("0")
-        every { anyConstructed<LoanCalculatorViewModel>().downPayment() } returns TextFieldValue("0")
-        every { anyConstructed<LoanCalculatorViewModel>().managementFee() } returns TextFieldValue("0")
-        every { anyConstructed<LoanCalculatorViewModel>().renovationReserves() } returns TextFieldValue("0")
+        every { anyConstructed<LoanCalculatorViewModel>().loanAmount() } returns TextFieldState("0")
+        every { anyConstructed<LoanCalculatorViewModel>().loanTerm() } returns TextFieldState("0")
+        every { anyConstructed<LoanCalculatorViewModel>().interestRate() } returns TextFieldState("0")
+        every { anyConstructed<LoanCalculatorViewModel>().downPayment() } returns TextFieldState("0")
+        every { anyConstructed<LoanCalculatorViewModel>().managementFee() } returns TextFieldState("0")
+        every { anyConstructed<LoanCalculatorViewModel>().renovationReserves() } returns TextFieldState("0")
         every { anyConstructed<LoanCalculatorViewModel>().roundToIntSafely(any()) } returns "0"
         every { anyConstructed<LoanCalculatorViewModel>().launch() } just Runs
         every { anyConstructed<LoanCalculatorViewModel>().listState() } returns LazyListState(0)
