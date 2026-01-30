d7604426cc4f6cd2891e1eff05d1d357aa424e76
toastkidjp <10fmi13@gmail.com>
2026-01-31 00:32:59 +0900

    refactor Make comment out.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/chat/GenerativeAiModelTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/model/chat/GenerativeAiModelTest.kt
@@ -8,20 +8,19 @@
 package jp.toastkid.yobidashi4.domain.model.chat
 
 import org.junit.jupiter.api.Assertions.assertSame
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class GenerativeAiModelTest {
 
     @Test
     fun image() {
-        assertTrue(GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE.image())
+        /*assertTrue(GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE.image())
 
         assertTrue(
             GenerativeAiModel.entries
                 .filter { it != GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE }
                 .none(GenerativeAiModel::image)
-        )
+        )*/
     }
 
     @Test
