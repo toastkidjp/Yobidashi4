5e895d6e50a055414575eddd794ad4f02a10b874
toastkidjp <10fmi13@gmail.com>
2026-01-31 00:45:52 +0900

    refactor Fix test code.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/ChatModelIconMapperTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/ChatModelIconMapperTest.kt
@@ -1,7 +1,7 @@
 package jp.toastkid.yobidashi4.presentation.chat
 
 import jp.toastkid.yobidashi4.domain.model.chat.GenerativeAiModel
-import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -16,9 +16,9 @@ class ChatModelIconMapperTest {
 
     @Test
     fun invoke() {
-        assertNotEquals(
-            subject.invoke(GenerativeAiModel.GEMINI_2_0_FLASH),
-            subject.invoke(GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE)
+        assertEquals(
+            subject.invoke(GenerativeAiModel.GEMINI_2_5_FLASH),
+            subject.invoke(GenerativeAiModel.GEMINI_2_5_FLASH_LITE),
         )
     }
 
