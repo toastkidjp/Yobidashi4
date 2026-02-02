c5338bc5c0bd5fe819d9f7547ddca6e7c13e9593
toastkidjp <10fmi13@gmail.com>
2026-02-03 07:02:59 +0900

    feature Implement sales tax aggregator.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/SalesTaxAggregator.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.domain.service.aggregation
+
+import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
+import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
+
+class SalesTaxAggregator(articlesReaderService: ArticlesReaderService) : ArticleAggregator {
+
+    private val behavior = OutgoCalculationBehavior(articlesReaderService) { it.contains(TARGET_LINE_LABEL).not() }
+
+    override operator fun invoke(keyword: String): AggregationResult {
+        return behavior.invoke(keyword)
+    }
+
+    override fun label() = "Sales tax"
+
+}
+
+private const val TARGET_LINE_LABEL = "消費税"
