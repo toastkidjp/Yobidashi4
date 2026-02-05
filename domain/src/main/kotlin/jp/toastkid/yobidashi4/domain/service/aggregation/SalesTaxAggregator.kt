ca6def0db7484b818b09d9a6e08b6c0a2b36cbcb
toastkidjp <10fmi13@gmail.com>
2026-02-05 09:12:31 +0900

    refactor Wrap too long line.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/SalesTaxAggregator.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/SalesTaxAggregator.kt
@@ -12,7 +12,9 @@ import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
 
 class SalesTaxAggregator(articlesReaderService: ArticlesReaderService) : ArticleAggregator {
 
-    private val behavior = OutgoCalculationBehavior(articlesReaderService) { it.contains(TARGET_LINE_LABEL).not() }
+    private val behavior = OutgoCalculationBehavior(articlesReaderService) {
+        it.contains(TARGET_LINE_LABEL).not()
+    }
 
     override operator fun invoke(keyword: String): AggregationResult {
         return behavior.invoke(keyword)
