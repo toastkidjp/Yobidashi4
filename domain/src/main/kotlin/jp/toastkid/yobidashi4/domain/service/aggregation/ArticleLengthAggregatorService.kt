5049a8cb904dd1280041a3f41551c2598d78e20a
toastkidjp <10fmi13@gmail.com>
2026-02-10 07:27:32 +0900

    refactor Add license.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/ArticleLengthAggregatorService.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/aggregation/ArticleLengthAggregatorService.kt
@@ -1,9 +1,16 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.service.aggregation
 
-import java.nio.file.Files
 import jp.toastkid.yobidashi4.domain.model.aggregation.AggregationResult
 import jp.toastkid.yobidashi4.domain.model.aggregation.ArticleLengthAggregationResult
 import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
+import java.nio.file.Files
 import kotlin.io.path.nameWithoutExtension
 
 class ArticleLengthAggregatorService(
