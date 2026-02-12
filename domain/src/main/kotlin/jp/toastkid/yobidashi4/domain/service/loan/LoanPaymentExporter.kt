d755a3cfd7cdec1b4dc677926ee261890e0b22c0
toastkidjp <10fmi13@gmail.com>
2026-02-13 07:40:14 +0900

    refactor Add license.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/loan/LoanPaymentExporter.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/loan/LoanPaymentExporter.kt
@@ -1,9 +1,16 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.service.loan
 
-import java.nio.file.Files
-import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.model.loan.Factor
 import jp.toastkid.yobidashi4.domain.model.loan.LoanPayment
+import java.nio.file.Files
+import java.nio.file.Path
 
 class LoanPaymentExporter {
 
