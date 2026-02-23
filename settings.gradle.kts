9b98a88fa3cb52f1995681f3a9524839bfaf0a58
toastkidjp <10fmi13@gmail.com>
2026-02-24 07:19:18 +0900

    feature Upgrade Compose to 1.10.1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
             library("zxing", "com.google.zxing", "core").version("3.5.4")
             library("jcef", "me.friwi", "jcefmaven").version("143.0.14")
             library("jsoup", "org.jsoup", "jsoup").version("1.21.2")
-            version("compose", "1.10.0")
+            version("compose", "1.10.1")
             version("kover", "0.9.7")
             version("kotlin", "2.2.10")
             version("ksp", "2.2.10-2.0.2")
