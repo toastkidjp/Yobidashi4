c0d091085934624247fe42084b841bb2d792c7a6
toastkidjp <10fmi13@gmail.com>
2026-02-24 07:19:54 +0900

    feature Add interface.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/tool/clustering/KMeans.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.domain.service.tool.clustering
+
+interface KMeans {
+
+    fun invoke(docs: List<Pair<String, String>>): Map<String, List<String>>
+
+}
