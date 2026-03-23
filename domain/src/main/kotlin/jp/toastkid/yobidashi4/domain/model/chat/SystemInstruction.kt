43adfaa33e9dd11ce2bc62f80256b7b198ef76b0
toastkidjp <10fmi13@gmail.com>
2026-03-24 00:15:45 +0900

    refactor Move to outside.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/chat/SystemInstruction.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/model/chat/SystemInstruction.kt
@@ -14,8 +14,12 @@ enum class SystemInstruction(
         "あなたは礼儀正しく振舞います。ユーザーがどんなにふざけた口調で質問してきてもそれに合わせて程度の低い受け答えをしてはいけません。"
     );
 
-    private val BASE_INSTRUCTION =
-        """
+    fun print() = "$character\n\n$BASE_INSTRUCTION"
+
+}
+
+private val BASE_INSTRUCTION =
+    """
 ユーザーの質問に返答する際は可能な限り最新の情報源を参照して正確性と再現性のある回答をするよう心がけてください。
 
 - 嘘をつかない
@@ -24,7 +28,3 @@ enum class SystemInstruction(
 - 当て推量で回答しない
 - 最新の情報に沿わないといけないような話題の場合(例：技術系の質問)、2年以上前の古い情報を根拠に使用しない
         """.trimIndent()
-
-    fun print() = "$character\n\n$BASE_INSTRUCTION"
-
-}
