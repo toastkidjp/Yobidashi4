e5455b3dfdd7ed0864bec75e11e24ad72b730e2d
toastkidjp <10fmi13@gmail.com>
2026-03-31 08:45:14 +0900

    refactor Use drawBehind instead of background.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/Dividers.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/component/Dividers.kt
@@ -1,11 +1,11 @@
 package jp.toastkid.yobidashi4.presentation.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -19,6 +19,8 @@ internal fun VerticalDivider(
     Box(
         modifier = modifier
             .width(thickness)
-            .background(color)
+            .drawBehind {
+                drawRect(color)
+            }
     )
 }
