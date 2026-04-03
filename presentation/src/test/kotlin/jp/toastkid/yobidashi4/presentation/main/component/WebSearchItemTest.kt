d75b6d21ce9149c884c0393b9168173f02ccea20
toastkidjp <10fmi13@gmail.com>
2026-04-04 00:30:01 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/WebSearchItemTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/component/WebSearchItemTest.kt
@@ -12,12 +12,28 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.chat.GenerativeAiModel
 import jp.toastkid.yobidashi4.domain.model.web.search.SearchSite
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class WebSearchItemTest {
+
+    @Test
+    fun from() {
+        val viewModel = mockk<MainViewModel>()
+        every { viewModel.openUrl(any(), any()) } just Runs
+        every { viewModel.askGenerativeAi(any(), any()) } just Runs
+        every { viewModel.currentTab() } returns null
+
+        val from = WebSearchItem.from(GenerativeAiModel.GEMINI_3_0_FLASH)
+
+        assertEquals(GenerativeAiModel.GEMINI_3_0_FLASH.label(), from.label)
+        from.action(viewModel, "test")
+        verify(inverse = true) { viewModel.openUrl(any(), any()) }
+        verify { viewModel.askGenerativeAi(any(), any()) }
+    }
 
     @Test
     fun fromSearchSite() {
