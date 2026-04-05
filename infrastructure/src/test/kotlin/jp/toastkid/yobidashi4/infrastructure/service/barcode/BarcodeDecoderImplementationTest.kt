06288438605c0a5a93d3e825899ffc22ae767565
toastkidjp <10fmi13@gmail.com>
2026-04-05 10:13:55 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/barcode/BarcodeDecoderImplementationTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/barcode/BarcodeDecoderImplementationTest.kt
@@ -1,10 +1,13 @@
 package jp.toastkid.yobidashi4.infrastructure.service.barcode
 
+import com.google.zxing.NotFoundException
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.fail
+import java.awt.image.BufferedImage
 
 class BarcodeDecoderImplementationTest {
 
@@ -25,6 +28,16 @@ class BarcodeDecoderImplementationTest {
             ?: fail("Failed generate barcode.")
 
         assertEquals("https;//www.yahoo.co.jp", barcodeDecoder.invoke(barcodeImage))
+    }
+
+    @Test
+    fun testTransparentPixelEntersIfBlock() {
+        val testImage = BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)
+
+        val transparentColor = 0x00FF0000
+        testImage.setRGB(0, 0, transparentColor)
+
+        assertThrows<NotFoundException> { barcodeDecoder.invoke(testImage) }
     }
 
 }
