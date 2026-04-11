26e892d8e9463b46ecbe1f706592a12620b67366
toastkidjp <10fmi13@gmail.com>
2026-04-11 09:56:59 +0900

    refactor Extract to other function.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/clustering/KMeansImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/clustering/KMeansImplementation.kt
@@ -11,6 +11,7 @@ import jp.toastkid.yobidashi4.domain.service.tool.clustering.KMeans
 import org.koin.core.annotation.Single
 import org.tribuo.Feature
 import org.tribuo.MutableDataset
+import org.tribuo.clustering.ClusterID
 import org.tribuo.clustering.ClusteringFactory
 import org.tribuo.clustering.kmeans.KMeansTrainer
 import org.tribuo.impl.ArrayExample
@@ -26,40 +27,7 @@ class KMeansImplementation : KMeans {
     private val stopWords = setOf("食べ", "を食", "室温")
 
     override fun invoke(docs: List<Pair<String, String>>): Map<String, List<String>> {
-        val factory = ClusteringFactory()
-
-        val provenance = SimpleDataSourceProvenance("empty-dataset", OffsetDateTime.now(), factory)
-
-        val dataset = MutableDataset(provenance, factory)
-
-        val dfMap = docs
-            .flatMap { text -> makeBiGram(text).distinct() }
-            .groupingBy { it }
-            .eachCount()
-
-        docs.forEach { text ->
-            // ここで一旦、このドキュメント内の 2-gram の頻度を計算する
-            val counts = makeBiGram(text)
-                .groupingBy { it }
-                .eachCount()
-                .filter { it.value > 2 }
-
-            val example = ArrayExample(factory.unknownOutput)
-
-            // 集計済みの頻度を重み(Value)として add する
-            counts.forEach { (word, tf) ->
-                val df = dfMap[word] ?: 1
-                // 標準的な TF-IDF (スムージング版)
-                val tfIdf = tf * ln(docs.size / df.toDouble() + 1.0)
-
-                if (tfIdf > 0.0) {
-                    example.add(word, tfIdf)
-                }
-            }
-            if (example.any()) {
-                dataset.add(example)
-            }
-        }
+        val dataset = prepareDataSet(docs)
 
         val k = (sqrt(docs.size.toDouble() / 2.0).toInt()).coerceIn(1, docs.size / 3)
         val trainer = KMeansTrainer(k, 10, CosineDistance(), 1, 42L)
@@ -102,6 +70,44 @@ class KMeansImplementation : KMeans {
          */
 
         return groupBy
+    }
+
+    private fun prepareDataSet(docs: List<Pair<String, String>>): MutableDataset<ClusterID?> {
+        val factory = ClusteringFactory()
+
+        val provenance = SimpleDataSourceProvenance("empty-dataset", OffsetDateTime.now(), factory)
+
+        val dataset = MutableDataset(provenance, factory)
+
+        val dfMap = docs
+            .flatMap { text -> makeBiGram(text).distinct() }
+            .groupingBy { it }
+            .eachCount()
+
+        docs.forEach { text ->
+            // ここで一旦、このドキュメント内の 2-gram の頻度を計算する
+            val counts = makeBiGram(text)
+                .groupingBy { it }
+                .eachCount()
+                .filter { it.value > 2 }
+
+            val example = ArrayExample(factory.unknownOutput)
+
+            // 集計済みの頻度を重み(Value)として add する
+            counts.forEach { (word, tf) ->
+                val df = dfMap[word] ?: 1
+                // 標準的な TF-IDF (スムージング版)
+                val tfIdf = tf * ln(docs.size / df.toDouble() + 1.0)
+
+                if (tfIdf > 0.0) {
+                    example.add(word, tfIdf)
+                }
+            }
+            if (example.any()) {
+                dataset.add(example)
+            }
+        }
+        return dataset
     }
 
     /**
