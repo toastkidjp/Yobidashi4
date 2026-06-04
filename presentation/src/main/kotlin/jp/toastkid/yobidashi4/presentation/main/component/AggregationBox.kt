1144c8931889972b1a634c3c160b08af26c72b6d
toastkidjp <10fmi13@gmail.com>
2026-06-05 06:16:20 +0900

    feature Free focus on dispose.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/component/AggregationBox.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/component/AggregationBox.kt
@@ -22,12 +22,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -166,5 +168,12 @@ internal fun AggregationBox() {
 
     LaunchedEffect(viewModel.dateInput().text) {
         viewModel.onDateInputValueChange()
+    }
+
+    val focusManager = LocalFocusManager.current
+    DisposableEffect(Unit) {
+        onDispose {
+            focusManager.clearFocus(true)
+        }
     }
 }
