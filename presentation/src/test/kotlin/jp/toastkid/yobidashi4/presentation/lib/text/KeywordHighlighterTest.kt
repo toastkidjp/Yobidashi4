a16d3443463f4898164ad4cd5b4d0e74767d3f85
toastkidjp <10fmi13@gmail.com>
2026-06-10 00:31:37 +0900

    refactor Modify fixture.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/text/KeywordHighlighterTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/lib/text/KeywordHighlighterTest.kt
@@ -8,18 +8,51 @@
 package jp.toastkid.yobidashi4.presentation.lib.text
 
 import androidx.compose.ui.text.style.TextDecoration.Companion.Underline
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import io.mockk.unmockkAll
+import jp.toastkid.yobidashi4.domain.model.article.ArticleFactory
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
 class KeywordHighlighterTest {
 
     private lateinit var subject: KeywordHighlighter
 
+    @MockK
+    private lateinit var viewModel: MainViewModel
+
+    @MockK
+    private lateinit var articleFactory: ArticleFactory
+
     @BeforeEach
     fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { viewModel } bind(MainViewModel::class)
+                    single(qualifier=null) { articleFactory } bind(ArticleFactory::class)
+                }
+            )
+        }
+
         subject = KeywordHighlighter()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
     }
 
     @Test
