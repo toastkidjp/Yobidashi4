fef51d4e1ec5b189c3f2ff761b39e1f31f3c7efa
toastkidjp <10fmi13@gmail.com>
2026-06-13 10:37:48 +0900

    refactor Add new data class.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/data/LineNumber.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.presentation.editor.data
+
+data class LineNumber(
+    val number: Int,
+    val text: String
+) {
+}
