03a2b75c0abc032df82649295be72de34b5e6503
toastkidjp <10fmi13@gmail.com>
2026-06-21 10:02:32 +0900

    refactor Add dependencies.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/di/DependencyInjectionContainer.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/di/DependencyInjectionContainer.kt
@@ -1,9 +1,11 @@
 package jp.toastkid.yobidashi4.infrastructure.di
 
+import okio.FileSystem
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.dsl.module
 import org.koin.ksp.generated.module
 
 @Module
@@ -15,6 +17,11 @@ class DependencyInjectionContainer {
         fun start() {
             startKoin {
                 modules(DependencyInjectionContainer().module)
+                modules(
+                    module {
+                        single<FileSystem> { FileSystem.SYSTEM }
+                    }
+                )
             }
         }
 
