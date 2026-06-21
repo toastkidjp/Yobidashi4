a9fef19c2e9cbd2a44a595816f8c79a453de8d16
toastkidjp <10fmi13@gmail.com>
2026-06-22 00:00:33 +0900

    refactor Add new implementation.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/model/file/ArticleFilesFinderImplementation.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.infrastructure.model.file
+
+import jp.toastkid.yobidashi4.domain.model.file.ArticleFilesFinder
+import okio.FileSystem
+import okio.Path.Companion.toOkioPath
+import org.koin.core.annotation.Single
+import java.nio.file.Path
+
+@Single
+class ArticleFilesFinderImplementation(
+    private val fileSystem: FileSystem
+) : ArticleFilesFinder {
+
+    override operator fun invoke(path: Path): MutableList<Path> {
+        return fileSystem.list(path.toOkioPath())
+            .asSequence()
+            .map { it to (fileSystem.metadata(it).lastModifiedAtMillis ?: -1) }
+            .sortedByDescending { it.second }
+            .filter(::containsSpecificCharacters)
+            .map { it.first.toNioPath() }
+            .toMutableList()
+    }
+
+    private fun containsSpecificCharacters(pair: Pair<okio.Path, Long>): Boolean {
+        val name = pair.first.name
+        return name.startsWith("20") || name.startsWith("『")
+    }
+
+}
