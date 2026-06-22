5ce9782fee4fd22e2d66bbb32340dbcafb133f8f
toastkidjp <10fmi13@gmail.com>
2026-06-23 00:11:54 +0900

    refactor Use Okio.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/repository/bookmark/BookmarkFileStore.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/repository/bookmark/BookmarkFileStore.kt
@@ -3,23 +3,28 @@ package jp.toastkid.yobidashi4.infrastructure.repository.bookmark
 import jp.toastkid.yobidashi4.domain.model.web.bookmark.Bookmark
 import jp.toastkid.yobidashi4.domain.model.web.bookmark.WebBookmarkPath
 import jp.toastkid.yobidashi4.domain.repository.BookmarkRepository
+import okio.FileSystem
+import okio.Path.Companion.toOkioPath
+import okio.buffer
 import org.koin.core.annotation.Single
-import java.nio.file.Files
 
 @Single
-class BookmarkFileStore : BookmarkRepository {
+class BookmarkFileStore(private val fileSystem: FileSystem) : BookmarkRepository {
 
     override fun list(): List<Bookmark> {
-        if (Files.exists(path).not()) {
+        if (fileSystem.exists(path.toOkioPath()).not()) {
             return emptyList()
         }
 
-        return Files.readAllLines(path).map {
-            val split = it.split("\t")
-            Bookmark(
-                title = split[0],
-                url = split[1]
-            )
+        return fileSystem.source(path.toOkioPath()).buffer().use {
+            val lines = it.readUtf8().split("\n")
+            lines.map {
+                val split = it.split("\t")
+                Bookmark(
+                    title = split[0],
+                    url = split[1]
+                )
+            }
         }
     }
 
@@ -28,11 +33,19 @@ class BookmarkFileStore : BookmarkRepository {
     }
 
     private fun save(list: List<Bookmark>) {
-        if (Files.exists(path.parent).not()) {
-            Files.createDirectories(path.parent)
+        if (fileSystem.exists(path.parent.toOkioPath()).not()) {
+            fileSystem.createDirectories(path.parent.toOkioPath())
         }
 
-        Files.write(path, list.map(Bookmark::toTsv))
+        fileSystem.sink(path.toOkioPath())
+            .buffer()
+            .use {
+                it.writeUtf8(
+                    list.map(Bookmark::toTsv).joinToString(System.lineSeparator())
+                )
+                    .emit()
+            }
+
     }
 
     override fun delete(item: Bookmark) {
