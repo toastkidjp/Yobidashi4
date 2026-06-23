23f41bd0e4dae1c3cd03b5d61cc81ceb8d265d12
toastkidjp <10fmi13@gmail.com>
2026-06-24 00:13:01 +0900

    refactor Add license.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/media/MediaPlayerInvokerImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/media/MediaPlayerInvokerImplementation.kt
@@ -1,14 +1,21 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.infrastructure.service.media
 
-import java.io.IOException
-import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.service.media.MediaPlayerInvoker
-import kotlin.io.path.absolutePathString
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.nio.file.Path
+import kotlin.io.path.absolutePathString
 
 @Single
 class MediaPlayerInvokerImplementation : KoinComponent, MediaPlayerInvoker {
