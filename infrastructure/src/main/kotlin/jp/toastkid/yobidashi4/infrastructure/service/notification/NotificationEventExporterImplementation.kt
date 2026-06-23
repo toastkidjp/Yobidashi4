677750b5e2ec55ff3c6fe940fbde2877792b6d47
toastkidjp <10fmi13@gmail.com>
2026-06-24 00:03:19 +0900

    refactor Implement interface.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/notification/NotificationEventExporterImplementation.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.infrastructure.service.notification
+
+import jp.toastkid.yobidashi4.domain.repository.notification.NotificationEventRepository
+import jp.toastkid.yobidashi4.domain.service.notification.NotificationEventExporter
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import org.koin.core.annotation.Single
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Single
+class NotificationEventExporterImplementation(
+    private val fileSystem: FileSystem
+) : NotificationEventExporter, KoinComponent {
+
+    private val currentTimeSuffixDateTimeFormatter = DateTimeFormatter.ofPattern("_yyyyMMdd_HHmmss")
+
+    private val repo: NotificationEventRepository by inject()
+
+    override fun invoke() {
+        val path = "notification${
+            makeCurrentTimeSuffix()
+        }.tsv".toPath()
+        fileSystem.write(path) {
+            writeUtf8(repo.readAll().map { it.toTsv() }.joinToString(System.lineSeparator()))
+        }
+    }
+
+    private fun makeCurrentTimeSuffix(): String =
+        LocalDateTime.now().format(currentTimeSuffixDateTimeFormatter)
+
+}
