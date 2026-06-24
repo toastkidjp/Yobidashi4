39f676d6b618b07cf0c0cd75da72bff45703c4eb
toastkidjp <10fmi13@gmail.com>
2026-06-25 06:44:23 +0900

    refactor Use Okio.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/TodayArticleGeneratorImplementation.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/TodayArticleGeneratorImplementation.kt
@@ -6,13 +6,16 @@ import jp.toastkid.yobidashi4.domain.service.article.ArticleTemplate
 import jp.toastkid.yobidashi4.domain.service.article.ArticleTitleGenerator
 import jp.toastkid.yobidashi4.domain.service.article.OffDayFinderService
 import jp.toastkid.yobidashi4.domain.service.article.TodayArticleGenerator
+import okio.FileSystem
+import okio.Path.Companion.toOkioPath
 import org.koin.core.annotation.Single
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import java.nio.file.Files
 
 @Single
-class TodayArticleGeneratorImplementation : KoinComponent, TodayArticleGenerator {
+class TodayArticleGeneratorImplementation(
+    private val fileSystem: FileSystem = FileSystem.SYSTEM
+) : KoinComponent, TodayArticleGenerator {
 
     private val setting: Setting by inject()
 
@@ -21,13 +24,13 @@ class TodayArticleGeneratorImplementation : KoinComponent, TodayArticleGenerator
     private val offDayFinderService: OffDayFinderService by inject()
 
     override operator fun invoke() {
-        if (Files.exists(setting.articleFolderPath()).not()) {
+        if (fileSystem.exists(setting.articleFolderPath().toOkioPath()).not()) {
             return
         }
 
         val title = ArticleTitleGenerator().invoke() ?: return
         val path = setting.articleFolderPath().resolve("${title}.md")
-        if (Files.exists(path)) {
+        if (fileSystem.exists(path.toOkioPath())) {
             return
         }
 
