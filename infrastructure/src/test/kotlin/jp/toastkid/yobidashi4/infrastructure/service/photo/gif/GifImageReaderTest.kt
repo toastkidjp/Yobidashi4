04fdba67a9ad0f095741d478b9fb07a81426f2c4
toastkidjp <10fmi13@gmail.com>
2026-06-25 06:44:58 +0900

    refactor Fix incorrect test code.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/photo/gif/GifImageReaderTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/service/photo/gif/GifImageReaderTest.kt
@@ -7,12 +7,15 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.just
 import io.mockk.unmockkAll
 import io.mockk.verify
-import java.awt.image.BufferedImage
-import java.nio.file.Path
-import kotlin.io.path.toPath
+import okio.Path.Companion.toPath
+import okio.buffer
+import okio.fakefilesystem.FakeFileSystem
+import okio.source
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.awt.image.BufferedImage
+import java.nio.file.Path
 
 class GifImageReaderTest {
 
@@ -27,10 +30,19 @@ class GifImageReaderTest {
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
-        every { path.toFile() } returns javaClass.classLoader.getResource("photo/gif/empty.gif").toURI().toPath().toFile()
+        val fakeFileSystem = FakeFileSystem()
+        val fakePath = "test.gif".toPath()
+        val byteArray = javaClass.classLoader.getResource("photo/gif/empty.gif").openStream().source().buffer().use { stream ->
+            val source = stream.readByteArray()
+            source
+        }
+        fakeFileSystem.write(fakePath) {
+            write(byteArray)
+        }
+        path = fakePath.toNioPath()
         every { consumer.invoke(any(), any()) } just Runs
 
-        subject = GifImageReader()
+        subject = GifImageReader(fakeFileSystem)
     }
 
     @AfterEach
@@ -42,7 +54,7 @@ class GifImageReaderTest {
     fun invoke() {
         subject.invoke(path, consumer)
 
-        verify { path.toFile() }
+        verify { consumer.invoke(any(), any()) }
     }
 
 }
