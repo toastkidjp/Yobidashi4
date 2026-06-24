139f4796ce53703858562f3de93b25f3b789555d
toastkidjp <10fmi13@gmail.com>
2026-06-25 06:44:46 +0900

    refactor Use Okio.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/photo/gif/GifImageReader.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/photo/gif/GifImageReader.kt
@@ -1,16 +1,19 @@
 package jp.toastkid.yobidashi4.infrastructure.service.photo.gif
 
+import okio.FileSystem
+import okio.Path.Companion.toOkioPath
+import okio.buffer
 import java.awt.image.BufferedImage
 import java.nio.file.Path
 import javax.imageio.ImageIO
 import javax.imageio.ImageReader
 
-class GifImageReader {
+class GifImageReader(private val fileSystem: FileSystem = FileSystem.SYSTEM) {
 
     operator fun invoke(path: Path, consumer: (BufferedImage, Int) -> Unit) {
         val it = ImageIO.getImageReadersByFormatName("gif")
         val imageReader: ImageReader = it.next()
-        imageReader.input = ImageIO.createImageInputStream(path.toFile())
+        imageReader.input = ImageIO.createImageInputStream(fileSystem.source(path.toOkioPath()).buffer().inputStream())
         val count: Int = imageReader.getNumImages(true)
         for (i in 0 until count) {
             consumer(imageReader.read(i), i)
