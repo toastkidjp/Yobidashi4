00e5353580ba05d8196cc74a05e8555035ac32a3
toastkidjp <10fmi13@gmail.com>
2026-07-02 08:02:12 +0900

    refactor Use function reference.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/viewmodel/TextEditorViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/viewmodel/TextEditorViewModel.kt
@@ -108,8 +108,9 @@ class TextEditorViewModel : KoinComponent {
     private val transformation =
         TextEditorOutputTransformation(
             content,
-            mainViewModel.darkMode()
-        ) { parseResult.value }
+            mainViewModel.darkMode(),
+            parseResult::value
+        )
 
     fun content() = content
 
