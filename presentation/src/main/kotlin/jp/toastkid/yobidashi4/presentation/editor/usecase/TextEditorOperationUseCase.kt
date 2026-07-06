660ebdfbfcbe838839536fd75332c713a285450e
toastkidjp <10fmi13@gmail.com>
2026-07-07 00:08:58 +0900

    refactor Use data class.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/usecase/TextEditorOperationUseCase.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/usecase/TextEditorOperationUseCase.kt
@@ -174,10 +174,7 @@ class TextEditorOperationUseCase(
 
     // it.isCtrlPressed && it.key == Key.Zero
     fun toTaskList(): Boolean {
-        val rawSelectionStartIndex = content.selection.start
-        val rauSelectionEndIndex = content.selection.end
-        val selectionStartIndex = min(rawSelectionStartIndex, rauSelectionEndIndex)
-        val selectionEndIndex = max(rawSelectionStartIndex, rauSelectionEndIndex)
+        val (selectionStartIndex, selectionEndIndex) = calculateSelectionIndices()
 
         val selected = content.text.substring(selectionStartIndex, selectionEndIndex)
         if (selected.isEmpty()) {
