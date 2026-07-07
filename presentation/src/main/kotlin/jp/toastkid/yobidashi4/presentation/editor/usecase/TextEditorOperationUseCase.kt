6b30f1cf700ceb65d8d05cee07f771a5967b9f3a
toastkidjp <10fmi13@gmail.com>
2026-07-08 00:06:57 +0900

    refactor Use data class.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/usecase/TextEditorOperationUseCase.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/editor/usecase/TextEditorOperationUseCase.kt
@@ -365,10 +365,7 @@ class TextEditorOperationUseCase(
 
     // it.isCtrlPressed && it.isShiftPressed && it.key == Key.O
     fun openUrl(): Boolean {
-        val rawSelectionStartIndex = content.selection.start
-        val rauSelectionEndIndex = content.selection.end
-        val selectionStartIndex = min(rawSelectionStartIndex, rauSelectionEndIndex)
-        val selectionEndIndex = max(rawSelectionStartIndex, rauSelectionEndIndex)
+        val (selectionStartIndex, selectionEndIndex) = calculateSelectionIndices()
 
         val selected = content.text.substring(selectionStartIndex, selectionEndIndex)
         if (selected.isEmpty()) {
