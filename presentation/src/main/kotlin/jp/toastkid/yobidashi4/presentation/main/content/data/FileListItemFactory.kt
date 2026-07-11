85e49ec12131971d72917581d284a9bd534ac24a
toastkidjp <10fmi13@gmail.com>
2026-07-12 00:04:00 +0900

    refactor Add factory.

---
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/main/content/data/FileListItemFactory.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.presentation.main.content.data
+
+import java.nio.file.Path
+
+class FileListItemFactory(
+    private val metaDataExtractor: FileListItemMetaExtractor
+) {
+
+    operator fun invoke(
+        path: Path,
+        selected: Boolean = false,
+        editable: Boolean = true
+    ): FileListItem {
+        val item = FileListItem(path, selected, editable)
+        metaDataExtractor.make(path)?.let { item.setMeta(it) }
+        return item
+    }
+
+}
