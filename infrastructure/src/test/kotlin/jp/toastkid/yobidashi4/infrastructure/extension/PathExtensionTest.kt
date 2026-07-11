4808b9051b1327ff0efe871ba9c00fd5a75dab5e
toastkidjp <10fmi13@gmail.com>
2026-07-11 09:20:06 +0900

    refactor Rename test file.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/extension/PathExtensionTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/extension/PathExtensionTest.kt
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class ExtensionTest {
+class PathExtensionTest {
 
     @ParameterizedTest
     @CsvSource(
