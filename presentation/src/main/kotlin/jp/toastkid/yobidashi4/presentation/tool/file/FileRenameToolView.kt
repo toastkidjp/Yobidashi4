55cc42885ae3c69f14f73c1a3120688823601b32
toastkidjp <10fmi13@gmail.com>
2026-07-26 06:00:42 +0900

    feature Implement resize menu.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolView.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,6 +43,18 @@ fun FileRenameToolView() {
                 modifier = Modifier.onKeyEvent(viewModel::onKeyEvent)
             )
 
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.clickable { viewModel.switchUseResize() }
+            ) {
+                Checkbox(
+                    checked = viewModel.useResize().collectAsState().value,
+                    onCheckedChange = { viewModel.switchUseResize() },
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text("Resize to 50%")
+            }
+
             Row {
                 Button(
                     onClick = viewModel::clearPaths,
@@ -49,7 +63,7 @@ fun FileRenameToolView() {
                     Text("Clear files")
                 }
                 Button(
-                    onClick = viewModel::rename,
+                    onClick = { viewModel.rename() },
                     modifier = Modifier.padding(8.dp)
                 ) {
                     Text("Rename")
