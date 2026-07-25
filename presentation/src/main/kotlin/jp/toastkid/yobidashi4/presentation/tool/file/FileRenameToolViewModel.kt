5b205a62d168d7981d992b8624a6fdc481280478
toastkidjp <10fmi13@gmail.com>
2026-07-26 06:00:19 +0900

    feature Implement resize menu.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewModel.kt
@@ -10,12 +10,21 @@ import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.type
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.awt.image.BufferedImage
 import java.nio.file.Files
 import java.nio.file.Path
 import java.text.DecimalFormat
+import javax.imageio.ImageIO
 import kotlin.io.path.extension
+import kotlin.math.max
 
 class FileRenameToolViewModel : KoinComponent {
 
@@ -23,7 +32,13 @@ class FileRenameToolViewModel : KoinComponent {
 
     private val paths = mutableStateListOf<Path>()
 
-    private val input = TextFieldState("img")
+    private val useResize50Percent = MutableStateFlow(false)
+
+    fun useResize(): StateFlow<Boolean> = useResize50Percent
+
+    fun switchUseResize() {
+        useResize50Percent.tryEmit(useResize50Percent.value.not())
+    }
 
     private val listState = LazyListState()
 
@@ -31,7 +46,38 @@ class FileRenameToolViewModel : KoinComponent {
 
     fun listState() = listState
 
+    private val input = TextFieldState("img")
+
     fun input() = input
+
+    /**
+     * 画像の縦横サイズを半分にして保存する関数
+     * @return 処理が成功したかどうか
+     */
+    fun resizeImageToHalf(sourcePath: Path, targetPath: Path): Boolean {
+        val originalImage: BufferedImage = ImageIO.read(sourcePath.toFile()) ?: return false
+
+        val newWidth = max(1, originalImage.width / 2)
+        val newHeight = max(1, originalImage.height / 2)
+
+        val resizedImage = BufferedImage(newWidth, newHeight, originalImage.type.let {
+            if (it == BufferedImage.TYPE_CUSTOM) BufferedImage.TYPE_INT_ARGB else it
+        })
+
+        val g2d = resizedImage.createGraphics()
+        try {
+            g2d.setRenderingHint(
+                java.awt.RenderingHints.KEY_INTERPOLATION,
+                java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR
+            )
+            g2d.drawImage(originalImage, 0, 0, newWidth, newHeight, null)
+        } finally {
+            g2d.dispose()
+        }
+
+        val formatName = sourcePath.extension.ifEmpty { "png" }
+        return ImageIO.write(resizedImage, formatName, targetPath.toFile())
+    }
 
     fun rename() {
         if (paths.isEmpty()) {
@@ -39,16 +85,25 @@ class FileRenameToolViewModel : KoinComponent {
         }
 
         val decimalFormat = DecimalFormat("0".repeat(paths.size.toString().length))
-        paths.forEachIndexed { i, p ->
-            Files.copy(p, p.resolveSibling("${input.text}_${decimalFormat.format(i + 1)}.${p.extension}"))
-        }
+        CoroutineScope(Dispatchers.IO).launch {
+            paths.forEachIndexed { i, p ->
+                if (useResize50Percent.value) {
+                    resizeImageToHalf(p, p.resolveSibling("${input.text}_${decimalFormat.format(i + 1)}.${p.extension}"))
+                    return@forEachIndexed
+                }
 
-        viewModel
-            .showSnackbar(
-                "Rename completed!",
-                "Open folder",
-                ::openFolder
-            )
+                Files.copy(p, p.resolveSibling("${input.text}_${decimalFormat.format(i + 1)}.${p.extension}"))
+            }
+
+            withContext(Dispatchers.Unconfined) {
+                viewModel
+                    .showSnackbar(
+                        "Rename completed!",
+                        "Open folder",
+                        ::openFolder
+                    )
+            }
+        }
     }
 
     private fun openFolder() {
