1e64f8e2ee2dd88c06ed5e1adae8c48663397ccc
toastkidjp <10fmi13@gmail.com>
2026-07-31 00:05:13 +0900

    refactor Add test case.

--- infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/model/file/ArticleFilesFinderImplementationTest.kt
+++ infrastructure/src/test/kotlin/jp/toastkid/yobidashi4/infrastructure/model/file/ArticleFilesFinderImplementationTest.kt
@@ -7,15 +7,23 @@
  */
 package jp.toastkid.yobidashi4.infrastructure.model.file
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
 import io.mockk.unmockkAll
 import jp.toastkid.yobidashi4.domain.model.file.ArticleFilesFinder
+import kotlinx.datetime.toKotlinInstant
+import okio.FileMetadata
 import okio.FileSystem
+import okio.Path
 import okio.Path.Companion.toPath
 import okio.fakefilesystem.FakeFileSystem
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
 
 class ArticleFilesFinderImplementationTest {
 
@@ -41,14 +49,41 @@ class ArticleFilesFinderImplementationTest {
         val path3 = "data/『2021-01-02』".toPath()
         val path1 = "data/2021-01-02".toPath()
         val path2 = "data/Test".toPath()
+        val path4 = "data/path4".toPath()
+
+        val now = LocalDateTime.now()
+
+        fakeFileSystem = spyk(fakeFileSystem)
         fakeFileSystem.createDirectories(folder)
         fakeFileSystem.write(path1) { writeUtf8("log_content") }
         fakeFileSystem.write(path2) { writeUtf8("log_content") }
         fakeFileSystem.write(path3) { writeUtf8("log_content") }
+        fakeFileSystem.write(path4) { writeUtf8("log_content") }
+        every { fakeFileSystem.metadata(any()) } answers {
+            if (arg<Path>(0).name.endsWith("2"))
+                makeFakeMetadata(now.minusDays(8))
+            else if (arg<Path>(0).name.endsWith("3"))
+                makeFakeMetadata(now.minusDays(9))
+            else if (arg<Path>(0).name.endsWith("4")) {
+                val makeFakeMetadata = makeFakeMetadata(now.minusDays(9))
+                every { makeFakeMetadata.lastModifiedAtMillis } returns null
+                makeFakeMetadata
+            } else
+                makeFakeMetadata(now)
+        }
 
         val paths = subject.invoke(folder.toNioPath())
 
-        assertEquals(3, paths.size)
+        assertEquals(4, paths.size)
+    }
+
+    private fun makeFakeMetadata(dateTime: LocalDateTime): FileMetadata {
+        val fileMetadata2 = mockk<FileMetadata>()
+        every { fileMetadata2.lastModifiedAtMillis } returns dateTime
+            .toInstant(OffsetDateTime.now().offset)
+            .toKotlinInstant()
+            .toEpochMilliseconds()
+        return fileMetadata2
     }
 
 }
