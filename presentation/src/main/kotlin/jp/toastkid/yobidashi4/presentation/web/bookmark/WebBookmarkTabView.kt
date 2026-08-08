34048f0e09fad8c52b534d6f0b487d2396cdf5f1
toastkidjp <10fmi13@gmail.com>
2026-08-09 08:00:38 +0900

    refactor Use function reference.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabView.kt
@@ -47,9 +47,7 @@ internal fun WebBookmarkTabView(tab: WebBookmarkTab) {
     Surface(
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
         elevation = 4.dp,
-        modifier = Modifier.onKeyEvent {
-            return@onKeyEvent viewModel.onKeyEvent(it)
-        }.focusRequester(viewModel.focusRequester()).focusable(true)
+        modifier = Modifier.onKeyEvent(viewModel::onKeyEvent).focusRequester(viewModel.focusRequester()).focusable(true)
     ) {
         Box {
             LazyColumn(
