103f06714099727f8024ccaa827213e7c1f93bf9
toastkidjp <10fmi13@gmail.com>
2026-08-09 08:00:22 +0900

    refactor Use function reference.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/history/WebHistoryView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/history/WebHistoryView.kt
@@ -48,9 +48,7 @@ internal fun WebHistoryView(tab: WebHistoryTab) {
     Surface(
         color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
         elevation = 4.dp,
-        modifier = Modifier.onKeyEvent {
-            return@onKeyEvent viewModel.onKeyEvent(it)
-        }.focusRequester(viewModel.focusRequester()).focusable(true)
+        modifier = Modifier.onKeyEvent(viewModel::onKeyEvent).focusRequester(viewModel.focusRequester()).focusable(true)
     ) {
         Box {
             LazyColumn(
