98b66348f99cca6ae6338922cde84638f5c41806
toastkidjp <10fmi13@gmail.com>
2026-08-15 09:15:40 +0900

    refactor Clean up test cases.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowKtTest.kt
@@ -22,26 +22,16 @@ import androidx.compose.ui.unit.toOffset
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
-import io.mockk.mockk
 import io.mockk.mockkConstructor
-import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.slideshow.Slide
 import jp.toastkid.yobidashi4.domain.model.slideshow.SlideDeck
-import jp.toastkid.yobidashi4.domain.model.slideshow.data.CodeBlockLine
-import jp.toastkid.yobidashi4.domain.model.slideshow.data.ImageLine
-import jp.toastkid.yobidashi4.domain.model.slideshow.data.TableLine
-import jp.toastkid.yobidashi4.domain.service.slideshow.SlideDeckReader
+import jp.toastkid.yobidashi4.domain.model.slideshow.data.TextLine
 import kotlinx.coroutines.flow.MutableSharedFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.awt.Image
-import java.awt.image.BufferedImage
-import java.net.URL
-import java.nio.file.Files
-import javax.imageio.ImageIO
 
 class SlideshowKtTest {
 
@@ -51,50 +41,14 @@ class SlideshowKtTest {
 
     @BeforeEach
     fun setUp() {
-        mockkStatic(Files::class)
-        every { Files.lines(any()) } returns """
-![background](https://www.yahoo.co.jp/all)
-
-# Title slide
-test
-
-## Background
-![background](https://www.yahoo.co.jp)
-
-[footer](Footer text)
-
-## Image
-![image](https://www.yahoo.co.jp)
-
-## Table
-
-| Time | ID | Title
-|:---|:---|:---
-| 09:30 -11:30 | D1-KY| Java Day Tokyo 2017 基調講演
-| 13:00 -13:50 | D1-A1| Java 9 and Beyond: Java Renaissance in the Cloud
-| 14:05 -14:55 | D1-A2| Modular Development with JDK 9
-
-## Code
-
-```kotlin
-val engine = KotlinJsr223DefaultScriptEngineFactory().scriptEngine
-result.value = engine.eval(input.value.text).toString()
-```
-
-#### Quote
-> To be, or not to be, that is the question.
-
-# 完            
-        """.trimIndent().split("\n").stream()
-        mockkStatic(ImageIO::class)
-        every { ImageIO.read(any<URL>()) } returns BufferedImage(1, 1, Image.SCALE_FAST)
-
         mockkConstructor(SlideshowViewModel::class)
         every { anyConstructed<SlideshowViewModel>().focusRequester() } returns FocusRequester()
         every { anyConstructed<SlideshowViewModel>().requestFocus() } just Runs
         every { anyConstructed<SlideshowViewModel>().scrollEventFlow() } returns scrollEventFlow
 
-        slideDeck = SlideDeckReader(mockk()).invoke()
+        slideDeck = SlideDeck()
+        slideDeck.add(Slide().also { it.addLine(TextLine("test")) })
+        slideDeck.add(Slide())
     }
 
     @AfterEach
@@ -139,65 +93,4 @@ result.value = engine.eval(input.value.text).toString()
         }
     }
 
-    @OptIn(ExperimentalTestApi::class)
-    @Test
-    fun imagePage() {
-        runComposeUiTest {
-            setContent {
-                Slideshow(
-                    SlideDeck(slideDeck.slides.filter { it.lines().any { l -> l is ImageLine } }.toMutableList()),
-                    {},
-                    {},
-                    Modifier
-                )
-            }
-        }
-    }
-
-    @OptIn(ExperimentalTestApi::class)
-    @Test
-    fun codeBlockPage() {
-        runComposeUiTest {
-            setContent {
-                Slideshow(
-                    SlideDeck(slideDeck.slides.filter { it.lines().any { l -> l is CodeBlockLine } }.toMutableList()),
-                    {},
-                    {},
-                    Modifier
-                )
-            }
-        }
-    }
-
-    @OptIn(ExperimentalTestApi::class)
-    @Test
-    fun tableLinePage() {
-        runComposeUiTest {
-            setContent {
-                Slideshow(
-                    SlideDeck(slideDeck.slides.filter { it.lines().any { l -> l is TableLine } }.toMutableList()),
-                    {},
-                    {},
-                    Modifier
-                )
-            }
-        }
-    }
-
-    @OptIn(ExperimentalTestApi::class)
-    @Test
-    fun otherPage() {
-        runComposeUiTest {
-            setContent {
-                val slide = Slide()
-                slide.addLine(mockk())
-                Slideshow(
-                    SlideDeck(mutableListOf(slide)),
-                    {},
-                    {},
-                    Modifier
-                )
-            }
-        }
-    }
 }
