897a85566e1e541a2188db2d4ac13d223d51df27
toastkidjp <10fmi13@gmail.com>
2026-08-15 09:01:32 +0900

    refactor Implement interface.

---
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/slideshow/SlideDeckReaderImplementation.kt
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.infrastructure.service.slideshow
+
+import jp.toastkid.yobidashi4.domain.model.slideshow.Slide
+import jp.toastkid.yobidashi4.domain.model.slideshow.SlideDeck
+import jp.toastkid.yobidashi4.domain.service.slideshow.BackgroundExtractor
+import jp.toastkid.yobidashi4.domain.service.slideshow.CodeBlockBuilder
+import jp.toastkid.yobidashi4.domain.service.slideshow.ImageExtractor
+import jp.toastkid.yobidashi4.domain.service.slideshow.SlideDeckReader
+import jp.toastkid.yobidashi4.domain.service.slideshow.TableBuilder
+import org.koin.core.annotation.Single
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.concurrent.atomic.AtomicReference
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import java.util.stream.Stream
+
+@Single
+class SlideDeckReaderImplementation : SlideDeckReader {
+
+    /** Slide builder.  */
+    private val builder = AtomicReference(Slide())
+
+    /** Table builder.  */
+    private val tableBuilder = TableBuilder()
+
+    private val codeBlockBuilder = CodeBlockBuilder()
+
+    private val imageExtractor = ImageExtractor()
+
+    private val backgroundExtractor = BackgroundExtractor()
+
+    /**
+     * Convert to Slides.
+     * @return List&lt;Slide&gt;
+     */
+    override operator fun invoke(pathToMarkdown: Path): SlideDeck {
+        try {
+            return Files.lines(pathToMarkdown).use(::readLines)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return SlideDeck()
+    }
+
+    private fun readLines(lines: Stream<String>): SlideDeck {
+        val deck = SlideDeck()
+
+        lines.forEach { line: String ->
+            if (line.startsWith("#")) {
+                if (this.builder.get().hasTitle()) {
+                    this.builder.get().let {
+                        deck.add(it)
+                    }
+                    this.builder.set(Slide())
+                }
+
+                val builder = this.builder.get()
+                if (line.startsWith("# ")) {
+                    builder.setFront(true)
+                }
+                val titleText = line.substring(line.indexOf(" ")).trim { it <= ' ' }
+                builder.setTitle(titleText)
+                if (deck.title.isEmpty()) {
+                    deck.title = titleText
+                }
+                this.builder.set(builder)
+                return@forEach
+            }
+            if (line.startsWith("![")) {
+                if (BackgroundExtractor.shouldInvoke(line)) {
+                    backgroundExtractor(line)?.let {
+                        if (deck.shouldSetBackground()) {
+                            deck.background = it
+                            return@forEach
+                        }
+                        val slide = builder.get()
+                        slide.setBackground(it)
+                        this.builder.set(slide)
+                    }
+                    return@forEach
+                }
+
+                val slide = builder.get()
+                slide.addLines(imageExtractor.invoke(line))
+                this.builder.set(slide)
+                return@forEach
+            }
+            if (line.startsWith("[footer](")) {
+                val matcher: Matcher = FOOTER_TEXT.matcher(line)
+                if (matcher.find()) {
+                    deck.footerText = matcher.group(1)
+                }
+                return@forEach
+            }
+            if (line.startsWith("> ")) {
+                val slide = builder.get()
+                slide.addQuotedLines(line)
+                this.builder.set(slide)
+                return@forEach
+            }
+            // Adding code block.
+            if (line.startsWith("```")) {
+                if (codeBlockBuilder.inCodeBlock()) {
+                    codeBlockBuilder.build().let {
+                        val slide = builder.get()
+                        slide.addLine(it)
+                        this.builder.set(slide)
+                        codeBlockBuilder.initialize()
+                    }
+                    return@forEach
+                }
+
+                codeBlockBuilder.startCodeBlock()
+                val index = line.indexOf(":")
+                val lastIndex = if (index == -1) line.length else index
+                codeBlockBuilder.setCodeFormat(line.substring(3, lastIndex))
+                return@forEach
+            }
+            if (codeBlockBuilder.shouldAppend(line)) {
+                codeBlockBuilder.append(line)
+                return@forEach
+            }
+
+            if (TableBuilder.isTableStart(line)) {
+                if (!tableBuilder.active()) {
+                    tableBuilder.setActive()
+                }
+
+                if (TableBuilder.shouldIgnoreLine(line)) {
+                    return@forEach
+                }
+
+                if (tableBuilder.hasColumns() == false) {
+                    tableBuilder.setColumns(line)
+                    return@forEach
+                }
+
+                tableBuilder.addTableLines(line)
+                return@forEach
+            }
+
+            if (tableBuilder.active()) {
+                insertTable()
+            }
+            // Not code.
+            if (line.isNotEmpty()) {
+                val slide = this.builder.get()
+                slide.addText(line)
+                this.builder.set(slide)
+            }
+        }
+
+        if (tableBuilder.active()) {
+            insertTable()
+        }
+
+        builder.let {
+            deck.add(it.get())
+        }
+
+        return deck
+    }
+
+    private fun insertTable() {
+        val slide = builder.get()
+        slide.addLine(tableBuilder.build())
+        this.builder.set(slide)
+        tableBuilder.setInactive()
+        tableBuilder.clear()
+    }
+
+    companion object {
+
+        /** In-line image pattern.  */
+        private val FOOTER_TEXT: Pattern = Pattern.compile("\\[footer\\]\\((.+?)\\)")
+
+    }
+
+}
