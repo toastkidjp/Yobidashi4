ef5ac9cde05e41355fd810afd749e30ac1dcab01
toastkidjp <10fmi13@gmail.com>
2026-08-18 00:10:15 +0900

    refactor Modify interface for testable.

--- src/main/kotlin/jp/toastkid/yobidashi4/main/handler/UncaughtExceptionHandler.kt
+++ src/main/kotlin/jp/toastkid/yobidashi4/main/handler/UncaughtExceptionHandler.kt
@@ -1,10 +1,11 @@
 package jp.toastkid.yobidashi4.main.handler
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class UncaughtExceptionHandler : Thread.UncaughtExceptionHandler {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
+class UncaughtExceptionHandler(
+    private val logger: Logger = LoggerFactory.getLogger(UncaughtExceptionHandler::class.java)
+) : Thread.UncaughtExceptionHandler {
 
     override fun uncaughtException(t: Thread?, e: Throwable?) {
         logger.error(t?.name, e)
