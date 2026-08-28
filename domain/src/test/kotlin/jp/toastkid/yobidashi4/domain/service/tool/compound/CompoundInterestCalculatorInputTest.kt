1c08d7a17b561d7f00832cdce0bc0db084e50b7f
toastkidjp <10fmi13@gmail.com>
2026-08-29 00:38:02 +0900

    refactor Make parameterized test.

--- domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorInputTest.kt
+++ domain/src/test/kotlin/jp/toastkid/yobidashi4/domain/service/tool/compound/CompoundInterestCalculatorInputTest.kt
@@ -3,19 +3,35 @@ package jp.toastkid.yobidashi4.domain.service.tool.compound
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class CompoundInterestCalculatorInputTest {
 
+    @ParameterizedTest
+    @CsvSource(
+        "null, null, null, null",
+        "'', null, null, null",
+        "'', '', null, null",
+        "'', '', '', null",
+        "'', '', '', ''",
+        "1, null, null, null",
+        "2, 2, null, null",
+        "3, 3, 2, null",
+        "test, 4, '', ''",
+        nullValues = ["null"]
+    )
+    fun nullCases(
+        capitalInput: String?,
+        installmentInput: String?,
+        annualInterestInput: String?,
+        yearInput: String?
+    ) {
+        assertNull(CompoundInterestCalculatorInput.from(capitalInput, installmentInput, annualInterestInput, yearInput))
+    }
+
     @Test
     fun test() {
-        assertNull(CompoundInterestCalculatorInput.from(null, null, null, null))
-        assertNull(CompoundInterestCalculatorInput.from("", null, null, null))
-        assertNull(CompoundInterestCalculatorInput.from("", "", null, null))
-        assertNull(CompoundInterestCalculatorInput.from("", "", "", null))
-        assertNull(CompoundInterestCalculatorInput.from("", "", "", ""))
-        assertNull(CompoundInterestCalculatorInput.from("1", null, null, null))
-        assertNull(CompoundInterestCalculatorInput.from("2", "2", null, null))
-        assertNull(CompoundInterestCalculatorInput.from("3", "3", "2", null))
         assertNotNull(CompoundInterestCalculatorInput.from("4", "4", "2", "1"))
     }
 
