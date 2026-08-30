0033b3240f4b93df3945cb09659ea5aafa80c79a
toastkidjp <10fmi13@gmail.com>
2026-08-31 06:58:56 +0900

    feature Add function.

--- domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/notification/NotificationEventRepository.kt
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/repository/notification/NotificationEventRepository.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
 package jp.toastkid.yobidashi4.domain.repository.notification
 
 import jp.toastkid.yobidashi4.domain.model.notification.NotificationEvent
@@ -11,6 +18,8 @@ interface NotificationEventRepository {
     fun update(index: Int, event: NotificationEvent)
 
     fun deleteAt(index: Int)
+
+    fun delete(event: NotificationEvent)
 
     fun clear()
 
