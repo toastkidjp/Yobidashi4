80253ed18510e2b11443410dc9b50df7d1040363
toastkidjp <10fmi13@gmail.com>
2026-09-01 08:58:39 +0900

    refactor Safe delete parameter 'onEscapeKeyReleased'.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/slideshow/SlideshowKtTest.kt
@@ -66,7 +66,6 @@ class SlideshowKtTest {
                 Slideshow(
                     slideDeck,
                     {},
-                    {},
                     Modifier
                 )
             }
