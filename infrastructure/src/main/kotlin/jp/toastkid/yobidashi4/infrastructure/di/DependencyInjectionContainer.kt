ae8c16aebaf1e5a740887c66b2bf61fa0ad4d73a
toastkidjp <10fmi13@gmail.com>
2026-09-07 00:08:19 +0900

    refactor Modify DI.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/di/DependencyInjectionContainer.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/di/DependencyInjectionContainer.kt
@@ -1,22 +1,20 @@
 package jp.toastkid.yobidashi4.infrastructure.di
 
+import jp.toastkid.yobidashi4.presentation.di.PresentationModule
 import okio.FileSystem
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.ksp.generated.module
 
-@Module
-@ComponentScan("jp.toastkid.yobidashi4.infrastructure")
 class DependencyInjectionContainer {
 
     companion object {
 
         fun start() {
             startKoin {
-                modules(DependencyInjectionContainer().module)
+                modules(InfrastructureModule().module)
+                modules(PresentationModule().module)
                 modules(
                     module {
                         single<FileSystem> { FileSystem.SYSTEM }
