335486ca653e4768df8ead75dca1a1def2def398
toastkidjp <10fmi13@gmail.com>
2026-09-06 10:36:26 +0900

    refactor Add interface.

---
+++ domain/src/main/kotlin/jp/toastkid/yobidashi4/domain/service/tool/clustering/ClusteringDocumentReader.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026 toastkidjp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package jp.toastkid.yobidashi4.domain.service.tool.clustering
+
+import java.nio.file.Path
+
+interface ClusteringDocumentReader {
+
+    operator fun invoke(paths: Collection<Path>): List<Pair<String, String>>
+
+}
