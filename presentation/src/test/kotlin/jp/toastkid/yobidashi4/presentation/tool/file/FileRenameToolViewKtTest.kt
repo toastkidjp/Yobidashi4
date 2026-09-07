ad6dc89f2cb60a861d4ada97077177fde8d61356
toastkidjp <10fmi13@gmail.com>
2026-09-08 00:08:51 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/tool/file/FileRenameToolViewKtTest.kt
@@ -1,18 +1,21 @@
 package jp.toastkid.yobidashi4.presentation.tool.file
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.v2.runDesktopComposeUiTest
+import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
 import io.mockk.mockk
-import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -20,17 +23,24 @@ import java.nio.file.Path
 
 class FileRenameToolViewKtTest {
 
+    @RelaxedMockK
+    private lateinit var viewModel: FileRenameToolViewModel
+    
     @BeforeEach
     fun setUp() {
-        mockkConstructor(FileRenameToolViewModel::class)
+        MockKAnnotations.init(this)
+
         val element = mockk<Path>()
         every { element.fileName } returns element
         every { element.toString() } returns "test"
-        every { anyConstructed<FileRenameToolViewModel>().items() } returns listOf(element)
-        every { anyConstructed<FileRenameToolViewModel>().dispose() } just Runs
-        every { anyConstructed<FileRenameToolViewModel>().remove(any()) } just Runs
-        coEvery { anyConstructed<FileRenameToolViewModel>().collectDroppedPaths() } just Runs
-        every { anyConstructed<FileRenameToolViewModel>().renamedSampleFileName() } returns ""
+        every { viewModel.items() } returns listOf(element)
+        every { viewModel.dispose() } just Runs
+        every { viewModel.remove(any()) } just Runs
+        coEvery { viewModel.collectDroppedPaths() } just Runs
+        every { viewModel.renamedSampleFileName() } returns ""
+        every { viewModel.useResize() } returns MutableStateFlow(false)
+        every { viewModel.listState() } returns LazyListState()
+        every { viewModel.input() } returns TextFieldState()
     }
 
     @AfterEach
@@ -43,11 +53,11 @@ class FileRenameToolViewKtTest {
     fun fileRenameToolView() {
         runDesktopComposeUiTest {
             setContent {
-                FileRenameToolView()
+                FileRenameToolView(viewModel)
             }
 
             onNodeWithText("x").performClick()
-            verify { anyConstructed<FileRenameToolViewModel>().remove(any()) }
+            verify { viewModel.remove(any()) }
         }
     }
 
@@ -55,12 +65,12 @@ class FileRenameToolViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun emptyCase() {
-        every { anyConstructed<FileRenameToolViewModel>().items() } returns emptyList()
-        every { anyConstructed<FileRenameToolViewModel>().input() } returns TextFieldState()
+        every { viewModel.items() } returns emptyList()
+        every { viewModel.input() } returns TextFieldState()
 
         runDesktopComposeUiTest {
             setContent {
-                FileRenameToolView()
+                FileRenameToolView(viewModel)
             }
 
             onNodeWithText("x").assertDoesNotExist()
