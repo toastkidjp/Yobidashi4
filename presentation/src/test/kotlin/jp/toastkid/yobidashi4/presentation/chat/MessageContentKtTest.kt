1c01f5934eae8325ff6b8ddb9674a18b65883e49
toastkidjp <10fmi13@gmail.com>
2026-09-08 00:23:37 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContentKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/MessageContentKtTest.kt
@@ -17,11 +17,12 @@ import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performMouseInput
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
+import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
-import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.chat.Source
@@ -31,15 +32,18 @@ import org.junit.jupiter.api.Test
 
 class MessageContentKtTest {
 
+    @RelaxedMockK
+    private lateinit var viewModel: MessageContentViewModel
+    
     @BeforeEach
     fun setUp() {
-        mockkConstructor(MessageContentViewModel::class)
-        every { anyConstructed<MessageContentViewModel>().storeImage(any()) } just Runs
-        every { anyConstructed<MessageContentViewModel>().openLink(any()) } just Runs
-        every { anyConstructed<MessageContentViewModel>().openLinkOnBackground(any()) } just Runs
+        MockKAnnotations.init(this)
+        every { viewModel.storeImage(any()) } just Runs
+        every { viewModel.openLink(any()) } just Runs
+        every { viewModel.openLinkOnBackground(any()) } just Runs
         val contextMenuState = ContextMenuState()
         contextMenuState.status = ContextMenuState.Status.Open(Rect(4f, 25f, 116f, 57f))
-        every { anyConstructed<MessageContentViewModel>().contextMenuState() } returns contextMenuState
+        every { viewModel.contextMenuState() } returns contextMenuState
     }
 
     @AfterEach
@@ -58,19 +62,21 @@ class MessageContentKtTest {
                     "test\n* **test**\n* ***Good***",
                     null,
                     emptyList(),
-                    Modifier
+                    Modifier,
+                    viewModel
                 )
                 MessageContent(
                     "image",
                     "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBASfqKgwAAAAASUVORK5CYII=",
                     listOf(source),
-                    Modifier
+                    Modifier,
+                    viewModel
                 )
             }
 
             onNode(hasText("Store image")).performClick()
 
-            verify { anyConstructed<MessageContentViewModel>().storeImage(any()) }
+            verify { viewModel.storeImage(any()) }
 
             onNodeWithContentDescription("0,${source}", useUnmergedTree = true)
                 .performMouseInput {
@@ -79,8 +85,8 @@ class MessageContentKtTest {
                     longClick()
                 }
 
-            verify { anyConstructed<MessageContentViewModel>().openLink(any()) }
-            verify { anyConstructed<MessageContentViewModel>().openLinkOnBackground(any()) }
+            verify { viewModel.openLink(any()) }
+            verify { viewModel.openLinkOnBackground(any()) }
         }
     }
 
