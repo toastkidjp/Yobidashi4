bc43e9254a9ca4606fa2f5832e90b4f8a983d5fb
toastkidjp <10fmi13@gmail.com>
2026-09-09 00:17:06 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/barcode/BarcodeToolTabViewKtTest.kt
@@ -7,60 +7,34 @@ import androidx.compose.ui.test.runDesktopComposeUiTest
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
-import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
-import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeDecoder
-import jp.toastkid.yobidashi4.domain.service.barcode.BarcodeEncoder
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.bind
-import org.koin.dsl.module
 
 class BarcodeToolTabViewKtTest {
 
-    @MockK
-    private lateinit var mainViewModel: MainViewModel
-
-    @MockK
-    private lateinit var barcodeEncoder: BarcodeEncoder
-
-    @MockK
-    private lateinit var barcodeDecoder: BarcodeDecoder
+    @RelaxedMockK
+    private lateinit var viewModel: BarcodeToolTabViewModel
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
 
-        startKoin {
-            modules(
-                module {
-                    single(qualifier = null) { mainViewModel } bind (MainViewModel::class)
-                    single(qualifier = null) { barcodeEncoder } bind (BarcodeEncoder::class)
-                    single(qualifier = null) { barcodeDecoder } bind (BarcodeDecoder::class)
-                }
-            )
-        }
-
-        mockkConstructor(BarcodeToolTabViewModel::class)
-        every { anyConstructed<BarcodeToolTabViewModel>().barcodeImage() } returns ImageBitmap(1, 1)
-        every { anyConstructed<BarcodeToolTabViewModel>().onClickImage() } just Runs
-        every { anyConstructed<BarcodeToolTabViewModel>().onClickDecodeResult() } just Runs
-        every { anyConstructed<BarcodeToolTabViewModel>().encodeInputValue() } returns TextFieldState()
-        every { anyConstructed<BarcodeToolTabViewModel>().setEncodeInputValue() } just Runs
-        every { anyConstructed<BarcodeToolTabViewModel>().decodeInputValue() } returns TextFieldState()
-        every { anyConstructed<BarcodeToolTabViewModel>().setDecodeInputValue() } just Runs
-        every { anyConstructed<BarcodeToolTabViewModel>().decodeResult() } returns "test"
+        every { viewModel.barcodeImage() } returns ImageBitmap(1, 1)
+        every { viewModel.onClickImage() } just Runs
+        every { viewModel.onClickDecodeResult() } just Runs
+        every { viewModel.encodeInputValue() } returns TextFieldState()
+        every { viewModel.setEncodeInputValue() } just Runs
+        every { viewModel.decodeInputValue() } returns TextFieldState()
+        every { viewModel.setDecodeInputValue() } just Runs
+        every { viewModel.decodeResult() } returns "test"
     }
 
     @AfterEach
     fun tearDown() {
-        stopKoin()
         unmockkAll()
     }
 
@@ -69,7 +43,7 @@ class BarcodeToolTabViewKtTest {
     fun barcodeToolTabView() {
         runDesktopComposeUiTest {
             setContent {
-                BarcodeToolTabView()
+                BarcodeToolTabView(viewModel)
             }
         }
     }
@@ -77,11 +51,11 @@ class BarcodeToolTabViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun nullImage() {
-        every { anyConstructed<BarcodeToolTabViewModel>().barcodeImage() } returns null
+        every { viewModel.barcodeImage() } returns null
 
         runDesktopComposeUiTest {
             setContent {
-                BarcodeToolTabView()
+                BarcodeToolTabView(viewModel)
             }
         }
     }
@@ -89,11 +63,11 @@ class BarcodeToolTabViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun decodeResultBlankCase() {
-        every { anyConstructed<BarcodeToolTabViewModel>().decodeResult() } returns "  "
+        every { viewModel.decodeResult() } returns "  "
 
         runDesktopComposeUiTest {
             setContent {
-                BarcodeToolTabView()
+                BarcodeToolTabView(viewModel)
             }
         }
     }
