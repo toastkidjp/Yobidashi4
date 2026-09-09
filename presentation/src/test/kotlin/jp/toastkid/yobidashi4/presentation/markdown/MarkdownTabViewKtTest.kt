ecef5f1eb6ba96ef2580274e45a04f5ef5d3595d
toastkidjp <10fmi13@gmail.com>
2026-09-10 00:04:54 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewKtTest.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.yobidashi4.presentation.markdown
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.v2.runDesktopComposeUiTest
@@ -7,31 +8,26 @@ import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
-import java.nio.file.Files
-import java.nio.file.Path
 import jp.toastkid.yobidashi4.domain.model.tab.MarkdownPreviewTab
 import jp.toastkid.yobidashi4.domain.service.markdown.MarkdownParser
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
-import kotlin.io.path.nameWithoutExtension
-import kotlinx.coroutines.flow.emptyFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.bind
-import org.koin.dsl.module
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.nameWithoutExtension
 
 class MarkdownTabViewKtTest {
 
-    @MockK
-    private lateinit var path: Path
+    @RelaxedMockK
+    private lateinit var viewModel: MarkdownTabViewModel
 
     @MockK
-    private lateinit var mainViewModel: MainViewModel
+    private lateinit var path: Path
 
     @MockK
     private lateinit var tab: MarkdownPreviewTab
@@ -43,72 +39,15 @@ class MarkdownTabViewKtTest {
         every { path.nameWithoutExtension } returns "test.md"
 
         mockkStatic(Files::class)
-        every { Files.lines(any()) } returns """
-# Title
-First description
-
-## Image
-![test](https://www.yahoo.co.jp/favicon.ico)
-
-### Link
-S&P 500 and [VIX](https://www.yahoo.co.jp/indices/VIX:IND)
-
-## List
-- Aaron
-- Beck
-- Chief
-
-## Ordered List
-1. Soba
-2. Udon
-3. Tempra
-
-## Task list
-- [ ] Task1
-- [x] Done
-
----
-
-## Table
-| Time | Temperature
-|:---|:---
-| 8:10  | 29.4
-| 8:25  | 29.5
-
-## Sub
-Test
-
-## Code
-```kotlin
-println("Hello")
-```
-
-## Table2
-| Key | Value |
-|:---|:---|
-| First | 422
-| Second | 123
-| Third | 3442
-| Tax | 121
-
-        """.split("\n").stream()
+        every { Files.lines(any()) } returns """""".split("\n").stream()
+        every { viewModel.scrollState() } returns LazyListState()
+        every { viewModel.onDispose(any()) } just Runs
         every { tab.markdown() } returns MarkdownParser().invoke(path)
         every { tab.scrollPosition() } returns 0
-        every { mainViewModel.finderFlow() } returns emptyFlow()
-        every { mainViewModel.updateScrollableTab(any(), any()) } just Runs
-
-        startKoin {
-            modules(
-                module {
-                    single(qualifier = null) { mainViewModel } bind (MainViewModel::class)
-                }
-            )
-        }
     }
 
     @AfterEach
     fun tearDown() {
-        stopKoin()
         unmockkAll()
     }
 
@@ -117,7 +56,7 @@ println("Hello")
     fun markdownTabView() {
         runDesktopComposeUiTest {
             setContent {
-                MarkdownTabView(tab, Modifier)
+                MarkdownTabView(tab, Modifier, viewModel)
             }
         }
     }
