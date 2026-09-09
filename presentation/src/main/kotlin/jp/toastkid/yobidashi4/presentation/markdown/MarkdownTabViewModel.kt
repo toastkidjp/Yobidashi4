05cc7f524b0fe91921e19d14b9b0adaefbff87a9
toastkidjp <10fmi13@gmail.com>
2026-09-10 00:04:16 +0900

    refactor Use constructor injection.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/markdown/MarkdownTabViewModel.kt
@@ -11,16 +11,17 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.focus.FocusRequester
 import jp.toastkid.yobidashi4.domain.model.tab.ScrollableContentTab
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.koin.core.annotation.Factory
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
-class MarkdownTabViewModel : KoinComponent {
+@Factory
+class MarkdownTabViewModel(
+    private val mainViewModel: MainViewModel
+) : KoinComponent {
 
     private val scrollState = LazyListState(0)
 
     private val focusRequester = FocusRequester()
-
-    private val mainViewModel: MainViewModel by inject()
 
     suspend fun launch(scrollPosition: Int) {
         focusRequester().requestFocus()
