60949718384685d30177f180235c2a293908180e
toastkidjp <10fmi13@gmail.com>
2026-09-10 00:02:53 +0900

    refactor Fix incorrect unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/ChatTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/chat/ChatTabViewKtTest.kt
@@ -17,8 +17,8 @@ import androidx.compose.ui.semantics.getOrNull
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.click
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.test.performMouseInput
@@ -123,7 +123,7 @@ class ChatTabViewKtTest {
             onNodeWithContentDescription("Input message box.", useUnmergedTree = true)
                 .performClick()
 
-            onNodeWithTag("clip_icon")
+            onAllNodesWithTag("clip_icon")[0]
                 .fetchSemanticsNode()
                 .config.getOrNull(SemanticsActions.OnClick)?.action?.invoke()
 
