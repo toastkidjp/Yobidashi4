fb0f199d0563f371576d92ff6bc20681df08ccb9
toastkidjp <10fmi13@gmail.com>
2026-09-11 00:10:35 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/input/InputHistoryViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/input/InputHistoryViewKtTest.kt
@@ -1,76 +1,52 @@
 package jp.toastkid.yobidashi4.presentation.input
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.longClick
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performMouseInput
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
-import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.input.InputHistory
 import jp.toastkid.yobidashi4.domain.model.tab.InputHistoryTab
-import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
-import jp.toastkid.yobidashi4.domain.service.article.finder.FullTextArticleFinder
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.bind
-import org.koin.dsl.module
 
 class InputHistoryViewKtTest {
 
-    @MockK
-    private lateinit var mainViewModel: MainViewModel
-
-    @MockK
-    private lateinit var keywordSearch: FullTextArticleFinder
-
-    @MockK
-    private lateinit var articlesReaderService: ArticlesReaderService
+    @RelaxedMockK
+    private lateinit var viewModel: InputHistoryViewModel
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
 
-        startKoin {
-            modules(
-                module {
-                    single(qualifier = null) { mainViewModel } bind (MainViewModel::class)
-                    single(qualifier = null) { keywordSearch } bind (FullTextArticleFinder::class)
-                    single(qualifier = null) { articlesReaderService } bind (ArticlesReaderService::class)
-                }
-            )
-        }
-
-        mockkConstructor(InputHistoryViewModel::class)
-        every { anyConstructed<InputHistoryViewModel>().open(any()) } just Runs
-        every { anyConstructed<InputHistoryViewModel>().openOnBackground(any()) } just Runs
-        every { anyConstructed<InputHistoryViewModel>().onDispose(any()) } just Runs
-        every { anyConstructed<InputHistoryViewModel>().delete(any()) } just Runs
-        every { anyConstructed<InputHistoryViewModel>().dateTimeString(any()) } returns "test"
-        every { anyConstructed<InputHistoryViewModel>().items() } returns listOf(
+        every { viewModel.open(any()) } just Runs
+        every { viewModel.openOnBackground(any()) } just Runs
+        every { viewModel.onDispose(any()) } just Runs
+        every { viewModel.delete(any()) } just Runs
+        every { viewModel.dateTimeString(any()) } returns "test"
+        every { viewModel.listState() } returns LazyListState()
+        every { viewModel.items() } returns listOf(
             InputHistory("test", 1),
             InputHistory("test2", 2),
         )
-        coEvery { anyConstructed<InputHistoryViewModel>().launch(any()) } just Runs
+        coEvery { viewModel.launch(any()) } just Runs
     }
 
     @AfterEach
     fun tearDown() {
-        stopKoin()
         unmockkAll()
     }
 
@@ -79,7 +55,7 @@ class InputHistoryViewKtTest {
     fun test() {
         runComposeUiTest {
             setContent {
-                InputHistoryView(InputHistoryTab("test"))
+                InputHistoryView(InputHistoryTab("test"), viewModel)
             }
 
             onNodeWithText("test2", useUnmergedTree = true)
@@ -92,12 +68,12 @@ class InputHistoryViewKtTest {
             onNodeWithContentDescription("Delete item test", useUnmergedTree = true)
                 .performClick()
 
-            verify { anyConstructed<InputHistoryViewModel>().open(any()) }
-            verify { anyConstructed<InputHistoryViewModel>().openOnBackground(any()) }
-            verify { anyConstructed<InputHistoryViewModel>().delete(any()) }
-            verify { anyConstructed<InputHistoryViewModel>().dateTimeString(any()) }
-            verify { anyConstructed<InputHistoryViewModel>().items() }
-            coVerify { anyConstructed<InputHistoryViewModel>().launch(any()) }
+            verify { viewModel.open(any()) }
+            verify { viewModel.openOnBackground(any()) }
+            verify { viewModel.delete(any()) }
+            verify { viewModel.dateTimeString(any()) }
+            verify { viewModel.items() }
+            coVerify { viewModel.launch(any()) }
         }
     }
 }
