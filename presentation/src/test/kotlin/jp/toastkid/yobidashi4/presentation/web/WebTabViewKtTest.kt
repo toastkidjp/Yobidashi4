1b84900696bc4ca126b9d02028051a2e34319060
toastkidjp <10fmi13@gmail.com>
2026-09-13 00:08:47 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewKtTest.kt
@@ -17,59 +17,28 @@ import androidx.compose.ui.window.rememberWindowState
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
-import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
-import io.mockk.verify
-import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
-import kotlinx.coroutines.flow.emptyFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.bind
-import org.koin.dsl.module
-import java.awt.Panel
 
 class WebTabViewKtTest {
 
-    @MockK
-    private lateinit var webViewPool: WebViewPool
-
-    @MockK
-    private lateinit var mainViewModel: MainViewModel
+    @RelaxedMockK
+    private lateinit var viewModel: WebTabViewModel
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
 
-        startKoin {
-            modules(
-                module {
-                    single(qualifier = null) { webViewPool } bind(WebViewPool::class)
-                    single(qualifier = null) { mainViewModel } bind(MainViewModel::class)
-                }
-            )
-        }
-
-        every { webViewPool.find(any(), any(), any()) } just Runs
-        every { webViewPool.reload(any()) } just Runs
-        every { webViewPool.clearFind(any()) } just Runs
-        every { webViewPool.component(any(), any()) } returns Panel()
-        every { mainViewModel.showingSnackbar() } returns false
-        every { mainViewModel.finderFlow() } returns emptyFlow()
-        mockkConstructor(WebTabViewModel::class)
-        coEvery { anyConstructed<WebTabViewModel>().start(any()) } just Runs
+        coEvery { viewModel.start(any()) } just Runs
     }
 
     @AfterEach
     fun tearDown() {
-        stopKoin()
         unmockkAll()
     }
 
@@ -90,13 +59,11 @@ class WebTabViewKtTest {
                     visible = true,
                     state = rememberWindowState(width = 1.dp, height = 1.dp)
                 ) {
-                    WebTabView(tabs[index.value])
+                    WebTabView(tabs[index.value], viewModel)
 
                     index.value = 1
                 }
             }
-
-            verify(exactly = 2) { webViewPool.component(any(), any()) }
         }
     }
 
