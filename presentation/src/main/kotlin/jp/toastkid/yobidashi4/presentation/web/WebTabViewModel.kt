cb0294cfec6a768e6e084c687d8c8b7c415fd77f
toastkidjp <10fmi13@gmail.com>
2026-09-13 00:08:07 +0900

    refactor Use constructor injection.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewModel.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/WebTabViewModel.kt
@@ -2,21 +2,21 @@ package jp.toastkid.yobidashi4.presentation.web
 
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.unit.dp
-import java.awt.Component
 import jp.toastkid.yobidashi4.domain.model.browser.WebViewPool
 import jp.toastkid.yobidashi4.domain.model.find.FindOrder
 import jp.toastkid.yobidashi4.domain.model.tab.WebTab
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.koin.core.annotation.Factory
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
+import java.awt.Component
 
-class WebTabViewModel : KoinComponent {
+@Factory
+class WebTabViewModel(
+    private val mainViewModel: MainViewModel,
+    private val webViewPool: WebViewPool
+) : KoinComponent {
 
     private val focusRequester = FocusRequester()
-
-    private val mainViewModel: MainViewModel by inject()
-
-    private val webViewPool: WebViewPool by inject()
 
     fun component(tab: WebTab): Component {
         return webViewPool.component(tab.id(), tab.url())
