f55627430bb398a60277d7e42caa634de1899235
toastkidjp <10fmi13@gmail.com>
2026-09-14 00:00:21 +0900

    bugfix Update article search.

--- infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/finder/FullTextSearch.kt
+++ infrastructure/src/main/kotlin/jp/toastkid/yobidashi4/infrastructure/service/article/finder/FullTextSearch.kt
@@ -9,42 +9,57 @@ package jp.toastkid.yobidashi4.infrastructure.service.article.finder
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.document.Document
-import org.apache.lucene.index.CorruptIndexException
-import org.apache.lucene.index.DirectoryReader
 import org.apache.lucene.queryparser.classic.QueryParser
-import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.search.ScoreDoc
+import org.apache.lucene.search.SearcherManager
 import org.apache.lucene.search.TopDocs
 import org.apache.lucene.store.FSDirectory
+import java.io.Closeable
 import java.io.IOException
 import java.nio.file.Path
 import java.text.ParseException
 
-class FullTextSearch(private val indexSearcher: IndexSearcher) {
+class FullTextSearch(private val searcherManager: SearcherManager) : Closeable {
 
     private val queryParser: QueryParser = QueryParser("content", StandardAnalyzer())
 
-    private val indexReader = indexSearcher.indexReader
-
     @Throws(IOException::class, ParseException::class)
     fun search(searchQueryInput: String): TopDocs? {
+        searcherManager.maybeRefresh()
+
         val sanitized =
             if (searchQueryInput.startsWith("?") || searchQueryInput.startsWith("*")) "\\$searchQueryInput"
             else searchQueryInput
-        return indexSearcher.search(queryParser.parse(sanitized), 300)
+
+        val searcher = searcherManager.acquire()
+        return try {
+            searcher.search(queryParser.parse(sanitized), 300)
+        } finally {
+            searcherManager.release(searcher)
+        }
     }
 
-    @Throws(CorruptIndexException::class, IOException::class)
+    @Throws(IOException::class)
     fun getDocument(scoreDoc: ScoreDoc): Document? {
-        return indexReader.storedFields().document(scoreDoc.doc)
+        val searcher = searcherManager.acquire()
+        return try {
+            searcher.storedFields().document(scoreDoc.doc)
+        } finally {
+            searcherManager.release(searcher)
+        }
+    }
+
+    override fun close() {
+        searcherManager.close()
     }
 
     companion object {
-
-        fun make(indexDirectoryPath: Path) = FullTextSearch(
-            IndexSearcher(DirectoryReader.open(FSDirectory.open(indexDirectoryPath)))
-        )
-
+        fun make(indexDirectoryPath: Path): FullTextSearch {
+            val directory = FSDirectory.open(indexDirectoryPath)
+            // Directory から SearcherManager を作成
+            val manager = SearcherManager(directory, null)
+            return FullTextSearch(manager)
+        }
     }
 
 }
