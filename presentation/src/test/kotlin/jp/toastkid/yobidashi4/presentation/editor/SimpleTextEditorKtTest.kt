d2e7907400c5f14519f08791cfde4dffc7135464
toastkidjp <10fmi13@gmail.com>
2026-09-14 00:01:32 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/SimpleTextEditorKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/editor/SimpleTextEditorKtTest.kt
@@ -7,7 +7,11 @@
  */
 package jp.toastkid.yobidashi4.presentation.editor
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithContentDescription
@@ -16,28 +20,26 @@ import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.pressKey
 import androidx.compose.ui.test.v2.runDesktopComposeUiTest
+import androidx.compose.ui.unit.em
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
 import io.mockk.mockk
-import io.mockk.mockkConstructor
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.setting.Setting
 import jp.toastkid.yobidashi4.domain.model.tab.EditorTab
+import jp.toastkid.yobidashi4.presentation.editor.data.LineNumber
 import jp.toastkid.yobidashi4.presentation.editor.viewmodel.TextEditorViewModel
 import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.bind
-import org.koin.dsl.module
 
 class SimpleTextEditorKtTest {
 
@@ -46,18 +48,13 @@ class SimpleTextEditorKtTest {
 
     @MockK
     private lateinit var setting: Setting
+    
+    @RelaxedMockK
+    private lateinit var viewModel: TextEditorViewModel
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
-        startKoin {
-            modules(
-                module {
-                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
-                    single(qualifier=null) { setting } bind(Setting::class)
-                }
-            )
-        }
 
         every { mainViewModel.darkMode() } returns false
         every { mainViewModel.updateEditorContent(any(), any(), any(), any(), any()) } just Runs
@@ -65,18 +62,25 @@ class SimpleTextEditorKtTest {
         every { setting.editorFontSize() } returns 16
         every { setting.editorLineHeight() } returns 1.5f
 
-        mockkConstructor(TextEditorViewModel::class)
-        every { anyConstructed<TextEditorViewModel>().launchTab(any()) } just Runs
-        coEvery { anyConstructed<TextEditorViewModel>().initialScroll(any()) } just Runs
-        every { anyConstructed<TextEditorViewModel>().onClickLineNumber(any()) } just Runs
-        every { anyConstructed<TextEditorViewModel>().dispose() } just Runs
-        coEvery { anyConstructed<TextEditorViewModel>().adjustLineNumberState() } just Runs
+        every { viewModel.launchTab(any()) } just Runs
+        coEvery { viewModel.initialScroll(any()) } just Runs
+        every { viewModel.onClickLineNumber(any()) } just Runs
+        every { viewModel.dispose() } just Runs
+        coEvery { viewModel.adjustLineNumberState() } just Runs
+        every { viewModel.content() } returns TextFieldState()
+        every { viewModel.fontSize() } returns 16
+        every { viewModel.lineHeight() } returns 16.em.value
+        every { viewModel.currentLineOffset() } returns Offset.Zero
+        every { viewModel.currentLineHighlightColor() } returns Color.Red
+        every { viewModel.getHighlightSize() } returns Size(Float.MAX_VALUE, 37.em.value)
+        every { viewModel.verticalScrollState() } returns ScrollState(0)
+        every { viewModel.lineNumberScrollState() } returns ScrollState(0)
+        every { viewModel.scrollEventFlow() } returns MutableSharedFlow<Float>(extraBufferCapacity = 1)
     }
 
     @AfterEach
     fun tearDown() {
         unmockkAll()
-        stopKoin()
     }
 
     @OptIn(ExperimentalTestApi::class)
@@ -85,15 +89,23 @@ class SimpleTextEditorKtTest {
         val setStatus = mockk<(String) -> Unit>()
         every { setStatus(any()) } just Runs
         val textFieldValue = TextFieldState("test_content\nSecond line\n3rd line")
-        every { anyConstructed<TextEditorViewModel>().content() } returns textFieldValue
+        every { viewModel.content() } returns textFieldValue
         val mutableSharedFlow = MutableSharedFlow<Float>(extraBufferCapacity = 1)
-        every { anyConstructed<TextEditorViewModel>().scrollEventFlow() } returns mutableSharedFlow
+        every { viewModel.showLineNumber() } returns true
+        every { viewModel.scrollEventFlow() } returns mutableSharedFlow
+        every { viewModel.lineNumbers() } returns listOf(
+            LineNumber(1, "1"),
+            LineNumber(2, "2"),
+            LineNumber(3, "3"),
+            LineNumber(4, "4"),
+        )
 
         runDesktopComposeUiTest {
             setContent {
                 SimpleTextEditor(
                     EditorTab(mockk()),
-                    setStatus
+                    setStatus,
+                    viewModel = viewModel
                 )
             }
 
@@ -108,9 +120,8 @@ class SimpleTextEditorKtTest {
 
             onNodeWithContentDescription("Line number 1", useUnmergedTree = true)
                 .performClick()
-            verify { anyConstructed<TextEditorViewModel>().onClickLineNumber(any()) }
-            verify { mainViewModel.updateEditorContent(any(), any(), any(), any(), any()) }
-            verify { anyConstructed<TextEditorViewModel>().scrollEventFlow() }
+            verify { viewModel.onClickLineNumber(any()) }
+            verify { viewModel.scrollEventFlow() }
             mutableSharedFlow.tryEmit(1f)
             mainClock.advanceTimeBy(1000)
         }
@@ -122,14 +133,15 @@ class SimpleTextEditorKtTest {
         val setStatus = mockk<(String) -> Unit>()
         every { setStatus(any()) } just Runs
         val textFieldValue = TextFieldState("test_content\nSecond line\n3rd line")
-        every { anyConstructed<TextEditorViewModel>().content() } returns textFieldValue
-        every { anyConstructed<TextEditorViewModel>().showLineNumber() } returns false
+        every { viewModel.content() } returns textFieldValue
+        every { viewModel.showLineNumber() } returns false
 
         runDesktopComposeUiTest {
             setContent {
                 SimpleTextEditor(
                     EditorTab(mockk()),
-                    setStatus
+                    setStatus,
+                    viewModel = viewModel
                 )
             }
         }
