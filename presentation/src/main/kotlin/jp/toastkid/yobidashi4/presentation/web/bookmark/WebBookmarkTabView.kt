515e64a7d04f963a333bb4cd3fa2113f2a92dc42
toastkidjp <10fmi13@gmail.com>
2026-09-15 00:20:40 +0900

    refactor Use koinInject.

--- presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabView.kt
+++ presentation/src/main/kotlin/jp/toastkid/yobidashi4/presentation/web/bookmark/WebBookmarkTabView.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -39,11 +38,14 @@ import jp.toastkid.yobidashi4.presentation.component.HoverHighlightDropdownMenuI
 import jp.toastkid.yobidashi4.presentation.component.HoverHighlightRow
 import jp.toastkid.yobidashi4.presentation.component.LoadIcon
 import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
-internal fun WebBookmarkTabView(tab: WebBookmarkTab) {
-    val viewModel = remember { WebBookmarkTabViewModel() }
+internal fun WebBookmarkTabView(
+    tab: WebBookmarkTab,
+    viewModel: WebBookmarkTabViewModel = koinInject()
+) {
     val coroutineScope = rememberCoroutineScope()
 
     Surface(
