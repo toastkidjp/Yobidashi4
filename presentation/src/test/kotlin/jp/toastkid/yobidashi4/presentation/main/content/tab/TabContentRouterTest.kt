cf4f57b8cb81602816c9a1720d4325028b29e3af
toastkidjp <10fmi13@gmail.com>
2026-09-15 00:21:47 +0900

    refactor Add test case.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/tab/TabContentRouterTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/tab/TabContentRouterTest.kt
@@ -2,16 +2,29 @@ package jp.toastkid.yobidashi4.presentation.main.content.tab
 
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.v2.runDesktopComposeUiTest
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
 
 class TabContentRouterTest {
 
     @OptIn(ExperimentalTestApi::class)
     @Test
-    fun tabContentRouter() {
+    fun defaultParameter() {
         runDesktopComposeUiTest {
             setContent {
                 TabContentRouter(null)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun tabContentRouter() {
+        val registry = TabContentRegistry.Builder().build()
+
+        runDesktopComposeUiTest {
+            setContent {
+                TabContentRouter(mockk(), registry = registry)
             }
         }
     }
