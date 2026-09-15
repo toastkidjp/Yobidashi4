74a7c7f000415a01d3ff13234fc3d376539eb199
toastkidjp <10fmi13@gmail.com>
2026-09-16 00:01:11 +0900

    refactor Add test cases.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/main/content/MultiTabContentTest.kt
@@ -0,0 +1,277 @@
+package jp.toastkid.yobidashi4.presentation.main.content
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.click
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performMouseInput
+import androidx.compose.ui.test.v2.runDesktopComposeUiTest
+import io.mockk.MockKAnnotations
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import io.mockk.verify
+import jp.toastkid.yobidashi4.domain.model.markdown.Markdown
+import jp.toastkid.yobidashi4.domain.model.setting.Setting
+import jp.toastkid.yobidashi4.domain.model.tab.BarcodeToolTab
+import jp.toastkid.yobidashi4.domain.model.tab.ConverterToolTab
+import jp.toastkid.yobidashi4.domain.model.tab.LoanCalculatorTab
+import jp.toastkid.yobidashi4.domain.model.tab.MarkdownPreviewTab
+import jp.toastkid.yobidashi4.domain.repository.input.InputHistoryRepository
+import jp.toastkid.yobidashi4.domain.service.article.ArticlesReaderService
+import jp.toastkid.yobidashi4.domain.service.article.finder.FullTextArticleFinder
+import jp.toastkid.yobidashi4.library.resources.Res
+import jp.toastkid.yobidashi4.library.resources.ic_search
+import jp.toastkid.yobidashi4.presentation.main.component.AggregationBoxViewModel
+import jp.toastkid.yobidashi4.presentation.main.component.WebSearchBoxViewModel
+import jp.toastkid.yobidashi4.presentation.main.content.data.FileListItemMeta
+import jp.toastkid.yobidashi4.presentation.main.content.data.FileListItemMetaExtractor
+import jp.toastkid.yobidashi4.presentation.markdown.MarkdownTabViewModel
+import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import java.nio.file.Files
+import java.nio.file.attribute.FileTime
+
+class MultiTabContentTest {
+
+    @MockK
+    private lateinit var mainViewModel: MainViewModel
+
+    @MockK
+    private lateinit var setting: Setting
+
+    @MockK
+    private lateinit var articlesReaderService: ArticlesReaderService
+
+    @MockK
+    private lateinit var fullTextArticleFinder: FullTextArticleFinder
+
+    @MockK
+    private lateinit var inputHistoryRepository: InputHistoryRepository
+
+    @MockK
+    private lateinit var metaExtractor: FileListItemMetaExtractor
+
+    @RelaxedMockK
+    private lateinit var tabsViewModel: TabsViewModel
+
+    @RelaxedMockK
+    private lateinit var webSearchBoxViewModel: WebSearchBoxViewModel
+
+    @MockK
+    private lateinit var aggregationBoxViewModel: AggregationBoxViewModel
+
+    @RelaxedMockK
+    private lateinit var markdownTabViewModel: MarkdownTabViewModel
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        startKoin {
+            modules(
+                module {
+                    single(qualifier=null) { mainViewModel } bind(MainViewModel::class)
+                    single(qualifier=null) { setting } bind(Setting::class)
+                    single(qualifier=null) { articlesReaderService } bind(ArticlesReaderService::class)
+                    single(qualifier=null) { fullTextArticleFinder } bind(FullTextArticleFinder::class)
+                    single(qualifier=null) { inputHistoryRepository } bind(InputHistoryRepository::class)
+                    single(qualifier=null) { metaExtractor } bind(FileListItemMetaExtractor::class)
+                    single(qualifier=null) { tabsViewModel } bind(TabsViewModel::class)
+                    single(qualifier=null) { webSearchBoxViewModel } bind(WebSearchBoxViewModel::class)
+                    single(qualifier=null) { aggregationBoxViewModel } bind(AggregationBoxViewModel::class)
+                    single(qualifier=null) { markdownTabViewModel } bind(MarkdownTabViewModel::class)
+                }
+            )
+        }
+
+        every { mainViewModel.snackbarHostState() } returns SnackbarHostState()
+        every { mainViewModel.showBackgroundImage() } returns false
+        every { mainViewModel.showWebSearch() } returns false
+        every { mainViewModel.showAggregationBox() } returns false
+        every { mainViewModel.openFind() } returns false
+        every { mainViewModel.showInputBox() } returns false
+        every { mainViewModel.openMemoryUsageBox() } returns false
+        every { mainViewModel.openWorldTime() } returns false
+        every { mainViewModel.slideshowPath() } returns mockk()
+        every { mainViewModel.closeSlideshow() } just Runs
+        every { mainViewModel.loadBackgroundImage() } just Runs
+        every { mainViewModel.openArticleList() } returns false
+        every { mainViewModel.articles() } returns emptyList()
+        every { mainViewModel.reloadAllArticle() } just Runs
+        every { mainViewModel.selected } returns mutableStateOf(0)
+        every { mainViewModel.currentTab() } returns ConverterToolTab()
+        every { mainViewModel.tabs } returns mutableListOf()
+        every { setting.chatApiKey() } returns "test-key"
+        every { inputHistoryRepository.filter(any()) } returns emptyList()
+        every { metaExtractor.make(any()) } returns FileListItemMeta(
+            "test",
+            20000
+        )
+        every { webSearchBoxViewModel.query() } returns TextFieldState()
+        every { webSearchBoxViewModel.currentIconPath() } returns Res.drawable.ic_search
+        every { markdownTabViewModel.scrollState() } returns LazyListState()
+
+        mockkStatic(Files::class)
+        every { Files.exists(any()) } returns true
+        every { Files.size(any()) } returns 20000
+        every { Files.getLastModifiedTime(any()) } returns FileTime.fromMillis(System.currentTimeMillis())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+        unmockkAll()
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun mainScaffold() {
+        runDesktopComposeUiTest {
+            setContent {
+                MultiTabContent()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun useOptionalComponents() {
+        every { mainViewModel.backgroundImage() } returns ImageBitmap(1, 1)
+        every { mainViewModel.showBackgroundImage() } returns true
+        every { mainViewModel.showWebSearch() } returns true
+        every { mainViewModel.showAggregationBox() } returns true
+        every { mainViewModel.openFind() } returns true
+        every { mainViewModel.showInputBox() } returns true
+        every { mainViewModel.openMemoryUsageBox() } returns true
+
+        every { aggregationBoxViewModel.selectedCategoryIcon() } returns Res.drawable.ic_search
+        every { aggregationBoxViewModel.selectedCategoryName() } returns "test"
+        every { aggregationBoxViewModel.isCurrentSwingContent() } returns false
+        every { aggregationBoxViewModel.isOpeningChooser() } returns false
+        every { aggregationBoxViewModel.showAggregationBox() } returns false
+        every { aggregationBoxViewModel.shouldShowDateHistory() } returns false
+        every { aggregationBoxViewModel.useExactMatch() } returns false
+        every { aggregationBoxViewModel.dateHistories() } returns emptyList()
+        every { aggregationBoxViewModel.label() } returns ""
+        every { aggregationBoxViewModel.start() } just Runs
+        every { aggregationBoxViewModel.onDateInputValueChange() } just Runs
+        every { aggregationBoxViewModel.onKeywordInputValueChange() } just Runs
+        every { aggregationBoxViewModel.keyword() } returns TextFieldState()
+        every { aggregationBoxViewModel.dateInput() } returns TextFieldState()
+        every { aggregationBoxViewModel.focusingModifier() } returns Modifier
+        every { aggregationBoxViewModel.selectedCategoryIcon() } returns Res.drawable.ic_search
+
+        val markdownPreviewTab = mockk<MarkdownPreviewTab>()
+        val markdown = mockk<Markdown>()
+        every { markdown.lines() } returns emptyList()
+        every { markdownPreviewTab.markdown() } returns markdown
+        every { markdownPreviewTab.scrollPosition() } returns 1
+        every { mainViewModel.currentTab() } returns markdownPreviewTab
+        every { mainViewModel.initialAggregationType() } returns 0
+        every { mainViewModel.inputValue() } returns TextFieldState("search")
+        every { mainViewModel.findStatus() } returns "test"
+        every { mainViewModel.caseSensitive() } returns true
+        every { mainViewModel.updateScrollableTab(any(), any()) } just Runs
+
+        runDesktopComposeUiTest {
+            setContent {
+                MultiTabContent()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun useTabContents() {
+        every { mainViewModel.selected } returns mutableStateOf(0)
+        every { mainViewModel.currentTab() } returns LoanCalculatorTab()
+        every { mainViewModel.tabs } returns mutableListOf(
+            LoanCalculatorTab(),
+            BarcodeToolTab(),
+        )
+
+        runDesktopComposeUiTest {
+            setContent {
+                MultiTabContent()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun articles() {
+        runDesktopComposeUiTest {
+            setContent {
+                MultiTabContent()
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun articlesWithOpenArticleList() {
+        every { mainViewModel.openArticleList() } returns true
+        every { mainViewModel.hideArticleList() } just Runs
+
+        runDesktopComposeUiTest {
+            setContent {
+                MultiTabContent()
+            }
+
+            val listSwitch = onNodeWithContentDescription("Close file list.", useUnmergedTree = true)
+            listSwitch
+                .performMouseInput {
+                    enter()
+                    exit()
+                    enter()
+                    click()
+                    exit()
+                }
+
+            verify { mainViewModel.hideArticleList() }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun toggleWorldTimeArea() {
+        every { mainViewModel.openWorldTime() } returns true
+        every { mainViewModel.toggleWorldTime() } just Runs
+
+        runDesktopComposeUiTest {
+            setContent {
+                MultiTabContent()
+            }
+
+            val listSwitch = onNodeWithContentDescription("Close world time widget.", useUnmergedTree = true)
+            listSwitch
+                .performMouseInput {
+                    enter()
+                    exit()
+                    enter()
+                    click()
+                    exit()
+                }
+
+            verify { mainViewModel.toggleWorldTime() }
+        }
+    }
+    
+}
