a4d4c959be2ee0fe0ccb712eaeefb952afab88f5
toastkidjp <10fmi13@gmail.com>
2026-09-18 00:02:32 +0900

    refactor Fix unit test.

--- presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceViewKtTest.kt
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/number/NumberPlaceViewKtTest.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.test.runDesktopComposeUiTest
 import io.mockk.MockKAnnotations
 import io.mockk.Runs
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkConstructor
@@ -30,69 +30,42 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
 import jp.toastkid.yobidashi4.domain.model.number.NumberPlaceGame
-import jp.toastkid.yobidashi4.domain.model.setting.Setting
-import jp.toastkid.yobidashi4.domain.repository.number.GameRepository
 import jp.toastkid.yobidashi4.domain.service.number.GameFileProvider
-import jp.toastkid.yobidashi4.presentation.viewmodel.main.MainViewModel
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.bind
-import org.koin.dsl.module
 import java.nio.file.Files
 import java.util.concurrent.atomic.AtomicBoolean
 
 class NumberPlaceViewKtTest {
 
-    @MockK
-    private lateinit var viewModel: MainViewModel
+    @RelaxedMockK
+    private lateinit var viewModel: NumberPlaceViewModel
 
-    @MockK
-    private lateinit var gameRepository: GameRepository
-
-    @MockK
-    private lateinit var setting: Setting
 
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
-        startKoin {
-            modules(
-                module {
-                    single(qualifier = null) { viewModel } bind(MainViewModel::class)
-                    single(qualifier = null) { gameRepository } bind(GameRepository::class)
-                    single(qualifier = null) { setting } bind(Setting::class)
-                }
-            )
-        }
-
-        every { viewModel.showSnackbar(any(), any(), any()) }
-        every { setting.getMaskingCount() } returns 10
-        every { setting.setMaskingCount(any()) } just Runs
-        val numberPlaceGame = NumberPlaceGame()
-        numberPlaceGame.initialize(20)
-        every { gameRepository.load(any()) } returns numberPlaceGame
-        every { gameRepository.save(any(), any()) } just Runs
 
         mockkStatic(Files::class)
         every { Files.size(any()) } returns 1
         mockkConstructor(GameFileProvider::class, NumberPlaceViewModel::class)
         every { anyConstructed<GameFileProvider>().invoke() } returns mockk()
 
-        every { anyConstructed<NumberPlaceViewModel>().openingMaskingCount() } returns false
-        every { anyConstructed<NumberPlaceViewModel>().openingDropdown() } returns false
-        every { anyConstructed<NumberPlaceViewModel>().openingCellOption(any(), any()) } returns false
-        every { anyConstructed<NumberPlaceViewModel>().openCellOption(any(), any()) } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().place(any(), any(), any()) } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().onCellLongClick(any(), any()) } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().saveCurrentGame() } just Runs
+        every { viewModel.openingMaskingCount() } returns false
+        every { viewModel.openingDropdown() } returns false
+        every { viewModel.openingCellOption(any(), any()) } returns false
+        every { viewModel.openCellOption(any(), any()) } just Runs
+        every { viewModel.place(any(), any(), any()) } just Runs
+        every { viewModel.onCellLongClick(any(), any()) } just Runs
+        every { viewModel.saveCurrentGame() } just Runs
+        val numberPlaceGame = NumberPlaceGame()
+        numberPlaceGame.initialize(20)
+        every { viewModel.masked() } returns numberPlaceGame.masked()
     }
 
     @AfterEach
     fun tearDown() {
-        stopKoin()
         unmockkAll()
     }
 
@@ -101,7 +74,7 @@ class NumberPlaceViewKtTest {
     fun numberPlaceView() {
         runDesktopComposeUiTest {
             setContent {
-                NumberPlaceView()
+                NumberPlaceView(viewModel)
             }
 
             onAllNodesWithContentDescription("Masked cell").onFirst()
@@ -111,41 +84,41 @@ class NumberPlaceViewKtTest {
                     longClick()
                 }
 
-            verify { anyConstructed<NumberPlaceViewModel>().openCellOption(any(), any()) }
-            verify { anyConstructed<NumberPlaceViewModel>().onCellLongClick(any(), any()) }
+            verify { viewModel.openCellOption(any(), any()) }
+            verify { viewModel.onCellLongClick(any(), any()) }
         }
     }
 
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun mainOption() {
-        every { anyConstructed<NumberPlaceViewModel>().renewGame() } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().setCorrect() } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().clear() } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().openingDropdown() } returns true
+        every { viewModel.renewGame() } just Runs
+        every { viewModel.setCorrect() } just Runs
+        every { viewModel.clear() } just Runs
+        every { viewModel.openingDropdown() } returns true
 
         runDesktopComposeUiTest {
             setContent {
-                NumberPlaceView()
+                NumberPlaceView(viewModel)
             }
 
             onNode(hasText("Other board"), useUnmergedTree = true)
                 .performMouseInput {
                     click()
                 }
-            verify { anyConstructed<NumberPlaceViewModel>().renewGame() }
+            verify { viewModel.renewGame() }
 
             onNode(hasText("Set answer"), useUnmergedTree = true)
                 .performMouseInput {
                     click()
                 }
-            verify { anyConstructed<NumberPlaceViewModel>().setCorrect() }
+            verify { viewModel.setCorrect() }
 
             onNode(hasText("Clear"), useUnmergedTree = true)
                 .performMouseInput {
                     click()
                 }
-            verify { anyConstructed<NumberPlaceViewModel>().clear() }
+            verify { viewModel.clear() }
 
             onNodeWithContentDescription("Surface", useUnmergedTree = true)
                 .performMouseInput {
@@ -161,25 +134,25 @@ class NumberPlaceViewKtTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun maskingCount() {
-        every { anyConstructed<NumberPlaceViewModel>().openingMaskingCount() } returns true
-        every { anyConstructed<NumberPlaceViewModel>().setMaskingCount(any()) } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().reloadGame() } just Runs
-        every { anyConstructed<NumberPlaceViewModel>().closeMaskingCount() } just Runs
+        every { viewModel.openingMaskingCount() } returns true
+        every { viewModel.setMaskingCount(any()) } just Runs
+        every { viewModel.reloadGame() } just Runs
+        every { viewModel.closeMaskingCount() } just Runs
 
         runDesktopComposeUiTest {
             setContent {
-                NumberPlaceView()
+                NumberPlaceView(viewModel)
             }
 
-            verify { anyConstructed<NumberPlaceViewModel>().openingMaskingCount() }
+            verify { viewModel.openingMaskingCount() }
 
             onNodeWithContentDescription("masking_count_10", useUnmergedTree = true).onParent()
                 .assertExists("Not found!")
                 .performClick()
 
-            verify { anyConstructed<NumberPlaceViewModel>().setMaskingCount(any()) }
-            verify { anyConstructed<NumberPlaceViewModel>().reloadGame() }
-            verify { anyConstructed<NumberPlaceViewModel>().closeMaskingCount() }
+            verify { viewModel.setMaskingCount(any()) }
+            verify { viewModel.reloadGame() }
+            verify { viewModel.closeMaskingCount() }
         }
     }
 
@@ -187,35 +160,35 @@ class NumberPlaceViewKtTest {
     @Test
     fun withCellsDropdown() {
         val firstOnly = AtomicBoolean(true)
-        every { anyConstructed<NumberPlaceViewModel>().openingCellOption(any(), any()) } answers {
+        every { viewModel.openingCellOption(any(), any()) } answers {
             val get = firstOnly.getAndSet(false)
             get
         }
 
         runDesktopComposeUiTest {
             setContent {
-                NumberPlaceView()
+                NumberPlaceView(viewModel)
             }
 
             onAllNodesWithContentDescription("chooser_3", useUnmergedTree = true).onFirst()
                 .assertExists("Not found!")
                 .performClick()
 
-            verify { anyConstructed<NumberPlaceViewModel>().place(any(), any(), any()) }
+            verify { viewModel.place(any(), any(), any()) }
         }
     }
 
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun withLoading() {
-        every { anyConstructed<NumberPlaceViewModel>().loading() } returns true
+        every { viewModel.loading() } returns true
 
         runDesktopComposeUiTest {
             setContent {
-                NumberPlaceView()
+                NumberPlaceView(viewModel)
             }
 
-            verify { anyConstructed<NumberPlaceViewModel>().loading() }
+            verify { viewModel.loading() }
         }
     }
 
