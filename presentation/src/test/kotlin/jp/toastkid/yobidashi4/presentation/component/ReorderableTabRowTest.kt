f4f2f46db138801edd3094de927fd74da7e80c0f
toastkidjp <10fmi13@gmail.com>
2026-09-24 06:47:30 +0900

    refactor Add test case.

---
+++ presentation/src/test/kotlin/jp/toastkid/yobidashi4/presentation/component/ReorderableTabRowTest.kt
@@ -0,0 +1,40 @@
+package jp.toastkid.yobidashi4.presentation.component
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.v2.runComposeUiTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ReorderableTabRowTest {
+
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun test() {
+        runComposeUiTest {
+            setContent {
+                ReorderableTabRow(
+                    listOf("test", "test2"),
+                    0,
+                    Color.Red,
+                    {},
+                    { _, _ ->}
+                ) { _, _ ->
+
+                }
+            }
+        }
+    }
+
+}
